a51a25fbbb91174538c597344027bfaba474b586
dcoates <dcoates@users.sourceforge.net>
2008-07-11 17:22:00 +0000

    Created animated movie from spikes. Mostly copied from 'plot reconstructed image,' except instead of using firing rate, plot a segment at each timestep if the appropriate neuron fired.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@47 7c0444ca-5550-0410-9008-faecdbe78eb5

--- analysis/pv_plot3.m
+++ analysis/pv_plot3.m
@@ -13,6 +13,8 @@ DTH  = 180./NO;
 %output_path = 'C:\cygwin\home\gkenyon\syn_cogn\output\';
 %output_path = 'C:\cygwin\home\gkenyon\PetaVision\src\output\';
 output_path = 'C:\cygwin\home\gkenyon\trunk\src\output\';
+%output_path = 'C:\Users\admin\linux\petavision\trunk\src\output\'; % Dan
+
 
 %read spike events
 spike_array = cell(2,1);
@@ -332,3 +334,58 @@ if save_mat_file == 1
     save( save_file, 'spike_array', 'vmem_array', 'input_array');
 end
 %%
+
+% make movie out of spikes
+play_movie = 0;
+if play_movie
+    figure;
+    numFrames=n_time_steps;
+    A=moviein(numFrames);
+    set(gca,'NextPlot','replacechildren');
+    if ~isempty(spike_array{1})
+        edge_len = sqrt(2)/2;
+        max_line_width = 3;
+         for which_time=1:numFrames
+             
+            % need to do the following inside loop due to clf
+            axis([-1 NX -1 NY]);
+            axis square
+            box ON
+            hold on;
+
+            spike3D = reshape(spike_array{1}(which_time,:), [NO, NX, NY]);
+            for i_theta = 0:NO-1
+                delta_x = edge_len * ( cos(i_theta * DTH * pi / 180 ) );
+                delta_y = edge_len * ( sin(i_theta * DTH * pi / 180 ) );
+                for i_x = 1:NX
+                    for i_y = 1:NY
+                        if spike3D(i_theta+1,i_x,i_y) == 0
+                            continue;
+                        end
+                        lh = line( [i_x - delta_x, i_x + delta_x]', ...
+                            [i_y - delta_y, i_y + delta_y]' );
+                        line_width = max_line_width;
+                        set( lh, 'LineWidth', line_width );
+                        line_color = 0;
+                        set( lh, 'Color', line_color*[1 1 1]);
+                        
+                        % Progress bar for movie at bottom of plot
+                        % Ignore scale for progress bar.
+                        lh = line( [which_time/numFrames*NX, ...
+                            which_time/numFrames*NX]', [-1 0]');
+                        set (lh, 'LineWidth', 1);
+                        set (lh, 'Color', [0 0 1.0]);
+                    end  % y
+                end % x
+            end % orientation
+            
+            A(:,which_time)=getframe;
+            clf;
+        end % frame
+        
+        axis([-1 NX -1 NY]);
+        axis square
+        box ON
+        movie(A,10) % play movie ten times
+    end
+end
