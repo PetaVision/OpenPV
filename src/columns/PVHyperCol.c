9e4c31887a32be451695746d6387830c3202f47b
dcoates <dcoates@users.sourceforge.net>
2008-07-28 00:18:57 +0000

    Added #include <string.h> to fix compiler warnings.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@59 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/columns/PVHyperCol.c
+++ src/columns/PVHyperCol.c
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static int hc_init_layers(PVHyperCol* hc);
 
--- src/pv.c
+++ src/pv.c
@@ -6,6 +6,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <unistd.h> // for options parsing
 
