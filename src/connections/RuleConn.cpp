6423c1a83db262c555b93a15d6beeece4f658a91
rasmussn <rasmussn@users.sourceforge.net>
2009-04-08 16:12:21 +0000

    Initial version.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1192 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/connections/RuleConn.cpp
@@ -0,0 +1,183 @@
+/*
+ * RuleConn.cpp
+ *
+ *  Created on: Apr 5, 2009
+ *      Author: rasmussn
+ */
+
+#include "RuleConn.hpp"
+#include <assert.h>
+#include <string.h>
+
+namespace PV {
+
+RuleConn::RuleConn(const char * name,
+                   HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post, int channel)
+{
+   this->connId = hc->numberOfConnections();
+   this->name   = strdup(name);
+   this->parent = hc;
+
+   this->numBundles = 1;
+
+   initialize(NULL, pre, post, channel);
+
+   hc->addConnection(this);
+}
+
+int RuleConn::initializeWeights(const char * filename)
+{
+   if (filename == NULL) {
+      PVParams * params = parent->parameters();
+      const float strength = params->value(name, "strength");
+
+      const int xScale = pre->clayer->xScale;
+      const int yScale = pre->clayer->yScale;
+
+      int nfPre = pre->clayer->numFeatures;
+
+      const int numPatches = numberOfWeightPatches();
+      for (int i = 0; i < numPatches; i++) {
+         int fPre = i % nfPre;
+         ruleWeights(wPatches[i], fPre, xScale, yScale, strength);
+      }
+   }
+   else {
+      fprintf(stderr, "Initializing weights from a file not implemented for RuleConn\n");
+      exit(1);
+   } // end if for filename
+
+   return 0;
+}
+
+int RuleConn::ruleWeights(PVPatch * wp, int fPre, int xScale, int yScale, float strength)
+{
+   pvdata_t * w = wp->data;
+
+   // get parameters
+
+   PVParams * params = parent->parameters();
+
+   const int nx = (int) wp->nx;
+   const int ny = (int) wp->ny;
+   const int nf = (int) wp->nf;
+
+   // strides
+   const int sx = (int) wp->sx;  assert(sx == nf);
+   const int sy = (int) wp->sy;  assert(sy == nf*nx);
+   const int sf = (int) wp->sf;  assert(sf == 1);
+
+   const float dx = powf(2, xScale);
+   const float dy = powf(2, yScale);
+
+   assert(fPre >= 0 && fPre <= 1);
+   assert(ny == 1);
+   assert(nf == 16);
+
+   // rule 16 (only 100 applies, left neighbor fires, I fire, all other patterns fire 0)
+   // left (post view) -> right (pre view) -> 100 -> 000
+
+   // loop over all post synaptic neurons in patch
+
+   // initialize connections of OFF and ON cells to 0
+   for (int f = 0; f < nf; f++) {
+      for (int j = 0; j < ny; j++) {
+         for (int i = 0; i < nx; i++) {
+            w[i*sx + j*sy + f*sf] = 0;
+         }
+      }
+   }
+
+   // now set the actual pattern for rule 16 (0 0 0 1 0 0 0 0)
+
+   // pre-synaptic neuron is an OFF cell
+   if (fPre == 0) {
+      for (int j = 0; j < ny; j++) {
+         // sub-rule 000 (first OFF cell fires)
+         int f = 0;
+         w[0*sx + j*sy + f*sf] = 1;
+         w[1*sx + j*sy + f*sf] = 1;
+         w[2*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 001 (second OFF cell fires)
+         f = 2;
+         w[1*sx + j*sy + f*sf] = 1;
+         w[2*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 010 (third OFF cell fires)
+         f = 4;
+         w[0*sx + j*sy + f*sf] = 1;
+         w[2*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 011 (fourth OFF cell fires)
+         f = 6;
+         w[2*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 100 (fifth _ON_ cell fires)
+         f = 9;
+         w[0*sx + j*sy + f*sf] = 1;
+         w[1*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 101 (six OFF cell fires)
+         f = 10;
+         w[1*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 110 (seventh OFF cell fires)
+         f = 12;
+         w[0*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 111 (eighth OFF cell fires)
+         f = 14;
+      }
+   }
+
+   // pre-synaptic neuron is an ON cell
+   if (fPre == 1) {
+      for (int j = 0; j < ny; j++) {
+         // sub-rule 000 (first OFF cell fires)
+         int f = 0;
+
+         // sub-rule 001 (second OFF cell fires)
+         f = 2;
+         w[0*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 010 (third OFF cell fires)
+         f = 4;
+         w[1*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 011 (fourth OFF cell fires)
+         f = 6;
+         w[0*sx + j*sy + f*sf] = 1;
+         w[1*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 100 (fifth _ON_ cell fires)
+         f = 9;
+         w[2*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 101 (six OFF cell fires)
+         f = 10;
+         w[0*sx + j*sy + f*sf] = 1;
+         w[2*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 110 (seventh OFF cell fires)
+         f = 12;
+         w[1*sx + j*sy + f*sf] = 1;
+         w[2*sx + j*sy + f*sf] = 1;
+
+         // sub-rule 111 (eighth OFF cell fires)
+         f = 14;
+         w[0*sx + j*sy + f*sf] = 1;
+         w[1*sx + j*sy + f*sf] = 1;
+         w[2*sx + j*sy + f*sf] = 1;
+      }
+   }
+
+   for (int f = 0; f < nf; f++) {
+      float factor = strength;
+      for (int i = 0; i < nx*ny; i++) w[f + i*nf] *= factor;
+   }
+
+   return 0;
+}
+
+} // namespace PV
---
+++ src/connections/RuleConn.hpp
@@ -0,0 +1,32 @@
+/*
+ * RuleConn.hpp
+ *
+ *  Created on: Apr 5, 2009
+ *      Author: rasmussn
+ */
+
+#ifndef RULECONN_HPP_
+#define RULECONN_HPP_
+
+#include "HyPerConn.hpp"
+
+namespace PV {
+
+class RuleConn: public PV::HyPerConn {
+public:
+   RuleConn(const char * name,
+            HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post, int channel);
+
+   PVPatch* weights(int k)
+   {
+      return wPatches[k];
+   }
+
+private:
+   virtual int initializeWeights(const char * filename);
+   int ruleWeights(PVPatch * wp, int fPre, int xScale, int yScale, float strength);
+};
+
+}
+
+#endif /* RULECONN_HPP_ */
