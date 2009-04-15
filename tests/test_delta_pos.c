95b4d5da9d4d1f36a58ee955bd761b89738dcab7
rasmussn <rasmussn@users.sourceforge.net>
2009-04-15 12:58:24 +0000

    Fixed to work with correct layer size scale.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1226 7c0444ca-5550-0410-9008-faecdbe78eb5

--- tests/test_delta_pos.c
+++ tests/test_delta_pos.c
@@ -5,57 +5,57 @@
 int main(int argc, char* argv[])
 {
    float dx;
-   int iPre;
+   int kPre;
    int scale = 0;
 
-   for (iPre = 0; iPre < 7; iPre++) {
-      dx = deltaPosLayers(iPre, scale);
+   for (kPre = 0; kPre < 7; kPre++) {
+      dx = deltaPosLayers(kPre, scale);
       if (dx != 0.0) {
-         printf("FAILED:TEST_DELTA_POS: scale=%d, iPre=%d, dx=%f\n", scale, iPre, dx);
-         exit(1);
-      }
-   }
-
-   scale = -1;
-   for (iPre = 0; iPre < 7; iPre++) {
-      dx = deltaPosLayers(iPre, scale);
-      float val = (iPre % 2) ? -0.25 : 0.25;
-      if (dx != val) {
-         printf("FAILED:TEST_DELTA_POS: scale=%d, iPre=%d, dx=%f\n", scale, iPre, dx);
-         exit(1);
-      }
-   }
-
-   scale = -2;
-   for (iPre = 0; iPre < 7; iPre++) {
-      dx = deltaPosLayers(iPre, scale);
-      float val = 1./8.;
-      if (iPre % 4 == 0) val =  3.0 * val;
-      if (iPre % 4 == 1) val =  1.0 * val;
-      if (iPre % 4 == 2) val = -1.0 * val;
-      if (iPre % 4 == 3) val = -3.0 * val;
-      if (dx != val) {
-         printf("FAILED:TEST_DELTA_POS: scale=%d, iPre=%d, dx=%f\n", scale, iPre, dx);
+         printf("FAILED:TEST_DELTA_POS: scale=%d, kPre=%d, dx=%f\n", scale, kPre, dx);
          exit(1);
       }
    }
 
    scale = 1;
-   for (iPre = 0; iPre < 7; iPre++) {
-      dx = deltaPosLayers(iPre, scale);
-      float val = -0.5;
+   for (kPre = 0; kPre < 7; kPre++) {
+      dx = deltaPosLayers(kPre, scale);
+      float val = (kPre % 2) ? -0.25 : 0.25;
       if (dx != val) {
-         printf("FAILED:TEST_DELTA_POS: scale=%d, iPre=%d, dx=%f\n", scale, iPre, dx);
+         printf("FAILED:TEST_DELTA_POS: scale=%d, kPre=%d, dx=%f\n", scale, kPre, dx);
          exit(1);
       }
    }
 
    scale = 2;
-   for (iPre = 0; iPre < 7; iPre++) {
-      dx = deltaPosLayers(iPre, scale);
+   for (kPre = 0; kPre < 7; kPre++) {
+      dx = deltaPosLayers(kPre, scale);
+      float val = 1./8.;
+      if (kPre % 4 == 0) val =  3.0 * val;
+      if (kPre % 4 == 1) val =  1.0 * val;
+      if (kPre % 4 == 2) val = -1.0 * val;
+      if (kPre % 4 == 3) val = -3.0 * val;
+      if (dx != val) {
+         printf("FAILED:TEST_DELTA_POS: scale=%d, kPre=%d, dx=%f\n", scale, kPre, dx);
+         exit(1);
+      }
+   }
+
+   scale = -1;
+   for (kPre = 0; kPre < 7; kPre++) {
+      dx = deltaPosLayers(kPre, scale);
+      float val = -0.5;
+      if (dx != val) {
+         printf("FAILED:TEST_DELTA_POS: scale=%d, kPre=%d, dx=%f\n", scale, kPre, dx);
+         exit(1);
+      }
+   }
+
+   scale = -2;
+   for (kPre = 0; kPre < 7; kPre++) {
+      dx = deltaPosLayers(kPre, scale);
       float val = -1.5;
       if (dx != val) {
-         printf("FAILED:TEST_DELTA_POS: scale=%d, iPre=%d, dx=%f\n", scale, iPre, dx);
+         printf("FAILED:TEST_DELTA_POS: scale=%d, kPre=%d, dx=%f\n", scale, kPre, dx);
          exit(1);
       }
    }
