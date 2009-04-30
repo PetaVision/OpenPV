29e2f49d205d66a4a5f3648f10539f87c27f8779
garkenyon <garkenyon@users.sourceforge.net>
2009-04-30 16:09:00 +0000

    fixed target to match global coordinates
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@1328 7c0444ca-5550-0410-9008-faecdbe78eb5

--- STDP/Retina1D.cpp
+++ STDP/Retina1D.cpp
@@ -8,6 +8,7 @@
 #include "Retina1D.hpp"
 #include <stdlib.h>
 #include <assert.h>
+#include "src/io/io.h"
 
 namespace PV {
 
@@ -17,7 +18,7 @@ namespace PV {
 
 Retina1D::Retina1D(const char * name, HyPerCol * hc) :
 	Retina(name, hc) {
-	targ1D = (pvdata_t *) malloc(sizeof(clayer->numNeurons));
+	targ1D = (pvdata_t *) malloc(sizeof(pvdata_t)*(clayer->numNeurons));
 	createImage(clayer->V);
 }
 
@@ -30,69 +31,82 @@ int Retina1D::createImage(pvdata_t * localBuf) {
 
 	// local HyPerCube dimensions
 	const int nx = clayer->loc.nx;
-	const int ny = clayer->loc.ny;
+//	const int ny = clayer->loc.ny;
 	const int nf = clayer->numFeatures;
+	const int numNeurons = clayer->numNeurons;
+
+	// global layer dimensions: every node processes whole image
+	const int nxGlobal = clayer->loc.nxGlobal;
+	const int nyGlobal = clayer->loc.nyGlobal;
+	const int nfGlobal = nf; //clayer->numFeatures;
+	const int numNeuronsGlobal = nxGlobal * nyGlobal * nfGlobal;
+//	const int kx0 = clayer->loc.kx0; // assert(kx0 == 0);
+//	const int ky0 = clayer->loc.ky0;
+//	const int xMidGlobal = nxGlobal / 2.0;
+	const int yMidGlobal = nyGlobal / 2.0;
+	pvdata_t * globalBuf = (pvdata_t *) malloc(sizeof(pvdata_t) * numNeuronsGlobal);
 
 	// only one MPI node creates global image
-	if (l->columnId == 0) {
-
-		// global layer dimensions
-		float nxGlobal = clayer->loc.nxGlobal;
-		float nyGlobal = clayer->loc.nyGlobal;
-		float kx0 = clayer->loc.kx0;
-		float ky0 = clayer->loc.ky0;
-		float xMidGlobal = nxGlobal / 2.0;
-		float yMidGlobal = nyGlobal / 2.0;
+	if (clayer->columnId == 0) {
 
 		// target is built from a specified number of
 		// spaced segments separated by gaps
 		// keeping the average density == clutterProb
 		int numTargs = 2;  // TODO: read from params file
 		int lenTarg = 8;   // TODO: read from params file
-		lenTarg = (lenTarg > 0) ? lenTarg : 1; //can't have lenTarg == 0
 		float clutterProb = 0.5;     // TODO: read from params file
+		lenTarg = (lenTarg > 0) ? lenTarg : 1; //can't have lenTarg == 0
 		float clutterProbMin = ((numTargs * lenTarg) > 0) ? (numTargs * lenTarg
-				/ (float) nx) : clutterProb;
+				/ (float) nxGlobal) : clutterProb;
 		clutterProb = (clutterProb < clutterProbMin) ? clutterProbMin : clutterProb;
+
 		// default all pixels to clutterProb
-		for (int k = 0; k < clayer->numNeurons; k++) {
-			localBuf[k] = rand() < (int) ( clutterProb * RAND_MAX );
-			targ1D[k] = 0;
+		for (long kg = 0; kg < numNeuronsGlobal; kg++) {
+			globalBuf[kg] = rand() < (int) ( clutterProb * RAND_MAX );
 		}
-
-
 
 		// set lenGap so that average density equals clutterProb
 		int lenGap = (clutterProb > 0) ? (lenTarg * (1.0 - clutterProb)
 				/ clutterProb) : 0;
-		int beginTarg = rand() % ( nx - numTargs * (lenGap + lenTarg) );
 
+		// start 1D target at random location (in global coordinates)
+		int beginTargX = rand() % ( nxGlobal - numTargs * (lenGap + lenTarg) );
+		int beginTargY = yMidGlobal;
+		int beginTargF = 1;
+
+		// make each target segment + gap
 		for (int iSeg = 0; iSeg < numTargs; iSeg++) {
-			int startSeg = iSeg * (lenTarg + lenGap) + beginTarg;
-			int endSeg = startSeg + lenTarg - 1;
-			for (int iTarg = startSeg; iTarg < endSeg; iTarg++) {
-				localBuf[iTarg] = 1;
-				targ1D[iTarg] = 1;
+			int startSegX = iSeg * (lenTarg + lenGap) + beginTargX;
+			int endSegX = startSegX + lenTarg - 1;
+			for (int iTargX = startSegX; iTargX < endSegX; iTargX++) {
+				int iTargK = kIndex(iTargX, beginTargY, beginTargF, nxGlobal,
+						nyGlobal, nfGlobal);
+				globalBuf[iTargK] = 1;
 			}
-			int startGap = endSeg + 1;
-			int endGap = startGap + lenGap - 1;
-			for (int iGap = startGap; iGap < endGap; iGap++) {
-				localBuf[iGap] = 0;
+			int startGapX = endSegX + 1;
+			int endGapX = startGapX + lenGap - 1;
+			for (int iGapX = startGapX; iGapX < endGapX; iGapX++) {
+				int iGapK = kIndex(iGapX, beginTargY, beginTargF, nxGlobal,
+						nyGlobal, nfGlobal);
+				globalBuf[iGapK] = 0;
 			}
 		}
-		// f[0] are OFF, f[1] are ON cells
+
+		// if nf == 2 -> f[0] are OFF, f[1] are ON cells
 		assert(nf == 1 || nf == 2);
 		if (nf == 2) {
-			for (int k = 0; k < nx * ny; k++) {
-				localBuf[2 * k] = localBuf[k];
-				localBuf[2 * k + 1] = 0;
+			for (int kg = 0; kg < nxGlobal * nyGlobal; kg++) {
+				globalBuf[2 * kg] = globalBuf[kg];
+				globalBuf[2 * kg + 1] = 0;
 			}
 		}
+	}  // end if (l->columnId == 0)
+
+	scatterReadBuf(this->clayer, globalBuf, localBuf, 0);
+	for (int k = 0; k < numNeurons; k++) {
+		targ1D[k] = localBuf[k];
 	}
-
-	scatterReadBuf(l, globalBuf, localBuf, comm);
-
-
+	free(globalBuf);
 
 	return 0;
 }
