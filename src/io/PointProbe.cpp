342d29d2f87f0465ca493d8d0dc4c82618997108
rasmussn <rasmussn@users.sourceforge.net>
2009-05-18 16:46:52 +0000

    Changed output format (%6.3 -> %6.4) so everything lines up properly.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1363 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/io/PointProbe.cpp
+++ src/io/PointProbe.cpp
@@ -42,10 +42,10 @@ int PointProbe::outputState(float time, PVLayer * l)
    int offset = yLoc * sy + xLoc * sx + fLoc * sf;
 
    fprintf(fp, "%s t=%6.1f", msg, time);
-   fprintf(fp, " G_E=%6.4f", l->G_E[offset]);
-   fprintf(fp, " G_I=%6.4f", l->G_I[offset]);
-   fprintf(fp, " V=%6.4f", l->V[offset]);
-   fprintf(fp, " Vth=%6.4f", l->Vth[offset]);
+   fprintf(fp, " G_E=%6.3f", l->G_E[offset]);
+   fprintf(fp, " G_I=%6.3f", l->G_I[offset]);
+   fprintf(fp, " V=%6.3f", l->V[offset]);
+   fprintf(fp, " Vth=%6.3f", l->Vth[offset]);
    fprintf(fp, " a=%3.1f\n", l->activity->data[offset]);
 
    fflush(fp);
