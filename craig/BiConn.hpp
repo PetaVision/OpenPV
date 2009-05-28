a1befd920fa485e1d66d023c4f620758369bd2bd
rasmussn <rasmussn@users.sourceforge.net>
2009-05-28 06:38:31 +0000

    Initial version.  Sets weights to recognize mom pattern.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@1389 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ craig/BiConn.hpp
@@ -0,0 +1,34 @@
+/*
+ * STDPConn.hpp
+ */
+
+#ifndef BICONN_HPP_
+#define BICONN_HPP_
+
+#include "src/connections/HyPerConn.hpp"
+
+namespace PV {
+
+class BiConn: public PV::HyPerConn {
+public:
+   BiConn(const char * name,
+          HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post, int channel,
+          int type);
+
+   virtual int outputState(FILE * fp, int k);
+
+   PVPatch* weights(int k)
+   {
+      return wPatches[k];
+   }
+
+private:
+   virtual int initializeWeights(const char * filename);
+   int ruleWeights(PVPatch * wp, int kPre, float strength);
+
+   int type;
+};
+
+}
+
+#endif /* BICONN_HPP_ */
