bd7fb5f347d480bad74b6c4a37e51c1b819a473c
rasmussn <rasmussn@users.sourceforge.net>
2009-06-16 17:23:06 +0000

    Added EXTEND_BORDER_INDEX preprocessor guard to remove extending the border.  Borders are now handled by border regions (neighbors).
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1414 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/V1.cpp
+++ src/layers/V1.cpp
@@ -25,7 +25,7 @@ V1Params V1DefaultParams =
 {
     V_REST, V_EXC, V_INH, V_INHB,            // V (mV)
     TAU_VMEM, TAU_EXC, TAU_INH, TAU_INHB,
-    VTH_REST,  TAU_VTH, DELTA_VTH,	     // tau (ms)
+    VTH_REST,  TAU_VTH, DELTA_VTH, DELTA_VTH_DECAY,	     // tau (ms)
     250, NOISE_AMP*( 1.0/TAU_EXC ) * ( ( TAU_INH * (V_REST-V_INH) + TAU_INHB * (V_REST-V_INHB) ) / (V_EXC-V_REST) ),
     250, NOISE_AMP*1.0,
     250, NOISE_AMP*1.0                       // noise (G)
@@ -61,7 +61,7 @@ int V1::setParams(PVParams * params, V1Params * p)
    memcpy(clayer->params, p, sizeof(*p));
 
    clayer->numParams = sizeof(*p) / sizeof(float);
-   assert(clayer->numParams == 17);
+   assert(clayer->numParams == 18);
 
    V1Params * cp = (V1Params *) clayer->params;
 
@@ -78,6 +78,7 @@ int V1::setParams(PVParams * params, V1Params * p)
    if (params->present(name, "VthRest"))  cp->VthRest  = params->value(name, "VthRest");
    if (params->present(name, "tauVth"))   cp->tauVth   = params->value(name, "tauVth");
    if (params->present(name, "deltaVth")) cp->deltaVth = params->value(name, "deltaVth");
+   if (params->present(name, "deltaVthDecay")) cp->deltaVthDecay = params->value(name, "deltaVthDecay");
 
    if (params->present(name, "noiseAmpE"))   cp->noiseAmpE   = params->value(name, "noiseAmpE");
    if (params->present(name, "noiseAmpI"))   cp->noiseAmpI   = params->value(name, "noiseAmpI");
@@ -123,8 +124,12 @@ int V1::updateState(float time, float dt)
    pvdata_t * activity = clayer->activity->data;
 
    for (int k = 0; k < clayer->numNeurons; k++) {
+#ifdef EXTEND_BORDER_INDEX
       int kPhi = kIndexExtended(k, clayer->loc.nx, clayer->loc.ny, clayer->numFeatures,
                                    clayer->numBorder);
+#else
+      int kPhi = k;
+#endif
       V[k] = phiExc[kPhi] - phiInh[kPhi];
       activity[k] = V[k];
 
