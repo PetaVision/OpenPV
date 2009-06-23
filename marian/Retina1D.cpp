1c0509f6e8b2eb0c82c4929b05e7b99f7da6506f
marian_anghel <marian_anghel@users.sourceforge.net>
2009-06-23 16:48:30 +0000

    explicit mom definition, constrained training, followed by mixed mom and random patterns.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@1418 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 0458dae8efd36dad03930c990dc7740762d231f7

--- marian/Retina1D.cpp
+++ marian/Retina1D.cpp
@@ -8,6 +8,10 @@
 #include "Retina1D.hpp"
 #include <stdlib.h>
 #include <assert.h>
+#include <iostream>
+
+using namespace std;
+
 
 namespace PV {
 
@@ -18,14 +22,24 @@ namespace PV {
 Retina1D::Retina1D(const char * name, HyPerCol * hc) :
    Retina(name, hc)
 {
-   targ1D = (pvdata_t *) malloc(clayer->numNeurons * sizeof(pvdata_t));;
+   mom = (pvdata_t *) malloc(clayer->numNeurons * sizeof(pvdata_t));;
 //   createImage(clayer->V);
    createRandomImage(clayer->V);
+   cout << "mom: ";
+   for (int i = 0; i < clayer->numNeurons; i++) {
+     mom[i] = clayer->V[i];
+     if (!(i%2)){
+        cout << mom[i];
+     }
+   }
+   cout << endl;
+   //getchar();
+
 }
 
 Retina1D::~Retina1D()
 {
-   free(targ1D);
+   free(mom);
 }
 
 int Retina1D::createImage(pvdata_t * buf) {
@@ -115,28 +129,90 @@ int Retina1D::createRandomImage(pvdata_t * buf) {
    return 0;
 }
 
+
 int Retina1D::updateState(float time, float dt)
 {
    static int fire = 0;
+   static int plot_message = 1;
+   static int present_mom = 1;
+   static int F = 30; // fire every F steps
+   static int P = 1; // present mom every P patterns; the pattern presentation is every F steps
+                      // then for F-1 step fire empty
 
    pvdata_t * V = clayer->V;
    pvdata_t * activity = clayer->activity->data;
 
-   if (fire == 0) {
-      fire = 30;
-      for (int k = 0; k < clayer->numNeurons; k++) {
-         activity[k] = V[k];
-      }
-      // create a new image for next time
-      //this->createRandomImage(V);
-   }
-   else {
-      fire -= 1;
-      for (int k = 0; k < clayer->numNeurons; k++) {
-         activity[k] = 0;
-      }
+   if(time < 8000){ // periodic input - one pattern
 
-   }
+     // fire mom every F steps
+
+     if (fire == 0) {
+       fire = F;
+
+       for (int k = 0; k < clayer->numNeurons; k++) {
+    	   activity[k] = mom[k];
+       }
+
+
+     }
+     else {
+
+       fire -= 1;
+       for (int k = 0; k < clayer->numNeurons; k++) {
+         activity[k] = 0;
+       }
+
+     }
+
+
+   } else { // mixed input: mom every P patterns and random pattern otherwise.
+
+     // present mom every P steps and random patterns
+     // otherwise
+
+     if(plot_message){
+       plot_message = 0;
+       //printf("start alternating mom and random patterns\n");
+       //getchar();
+     }
+
+     if (fire == 0) {
+
+
+
+       if(present_mom==P){
+
+    	   //printf("%d: fire mom\n",fire);
+    	   for (int k = 0; k < clayer->numNeurons; k++) {
+    		   activity[k] = mom[k];
+    	   }
+    	   present_mom=0;
+
+       } else {
+
+    	   //printf("%d: fire random\n",fire);
+    	   this->createRandomImage(V);
+    	   for (int k = 0; k < clayer->numNeurons; k++) {
+    		   activity[k] = V[k];
+    	   }
+    	   present_mom++;
+
+       }
+
+       fire = F;
+
+     }else {
+
+       //printf("%d: fire empty\n",fire);
+       fire -= 1;
+       for (int k = 0; k < clayer->numNeurons; k++) {
+    	   activity[k] = 0;
+       }
+
+     }
+
+
+   } // if train or random phase
 
    return 0;
 }
