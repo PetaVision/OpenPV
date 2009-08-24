408cde7e6902070147656a2bf422645a8bf9c20a
rasmussn <rasmussn@users.sourceforge.net>
2009-08-24 14:26:09 +0000

    Made a base class of Communicator.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1549 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/columns/InterColComm.hpp
+++ src/columns/InterColComm.hpp
@@ -5,31 +5,18 @@
  *      Author: rasmussn
  */
 
-#ifndef INTERCOLCOMM_H_
-#define INTERCOLCOMM_H_
+#ifndef INTERCOLCOMM_HPP_
+#define INTERCOLCOMM_HPP_
 
+#include "Communicator.hpp"
 #include "DataStore.hpp"
 #include "../include/pv_common.h"
-
-#include <mpi.h>
+#include "../layers/PVLayer.h"
 
 // maximum number of messages (each layer publishes to all neighbors)
 #define MAX_MESSAGES    MAX_NEIGHBORS
 #define MAX_PUBLISHERS  MAX_LAYERS
 #define MAX_SUBSCRIBERS MAX_LAYERS
-
-// directional indices
-#define LOCAL     0
-#define NORTHWEST 1
-#define NORTH     2
-#define NORTHEAST 3
-#define WEST      4
-#define EAST      5
-#define SOUTHWEST 6
-#define SOUTH     7
-#define SOUTHEAST 8
-
-#include "../layers/PVLayer.h"
 
 namespace PV {
 
@@ -71,13 +58,10 @@ private:
    DataStore *  store;
 };
 
-class InterColComm {
+class InterColComm: public PV::Communicator {
 public:
    InterColComm(int * argc, char *** argv);
    virtual ~InterColComm();
-
-   int commInit(int * argc, char *** argv);
-   int commFinalize();
 
    int addPublisher(HyPerLayer * pub, size_t size1, size_t size2, int numLevels);
    int publish(HyPerLayer * pub, PVLayerCube * cube);
@@ -85,84 +69,14 @@ public:
    int deliver(HyPerCol * hc, int pubId);
    int increaseTimeLevel(int pubId)       {return publishers[pubId]->increaseTimeLevel();}
 
-   int commRank()               { return icRank; }
-   int commSize()               { return icSize; }
-   MPI_Comm communicator()      { return icComm; }
-
    DataStore* publisherStore(int pubId)   {return publishers[pubId]->dataStore();}
-
-   int numberOfNeighbors(); // includes interior (self) as a neighbor
-   int numberOfBorders()                  {return numBorders;}
-
-   bool hasNeighbor(int neighborId);
-
-   int commRow()          {return commRow(icRank);}
-   int commColumn()       {return commColumn(icRank);}
-   int numCommRows()      {return numRows;}
-   int numCommColumns()   {return numCols;}
-
-   size_t recvOffset(int n, const PVLayerLoc * loc);
-   size_t sendOffset(int n, const PVLayerLoc * loc);
-   MPI_Datatype * newDatatypes(const PVLayerLoc * loc);
-
-   int send(pvdata_t * data, const MPI_Datatype neighborDatatypes [],
-            const PVLayerLoc * loc);
-
-   int recv(pvdata_t * data, const MPI_Datatype neighborDatatypes [],
-            const PVLayerLoc * loc);
-
-protected:
-
-   int commRow(int commId);
-   int commColumn(int commId);
 
 private:
 
-   int icRank;
-   int icSize;
-   int worldRank;
-   int worldSize;
-   int numRows;
-   int numCols;
-   int numHyPerCols;
-   int numNeighbors;
-   int numBorders;
    int numPublishers;
-
-   MPI_Comm icComm;
-   MPI_Request requests[MAX_NEIGHBORS];
-
-   int borders[MAX_NEIGHBORS];
-   int neighbors[MAX_NEIGHBORS + 1];        // [0] is interior (local)
-   int remoteNeighbors[MAX_NEIGHBORS + 1];
    Publisher* publishers[MAX_PUBLISHERS];
-
-   // These methods are private for now, move to public as needed
-
-   int neighborInit();
-
-   bool hasNorthwesternNeighbor(int commId);
-   bool hasNorthernNeighbor(int commId);
-   bool hasNortheasternNeighbor(int commId);
-   bool hasWesternNeighbor(int commId);
-   bool hasEasternNeighbor(int commId);
-   bool hasSouthwesternNeighbor(int commId);
-   bool hasSouthernNeighbor(int commId);
-   bool hasSoutheasternNeighbor(int commId);
-
-   int northwest(int commId);
-   int north(int commId);
-   int northeast(int commId);
-   int west(int commId);
-   int east(int commId);
-   int southwest(int commId);
-   int south(int commId);
-   int southeast(int commId);
-
-   int neighborIndex(int commId, int index);
-
 };
 
 }
 
-#endif /* INTERCOLCOMM_H_ */
+#endif /* INTERCOLCOMM_HPP_ */
