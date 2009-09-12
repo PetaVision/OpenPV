dad12425e411b26524438cc1099d141e01d0a824
rasmussn <rasmussn@users.sourceforge.net>
2009-09-12 15:06:11 +0000

    Initial attempt at a Image base class.  Renamed Image class that Shreyas created to ImageCreate class (deriving from Image).
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1591 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/layers/ImageCreator.hpp
@@ -0,0 +1,102 @@
+/*
+ * ImageCreator.hpp
+ *
+ *  Created on: Aug 25, 2009
+ *      Author: Shreyas
+ */
+
+#ifndef IMAGECREATOR_HPP_
+#define IMAGECREATOR_HPP_
+
+#include "Image.hpp"
+#include "../include/pv_common.h"
+#include "../include/pv_types.h"
+
+#include <iostream>
+
+namespace PV {
+
+const unsigned int BIN = 1;
+const unsigned int TXT = 2;
+const unsigned int TIF = 4;
+
+class Point2D {
+public:
+   Point2D(const unsigned int x, const unsigned int y):
+             posx(x), posy(y) { };
+
+   unsigned int getX() { return posx; };
+   unsigned int getY() { return posy; };
+
+private:
+   unsigned int posx, posy;
+};
+
+class ImageCreator : public Image {
+
+public:
+    ImageCreator(unsigned int nxInit, unsigned int nyInit);
+    ImageCreator(unsigned int nxInit, unsigned int nyInit, pvdata_t *targetbuf);
+
+	int clearImage();
+	int fillImage(pvdata_t val);
+	virtual int updateImage(float time, float dt, pvdata_t *targetbuf);
+
+	int createRandomImage();
+	int drawMultipleRandomShapes(int n_images);
+	int createMultipleImages();
+
+	int drawLine(Point2D pt1, Point2D pt2);
+
+	int drawLine(Point2D origin,
+			unsigned int length, float theta);
+
+	int drawSquare(Point2D origin,
+			unsigned int length, unsigned int theta);
+
+	int drawSquare(Point2D pt1, Point2D pt2,
+			Point2D pt3, Point2D pt4);
+
+	int drawRectangle(Point2D origin,
+			unsigned int lengtha, unsigned int lengthb,
+			unsigned int theta);
+
+	int drawRectangle(Point2D pt1, Point2D pt2,
+			Point2D pt3, Point2D pt4);
+
+	int drawQuadrilateral(Point2D pt1, Point2D pt2,
+			Point2D pt3, Point2D pt4);
+
+	int copyImage(pvdata_t *targetbuf);
+
+	void testImage();
+
+	void mark(unsigned int i, unsigned int j, int value);
+	void mark(unsigned int i, int value);
+	pvdata_t getmark(unsigned int i, unsigned int j);
+
+	virtual int writeImageToFile(const float time,
+			                     const unsigned char options);
+
+	void setModified(bool val) { modified = val; };
+	bool ifModified() { return modified; };
+
+private:
+	bool     modified;
+	pvdata_t *buf;
+	unsigned int nx;
+	unsigned int ny;
+
+	int writeImageToTxt(const char *filename);
+	int writeImageToBin(const char *filename);
+	int           drawBresenhamLine(int x0, int y0, int x1, int y1);
+	void          swap(int &a, int &b);
+	inline double deg2rad(int angleInDegrees);
+	inline int    approx(double n);
+	int           threewaytoss(double probStay,
+			                   double probBack,
+			                   double probForward);
+};
+
+}
+#endif /* IMAGECREATOR_HPP_ */
