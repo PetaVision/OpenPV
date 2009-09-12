e1df4fe3dd01d5569e7e217e5066a533a1a450f2
rasmussn <rasmussn@users.sourceforge.net>
2009-09-12 15:00:28 +0000

    Changed clayer->numBorder to clayer->loc.nPad.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1588 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/V1.cpp
+++ src/layers/V1.cpp
@@ -125,7 +125,7 @@ int V1::updateState(float time, float dt)
    for (int k = 0; k < clayer->numNeurons; k++) {
 #ifdef EXTEND_BORDER_INDEX
       int kPhi = kIndexExtended(k, clayer->loc.nx, clayer->loc.ny, clayer->numFeatures,
-                                   clayer->numBorder);
+                                   clayer->loc.nPad);
 #else
       int kPhi = k;
 #endif
