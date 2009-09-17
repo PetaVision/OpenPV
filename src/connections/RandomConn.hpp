399b69d738ee29a38a2e710bd102e0b1e840f611
marian_anghel <marian_anghel@users.sourceforge.net>
2009-09-17 19:59:57 +0000

    Shreyas's chnages to this class: includes a constructor with a distribution type as argument.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1618 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/connections/RandomConn.hpp
+++ src/connections/RandomConn.hpp
@@ -12,12 +12,40 @@
 
 namespace PV {
 
+enum RandDistType {
+   UNDEFINED = 0,
+   UNIFORM = 1,
+   GAUSSIAN = 2
+};
+
 class RandomConn: public PV::HyPerConn {
 public:
-   RandomConn(const char * name,
-              HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post, int channel);
+   RandomConn(const char * name, HyPerCol * hc, HyPerLayer * pre,
+              HyPerLayer * post, int channel);
+
+   RandomConn(const char * name, HyPerCol * hc, HyPerLayer * pre,
+              HyPerLayer * post, int channel, RandDistType distrib);
 
    virtual int initializeWeights(const char * filename);
+   int initializeUniformWeights(int seed);
+   int uniformWeights(PVPatch * wp, float wMin, float wMax, int seed);
+   int initializeGaussianWeights(int seed);
+   int gaussianWeights(PVPatch *wp, float mean, float stdev, int seed);
+   int gaussianWeightsMA(PVPatch *wp, float mean, float stdev, long *);
+   float randgauss(float mean, float stdev);
+   float randgaussMA(float mean, float stdev, long *);
+private:
+   float          wMin;
+   RandDistType   randDistType; // the type of distribution
+   float          wGaussMean;   // mean of the Gaussian distribution
+   float          wGaussStdev;  // std deviation of the Gaussian distribution
+   long           idum;
+
+   void InitRandomConn(const char *name, HyPerCol *hc,
+                       HyPerLayer *pre, HyPerLayer *post, int channel);
+
+   float ranf();
+   float ran1(long *);
 };
 
 }
