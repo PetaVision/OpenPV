1b669b536e63ba81b1cd0bfa22df88d05430c938
rasmussn <rasmussn@users.sourceforge.net>
2009-09-22 16:42:21 +0000

    Initial version.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@1635 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 8526c0f9b2c0418d21dd750eca3c07b397b80c18

---
+++ STDP/stdp.cpp
@@ -0,0 +1,61 @@
+/*
+ * pv_ca.cpp
+ *
+ *  Created on: Apr 2, 2009
+ *      Author: rasmussn
+ */
+
+#include <stdlib.h>
+
+#include <src/columns/HyPerCol.hpp>
+#include <src/connections/RandomConn.hpp>
+#include <src/layers/ImageCreator.hpp>
+#include <src/layers/Retina.hpp>
+#include <src/layers/V1.hpp>
+#include <src/io/ConnectionProbe.hpp>
+#include <src/io/PostConnProbe.hpp>
+#include <src/io/LinearActivityProbe.hpp>
+#include <src/io/PointProbe.hpp>
+
+using namespace PV;
+
+int main(int argc, char* argv[])
+{
+   // create the managing hypercolumn
+   HyPerCol * hc = new HyPerCol("column", argc, argv);
+
+   // read an image
+   ImageCreator * image = new ImageCreator(hc);
+
+   // create the layers
+   HyPerLayer * retina = new Retina("Retina", hc, image);
+   HyPerLayer * l1     = new V1("L1", hc);
+
+   // connect the layers
+   HyPerConn * r_l1 = new RandomConn("Retina to L1", hc, retina, l1, CHANNEL_EXC);
+
+   // add probes
+
+//   PVLayerProbe * ptprobeI = new PointProbe(61, locY, 0, "LI:x=61 f=0");
+
+//   ConnectionProbe * cProbe0 = new ConnectionProbe(2*5 + 0);
+//   ConnectionProbe * cProbe0 = new PostConnProbe(0 + 5*nfPost);
+
+//   retina->insertProbe(rProbe0);
+//   retina->insertProbe(rProbe1);
+   //r_l1->insertProbe(cProbe);
+
+//   l1->insertProbe(probe0);
+
+//   r_l1->insertProbe(cProbe0);
+
+   // run the simulation
+   hc->initFinish();
+   hc->run();
+
+   /* clean up (HyPerCol owns layers and connections, don't delete them) */
+   delete image;
+   delete hc;
+
+   return 0;
+}
