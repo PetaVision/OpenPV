5d592ed213cd8a274fbe9b0ec9082bf984d9795c
rasmussn <rasmussn@users.sourceforge.net>
2009-10-01 17:22:07 +0000

    Added support for .pvp (parallel petavision format) files.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1709 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/io/imageio.hpp
+++ src/io/imageio.hpp
@@ -11,11 +11,19 @@
 #include "../columns/Communicator.hpp"
 #include "../include/pv_types.h"
 
-int getImageInfo(const char * filename, PV::Communicator * comm, LayerLoc * loc);
+int getImageInfo    (const char * filename, PV::Communicator * comm, LayerLoc * loc);
+int getImageInfoPVP (const char * filename, PV::Communicator * comm, LayerLoc * loc);
+int getImageInfoGDAL(const char * filename, PV::Communicator * comm, LayerLoc * loc);
+
+int gatherImageFile (const char * filename,
+                     PV::Communicator * comm, LayerLoc * loc, unsigned char * buf);
 int scatterImageFile(const char * filename,
-                     PV::Communicator * comm, LayerLoc * loc, float * buf);
-int gatherImageFile( const char * filename,
-                     PV::Communicator * comm, LayerLoc * loc, float * buf);
+                     PV::Communicator * comm, LayerLoc * loc, unsigned char * buf);
+
+int gatherParByteFile (const char * filename,
+                       PV::Communicator * comm, LayerLoc * loc, unsigned char * buf);
+int scatterParByteFile(const char * filename,
+                       PV::Communicator * comm, LayerLoc * loc, unsigned char * buf);
 
 int scatter(PV::Communicator * comm, LayerLoc * loc, float * buf);
 int gather (PV::Communicator * comm, LayerLoc * loc, float * buf);
