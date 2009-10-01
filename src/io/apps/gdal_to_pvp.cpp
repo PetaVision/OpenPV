bf4e101eac6ab860b4b67b5076d931a12b79293b
rasmussn <rasmussn@users.sourceforge.net>
2009-10-01 22:47:43 +0000

    Modified to work only with PV::Communicator rather than requiring a HyPerCol object.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1714 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/io/apps/gdal_to_pvp.cpp
+++ src/io/apps/gdal_to_pvp.cpp
@@ -5,9 +5,8 @@
  *      Author: rasmussn
  */
 
-#include "src/io/imageio.hpp"
-#include "src/columns/HyPerCol.hpp"
 #include "src/layers/Image.hpp"
+#include "src/io/imageio.hpp"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,53 +19,49 @@ using namespace PV;
  */
 int main(int argc, char* argv[])
 {
-   int n, nx, ny;
    unsigned char * buf;
-
    LayerLoc loc;
-
    int status = 0;
 
-   if (argc < 5) {
-     printf("usage: gdal_to_pvpar infile outfile nx ny\n");
-//     exit(1);
+   if (argc < 3) {
+     printf("usage: gdal_to_pvp infile outfile.pvp [outfile.jpg]\n");
+     exit(1);
    }
 
-   HyPerCol * hc = new HyPerCol("column", argc, argv);
+   const char * infile = argv[1];
+   const char * outfile_pvp = argv[2];
+   const char * outfile_jpg = NULL;
 
-   const char * infile = hc->inputFile();
-   const char * outfile = "image.pvp";
+   if (argc > 3) {
+      outfile_jpg = argv[3];
+   }
 
-   // read an image
-   Image * image = new Image("Image", hc, infile);
+   Communicator * comm = new Communicator(&argc, &argv);
+
+   // allocate memory for image buffer
    
-   loc = image->getImageLoc();
+   loc.nPad = 0;
+   status = getImageInfo(infile, comm, &loc);
 
-   return 0;
-
-   int numItems = loc.nx * loc.ny * loc.nBands;
-
+   const int numItems = loc.nx * loc.ny * loc.nBands;
    buf = new unsigned char[numItems];
    assert(buf != NULL);
 
-   float * data = image->getImageBuffer();
-   for (int k = 0; k < numItems; k++) {
-      buf[k] = (unsigned char) data[k];
+   // read the image and scatter the local portions
+   status = scatterImageFile(infile, comm, &loc, buf);
+
+   if (loc.nBands > 1) {
+      Image::convertToGrayScale(&loc, buf);
    }
 
-   status = gatherParByteFile(outfile, hc->icCommunicator(), &loc, buf);
+   // write the image back out to files
 
-   return 0;
-
-   status = scatterParByteFile(outfile, hc->icCommunicator(), &loc, buf);
-   for (int k = 0; k < numItems; k++) {
-      data[k] = (float) buf[k];
+   status = gatherImageFile(outfile_pvp, comm, &loc, buf);
+   if (outfile_jpg != NULL) {
+      status = gatherImageFile(outfile_jpg, comm, &loc, buf);
    }
-
-   image->write("image.jpg");
 
    delete buf;
-   delete hc;
 
-   return 0;
+   return status;
 }
