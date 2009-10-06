508aa2610592a80ff397bf042a409e01ec626d17
rasmussn <rasmussn@users.sourceforge.net>
2009-10-06 20:07:03 +0000

    Modified to use Image class.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1767 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/pv_example.cpp
+++ src/pv_example.cpp
@@ -10,8 +10,9 @@
 #include "include/pv_common.h"
 #include "columns/HyPerCol.hpp"
 
-#include "layers/Example.hpp"
+#include "layers/Image.hpp"
 #include "layers/Retina.hpp"
+#include "layers/V1.hpp"
 
 #include "layers/fileread.h"
 
@@ -19,24 +20,25 @@ using namespace PV;
 
 int main(int argc, char* argv[])
 {
-   const char input[] = INPUT_PATH "const_one_64x64.tif";
-
    HyPerCol * hc = new HyPerCol("column", argc, argv);
 
+   Image * image = new Image("Image", hc, hc->inputFile());
+
    // construct layers
-   HyPerLayer * retina  = new Retina("Example Retina", hc, input);
-   HyPerLayer * example = new Example("Example Layer", hc);
+   HyPerLayer * retina  = new Retina("Retina", hc, image);
+   HyPerLayer * l1      = new V1("L1", hc);
 
    // connect the layers
-   new PV::HyPerConn("Retina to Example", hc, retina, example, CHANNEL_EXC);
+   new PV::HyPerConn("Retina to L1", hc, retina, l1, CHANNEL_EXC);
 
    // finish initialization now that everything is connected
    hc->initFinish();
 
    // run the simulation for 2 time steps
-   hc->run(2);
+   hc->run(1);
 
-   hc->writeState();
+   retina->writeActivity(OUTPUT_PATH "retina_last.jpg");
+   retina->writeActivity(OUTPUT_PATH "l1_last.jpg");
 
    // clean up (HyPerCol owns the layers, so don't delete them here)
    delete hc;
