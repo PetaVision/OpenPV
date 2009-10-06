295fe9a3a70cb8637c165a77a024394bb6ca381e
rasmussn <rasmussn@users.sourceforge.net>
2009-10-06 05:03:48 +0000

    Initial version of connection class for applying a convolution kernel.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1756 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/connections/ConvolveConn.hpp
@@ -0,0 +1,32 @@
+/*
+ * ConvolveConn.hpp
+ *
+ *  Created on: Oct 5, 2009
+ *      Author: rasmussn
+ */
+
+#ifndef CONVOLVECONN_HPP_
+#define CONVOLVECONN_HPP_
+
+#include "HyPerConn.hpp"
+
+namespace PV {
+
+class ConvolveConn: public PV::HyPerConn {
+public:
+   ConvolveConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+                int channel);
+
+   virtual int deliver(PVLayerCube * cube, int neighbor);
+   void convolve(PVLayerCube * dst, PVLayerCube * src, PVPatch * patch);
+
+protected:
+   PVPatch patch;
+
+   virtual int initialize(const char * filename);
+
+};
+
+} // namespace PV
+
+#endif /* CONVOLVECONN_HPP_ */
