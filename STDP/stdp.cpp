3bfb30cc46b531ff556a091c02e1f48cd72d6be8
rasmussn <rasmussn@users.sourceforge.net>
2009-10-22 22:43:26 +0000

    Working with Marian and Gar to set parameters for STDP paper.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@1843 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 0ad1a6483fa7793e197a12ba00dfad9ae55a357c

--- STDP/stdp.cpp
+++ STDP/stdp.cpp
@@ -1,5 +1,5 @@
 /*
- * pv_ca.cpp
+ * stdp.cpp
  *
  *  Created on: Apr 2, 2009
  *      Author: rasmussn
@@ -10,55 +10,54 @@
 #include <src/columns/HyPerCol.hpp>
 #include <src/connections/RandomConn.hpp>
 #include <src/layers/Image.hpp>
-#include <src/layers/Movie.hpp>
+#include <src/layers/ImageCreator.hpp>
 #include <src/layers/Retina.hpp>
 #include <src/layers/V1.hpp>
 #include <src/io/ConnectionProbe.hpp>
 #include <src/io/PostConnProbe.hpp>
 #include <src/io/LinearActivityProbe.hpp>
 #include <src/io/PointProbe.hpp>
+#include <src/io/StatsProbe.hpp>
+
+#include <src/io/imageio.hpp>
 
 using namespace PV;
 
 int main(int argc, char* argv[])
 {
-   const char * files = "input/movies/movies.files";
-
    // create the managing hypercolumn
    HyPerCol * hc = new HyPerCol("column", argc, argv);
 
-   // read an image
-   float displayPeriod = 20;  // ms
-   Movie * movie = new Movie(files, hc, displayPeriod);
-
    // create the layers
-   HyPerLayer * retina = new Retina("Retina", hc, movie);
-   HyPerLayer * l1     = new V1("L1", hc);
+   ImageCreator * image = new ImageCreator("Image", hc);
+   HyPerLayer * retina = new Retina("Retina", hc, image);
+   HyPerLayer * l1 = new V1("L1", hc);
 
-   // connect the layers
-   HyPerConn * r_l1 = new RandomConn("Retina to L1", hc, retina, l1, CHANNEL_EXC);
+   HyPerConn * r_l1 = new HyPerConn("Retina to L1", hc, retina, l1, CHANNEL_EXC);
 
    // add probes
+
+   const int ny = retina->clayer->loc.ny;
+   PVLayerProbe * laProbes[ny]; // array of ny pointers to LinearActivityProbe
+
+   for (int iy = 1; iy < ny-1; iy++) {
+	   laProbes[iy] = new PV::LinearActivityProbe(hc, DimX, iy, 0);
+       l1->insertProbe(laProbes[iy]);
+   }
+
+   PVLayerProbe * statsProbe = new StatsProbe(BufActivity, "L1");
+   l1->insertProbe(statsProbe);
 
 //   PVLayerProbe * ptprobeI = new PointProbe(61, locY, 0, "LI:x=61 f=0");
 
 //   ConnectionProbe * cProbe0 = new ConnectionProbe(2*5 + 0);
 //   ConnectionProbe * cProbe0 = new PostConnProbe(0 + 5*nfPost);
 
-//   retina->insertProbe(rProbe0);
-//   retina->insertProbe(rProbe1);
-   //r_l1->insertProbe(cProbe);
-
-//   l1->insertProbe(probe0);
-
-//   r_l1->insertProbe(cProbe0);
-
    // run the simulation
    hc->initFinish();
    hc->run();
 
    /* clean up (HyPerCol owns layers and connections, don't delete them) */
-   delete movie;
    delete hc;
 
    return 0;
