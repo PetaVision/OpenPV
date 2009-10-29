b26c6f8140615beb1bcb58ca460f5dfc2328600d
rasmussn <rasmussn@users.sourceforge.net>
2009-10-29 16:43:23 +0000

    Initial working version.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1869 7c0444ca-5550-0410-9008-faecdbe78eb5

--- analysis/python/PVReadWeights.py
+++ analysis/python/PVReadWeights.py
@@ -1,15 +1,24 @@
 from numpy import *
 
-class PVReadBin(object):
-   """A class to read PetaVision binary files"""
+class PVReadWeights(object):
+   """A class to read PetaVision binary weight files"""
 
    def __init__(self, filename):
       """Constructor: Open file and read in file metadata into params"""
       
+      '''initialize'''
+      self.numParams = 0
+      self.params = ()
+      self.nx = 0
+      self.ny = 0
+      self.nf = 0
+      self.numItems = 0
+
       self.open(filename)
       self.read_params()
-      '''current timestep'''
-      self.timestep = 0
+
+      '''current patch'''
+      self.patch = 0
 
    # end __init__
 
@@ -19,26 +28,74 @@ class PVReadBin(object):
       self.file = open(filename, mode='rb')
    # end open
    
-   def next_record(self):
-      """Read the next record and return an ndarray"""
-      a = fromfile(self.file, 'f', self.numItems)
-      self.timestep += 1
-      return a
+   def next_patch(self):
+      """Read the next patch and return an ndarray"""
+      
+      nx,ny = fromfile(self.file, int16(), 2)
+      count = nx*ny
+      bytes = fromfile(self.file, uint8(), count)
+
+      n_extra = count%4
+      extras = fromfile(self.file, uint8(), n_extra)
+
+      w = zeros(count, dtype=float) + bytes/255.
+      w = self.min + (self.max - self.min) * w      
+
+      self.patch += 1
+      return w
+   # end next
+
+   def next_patch_bytes(self):
+      """Read the next patch and return an ndarray"""
+      
+      nx,ny = fromfile(self.file, int16(), 2)
+      count = nx*ny
+      bytes = fromfile(self.file, uint8(), count)
+
+      n_extra = (4 - count%4) % 4
+      extras = fromfile(self.file, uint8(), n_extra)
+
+      self.patch += 1
+      return bytes
    # end next
 
    def read_params(self):
       """Read the file metadata parameters"""
-      self.numParams = fromfile(self.file, 'i', 1) [0]
-      self.params    = fromfile(self.file, 'i', self.numParams)
 
-      self.nx,self.ny,self.nf = self.params[0:3]
-      self.numItems = multiply.reduce(self.params[0:3])
+      head = fromfile(self.file, 'i', 3)
+      if head[2] != 2:
+         print "Incorrect file type"
+         return
+
+      self.headerSize = head[0]
+      self.numParams  = head[1]
+
+      self.file.seek(0)
+      self.params = fromfile(self.file, 'i', self.numParams)
+
+      self.nx,self.ny,self.nf = self.params[3:6]
+
+      self.min = self.params[6]
+      self.max = self.params[7]
+      self.numPatches = self.params[8]
+
+      self.numItems = multiply.reduce(self.params[3:6])
    # end read_params
 
    def rewind(self):
       """Rewind the file to the start of the data (just past metadata)"""
-      self.file.seek(4 * (self.numParams + 1))
+      self.file.seek(self.headerSize)
    # end rewind
+
+   def histogram(self):
+      self.rewind()
+      h = zeros(256, dtype=int)
+      for p in range(self.numPatches):
+         b = self.next_patch_bytes()
+         for k in range(len(b)):
+            h[b[k]] += 1
+      return h
+   # end histogram
 
    def valuesAt(self, k):
       """Return an ndarray of values at index k in each time slice (record)"""
