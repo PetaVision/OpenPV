c350917ab4ccd12f097870b6c1c13560d7484f7a
rasmussn <rasmussn@users.sourceforge.net>
2009-11-05 17:03:39 +0000

    Many more probes.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@1891 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: f98023ab04d21d522041215784c11ad596043b2b

--- STDP/stdp.cpp
+++ STDP/stdp.cpp
@@ -44,19 +44,19 @@ int main(int argc, char* argv[])
    // create the connections
    //
    HyPerConn * r_l1   = new HyPerConn("Retina to L1", hc, retina, l1, CHANNEL_EXC);
-//   HyPerConn * l1_inh = new HyPerConn("L1 to L1Inh", hc, l1, l1Inh, CHANNEL_EXC);
-//   HyPerConn * inh_l1 = new HyPerConn("L1Inh to L1", hc, l1Inh, l1, CHANNEL_INH);
+   HyPerConn * l1_inh = new HyPerConn("L1 to L1Inh", hc, l1, l1Inh, CHANNEL_EXC);
+   HyPerConn * inh_l1 = new HyPerConn("L1Inh to L1", hc, l1Inh, l1, CHANNEL_INH);
 
    // add probes
 
-   HyPerLayer * displayLayer = l1;
+   HyPerLayer * displayLayer = retina;
 
    const int ny = displayLayer->clayer->loc.ny;
    PVLayerProbe * laProbes[ny]; // array of ny pointers to LinearActivityProbe
 
-   for (int iy = 1; iy < ny-1; iy++) {
-   //   laProbes[iy] = new LinearActivityProbe(hc, DimX, iy, 0);
-   //   displayLayer->insertProbe(laProbes[iy]);
+   for (int iy = 2; iy < ny-2; iy++) {
+//      laProbes[iy] = new LinearActivityProbe(hc, DimX, iy, 0);
+//      displayLayer->insertProbe(laProbes[iy]);
    }
 
    PVLayerProbe * statsR     = new StatsProbe(BufActivity, "R    :");
@@ -67,17 +67,27 @@ int main(int argc, char* argv[])
    l1->insertProbe(statsL1);
    l1Inh->insertProbe(statsL1Inh);
 
-   PVLayerProbe * ptprobe = new PointProbe(16, 16, 0, "L1:(16,16)");
-//   l1->insertProbe(ptprobe);
+   PVLayerProbe * ptprobe0 = new PointProbe(16, 16, 0, "L1:(16,16)");
+   PVLayerProbe * ptprobe1 = new PointProbe(17, 16, 0, "L1:(17,16)");
+   PVLayerProbe * ptprobe2 = new PointProbe(18, 16, 0, "L1:(18,16)");
+   PVLayerProbe * ptprobe3 = new PointProbe(19, 16, 0, "L1:(19,16)");
+   PVLayerProbe * ptprobe4 = new PointProbe(20, 16, 0, "L1:(20,16)");
+   l1->insertProbe(ptprobe0);
+//   l1->insertProbe(ptprobe1);
+//   l1->insertProbe(ptprobe2);
+//   l1->insertProbe(ptprobe3);
+//   l1->insertProbe(ptprobe4);
 
    ConnectionProbe * cProbe0 = new PostConnProbe(16, 16, 0);
    ConnectionProbe * cProbe1 = new PostConnProbe(17, 16, 0);
    ConnectionProbe * cProbe2 = new PostConnProbe(18, 16, 0);
    ConnectionProbe * cProbe3 = new PostConnProbe(19, 16, 0);
+   ConnectionProbe * cProbe4 = new PostConnProbe(20, 16, 0);
    r_l1->insertProbe(cProbe0);
-   r_l1->insertProbe(cProbe1);
-   r_l1->insertProbe(cProbe2);
-   r_l1->insertProbe(cProbe3);
+//   r_l1->insertProbe(cProbe1);
+//   r_l1->insertProbe(cProbe2);
+//   r_l1->insertProbe(cProbe3);
+//   r_l1->insertProbe(cProbe4);
 
    // run the simulation
    hc->initFinish();
