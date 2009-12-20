93a86a8ac75dccf609014f4e21e44397e269fcef
marian_anghel <marian_anghel@users.sourceforge.net>
2009-12-20 02:44:53 +0000

    A new version that: 1) reads and plots the previous rate and histogram distance evolution and 2) it appends these plots for another number of time steps.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@1946 7c0444ca-5550-0410-9008-faecdbe78eb5

--- analysis/python/transient.py
+++ analysis/python/transient.py
@@ -6,6 +6,7 @@
 import os
 import re     # regular expression module
 import time
+import string
 
 import sys
 import numpy as np
@@ -19,7 +20,7 @@ import PVReadSparse as rs
 path = '/Users/manghel/Documents/workspace/STDP/'
 
 if len(sys.argv) < 6:
-   print "usage: python transient.py timeSteps writeSteps dT minD start_flag"
+   print "usage: python transient.py totalSteps timeSteps writeSteps dT minD"
    exit()
 
 def modify_input(flag):
@@ -44,6 +45,82 @@ def modify_input(flag):
     return 0
 # end modify_input
 
+# read the last histogram from the end of the previous simulation
+# plot it
+def read_histogram(totalSteps):
+
+    # read last histogram
+    input = open(path + 'output/w0_last_hist_' + str(totalSteps) + '.dat','r')
+    h = np.zeros(256, dtype=int)
+    k=0;
+    while 1:
+       line = input.readline()
+       if line == '':break
+       h[k] = int(line);
+       k+=1   
+    input.close()
+
+    # plot histogram
+    fig = plt.figure(1)
+    plt.subplot(2,2,1)
+    plt.plot(np.arange(len(h)), h, 'o', color='r')
+    plt.draw()
+
+# read and plot evolution of average firing rate
+def read_rate():
+
+   # initialize rate subplot
+    plt.figure(1)
+    plt.subplot(2,2,4)
+
+    # read rate evolution 
+    input = open(path + 'output/rate.stdp','r')
+
+    while 1:
+       line = input.readline()
+       if line == '':break
+       data = string.split(line,' ')
+       time = eval(data[0])
+       rate = eval(data[1])
+       plt.plot(time/1000,rate,'o',color='b')
+       
+    input.close()
+
+    plt.xlabel('TIME')
+    plt.ylabel('AVERAGE RATE')
+    plt.title('Firing Rate Evolution')
+    plt.hold(True)
+    plt.draw()
+
+# end read_rate
+
+
+# read and plot evolution of histogram distance
+def read_histD():
+
+   # initialize histogram distance subplot
+    plt.figure(1)
+    plt.subplot(2,2,3)
+
+    # read rate evolution 
+    input = open(path + 'output/histD.stdp','r')
+
+    while 1:
+       line = input.readline()
+       if line == '':break
+       data = string.split(line,' ')
+       time = eval(data[0])
+       histD = eval(data[1])
+       plt.plot(time/1000,histD,'o',color='b')
+       
+    input.close()
+
+    plt.xlabel('Time')
+    plt.ylabel('Histogram Distance')
+    plt.title('Weight Histogram Distance')
+    plt.hold(True)
+    plt.draw()
+
 def compute_histogram(totalSteps, writeSteps, dT):
     infile = path + 'output/' + 'w0_last.pvp'
     output = open(path + 'output/w0_last_hist_' + str(totalSteps) + '.dat','w')
@@ -64,8 +141,8 @@ def compute_histogram(totalSteps, writeSteps, dT):
     fig = plt.figure(1)
     plt.subplot(2,2,1)
     plt.plot(np.arange(len(h)), h, 'o', color='y')
-    plt.xlabel('WEIGHT BINS')
-    plt.ylabel('COUNT')
+    plt.xlabel('Weight Bins')
+    plt.ylabel('Count')
     plt.title('Weight Histogram')
     plt.xlim(0, 256)
     plt.grid(True)
@@ -75,6 +152,7 @@ def compute_histogram(totalSteps, writeSteps, dT):
     # compute histogram distance
 
     if totalSteps-writeSteps > 0:
+       output = open(path + 'output/histD.stdp','a')
        input = open(path + 'output/w0_last_hist_' + str(totalSteps-writeSteps) + '.dat','r')
        h_old = np.zeros(256, dtype=int)
        dh = np.zeros(256,dtype=float)
@@ -88,15 +166,18 @@ def compute_histogram(totalSteps, writeSteps, dT):
        input.close()
        histD = max(dh)     # histogram distance
        print 'histD = %f\n' % (histD)
+       output.write(str(time) + ' ' + str(histD) + '\n')
+       output.close()
+
        plt.plot(np.arange(len(h_old)), h_old, 'o', color='r')
        plt.draw()
 
        #bx = fig.add_subplot(122, axisbg='darkslategray')
        plt.subplot(2,2,2)
        plt.plot(np.arange(len(dh)), dh, 'o', color='y')
-       plt.xlabel('WEIGHT BINS')
-       plt.ylabel('RELATIVE ERROR')
-       plt.title('Weight Histogram RELATIVE ERROR')
+       plt.xlabel('Weight Bins')
+       plt.ylabel('Relative Bin Distance')
+       plt.title('Weight Histogram Bin Distance')
        plt.xlim(0, 256)
        plt.grid(True)
        plt.hold(False)
@@ -104,9 +185,9 @@ def compute_histogram(totalSteps, writeSteps, dT):
 
        plt.subplot(2,2,3)
        plt.plot(time/1000, histD, 'o', color='b')
-       plt.xlabel('TIME')
-       plt.ylabel('HISTOGRAM ERROR')
-       plt.title('Weight Histogram Error')
+       plt.xlabel('Time')
+       plt.ylabel('Histogram Distance')
+       plt.title('Weight Histogram Distance')
        plt.hold(True)
        plt.draw()
 
@@ -157,22 +238,28 @@ Main code:
 
 """
 
-timeSteps = sys.argv[1]     # length of simulation (timeSteps)
-writeSteps = sys.argv[2]    # estimate histogram every writeSteps
-dT         = float(sys.argv[3])    # simulation time step
-minD      = float(sys.argv[4])         # stoping condition
-start_flag = int(sys.argv[5])    # starting flag
+totalSteps = int(sys.argv[1])    # length of previous simulation
+timeSteps = sys.argv[2]     # length of simulation (timeSteps) from the beginning of 
+                            # simulation
+writeSteps = sys.argv[3]    # estimate histogram every writeSteps
+dT         = float(sys.argv[4])    # simulation time step
+minD      = float(sys.argv[5])         # stoping condition
+#start_flag = int(sys.argv[5])    # starting flag
 
-print '\ntimeSteps = %s writeSteps = %s dT = %f minD = %f start_flag = %d\n' % (timeSteps,writeSteps,dT, minD, start_flag)
-totalSteps = 0;
+print '\ntotalSteps = %d timeSteps = %s writeSteps = %s dT = %f minD = %f \n' \
+     % (totalSteps,timeSteps,writeSteps,dT, minD)
+
 histD = 1000;
 
 while histD > minD and totalSteps < int(timeSteps):
     
     print '\ntotalSteps = %d \n' % totalSteps
     if totalSteps == 0:
-       modify_input(start_flag)
+       modify_input(0)
     else:
+       read_histogram(totalSteps)
+       read_rate()
+       read_histD()
        modify_input(1)
 
     #time.sleep(10)
@@ -188,6 +275,7 @@ while histD > minD and totalSteps < int(timeSteps):
     rate = compute_rate(totalSteps*dT,int(writeSteps), int(writeSteps),dT)
     print '\n average rate = %f \n' % rate
 
+    #if abs(rate-old_rate) < 0.1:
     # remove files
     if 0:
        cmd = 'rm ' + path + '/output/images/*'
@@ -196,7 +284,8 @@ while histD > minD and totalSteps < int(timeSteps):
        cmd = 'rm ' + path + '/output/*.pvp'
        os.system(cmd)
 
-    
+    old_rate = rate
+
 #time.sleep(10)
 print 'pass control to plot'
 plt.show()
