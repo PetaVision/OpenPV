e6da21ecb0d7b89b511a1f0eac0f21d766080208
rasmussn <rasmussn@users.sourceforge.net>
2010-01-11 16:47:22 +0000

    Initial version.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2018 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/columns/HyPerColDelegate.cpp
@@ -0,0 +1,20 @@
+/*
+ * HyPerColDelegate.cpp
+ *
+ *  Created on: Jan 10, 2010
+ *      Author: Craig Rasmussen
+ */
+
+#include "HyPerColDelegate.hpp"
+
+namespace PV {
+
+HyPerColDelegate::HyPerColDelegate()
+{
+}
+
+HyPerColDelegate::~HyPerColDelegate()
+{
+}
+
+}
