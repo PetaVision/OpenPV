fbb9a510864c29005733d8ab2bab274efa9289f1
hwtsky <hwtsky@users.sourceforge.net>
2010-01-12 17:57:39 +0000

    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2036 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/layers/HMaxSimple.cpp
@@ -0,0 +1,76 @@
+/*
+ * HMaxSimple.cpp
+ *
+ *  Created on: Nov 20, 2008
+ *      Author: bjt
+ */
+
+#include "HMaxSimple.hpp"
+
+namespace PV {
+
+HMaxSimple::HMaxSimple(const char * name, HyPerCol * hc) : HyPerLayer(name, hc)
+{
+   initialize(TypeGeneric);
+}
+
+int HMaxSimple::recvSynapticInput(HyPerConn * conn, PVLayerCube * activity, int neighbor)
+{
+   pv_debug_info("[%d]: HMaxSimple::recvSynapticInput: layer %d from %d)",
+                 clayer->columnId,
+                 conn->preSynapticLayer()->clayer->layerId, clayer->layerId);
+
+   // use implementation in base class
+   HyPerLayer::recvSynapticInput(conn, activity, neighbor);
+
+   return 0;
+}
+
+int HMaxSimple::updateState(float time, float dt)
+{
+   pv_debug_info("[%d]: HMaxSimple::updateState:", clayer->columnId);
+
+   // just copy accumulation buffer to membrane potential
+   // and activity buffer (nonspiking)
+
+   pvdata_t * phi = clayer->phi[0];
+
+   for (int k = 0; k < clayer->numNeurons; k++) {
+#ifdef EXTEND_BORDER_INDEX
+      int kPhi = kIndexExtended(k, clayer->loc.nx, clayer->loc.ny, clayer->numFeatures,
+                                   clayer->loc.nPad);
+#else
+      int kPhi = k;
+#endif
+      clayer->V[k] = phi[kPhi];
+      clayer->activity->data[k] = phi[kPhi];
+      phi[kPhi] = 0.0;     // reset accumulation buffer
+   }
+
+   return 0;
+}
+
+int HMaxSimple::initFinish(int colId, int colRow, int colCol)
+{
+   pv_debug_info("[%d]: HMaxSimple::initFinish: colId=%d colRow=%d, colCol=%d",
+                 clayer->columnId, colId, colRow, colCol);
+   return 0;
+}
+
+int HMaxSimple::setParams(int numParams, float* params)
+{
+   pv_debug_info("[%d]: HMaxSimple::setParams: numParams=%d", clayer->columnId, numParams);
+   return 0;
+}
+
+int HMaxSimple::outputState(float time)
+{
+   pv_debug_info("[%d]: HMaxSimple::outputState:", clayer->columnId);
+
+   // use implementation in base class
+   HyPerLayer::outputState(time);
+
+   return 0;
+}
+
+}
---
+++ src/layers/HMaxSimple.hpp
@@ -0,0 +1,31 @@
+/*
+ * HMaxSimple.h
+ *
+ *  Created on: Nov 20, 2008
+ *      Author: bjt
+ */
+
+#ifndef HMAXSIMPLE_H_
+#define HMAXSIMPLE_H_
+
+#include "HyPerLayer.hpp"
+
+namespace PV {
+
+class HMaxSimple: public PV::HyPerLayer {
+public:
+   HMaxSimple(const char * name, HyPerCol * hc);
+
+   virtual int recvSynapticInput(HyPerConn* conn, PVLayerCube* activity, int neighbor);
+   virtual int updateState(float time, float dt);
+
+   virtual int initFinish(int colId, int colRow, int colCol);
+
+   virtual int setParams(int numParams, float* params);
+
+   virtual int outputState(float time);
+};
+
+}
+
+#endif /* SIMPLEPOGGIO_H_ */
