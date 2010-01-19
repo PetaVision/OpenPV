3b4c1200a5b035716f05a667dc402d2ed938077e
rasmussn <rasmussn@users.sourceforge.net>
2010-01-19 16:27:42 +0000

    Renamed HyPerColDelegate to HyPerColRunDelegate.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2111 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/columns/HyPerColRunDelegate.cpp
@@ -0,0 +1,20 @@
+/*
+ * HyPerColRunDelegate.cpp
+ *
+ *  Created on: Jan 10, 2010
+ *      Author: Craig Rasmussen
+ */
+
+#include "HyPerColRunDelegate.hpp"
+
+namespace PV {
+
+HyPerColRunDelegate::HyPerColRunDelegate()
+{
+}
+
+HyPerColRunDelegate::~HyPerColRunDelegate()
+{
+}
+
+}
---
+++ src/columns/HyPerColRunDelegate.hpp
@@ -0,0 +1,34 @@
+/*
+ * HyPerColRunDelegate.hpp
+ * 
+ * Virtual base class for HyPerCol runtime delegates.  A
+ * runtime delegate replaces the run loop in a HyPerCol.
+ * The delegate must call 
+ *     HyPerCol::advanceTime(float time);        // within run loop
+ *     HyPerCol::exitRunLoop(bool exitOnFinish); // at end of run
+ *     
+ * A runtime delegate was necessary for OpenGL displays as OpenGL
+ * runs its own event loop (never returning).
+ *
+ *  Created on: Jan 10, 2010
+ *      Author: Craig Rasmussen
+ */
+
+#ifndef HYPERCOLRUNDELEGATE_HPP_
+#define HYPERCOLRUNDELEGATE_HPP_
+
+namespace PV {
+
+class HyPerCol;
+
+class HyPerColRunDelegate {
+public:
+   HyPerColRunDelegate();
+   virtual ~HyPerColRunDelegate();
+
+   virtual void run(float time, float stopTime) = 0;
+};
+
+}
+
+#endif /* HYPERCOLRUNDELEGATE_HPP_ */
