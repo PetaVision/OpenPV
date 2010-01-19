269eeaf5fd0981bb1216be9fd5f6742fee6578b2
rasmussn <rasmussn@users.sourceforge.net>
2010-01-19 16:16:27 +0000

    Refactoring to display layers as well as images.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2097 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/io/GLDisplay.hpp
+++ src/io/GLDisplay.hpp
@@ -9,25 +9,35 @@
 #define GLPROBE_HPP_
 
 #include "../columns/HyPerCol.hpp"
+#include "../layers/HyPerLayer.hpp"
 #include "../layers/Image.hpp"
 
 namespace PV {
 
-class GLDisplay: public HyPerColDelegate {
+class GLDisplay: public HyPerColRunDelegate, public LayerProbe {
 public:
    GLDisplay(int * argc, char * argv[], HyPerCol * hc, float msec=0.0f);
    virtual ~GLDisplay();
 
+   int addLayer(HyPerLayer * l);
    void setImage(Image * image);
 
    void run(float time, float stopTime);
 
    void drawDisplay();
-   int  loadTexture(int id, Image * image);
+   int  loadTexture(int id, LayerDataInterface * image);
+
+   // implement LayerProbe interface
+   //
+   int outputState(float time, HyPerLayer * l);
 
 private:
-   HyPerCol * parent;
-   Image    * image;
+   HyPerCol   * parent;
+   Image      * image;
+   HyPerLayer * layer;
+
+   int layerTexId;
+
    float time;
    float stopTime;
    float lastUpdateTime;
