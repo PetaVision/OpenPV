8bc14f731e2cbc44fde3c4bbbdbb24e51a6f4461
rasmussn <rasmussn@users.sourceforge.net>
2010-01-19 16:18:02 +0000

    LayerProbe now uses HyPerLayer rather than just PVLayer.  Also renamed PVLayerProbe to LayerProbe.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2101 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/io/PointProbe.hpp
+++ src/io/PointProbe.hpp
@@ -8,17 +8,17 @@
 #ifndef POINTPROBE_HPP_
 #define POINTPROBE_HPP_
 
-#include "PVLayerProbe.hpp"
+#include "LayerProbe.hpp"
 
 namespace PV {
 
-class PointProbe: public PV::PVLayerProbe {
+class PointProbe: public PV::LayerProbe {
 public:
    PointProbe(const char * filename, int xLoc, int yLoc, int fLoc, const char * msg);
    PointProbe(int xLoc, int yLoc, int fLoc, const char * msg);
    virtual ~PointProbe();
 
-   virtual int outputState(float time, PVLayer * l);
+   virtual int outputState(float time, HyPerLayer * l);
 
 protected:
    int xLoc;
