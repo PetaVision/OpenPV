f5c430c1d9470fe8059c5fee87e454c795796dcd
rasmussn <rasmussn@users.sourceforge.net>
2010-02-02 18:12:59 +0000

    Fixed to work with conversions.h.  This required a modification of the test results.  The results of nearby_neighbor now returns the cell to the right if the density of the post-synaptic layer increases.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2139 7c0444ca-5550-0410-9008-faecdbe78eb5

--- tests/test_nearby_neighbor.c
+++ tests/test_nearby_neighbor.c
@@ -1,55 +1,89 @@
-#include "../src/layers/elementals.h"
+#include "../src/utils/conversions.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+/*
+ * The scale factor is a measure of the difference in distance scales between
+ * layers.  The distance scale, dx, is the distance between neurons in
+ * retinatopic units so dx in the retina is 1.
+ *
+ * In the PV code, the scale factor is usually stored as log base 2 of the
+ * relative scale factor.  Thus if xScaleLog2 == 1 for a layer, then dx == 2
+ * (the linear density of neurons is 1/2 that of the retina).  Likewise if
+ * xScaleLog2 == -1, then dx == 1/2 (the linear density of the neurons is 2 times
+ * that of the retina).
+ *
+ *  If the density of the post-synaptic layer increases, the nearby neighbor is
+ *  ambiguous and the neuron to the right is chosen.  If the density of the
+ *  post-synaptic layer decreases, there is no ambiguity and the nearby
+ *  neighbor is just (a * kzPre) where 0 < a < 1.
+ *
+ */
 
 int main(int argc, char* argv[])
 {
    int kPre, kPost;
-   int min = -10000;   
-   int max =  10000;   
+   int min = -100000;   
+   int max =  100000;   
 
-   int scale = 0;
+   int scaleLog2Pre  = 0;
+   int scaleLog2Post = 0;
 
+   // post-synaptic layer has same size
+   //
    for (kPre = min; kPre < max; kPre++) {
-      kPost = nearby_neighbor(kPre, scale);
+      kPost = nearby_neighbor(kPre, scaleLog2Pre, scaleLog2Post);
       if (kPost != kPre) {
-         printf("FAILED:TEST_NEARBY_NEIGHBOR: scale=%d, kPre=%d, kPost=%d\n", scale, kPre, kPost);
+         printf("FAILED:TEST_NEARBY_NEIGHBOR: scaleLog2Pre==%d scaleLog2Post==%d kPre==%d kPost==%d\n",
+                scaleLog2Pre, scaleLog2Post, kPre, kPost);
          exit(1);
       }
    }
 
-   scale = 1;
+   // post-synaptic layer density decreases by 2 (dx increases by 2)
+   //
+   scaleLog2Post = 1;
    for (kPre = min; kPre < max; kPre++) {
-      kPost = nearby_neighbor(kPre, scale);
+      kPost = nearby_neighbor(kPre, scaleLog2Pre, scaleLog2Post);
       if (kPost != kPre/2) {
-         printf("FAILED:TEST_NEARBY_NEIGHBOR: scale=%d, kPre=%d, kPost=%d\n", scale, kPre, kPost);
+         printf("FAILED:TEST_NEARBY_NEIGHBOR: scaleLog2Pre==%d scaleLog2Post==%d kPre==%d kPost==%d\n",
+                scaleLog2Pre, scaleLog2Post, kPre, kPost);
          exit(1);
       }
    }
 
-   scale = 2;
+   // post-synaptic layer density decreases by 4 (dx increases by 4)
+   //
+   scaleLog2Post = 2;
    for (kPre = min; kPre < max; kPre++) {
-      kPost = nearby_neighbor(kPre, scale);
+      kPost = nearby_neighbor(kPre, scaleLog2Pre, scaleLog2Post);
       if (kPost != kPre/4) {
-         printf("FAILED:TEST_NEARBY_NEIGHBOR: scale=%d, kPre=%d, kPost=%d\n", scale, kPre, kPost);
+         printf("FAILED:TEST_NEARBY_NEIGHBOR: scaleLog2Pre==%d scaleLog2Post==%d kPre==%d kPost==%d\n",
+                scaleLog2Pre, scaleLog2Post, kPre, kPost);
          exit(1);
       }
    }
 
-   scale = -1;
+   // post-synaptic layer density increases by 2 (dx decreases by 2)
+   //
+   scaleLog2Post = -1;
    for (kPre = min; kPre < max; kPre++) {
-      kPost = nearby_neighbor(kPre, scale);
-      if (kPost != 2*kPre) {
-         printf("FAILED:TEST_NEARBY_NEIGHBOR: scale=%d, kPre=%d, kPost=%d\n", scale, kPre, kPost);
+      kPost = nearby_neighbor(kPre, scaleLog2Pre, scaleLog2Post);
+      if (kPost != (2*kPre+1)) {
+         printf("FAILED:TEST_NEARBY_NEIGHBOR: scaleLog2Pre==%d scaleLog2Post==%d kPre==%d kPost==%d\n",
+                scaleLog2Pre, scaleLog2Post, kPre, kPost);
          exit(1);
       }
    }
 
-   scale = -2;
+   // post-synaptic layer density increases by 4 (dx decreases by 4)
+   //
+   scaleLog2Post = -2;
    for (kPre = min; kPre < max; kPre++) {
-      kPost = nearby_neighbor(kPre, scale);
-      if (kPost != 4*kPre) {
-         printf("FAILED:TEST_NEARBY_NEIGHBOR: scale=%d, kPre=%d, kPost=%d\n", scale, kPre, kPost);
+      kPost = nearby_neighbor(kPre, scaleLog2Pre, scaleLog2Post);
+      if (kPost != (4*kPre+2)) {
+         printf("FAILED:TEST_NEARBY_NEIGHBOR: scaleLog2Pre==%d scaleLog2Post==%d kPre==%d kPost==%d\n",
+                scaleLog2Pre, scaleLog2Post, kPre, kPost);
          exit(1);
       }
    }
