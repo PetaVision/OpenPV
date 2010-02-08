a39256fa1c24384743388ce28bfd0073550d2caf
rasmussn <rasmussn@users.sourceforge.net>
2010-02-08 19:26:59 +0000

    Initial version.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2144 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ tests/test_post_weights.cpp
@@ -0,0 +1,202 @@
+/**
+ * test_post_weights.cpp
+ *
+ *  Created on: Feb 4, 2010
+ *      Author: Craig Rasmussen
+ *
+ * This file tests the conversion of pre-synaptic weight patches to post-synaptic
+ * weight patches.
+ *
+ */
+
+#undef DEBUG_PRINT
+
+#include "../src/columns/HyPerCol.hpp"
+#include "../src/layers/HyPerLayer.hpp"
+#include "../src/connections/HyPerConn.hpp"
+#include "../src/layers/Example.hpp"
+#include "../src/io/ConnectionProbe.hpp"
+#include "../src/io/PostConnProbe.hpp"
+
+using namespace PV;
+
+static int set_weights_to_source_index(HyPerConn * c);
+static int check_weights(HyPerConn * c, PVPatch ** postWeights);
+
+int main(int argc, char * argv[])
+{
+   PVPatch ** postWeights;
+
+   int status = 0;
+
+   HyPerCol  * hc = new HyPerCol("column", argc, argv);
+   Example   * l1 = new Example("test_post_weights L1", hc);
+   Example   * l2 = new Example("test_post_weights L2", hc);
+   Example   * l3 = new Example("test_post_weights L3", hc);
+   HyPerConn * c1 = new HyPerConn("test_post_weights L1 to L1", hc, l1, l1, CHANNEL_EXC);
+   HyPerConn * c2 = new HyPerConn("test_post_weights L2 to L3", hc, l2, l3, CHANNEL_EXC);
+
+   // set weights to be k index source in pre-synaptic layer
+   //
+   status = set_weights_to_source_index(c1);
+   status = set_weights_to_source_index(c2);
+
+   postWeights = c1->convertPreSynapticWeights(0.0f);
+   status = check_weights(c1, postWeights);
+   if (status) return status;
+
+   postWeights = c2->convertPreSynapticWeights(0.0f);
+
+   ConnectionProbe * cp = new ConnectionProbe(-1, -2, 0);
+   c2->insertProbe(cp);
+
+   PostConnProbe * pcp = new PostConnProbe(125);
+   pcp->setOutputIndices(true);
+   c2->insertProbe(pcp);
+
+   c2->outputState(0);
+
+   status = check_weights(c2, postWeights);
+   if (status) return status;
+
+   return status;
+}
+
+static int check_weights(HyPerConn * c, PVPatch ** postWeights)
+{
+   int status = 0;
+
+   const int nxPre = c->preSynapticLayer()->clayer->loc.nx;
+   const int nyPre = c->preSynapticLayer()->clayer->loc.ny;
+   const int nfPre = c->preSynapticLayer()->clayer->loc.nBands;
+
+   const int nPadPre = c->preSynapticLayer()->clayer->loc.nPad;
+
+   const int nx = c->postSynapticLayer()->clayer->loc.nx;
+   const int ny = c->postSynapticLayer()->clayer->loc.ny;
+   const int nf = c->postSynapticLayer()->clayer->loc.nBands;
+
+   const int numPatches = nx * ny * nf;
+
+   // assume (or at least use) only one arbor (set of weight patches)
+   for (int kPost = 0; kPost < numPatches; kPost++) {
+      int kxPre, kyPre, kfPre = 0;
+
+      const int kxPost = kxPos(kPost, nx, ny, nf);
+      const int kyPost = kyPos(kPost, nx, ny, nf);
+      const int kfPost = featureIndex(kPost, nx, ny, nf);
+
+      PVPatch * p = postWeights[kPost];
+
+      const int nxp = p->nx;
+      const int nyp = p->ny;
+      const int nfp = p->nf;
+
+      // these strides are from the extended pre-synaptic layer, not the patch
+      // NOTE: assumes nf from layer == nf from patch
+      //
+      const int sx = nfPre;
+      const int sy = (nxPre + 2*nPadPre) * nfPre;
+      const int sf = p->sf;
+
+      pvdata_t * w = p->data;
+
+      c->preSynapticPatchHead(kxPost, kyPost, kfPost, &kxPre, &kyPre);
+
+      // convert to extended indices
+      //
+      kxPre += nPadPre;
+      kyPre += nPadPre;
+
+      int kPreHead = kIndex(kxPre, kyPre, kfPre, nxPre+2*nPadPre, nyPre+2*nPadPre, nfPre);
+
+      // FIND OUT WHY THIS DOESN'T WORK
+      //int kPreHead = kIndex(kxPre, kyPre, kfPre, nxPre, nyPre, nfPre);
+      //kPreHead = kIndexExtended(kPreHead, nxPre, nyPre, nfPre, nPadPre);
+
+      // loop over patch indices (writing out layer indices)
+      //
+      int kp = 0;
+      for (int f = 0; f < nfp; f++) {
+         for (int j = 0; j < nyp; j++) {
+            for (int i = 0; i < nxp; i++) {
+               int kPre = kPreHead + i*sx + j*sy + f*sf;
+
+               if (kPre != (int) w[kp++]) {
+                  status = -1;
+                  fprintf(stderr, "ERROR: check_weights: kPost==%d kPre==%d kp==%d != w==%d\n",
+                          kPost, kPre, kp, (int) w[kp-1]);
+                  return status;
+               }
+            }
+         }
+      }
+
+   } // end loop over weight patches
+
+   return status;
+}
+
+static int set_weights_to_source_index(HyPerConn * c)
+{
+   int status = 0;
+   int arbor = 0;
+   short wPacked[2];
+
+   assert(sizeof(short) == 2);
+
+   const PVLayer * lPre  = c->preSynapticLayer()->clayer;
+   const PVLayer * lPost = c->postSynapticLayer()->clayer;
+
+   const int nxPre = c->preSynapticLayer()->clayer->loc.nx;
+   const int nyPre = c->preSynapticLayer()->clayer->loc.ny;
+   const int nfPre = c->preSynapticLayer()->clayer->loc.nBands;
+
+   const int nPadPre = c->preSynapticLayer()->clayer->loc.nPad;
+
+   const int nxPost = c->postSynapticLayer()->clayer->loc.nx;
+   const int nyPost = c->postSynapticLayer()->clayer->loc.ny;
+   const int nfPost = c->postSynapticLayer()->clayer->loc.nBands;
+
+   int numPatches = c->numWeightPatches(arbor);
+
+   // assume (or at least use) only one arbor (set of weight patches)
+   // k index is in extended space
+   for (int kPre = 0; kPre < numPatches; kPre++) {
+      PVPatch * p = c->getWeights(kPre, arbor);
+
+      const int nxp = p->nx;
+      const int nyp = p->ny;
+      const int nfp = p->nf;
+
+      const int sxp = p->sx;
+      const int syp = p->sy;
+      const int sfp = p->sf;
+
+      pvdata_t * w = p->data;
+
+      int kxPre = kxPos(kPre, nxPre+2*nPadPre, nyPre+2*nPadPre, nfPre);
+      int kyPre = kxPos(kPre, nxPre+2*nPadPre, nyPre+2*nPadPre, nfPre);
+
+      int kxPatchHead = zPatchHead(kxPre-nPadPre, nxp, lPre->xScale, lPost->xScale);
+      int kyPatchHead = zPatchHead(kyPre-nPadPre, nyp, lPre->xScale, lPost->xScale);
+
+      for (int y = 0; y < nyp; y++) {
+         for (int x = 0; x < nxp; x++) {
+            for (int f = 0; f < nfp; f++) {
+               int kxPost = kxPatchHead + x;
+               int kyPost = kyPatchHead + y;
+               int kPost = kIndex(kxPost, kyPost, f, nxPost, nyPost, nfPost);
+
+               wPacked[0] = kPre;
+               wPacked[1] = kPost;
+               //w[x*sxp + y*syp + f*sfp] = * ((float *) wPacked);
+               w[x*sxp + y*syp + f*sfp] = kPre;
+            }
+         }
+      }
+
+   } // end loop over weight patches
+
+   return status;
+}
