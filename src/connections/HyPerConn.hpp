c35ab732f977362d7f60a3d4b38aa23c9227f607
rasmussn <rasmussn@users.sourceforge.net>
2010-02-10 16:28:36 +0000

    Gar's changes to add mirror boundary conditions.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2184 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/connections/HyPerConn.hpp
+++ src/connections/HyPerConn.hpp
@@ -179,9 +179,6 @@ protected:
    virtual int deleteWeights();
 
    virtual int createAxonalArbors();
-   virtual int adjustAxonalArborWeights();
-
-   int kIndexFromNeighbor(int k, int neighbor);
 
    // static member functions
 
