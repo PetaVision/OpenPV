c3bcc99c9cfc68980d7c0f23401c100887a620e5
rasmussn <rasmussn@users.sourceforge.net>
2010-03-16 16:26:07 +0000

    Fixed to build with latest PV library.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@2263 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 1293560f3a0face0fbae3a296277ee952da5a7ed

--- craig/heirarchy.cpp
+++ craig/heirarchy.cpp
@@ -20,55 +20,57 @@
 #include "BiConn.hpp"
 #include "LinearPostConnProbe.hpp"
 
+using namespace PV;
+
 int main(int argc, char* argv[])
 {
    // create the managing hypercolumn
-   PV::HyPerCol * hc = new PV::HyPerCol("column", argc, argv);
+   HyPerCol * hc = new HyPerCol("column", argc, argv);
 
    // create the layers
-   PV::HyPerLayer * retina, * l1s, * l2s, * l2c, * l3s, * l3c, * l4s, * l4c;
+   HyPerLayer * retina, * l1s, * l2s, * l2c, * l3s, * l3c, * l4s, * l4c;
 
-   retina = new PV::Retina1D("Retina", hc);
-   l1s = new PV::V1("L1 Simple",  hc);
-   l2s = new PV::V1("L2 Simple",  hc);
-   l2c = new PV::V1("L2 Complex", hc);
-   l3s = new PV::V1("L3 Simple", hc);
-   l3c = new PV::V1("L3 Complex", hc);
-   l4s = new PV::V1("L4 Simple", hc);
-   l4c = new PV::V1("L4 Complex", hc);     // readout layer
+   retina = new Retina1D("Retina", hc);
+   l1s = new V1("L1 Simple",  hc);
+   l2s = new V1("L2 Simple",  hc);
+   l2c = new V1("L2 Complex", hc);
+   l3s = new V1("L3 Simple", hc);
+   l3c = new V1("L3 Complex", hc);
+   l4s = new V1("L4 Simple", hc);
+   l4c = new V1("L4 Complex", hc);     // readout layer
 
    // inhibitory layers
-   PV::HyPerLayer * l1sInh;
+   HyPerLayer * l1sInh;
 
-   l1sInh = new PV::V1("L1 Simple Inh", hc);      // when l1s neuron fires, inhibit other features
-   //   l4Inh = new PV::V1("L4Inh", hc);
+   l1sInh = new V1("L1 Simple Inh", hc);      // when l1s neuron fires, inhibit other features
+   //   l4Inh = new V1("L4Inh", hc);
 
    // connect the layers
-   PV::HyPerConn * r_l1s, * l1s_l2s, * l2s_l2c, * l2s_l3s, * l2c_l3s, * l3s_l3c, * l3c_l4s;
-   PV::HyPerConn *l4s_l4c;
+   HyPerConn * r_l1s, * l1s_l2s, * l2s_l2c, * l2s_l3s, * l2c_l3s, * l3s_l3c, * l3c_l4s;
+   HyPerConn *l4s_l4c;
 
    // connections for STDP learning
-   r_l1s   = new PV::RandomConn("Retina to L1 Simple",     hc, retina, l1s, CHANNEL_EXC);
+   r_l1s   = new RandomConn("Retina to L1 Simple",     hc, retina, l1s, CHANNEL_EXC);
 
-// r_l1s   = new PV::BiConn("Retina to L1 Simple",     hc, retina, l1s, CHANNEL_EXC, 11);
-//   l1s_l2s = new PV::BiConn("L1 Simple to L2 Simple",  hc, l1s,    l2s, CHANNEL_EXC, 1121);
-//   l2s_l2c = new PV::BiConn("L2 Simple to L2 Complex", hc, l2s,    l2c, CHANNEL_EXC, 2122);
-//   l2c_l3s = new PV::BiConn("L2 Complex to L3 Simple", hc, l2c,    l3s, CHANNEL_EXC, 2231);
-//   l3s_l3c = new PV::BiConn("L3 Simple to L3 Complex", hc, l3s,    l3c, CHANNEL_EXC, 3132);
-//   l3c_l4s = new PV::BiConn("L3 Complex to L4 Simple", hc, l3c,    l4s, CHANNEL_EXC, 3241);
-//   l4s_l4c = new PV::BiConn("L4 Simple to L4 Complex", hc, l4s,    l4c, CHANNEL_EXC, 4142);
+// r_l1s   = new BiConn("Retina to L1 Simple",     hc, retina, l1s, CHANNEL_EXC, 11);
+//   l1s_l2s = new BiConn("L1 Simple to L2 Simple",  hc, l1s,    l2s, CHANNEL_EXC, 1121);
+//   l2s_l2c = new BiConn("L2 Simple to L2 Complex", hc, l2s,    l2c, CHANNEL_EXC, 2122);
+//   l2c_l3s = new BiConn("L2 Complex to L3 Simple", hc, l2c,    l3s, CHANNEL_EXC, 2231);
+//   l3s_l3c = new BiConn("L3 Simple to L3 Complex", hc, l3s,    l3c, CHANNEL_EXC, 3132);
+//   l3c_l4s = new BiConn("L3 Complex to L4 Simple", hc, l3c,    l4s, CHANNEL_EXC, 3241);
+//   l4s_l4c = new BiConn("L4 Simple to L4 Complex", hc, l4s,    l4c, CHANNEL_EXC, 4142);
 
    // inhibitory connections
-   PV::HyPerConn * l1s_l1sInh, * l1sInh_l1s;
+   HyPerConn * l1s_l1sInh, * l1sInh_l1s;
 
-   l1s_l1sInh = new PV::BiConn("L1 Simple to L1 Simple Inh", hc, l1s, l1sInh, CHANNEL_EXC, 11110);
-   l1sInh_l1s = new PV::BiConn("L1 Simple Inh to L1 Simple", hc, l1sInh, l1s, CHANNEL_INH, 11011);
+   l1s_l1sInh = new BiConn("L1 Simple to L1 Simple Inh", hc, l1s, l1sInh, CHANNEL_EXC, 11110);
+   l1sInh_l1s = new BiConn("L1 Simple Inh to L1 Simple", hc, l1sInh, l1s, CHANNEL_INH, 11011);
 
-//   l2_l3 = new PV::RandomConn("L2 to L3", hc, l2, l3, CHANNEL_EXC);
-//   l3_l4 = new PV::RandomConn("L3 to L4", hc, l3, l4, CHANNEL_EXC);
+//   l2_l3 = new RandomConn("L2 to L3", hc, l2, l3, CHANNEL_EXC);
+//   l3_l4 = new RandomConn("L3 to L4", hc, l3, l4, CHANNEL_EXC);
 
-//   l4_l4Inh = new PV::RandomConn("L4 to L4Inh", hc, l4, l4Inh, CHANNEL_EXC);
-//   l4Inh_l4 = new PV::RandomConn("L4Inh to L4", hc, l4Inh, l4, CHANNEL_INH);
+//   l4_l4Inh = new RandomConn("L4 to L4Inh", hc, l4, l4Inh, CHANNEL_EXC);
+//   l4Inh_l4 = new RandomConn("L4Inh to L4", hc, l4Inh, l4, CHANNEL_INH);
 
    int locX = 0;
    int locY = 0;  // image ON
@@ -77,32 +79,33 @@ int main(int argc, char* argv[])
    int locK = 0; // 7; // (7 for right cells)
 
    // add probes
-   PV::PVLayerProbe * probe0  = new PV::LinearActivityProbe(hc, PV::DimX, locY, locF);
-   PV::PVLayerProbe * probe1  = new PV::LinearActivityProbe(hc, PV::DimX, locY, locF+1);
-   PV::PVLayerProbe * probe2  = new PV::LinearActivityProbe(hc, PV::DimX, locY, locF+2);
-   PV::PVLayerProbe * probe3  = new PV::LinearActivityProbe(hc, PV::DimX, locY, locF+3);
-   PV::PVLayerProbe * probe4  = new PV::LinearActivityProbe(hc, PV::DimX, locY, locF+4);
-   PV::PVLayerProbe * probe5  = new PV::LinearActivityProbe(hc, PV::DimX, locY, locF+5);
-   PV::PVLayerProbe * probe6  = new PV::LinearActivityProbe(hc, PV::DimX, locY, locF+6);
+#ifdef PROBES
+   PVLayerProbe * probe0  = new LinearActivityProbe(hc, DimX, locY, locF);
+   PVLayerProbe * probe1  = new LinearActivityProbe(hc, DimX, locY, locF+1);
+   PVLayerProbe * probe2  = new LinearActivityProbe(hc, DimX, locY, locF+2);
+   PVLayerProbe * probe3  = new LinearActivityProbe(hc, DimX, locY, locF+3);
+   PVLayerProbe * probe4  = new LinearActivityProbe(hc, DimX, locY, locF+4);
+   PVLayerProbe * probe5  = new LinearActivityProbe(hc, DimX, locY, locF+5);
+   PVLayerProbe * probe6  = new LinearActivityProbe(hc, DimX, locY, locF+6);
 
    locK = 7;
 
-   PV::ConnectionProbe * cprobe0 = new PV::ConnectionProbe(locK);
-   PV::ConnectionProbe * cprobe1 = new PV::ConnectionProbe(locK+1);
-   PV::ConnectionProbe * cprobe2 = new PV::ConnectionProbe(locK+2);
-   PV::ConnectionProbe * cprobe3 = new PV::ConnectionProbe(locK+3);
-   PV::ConnectionProbe * cprobe4 = new PV::ConnectionProbe(locK+4);
-   PV::ConnectionProbe * cprobe5 = new PV::ConnectionProbe(locK+5);
+   ConnectionProbe * cprobe0 = new ConnectionProbe(locK);
+   ConnectionProbe * cprobe1 = new ConnectionProbe(locK+1);
+   ConnectionProbe * cprobe2 = new ConnectionProbe(locK+2);
+   ConnectionProbe * cprobe3 = new ConnectionProbe(locK+3);
+   ConnectionProbe * cprobe4 = new ConnectionProbe(locK+4);
+   ConnectionProbe * cprobe5 = new ConnectionProbe(locK+5);
 
-   PV::LinearPostConnProbe * lpcprobe0 = new PV::LinearPostConnProbe(PV::DimX, locY, 0);
-   PV::LinearPostConnProbe * lpcprobe1 = new PV::LinearPostConnProbe(PV::DimX, locY, 1);
-   PV::LinearPostConnProbe * lpcprobe2 = new PV::LinearPostConnProbe(PV::DimX, locY, 2);
-   PV::LinearPostConnProbe * lpcprobe3 = new PV::LinearPostConnProbe(PV::DimX, locY, 3);
+   LinearPostConnProbe * lpcprobe0 = new LinearPostConnProbe(DimX, locY, 0);
+   LinearPostConnProbe * lpcprobe1 = new LinearPostConnProbe(DimX, locY, 1);
+   LinearPostConnProbe * lpcprobe2 = new LinearPostConnProbe(DimX, locY, 2);
+   LinearPostConnProbe * lpcprobe3 = new LinearPostConnProbe(DimX, locY, 3);
 
-   PV::PostConnProbe * pcprobe0 = new PV::PostConnProbe(locK);
+   PostConnProbe * pcprobe0 = new PostConnProbe(locK);
 
    locF = 7;
-   PV::PVLayerProbe * ptprobe = new PV::PointProbe(locX, locY, locF, "l1i:");
+   PVLayerProbe * ptprobe = new PointProbe(locX, locY, locF, "l1i:");
 
    retina->insertProbe(probe0);
    retina->insertProbe(probe1);
@@ -161,6 +164,7 @@ int main(int argc, char* argv[])
 //   l2c_l3s->insertProbe(cprobe1);
 //   l2c_l3s->insertProbe(cprobe2);
 //   l2c_l3s->insertProbe(cprobe3);
+#endif
 
    // run the simulation
    hc->initFinish();
