6cea859028dad9bf50e9d21498ec3993155858a2
marian_anghel <marian_anghel@users.sourceforge.net>
2010-03-17 17:15:12 +0000

    A synthetic world of vertical and horizontal bars. They switch orientation and move randomly.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2286 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/layers/Bars.cpp
@@ -0,0 +1,184 @@
+/*
+ * Gratings.cpp
+ *
+ *  Created on: Oct 23, 2009
+ *      Author: Marian Anghel
+ */
+
+#include "Bars.hpp"
+#include "../include/pv_common.h"  // for PI
+namespace PV {
+
+Bars::Bars(const char * name, HyPerCol * hc) :
+   Image(name, hc)
+{
+   initialize_data(&loc);
+
+   // set default params
+   // set reference position of bars
+   this->position = 0;
+   this->lastPosition = position;
+
+   // set bars orientation to default values
+   this->orientation = vertical;
+   this->lastOrientation = orientation;
+
+   // set switching and moving probabilities
+   pSwitch = 0.0;
+   pMove = 0.0;
+
+   // check for explicit parameters in params.stdp
+   PVParams * params = hc->parameters();
+   if (params->present(name, "pMove")) {
+      pMove = params->value(name, "pMove");
+      //printf("pMove = %f\n", pMove);
+   }
+
+   if (params->present(name, "pSwitch")) {
+      pSwitch = params->value(name, "pSwitch");
+      //printf("pSwitch = %f\n",pSwitch);
+   }
+
+   updateImage(0.0, 0.0);
+
+}
+
+Bars::~Bars()
+{
+}
+
+/**
+ * NOTES:
+ *    - Retina calls updateImage(float time, float dt) and expects a bool variable
+ *    in return.
+ *    - If true, the image has been changed; if false the image has not been
+ * changed.
+ *    - If true, the retina also calls copyFromImageBuffer() to copy the Image
+ *    data buffer into the V buffer (it also normalizes the V buffer so that V <= 1).
+ *
+ */
+bool Bars::updateImage(float time, float dt)
+{
+   // extended frame
+   const int nx = loc.nx + 2 * loc.nPad;
+   const int ny = loc.ny + 2 * loc.nPad;
+   const int sx = 1;
+   const int sy = sx * nx;
+   char basicfilename[128] = { 0 };
+
+   const int width = 1;
+   const int step = 6;
+   int x, y;
+
+   // alternate between vertical and horizontal bars
+   double p = 1.0 * rand() / RAND_MAX;
+
+   if (orientation == vertical) { // current vertical gratings
+      if (p < pSwitch) { // switch with probability pSwitch
+         orientation = horizontal;
+      }
+   }
+   else {
+      if (p < pSwitch) { // current horizontal gratings
+         orientation = vertical;
+      }
+   }
+
+   if (orientation == vertical) { // vertical bars
+
+      // moving probability
+      double p_move = 1.0 * rand() / RAND_MAX;
+      if (p_move < pMove) {
+         calcPosition(1.0 * nx); // or step
+      }
+      else {
+         position = lastPosition;
+      }
+
+      for (int iy = 0; iy < ny; iy++) {
+         for (int ix = position; ix < nx + position; ix += width + step) {
+            x = (int) fmod(ix, nx);
+            for (int m = 0; m < width; m++) {
+               data[(x + m) * sx + iy * sy] = 1.0;
+            }
+            for (int m = 0; m < step; m++) {
+               data[(x + width + m) * sx + iy * sy] = -1.0;
+            }
+         }
+      }
+   }
+   else { // horizontal bars
+
+      // moving probability
+      double p_move = 1.0 * rand() / RAND_MAX;
+      if (p_move < pMove) {
+         calcPosition(1.0 * ny);
+      }
+      else {
+         position = lastPosition;
+      }
+
+      for (int ix = 0; ix < nx; ix++) {
+         for (int iy = position; iy < ny + position; iy += width + step) {
+            y = (int) fmod(iy, ny);
+            for (int m = 0; m < width; m++) {
+               data[ix * sx + (y + m) * sy] = 1.0;
+            }
+            for (int m = 0; m < step; m++) {
+               data[ix * sx + (y + width + m) * sy] = -1.0;
+            }
+         }
+      }
+   }
+
+   if (lastPosition != position || lastOrientation != orientation) {
+      lastPosition = position;
+      lastOrientation = orientation;
+      lastUpdateTime = time;
+      //snprintf(basicfilename, 127, "Bars_%.2f", time);
+      //write(basicfilename);
+      return true;
+   }
+   else {
+      return false;
+   }
+}
+
+/**
+ *
+ *  Return an integer between 0 ans (step-1)
+ */
+void Bars::calcPosition(float step)
+{
+   float dp = 1.0 / step;
+   double p = ((double) rand()) / (double) RAND_MAX;
+   int random_walk = 1;
+   int move_forward = 0;
+   int move_backward = 0;
+   int random_jump = 0;
+
+   if (random_walk) {
+      if(p < 0.5){
+         position = (int) fmod(position+1, step);
+      }else{
+         position = (int) fmod(position-1, step);
+      }
+      //printf("pos = %f\n",position);
+   } else if (move_forward){
+      position = (int) fmod(position+1, step);
+   } else if (move_backward){
+      position = (int) fmod(position-1, step);
+   }
+   else if (random_jump){
+      for (int i = 0; i < step; i++) {
+         if ((i * dp < p) && (p < (i + 1) * dp)) {
+            position = i;
+            return;
+         }
+      }
+   }
+   return;
+
+}
+
+} // namespace PV
