af6037560a9d783df79e04cbfa3a581e278ce970
marian_anghel <marian_anghel@users.sourceforge.net>
2010-03-17 17:17:49 +0000

    Simple random number generator.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2289 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/utils/rng.h
@@ -0,0 +1,32 @@
+/*
+ * rng.h
+ *
+ *  Created on: Feb 12, 2010
+ *      Author: manghel
+ */
+
+#ifndef RNG_H_
+#define RNG_H_
+
+/* define this variable if code is to be run (or transformed to be run) on a Cell processor */
+//#define CELL_BE
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+   float ranf();
+   float box_muller(float,float);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
+
+#endif /* RNG_H_ */
