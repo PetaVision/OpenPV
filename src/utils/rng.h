c6d99ebe253aa6fb6fea9659117c7a51fb9ca89a
rasmussn <rasmussn@users.sourceforge.net>
2010-04-03 21:24:23 +0000

    Fixed syntax error.  Not sure how this one slipped by earlier build.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2401 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/utils/rng.h
+++ src/utils/rng.h
@@ -8,24 +8,15 @@
 #ifndef RNG_H_
 #define RNG_H_
 
-/* define this variable if code is to be run (or transformed to be run) on a Cell processor */
-//#define CELL_BE
-
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #ifdef __cplusplus
 extern "C"
 {
-   float box_muller(float,float);
+#endif
+
+float box_muller(float,float);
 
 #ifdef __cplusplus
 }
 #endif
-
-#endif
-
-
 
 #endif /* RNG_H_ */
