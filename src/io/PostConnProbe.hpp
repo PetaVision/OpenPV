f40b612e9b2118cb9ff6916c8a19e553ed2ebb00
rasmussn <rasmussn@users.sourceforge.net>
2010-04-19 15:53:17 +0000

    Added text_write_patch_extra() to output more information about post-synaptic weights and what influences theirs changes.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2466 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/io/PostConnProbe.hpp
+++ src/io/PostConnProbe.hpp
@@ -19,14 +19,20 @@ public:
    PostConnProbe(int kxPost, int kyPost, int kfPost);
    PostConnProbe(const char * filename, int kPost);
    PostConnProbe(const char * filename, int kxPost, int kyPost, int kfPost);
+   virtual ~PostConnProbe();
 
    virtual int outputState(float time, HyPerConn * c);
    void setImage(Image * image)   {this->image = image;}
+
+   int text_write_patch_extra(FILE * fp, PVPatch * patch,
+                              pvdata_t * data, pvdata_t * prev, pvdata_t * activ);
 
 protected:
    int kPost;   // index of post-synaptic neuron
    int kxPost, kyPost, kfPost;
    Image * image;
+   pvdata_t * wPrev;
+   pvdata_t * wActiv;
 };
 
 }
