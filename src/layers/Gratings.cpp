037abdaf494b18b2ac29e30b6a7892ba0ad1041d
marian_anghel <marian_anghel@users.sourceforge.net>
2010-04-22 15:18:15 +0000

    A moving probability was introduced.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2498 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/Gratings.cpp
+++ src/layers/Gratings.cpp
@@ -17,10 +17,23 @@ Gratings::Gratings(const char * name, HyPerCol * hc) : Image(name, hc)
 
    // initialize to unused phase to trigger update
    //
-   this->lastPhase = -PI;
+   this->phase = 0.5 * PI;
+   this->lastPhase = phase;
+
+   // set moving (random phase) probability
+   pMove = 0;
 
    PVParams * params = hc->parameters();
    float freq = params->value(name, "burstFreq", 40.0);
+
+   // check for explicit parameters in params.stdp
+   if (params->present(name, "pMove")) {
+      pMove = params->value(name, "pMove");
+      //printf("pMove = %f\n", pMove);
+   }
+
+   // set parameters that controls writing of new images
+    writeImages = params->value(name, "writeImages",0);
 
    period = 25;
    if (freq > 0.0) {
@@ -35,30 +48,59 @@ Gratings::Gratings(const char * name, HyPerCol * hc) : Image(name, hc)
    updateImage(0.0, 0.0);
 }
 
-Gratings::~Gratings() {
+Gratings::~Gratings()
+{
 }
 
+/**
+ * NOTES:
+ *    - Retina calls updateImage(float time, float dt) and expects a bool variable
+ *    in return.
+ *    - If true, the image has been changed; if false the image has not been
+ * changed.
+ *    - If true, the retina also calls copyFromImageBuffer() to copy the Image
+ *    data buffer into the V buffer (it also normalizes the V buffer so that V <= 1).
+ *
+ */
 bool Gratings::updateImage(float time, float dt)
 {
    // extended frame
-   const int nx = loc.nx + 2*loc.nPad;
-   const int ny = loc.ny + 2*loc.nPad;
+   const int nx = loc.nx + 2 * loc.nPad;
+   const int ny = loc.ny + 2 * loc.nPad;
    const int sx = 1;
    const int sy = sx * nx;
+   float phi;
+   char basicfilename[128] = { 0 };
 
-   const float kx  = 1.0*PI/4.0;   // wavenumber
-   const float phi = calcPhase(time, dt);
+   const float kx = 2.0 * PI / 8.0; // wavenumber
+
+   double p_move = 1.0 * rand() / RAND_MAX;
+
+   if (p_move < pMove) {
+      calcPhase(time, dt);
+   }
+   else {
+      phase = lastPhase;
+   }
 
    for (int iy = 0; iy < ny; iy++) {
       for (int ix = 0; ix < nx; ix++) {
          float x = (float) ix;
-         data[ix*sx + iy*sy] = sinf(kx * x + phi);
+         data[ix * sx + iy * sy] = 1.0 + sinf(kx * x + phase);
       }
    }
 
-   if (lastPhase != phi) {
-      lastPhase = phi;
+   if (lastPhase != phase) {
+      lastPhase = phase;
       lastUpdateTime = time;
+      if (writeImages) {
+         snprintf(basicfilename, 127, "Gratings_%.2f", time);
+         write(basicfilename);
+      }
+      return true;
+   }
+   else {
+      return false;
    }
 
    return true;
@@ -79,6 +121,5 @@ float Gratings::calcPhase(float time, float dt)
 
 	return phase;
 }
-
 
 } // namespace PV
