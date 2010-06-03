b05b0e08e495ca024aa4673fcc8d40aef16159fb
garkenyon <garkenyon@users.sourceforge.net>
2010-06-03 04:07:44 +0000

    replaced
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2597 7c0444ca-5550-0410-9008-faecdbe78eb5

--- mlab/amoebaGen/plotAmoeba2D.m
+++
@@ -1,8 +0,0 @@
-function plotAmoeba2D(amoeba_image, t, nfour)
-
-plotAmoebaPTB(amoeba_image,t,true,nfour);
-plotAmoebaPTB(amoeba_image,t,false,nfour);
-plotAmoebaR(amoeba_image,t,nfour);
-plotAmoebaPTB(amoeba_image,t,true,nfour, true);
-
-
