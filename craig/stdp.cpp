8a4ce4dd0e4b7aef1bbd8d64e1e326d6c8936521
rasmussn <rasmussn@users.sourceforge.net>
2010-06-14 15:38:56 +0000

    Added Rectangle capabilities from soren.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@2647 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 8f88e1a34ce5798232b6f9746c62c749eb901bf9

--- craig/stdp.cpp
+++ craig/stdp.cpp
@@ -21,6 +21,7 @@
 #include <src/io/PointProbe.hpp>
 #include <src/io/StatsProbe.hpp>
 #include <src/layers/Gratings.hpp>
+#include <src/layers/Movie.hpp>
 #include <src/layers/Retina.hpp>
 #include <src/layers/V1.hpp>
 #include <src/connections/HyPerConn.hpp>
@@ -32,47 +33,55 @@ using namespace PV;
 
 void dump_weights(PVPatch ** patches, int numPatches);
 
+#undef INHIB
+
 int main(int argc, char* argv[])
 {
    // create the managing hypercolumn
    //
    HyPerCol * hc = new HyPerCol("column", argc, argv);
 
-   // create the image
-   //
-   //Image * image = new Gratings("Image", hc);
-   Image * image = new Patterns("Bars", hc);
-
    // create the layers
    //
-   HyPerLayer * retina = new Retina("Retina", hc, image);
-   HyPerLayer * l1     = new V1("L1", hc);
-//   HyPerLayer * l1Inh  = new V1("L1Inh", hc);
+
+   //Image * image = new Movie("Image", hc, "input/earth-files.txt", 1.0);
+   //Image * image = new Patterns("Image", hc, RECTANGLES);
+   Image * image = new Patterns("Image", hc, BARS);
+
+   HyPerLayer * retinaOn  = new Retina("RetinaOn", hc);
+   HyPerLayer * retinaOff = new Retina("RetinaOff", hc);
+   HyPerLayer * l1        = new V1("L1", hc);
+
+#ifdef INHIB
+   HyPerLayer * l1Inh  = new V1("L1Inh", hc);
+#endif
 
    // connect the layers
    //
-   HyPerConn * r_l1, * l1_l1Inh, * l1Inh_l1;
-//   r_l1     = new BiConn("Retina to L1", hc, retina, l1, CHANNEL_EXC, 0);
-   r_l1     = new HyPerConn("Retina to L1", hc, retina, l1, CHANNEL_EXC);
-//   r_l1     = new RandomConn("Retina to L1", hc, retina, l1, CHANNEL_EXC);
-//   l1_l1Inh = new HyPerConn( "L1 to L1Inh",  hc, l1,  l1Inh, CHANNEL_EXC);
-//   l1Inh_l1 = new HyPerConn("L1Inh to L1",  hc, l1Inh,  l1, CHANNEL_INH);
 
-//   dump_weights(r_l1->weights(0), r_l1->numWeightPatches(0));
+   HyPerConn * i_r1_c  = new HyPerConn("Image to RetinaOn Center",   hc, image, retinaOn, CHANNEL_EXC);
+   HyPerConn * i_r1_s  = new HyPerConn("Image to RetinaOn Surround", hc, image, retinaOn, CHANNEL_INH);
+   HyPerConn * i_r0_c  = new HyPerConn("Image to RetinaOff Center", hc, image, retinaOff, CHANNEL_INH);
+   HyPerConn * i_r0_s  = new HyPerConn("Image to RetinaOff Surround", hc, image, retinaOff, CHANNEL_EXC);
+   HyPerConn * r1_l1    = new HyPerConn("RetinaOn to L1", hc, retinaOn, l1, CHANNEL_EXC);
+   HyPerConn * r0_l1    = new HyPerConn("RetinaOff to L1", hc, retinaOff, l1, CHANNEL_EXC);
+
+#ifdef INHIB
+   HyPerConn * l1_l1Inh = new HyPerConn("L1 to L1Inh",  hc, l1,  l1Inh, CHANNEL_EXC);
+   HyPerConn * l1Inh_l1 = new HyPerConn("L1Inh to L1",  hc, l1Inh,  l1, CHANNEL_INH);
+#endif
 
 #ifdef DISPLAY
    GLDisplay * display = new GLDisplay(&argc, argv, hc, 2, 2);
-   display->setDelay(100);
+   display->setDelay(800);
    display->setImage(image);
-   display->addLayer(retina);
+   display->addLayer(retinaOn);
+   display->addLayer(retinaOff);
    display->addLayer(l1);
 #endif
 
    // add probes
    //
-
-//   ConnectionProbe * cProbe0 = new ConnectionProbe(2*5 + 0);
-//   PostConnProbe * pcProbe0 = new LinearPostConnProbe(PV::DimX, locY, 0);
 
 #undef LINEAR_PROBES
 #ifdef LINEAR_PROBES
@@ -82,12 +91,6 @@ int main(int argc, char* argv[])
    LayerProbe * rProbe3  = new LinearActivityProbe(hc, PV::DimX, 3, 0);
    LayerProbe * rProbe4  = new LinearActivityProbe(hc, PV::DimX, 4, 0);
    LayerProbe * rProbe5  = new LinearActivityProbe(hc, PV::DimX, 5, 0);
-   LayerProbe * rProbe6  = new LinearActivityProbe(hc, PV::DimX, 6, 0);
-   LayerProbe * rProbe7  = new LinearActivityProbe(hc, PV::DimX, 7, 0);
-   LayerProbe * rProbe8  = new LinearActivityProbe(hc, PV::DimX, 8, 0);
-   LayerProbe * rProbe9  = new LinearActivityProbe(hc, PV::DimX, 9, 0);
-   LayerProbe * rProbe10 = new LinearActivityProbe(hc, PV::DimX, 10, 0);
-   LayerProbe * rProbe11 = new LinearActivityProbe(hc, PV::DimX, 11, 0);
 
    retina->insertProbe(rProbe0);
    retina->insertProbe(rProbe1);
@@ -95,24 +98,25 @@ int main(int argc, char* argv[])
    retina->insertProbe(rProbe3);
    retina->insertProbe(rProbe4);
    retina->insertProbe(rProbe5);
-   retina->insertProbe(rProbe6);
-   retina->insertProbe(rProbe7);
-   retina->insertProbe(rProbe8);
-   retina->insertProbe(rProbe9);
-   retina->insertProbe(rProbe10);
-   retina->insertProbe(rProbe11);
 #endif
+
+   //ConnectionProbe * cProbe = new ConnectionProbe(277);
+   //i_r1_s->insertProbe(cProbe);
+
+//   PostConnProbe * pcProbe0 = new LinearPostConnProbe(PV::DimX, locY, 0);
 
 //   LayerProbe * rptprobe = new PointProbe(25, 0, 0, "R :");
 //   retina->insertProbe(rptprobe);
 
-//   LayerProbe * ptprobe1 = new PointProbe(53, 3, 0, "L1:");
-//   l1->insertProbe(ptprobe1);
+   LayerProbe * ptprobe1 = new PointProbe(12, 15, 0, "L1:");
+   l1->insertProbe(ptprobe1);
 
-   PostConnProbe * pcProbe = new PostConnProbe(778); //(245); // 8575=>127,66
-   pcProbe->setImage(image);
-   //pcProbe->setOutputIndices(true);
-   r_l1->insertProbe(pcProbe);
+   PostConnProbe * pcOnProbe  = new PostConnProbe(492); //(245); // 8575=>127,66
+   PostConnProbe * pcOffProbe = new PostConnProbe(492); //(245); // 8575=>127,66
+//   pcProbe->setImage(image);
+//   pcOnProbe->setOutputIndices(true);
+   //r0_l1->insertProbe(pcOffProbe);
+   //r1_l1->insertProbe(pcOnProbe);
 
 //   StatsProbe * sProbe = new StatsProbe(PV::BufActivity, "l1");
 //   l1->insertProbe(sProbe);
