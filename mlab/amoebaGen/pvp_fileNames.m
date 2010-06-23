6b0d1d4d801edd232c99d615c7ec32f377defb44
garkenyon <garkenyon@users.sourceforge.net>
2010-06-23 18:25:21 +0000

    code for generating list of amoeba images with random numbers of fourier components
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2657 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ mlab/amoebaGen/pvp_fileNames.m
@@ -0,0 +1,42 @@
+function filenames_cell = pvp_fileNames(N_ofeach, FC_list)
+
+  global NUM2STR_FORMAT
+  NUM2STR_FORMAT = '%03.3i';
+  file_path = ...
+      '../PetaVision/mlab/amoebaGen/256/';
+  file_root = 'tar_'; % 8969_a.png
+  target_flag = '_a'; % '_n'; %
+  file_dir = 'a'; % 't'; %'d'; % 
+  file_type = '.png';
+  if nargin < 2 || isempty(FC_list)
+    FC_list = [ 2 4 6 8 ];
+  end%%if
+  num_FCs = length(FC_list);
+  if nargin < 1 || isempty(N_ofeach)
+    N_ofeach = 2500;
+  end%%if
+  N = num_FCs * N_ofeach;
+  image_id = [];
+  for i_FC = 1 num_FCs
+    image_id = [ image_id; repmat(FC_list(i_FC), N_ofeach, 1) ];
+  end%%for
+  image_id = randperm(N) - 1;
+  filenames_cell = cell(N,1);
+  fid = fopen('fileNames.txt', 'w', 'native');
+  for i = 1 : N
+    FC_str = ...
+	num2str( FC_list( ceil( (1+image_id(i)) / N_ofeach ) ) );
+    id_str = ...
+	num2str( 1 + mod( image_id(i), N_ofeach ), NUM2STR_FORMAT );
+    filename_str = ...
+	[ file_path, ...
+	 FC_str, ...
+	 '/',  file_dir, '/', ...
+	 file_root, ...
+	 id_str, ...
+	 target_flag, ...
+	 file_type ];
+    filenames_cell{i} = filename_str;
+    fprintf(fid, '%s\n', filename_str);
+  end%%for
+  fclose(fid);
