5db9b3bd695ed3b4868a4b687603996cbbb242dd
rasmussn <rasmussn@users.sourceforge.net>
2010-07-31 00:39:21 +0000

    Initial version.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2716 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/arch/opencl/pv_opencl.h
@@ -0,0 +1,42 @@
+/*
+ * pv_opencl.h
+ *
+ *  Created on: Jul 30, 2010
+ *      Author: Craig Rasmussen
+ */
+
+#ifndef PV_OPENCL_H_
+#define PV_OPENCL_H_
+
+#include "../../include/pv_arch.h"
+
+#ifdef PV_USE_OPENCL
+#  include <OpenCL/opencl.h>
+#else
+
+#  define cl_mem_flags int
+
+#  define CL_MEM_READ_ONLY     0
+#  define CL_MEM_WRITE_ONLY    0
+#  define CL_MEM_USE_HOST_PTR  0
+
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+
+// guess at maxinum number of devices (CPU + GPU)
+//
+#define MAX_DEVICES (2)
+
+// guess at maximum work item dimensions
+//
+#define MAX_WORK_ITEM_DIMENSIONS (3)
+
+#define PVCL_GET_DEVICE_ID_FAILURE    1
+#define PVCL_CREATE_CONTEXT_FAILURE   2
+#define PVCL_CREATE_CMD_QUEUE_FAILURE 3
+#define PVCL_CREATE_PROGRAM_FAILURE   4
+#define PVCL_BUILD_PROGRAM_FAILURE    5
+#define PVCL_CREATE_KERNEL_FAILURE    6
+
+#endif /* PV_OPENCL_H_ */
