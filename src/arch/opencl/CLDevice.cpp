93d99a66a440dc0b1866961fb1ce36ce74c0217b
rasmussn <rasmussn@users.sourceforge.net>
2010-08-02 15:24:38 +0000

    Refactored to use new class CLKernel.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2731 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/arch/opencl/CLDevice.cpp
+++ src/arch/opencl/CLDevice.cpp
@@ -6,11 +6,12 @@
  */
 
 #include "CLDevice.hpp"
+#include "CLBuffer.hpp"
+#include "CLKernel.hpp"
 
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 
 namespace PV {
 
@@ -52,10 +53,8 @@ int CLDevice::initialize(int device)
        return PVCL_CREATE_CMD_QUEUE_FAILURE;
    }
 
-   // turn on profiling
+   // turn on profiling for this command queue
    //
-   elapsed = 0;
-   profiling = true;
    status = clSetCommandQueueProperty(commands, CL_QUEUE_PROFILING_ENABLE, CL_TRUE, NULL);
    if (status != CL_SUCCESS) {
       print_error_code(status);
@@ -66,152 +65,10 @@ int CLDevice::initialize(int device)
    return status;
 }
 
-#ifdef PV_USE_OPENCL
-
-static char * load_program_source(const char *filename);
-
-int CLDevice::run(size_t global_work_size)
+CLKernel * CLDevice::createKernel(const char * filename, const char * name)
 {
-   size_t local_work_size;
-   int status = 0;
-		
-   // get the maximum work group size for executing the kernel on the device
-   //
-   status = clGetKernelWorkGroupInfo(kernel, device_ids[device], CL_KERNEL_WORK_GROUP_SIZE,
-						             sizeof(size_t), &local_work_size, NULL);
-   if (status != CL_SUCCESS) {
-      fprintf(stderr, "Error: Failed to retrieve kernel work group info! %d\n", status);
-      print_error_code(status);
-      exit(status);
-   } else {
-      printf("run: local_work_size==%ld global_work_size==%ld\n", local_work_size, global_work_size);
-   }
-   // execute the kernel over the entire range of our 1d input data set
-   // using the maximum number of work group items for this device
-   //
-   status = clEnqueueNDRangeKernel(commands, kernel, 1, NULL,
-								   &global_work_size, &local_work_size, 0, NULL, NULL);
-   if (status != CL_SUCCESS) {
-      fprintf(stderr, "CLDevice::run(): Failed to execute kernel!\n");
-      print_error_code(status);
-      exit(status);
-   }
-		
-   // wait for the command commands to get serviced before reading back results
-   //
-   clFinish(commands);
-		
-   return status;
+   return new CLKernel(context, commands, device_ids[device], filename, name);
 }
-	
-int CLDevice::run(size_t gWorkSizeX, size_t gWorkSizeY, size_t lWorkSizeX, size_t lWorkSizeY)
-{
-   size_t local_work_size[2];
-   size_t global_work_size[2];
-   size_t max_local_size;
-   int status = 0;
-		
-   global_work_size[0] = gWorkSizeX;
-   global_work_size[1] = gWorkSizeY;
-
-   local_work_size[0] = lWorkSizeX;
-   local_work_size[1] = lWorkSizeY;
-	
-   // get the maximum work group size for executing the kernel on the device
-   //
-   status = clGetKernelWorkGroupInfo(kernel, device_ids[device], CL_KERNEL_WORK_GROUP_SIZE,
-								     sizeof(size_t), &max_local_size, NULL);
-   if (status != CL_SUCCESS) {
-      fprintf(stderr, "Error: Failed to retrieve kernel work group info! (status==%d)\n", status);
-      print_error_code(status);
-      exit(status);
-   } else {
-      printf("run: local_work_size==(%ld,%ld) global_work_size==(%ld,%ld)\n",
-             local_work_size[0], local_work_size[1], global_work_size[0], global_work_size[1]);
-   }
-
-   if (device == 1) {
-      // Apple's CPU device has only one thread per group
-	  local_work_size[0] = 1;
-      local_work_size[1] = 1;
-   }
-	
-   // execute the kernel over the entire range of our 1d input data set
-   // using the maximum number of work group items for this device
-   //
-   status = clEnqueueNDRangeKernel(commands, kernel, 2, NULL,
-                                   global_work_size, local_work_size, 0, NULL, &event);
-   if (status) {
-      fprintf(stderr, "CLDevice::run(): Failed to execute kernel! (status==%d)\n", status);
-      fprintf(stderr, "CLDevice::run(): max_local_work_size==%ld\n", max_local_size);
-      print_error_code(status);
-      exit(status);
-   }
-		
-   // wait for the command commands to get serviced before reading back results
-   //
-   clFinish(commands);
-		
-   // get profiling information
-   //
-   if (profiling) {
-      size_t param_size;
-      cl_ulong start, end;
-      status = clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START,
-                                       sizeof(start), &start, &param_size);
-      status = clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END,
-                                       sizeof(end), &end, &param_size);
-      if (status == 0) {
-         elapsed = (end - start) / 1000;  // microseconds
-      }
-   }
-   
-   return status;
-}
-	
-int CLDevice::createKernel(const char * filename, const char * name)
-{
-   int status = 0;
-
-   // Create the compute program from the source buffer
-   //
-   char * source = load_program_source(filename);
-   program = clCreateProgramWithSource(context, 1, (const char **) &source, NULL, &status);
-   if (!program || status != CL_SUCCESS)
-   {
-       printf("Error: Failed to create compute program!\n");
-       print_error_code(status);
-       exit(status);
-   }
-
-   // Build the program executable
-   //
-   status = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-   if (status != CL_SUCCESS)
-   {
-       size_t len;
-       char buffer[2048];
-
-       printf("Error: Failed to build program executable!\n");
-       clGetProgramBuildInfo(program, device_ids[device], CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
-       printf("%s\n", buffer);
-       print_error_code(status);
-       exit(status);
-   }
-
-   // Create the compute kernel in the program we wish to run
-   //
-   kernel = clCreateKernel(program, name, &status);
-   if (!kernel || status != CL_SUCCESS)
-   {
-       fprintf(stderr, "Error: Failed to create compute kernel!\n");
-       print_error_code(status);
-       exit(status);
-   }
-
-   return status;
-}
-#endif // PV_USE_OPENCL
 
 CLBuffer * CLDevice::createBuffer(cl_mem_flags flags, size_t size, void * host_ptr)
 {
@@ -223,50 +80,6 @@ CLBuffer * CLDevice::createBuffer(cl_mem_flags flags, size_t size, void * host_p
 }
 
 #ifdef PV_USE_OPENCL
-
-int CLDevice::addKernelArg(int argid, int arg)
-{
-   int status = 0;
-      
-   status = clSetKernelArg(kernel, argid, sizeof(int), &arg);
-   if (status != CL_SUCCESS) {
-      fprintf(stderr, "CLDevice::addKernelArg: Failed to set kernel argument! %d\n", status);
-      print_error_code(status);
-      exit(status);
-   }
-   
-   return status;
-}
-   
-int CLDevice::addKernelArg(int argid, CLBuffer * buf)
-{
-   int status = 0;
-
-   cl_mem mobj = buf->clMemObject();
-      
-   status = clSetKernelArg(kernel, argid, sizeof(cl_mem), &mobj);
-   if (status != CL_SUCCESS) {
-      fprintf(stderr, "CLDevice::addKernelArg: Failed to set kernel argument! %d\n", status);
-      print_error_code(status);
-      exit(status);
-   }
-   
-   return status;
-}
-   
-int CLDevice::addLocalArg(int argid, size_t size)
-{
-   int status = 0;
-
-   status = clSetKernelArg(kernel, argid, size, 0);
-   if (status != CL_SUCCESS) {
-      fprintf(stderr, "CLDevice::addLocalArg: Failed to set kernel argument! %d\n", status);
-      print_error_code(status);
-      exit(status);
-   }
-
-   return status;
-}
 
 int CLDevice::query_device_info()
 {
@@ -375,7 +188,7 @@ int CLDevice::query_device_info(int id, cl_device_id device)
 /////////////////////////////////////////////////////////////////////////////
 	
 void
-print_error_code(int code)
+CLDevice::print_error_code(int code)
 {
    char msg[256];
 
@@ -386,34 +199,20 @@ print_error_code(int code)
       case CL_INVALID_COMMAND_QUEUE:
          sprintf(msg, "%s (%d)", "CL_INVALID_COMMAND_QUEUE", code);
          break;
+      case CL_BUILD_PROGRAM_FAILURE:
+         sprintf(msg, "%s (%d)", "CL_BUILD_PROGRAM_FAILURE", code);
+         break;
       case CL_INVALID_KERNEL_ARGS:
          sprintf(msg, "%s (%d)", "CL_INVALID_KERNEL_ARGS", code);
          break;
-
+      case CL_INVALID_VALUE:
+         sprintf(msg, "%s (%d)", "CL_INVALID_VALUE", code);
+         break;
       default:
          sprintf(msg, "%s (%d)\n", "UNKNOWN_CODE", code);
          break;
    }
    printf("ERROR_CODE==%s\n", msg);
-}
-
-static char *
-load_program_source(const char *filename)
-{
-    struct stat statbuf;
-    FILE        *fh;
-    char        *source;
-
-    fh = fopen(filename, "r");
-    if (fh == 0)
-        return 0;
-
-    stat(filename, &statbuf);
-    source = (char *) malloc(statbuf.st_size + 1);
-    fread(source, statbuf.st_size, 1, fh);
-    source[statbuf.st_size] = '\0';
-
-    return source;
 }
 
 #endif // PV_USE_OPENCL
