b4d304a680a9f71db1ba15406f08749c73c966f9
rasmussn <rasmussn@users.sourceforge.net>
2010-08-02 15:23:16 +0000

    Refactored to use new class CLKernel.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2730 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/arch/opencl/CLDevice.hpp
+++ src/arch/opencl/CLDevice.hpp
@@ -9,15 +9,14 @@
 #define CLDEVICE_HPP_
 
 #include "pv_opencl.h"
-#include "CLBuffer.hpp"
-
 #include <stdlib.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 
 namespace PV {
    
-void print_error_code(int code);
+class CLBuffer;
+class CLKernel;
 
 class CLDevice {
 
@@ -29,49 +28,34 @@ public:
 
    int initialize(int device);
 
+   static void print_error_code(int code);
+
    CLBuffer * createBuffer(cl_mem_flags flags, size_t size, void * host_ptr);
 
    CLBuffer * createReadBuffer(size_t size)   { return createBuffer(CL_MEM_READ_ONLY, size, NULL); }
    CLBuffer * createWriteBuffer(size_t size)  { return createBuffer(CL_MEM_WRITE_ONLY, size, NULL); }
    CLBuffer * createBuffer(size_t size, void * host_ptr)
                                               { return createBuffer(CL_MEM_USE_HOST_PTR, size, host_ptr); }
+
+   CLKernel * createKernel(const char * filename, const char * name);
    
 #ifdef PV_USE_OPENCL
 
-   int createKernel(const char * filename, const char * name);
-
-   int addKernelArg(int argid, int arg);
-   int addKernelArg(int argid, CLBuffer * buf);
-   int addLocalArg(int argid, size_t size);
-   
-   int run(size_t global_work_size);
-   int run(size_t gWorkSizeX, size_t gWorkSizeY, size_t lWorkSizeX, size_t lWorkSizeY);
-	
 //   int copyResultsBuffer(cl_mem output, void * results, size_t size);
 
    int query_device_info();
-	
-   // execution time in microseconds
-   int get_execution_time()  { return elapsed; }
+   int query_device_info(int id, cl_device_id device);
 
 protected:
-
-   int query_device_info(int id, cl_device_id device);
 
    cl_uint num_devices;                  // number of computing devices
 
    cl_device_id device_ids[MAX_DEVICES]; // compute device id
    cl_context context;                   // compute context
    cl_command_queue commands;            // compute command queue
-   cl_program program;                   // compute program
-   cl_kernel kernel;                     // compute kernel
-   cl_event event;                       // event identifying the kernel execution instance
 
-   size_t global;                        // global domain size for our calculation
-   size_t local;                         // local domain size for our calculation
-
-   bool profiling;                       // flag to enable profiling
-   unsigned int elapsed;                 // elapsed time in microseconds
+   //size_t global;                        // global domain size for our calculation
+   //size_t local;                         // local domain size for our calculation
 
 #endif /* PV_USE_OPENCL */
 };
