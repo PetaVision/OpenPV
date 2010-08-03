967b51ae6b69f6516cc05515b3b501d3eff5089d
garkenyon <garkenyon@users.sourceforge.net>
2010-08-03 17:27:16 +0000

    modified parameters
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2745 7c0444ca-5550-0410-9008-faecdbe78eb5

--- mlab/amoebaGen/getAmoebaStats3.m
+++ mlab/amoebaGen/getAmoebaStats3.m
@@ -13,15 +13,16 @@ amoeba_struct.image_rect_size = image_dim(1);
 amoeba_struct.num_targets = 1;
 %amoeba_struct.num_distractors = 0*amoeba_struct.num_targets; %for targets
 amoeba_struct.num_distractors = 5; %for no targets
-amoeba_struct.segments_per_distractor = 2^(-3);  %as fraction of num_segments
+amoeba_struct.segments_per_distractor = 2^(-3); % 2^(-2); %  %as fraction
+				% of num_segments, 2nd value used in psychophysics
 amoeba_struct.target_outer_max = 0.5;%1; %max/min outer radius of target annulus, units of image rect
 amoeba_struct.target_outer_min = 0.5;%0.5;%1.0;%
 amoeba_struct.target_inner_max = 0.5;%0.75;%1.0;% %max/min inner radius in units of outer radius
 amoeba_struct.target_inner_min = 0.5;%0.25;%1.0;%
 amoeba_struct.num_phi = 1024;
 amoeba_struct.num_fourier = nfour; %min(3,amoeba_struct.num_phi);
-amoeba_struct.min_gap = 16;
-amoeba_struct.max_gap = 32;
+amoeba_struct.min_gap = 16; %32; % 
+amoeba_struct.max_gap = 32; %64; % 
 
 [amoeba_image] = amoeba2D2(amoeba_struct);
 plotAmoeba2D2(amoeba_image,  t, nfour);
