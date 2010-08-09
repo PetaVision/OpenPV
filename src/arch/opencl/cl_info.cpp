fc7dffdbba40efed29f7feea3f3ec868ad971c69
phelankell <phelankell@users.sourceforge.net>
2010-08-09 17:47:49 +0000

    Added CL_DEVICE_DEFAULT
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2750 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/arch/opencl/cl_info.cpp
+++ src/arch/opencl/cl_info.cpp
@@ -4,7 +4,7 @@ int main(int argc, char * argv[])
 {
 #ifdef PV_USE_OPENCL
 
-   PV::CLDevice * cld = new PV::CLDevice();
+   PV::CLDevice * cld = new PV::CLDevice(CL_DEVICE_DEFAULT);
 
    // query and print information about the devices found
    //
