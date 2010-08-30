f942c852d6514ff6f7b00399e9ee99b175032296
garkenyon <garkenyon@users.sourceforge.net>
2010-08-30 16:53:51 +0000

    minor updates to match new format
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2759 7c0444ca-5550-0410-9008-faecdbe78eb5

--- analysis/pvp_2AFC.m
+++ analysis/pvp_2AFC.m
@@ -1,7 +1,7 @@
 clear all
 setenv("GNUTERM", "x11");
 pvp_matlabPath;
-twoAFC_path = '/Users/gkenyon/Documents/eclipse-workspace/kernel/input/';
+twoAFC_path = '/Users/gkenyon/Documents/eclipse-workspace/kernel/input/256/';
 FC_list = [4]; %[2 4 6 8];
 len_FC = length(FC_list);
 max_expNum = 10;  % maximum number of independent experiments to be combined
@@ -9,11 +9,12 @@ twoAFC_array = cell(len_FC, max_expNum);
 expNum_list = [1];  % list for combining results from several experiments
 len_expNum = length(expNum_list);
 local_path = pwd;
-TRAINING_FLAG = -1
+TRAINING_FLAG = -2
 for i_fc = 1 : len_FC
   num_FC = FC_list(i_fc);
+  disp(['num_FC = ', num2str(num_FC)]);
 				%twoAFC_dir = [twoAFC_path, 'amoeba10K_', num2str(num_FC), 'fc'];
-  twoAFC_dir = [twoAFC_path, 'test_amoeba10K_target']; %, num2str(num_FC)];
+  twoAFC_dir = [twoAFC_path, 'test_amoeba20K_G75_target']; %, num2str(num_FC)];
   if abs(TRAINING_FLAG) == 1
     twoAFC_dir = [twoAFC_dir, '_G1'];
   elseif abs(TRAINING_FLAG) == 2
@@ -28,23 +29,34 @@ for i_fc = 1 : len_FC
   chdir(twoAFC_dir)
   for i_expNum = 1 : len_expNum
     expNum = expNum_list(i_expNum);
-    twoAFC_filename = ['twoAFC', num2str(expNum), '.mat.z'];
+    twoAFC_filename = ['twoAFC', num2str(expNum), '.mat'];
     load(twoAFC_filename)
     twoAFC_array{i_fc, i_expNum} = twoAFC;
-
     num_layers = size(twoAFC, 2);
     tot_trials = size(twoAFC, 3);
-    disp(['num_FC = ', num2str(num_FC)]);
-    for layer = 1 : num_layers
-      disp(['layer = ', num2str(layer)]);
-      twoAFC_correct(layer) = ...
-	  sum( squeeze( twoAFC(1,layer,:) > twoAFC(2,layer,:) ) ) / ...
-	  ( tot_trials + (tot_trials == 0) );
-      disp( ['twoAFC_correct(', num2str(layer), ') = ', ...
-	     num2str(twoAFC_correct(layer)) ] );
-    endfor
+    num_2AFC_tests = size(twoAFC, 4);
+    if ~exist('twoAFC_correct')
+      twoAFC_correct = zeros(num_layers, num_2AFC_tests);      
+      for i_2AFC_test = 1 : num_2AFC_tests
+	for layer = 1 : num_layers
+	  twoAFC_correct(layer, i_2AFC_test) = ...
+	      sum( squeeze( twoAFC(1,layer,:,i_2AFC_test) > ...
+			   twoAFC(2,layer,:,i_2AFC_test) ) ) / ...
+	      ( tot_trials + (tot_trials == 0) );
+        endfor
+      endfor % i_2AFC_test
+    endif
+    
+    for i_2AFC_test = 1 : num_2AFC_tests
+      disp(['i_2AFC_test = ', num2str(i_2AFC_test)]);
+      for layer = 1 : num_layers
+	disp( ['twoAFC_correct(', num2str(layer), ',', num2str(i_2AFC_test), ') = ', ...
+	       num2str(twoAFC_correct(layer, i_2AFC_test)) ] );
+      endfor
+    endfor % i_2AFC_test
     
   endfor % i_expNum
 endfor % i_fc
 chdir(local_path)
 
+%twoAFC = zeros(2, num_layers, num_trials, num_2AFC_tests);
