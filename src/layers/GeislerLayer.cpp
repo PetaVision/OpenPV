a6db5f26d0ba31d9c65307a1d4b26a76b1b87a29
garkenyon <garkenyon@users.sourceforge.net>
2010-08-30 19:18:54 +0000

    added compiler directives to limit the activity of non-spiking V1 layer neurons between max and threshold values
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2763 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/GeislerLayer.cpp
+++ src/layers/GeislerLayer.cpp
@@ -48,27 +48,44 @@ int GeislerLayer::updateState(float time, float dt)
       activity[k] = 0.0;
    }
 
-   pvdata_t max_direct = -FLT_MAX;
-   pvdata_t max_V = -FLT_MAX;
+   //pvdata_t max_bottomUp = -FLT_MAX;
+   //pvdata_t max_V = -FLT_MAX;
 
-   // assume direct input to phiExc, lateral input to phiInh
+   // assume bottomUp input to phiExc, lateral input to phiInh
    for (int k = 0; k < clayer->numNeurons; k++) {
-      pvdata_t direct_input = phiExc[k];
+      pvdata_t bottomUp_input = phiExc[k];
       pvdata_t lateral_input = phiInh[k];
-      V[k] = (direct_input > 0.0f) ? direct_input * lateral_input : direct_input;
-      max_direct = ( max_direct > direct_input ) ? max_direct : direct_input;
-      max_V = ( max_V > V[k] ) ? max_V : V[k];
+      V[k] = (bottomUp_input > 0.0f) ? bottomUp_input * lateral_input : bottomUp_input;
+      //max_bottomUp = ( max_bottomUp > bottomUp_input ) ? max_bottomUp : bottomUp_input;
+      //max_V = ( max_V > V[k] ) ? max_V : V[k];
       // reset accumulation buffers
       phiExc[k] = 0.0;
       phiInh[k] = 0.0;
       phiInhB[k] = 0.0;
    }
 
-   //pvdata_t scale_factor = fabs( max_direct ) / fabs( max_V + (max_V == 0.0f) );
+#define SET_MAX
+   //pvdata_t bottomUp_scale_factor = fabs( max_bottomUp ) / fabs( max_V + (max_V == 0.0f) );
+#ifdef SET_MAX
+   float max_bottomUp = 1.0f; //max_bottomUp * 2.0;
+#endif
+#define SET_THRESH
+#ifdef SET_THRESH
+   float thresh_bottomUp = 0.5f;
+#endif
    for (int k = 0; k < clayer->numNeurons; k++) {
       int kex = kIndexExtended(k, nx, ny, nf, marginWidth);
-      //V[k] *= scale_factor;
-      activity[kex] = ( V[k] > 0.0f ) ? V[k] : 0.0f;
+#undef RESCALE_ACTIVITY
+#ifdef RESCALE_ACTIVITY
+      V[k] *= bottomUp_scale_factor;
+#endif
+#ifdef SET_MAX
+      V[k] = V[k] > max_bottomUp ? max_bottomUp : V[k];
+#endif
+#ifdef SET_THRESH
+      V[k] = V[k] < thresh_bottomUp ? 0.0f : V[k];
+#endif
+      activity[kex] = V[k];
    }
 
    return 0;
