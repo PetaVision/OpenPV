22894c0604c9aab02c364a5d672085b1ad4e0cfb
rasmussn <rasmussn@users.sourceforge.net>
2010-10-19 22:34:58 +0000

    Added outputState, calcPosition, and offsetX and offsetY.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2821 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/Movie.hpp
+++ src/layers/Movie.hpp
@@ -20,14 +20,22 @@ public:
    virtual pvdata_t * getImageBuffer();
    virtual PVLayerLoc getImageLoc();
 
-   bool updateImage(float time, float dt);
+   virtual int outputState(float time, bool last=false);
    virtual int updateState(float time, float dt);
+   bool        updateImage(float time, float dt);
+
+   int calcPosition(int pos, int step, int sizeLength);
+
+private:
 
    int copyReducedImagePortion();
    const char * getNextFileName();
 
    float displayPeriod;     // length of time a frame is displayed
    float nextDisplayTime;   // time of next frame
+
+   int offsetX;             // offset of layer section within full movie frame
+   int offsetY;
 
    char inputfile[PV_PATH_MAX];  // current input file name
 
