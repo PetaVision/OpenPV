14e2742dfb8d9723f84d1ca99c98a747a9f14bfb
peteschultz <peteschultz@users.sourceforge.net>
2010-10-26 00:49:47 +0000

    Copied from PetaVision/analysis; changed "for i_fig = 2 : num_figs" to "for i_fig = 1 : num_figs"
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@2844 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 82a94d97c692d5d96b3820585a718d8e5b800228

---
+++ pete/mfiles/pvp_saveFigList.m
@@ -0,0 +1,14 @@
+function pvp_saveFigList( fig_list, fig_path, fig_suffix)
+num_figs = length(fig_list);
+for i_fig = 1 : num_figs
+  fig_hndl = fig_list(i_fig);
+  figure(fig_hndl);
+  axis normal
+  fig_filename = get(fig_hndl, 'Name');
+  fig_filename = [fig_path, fig_filename, '.', fig_suffix];
+  if exist(fig_filename, 'file')
+    delete(fig_filename);
+  end%%if
+  fig_option = ['-d', fig_suffix];
+  print(fig_hndl, fig_filename, fig_option);
+end%%for
