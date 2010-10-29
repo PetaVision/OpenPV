6933f4a3b9057621dd82b75922242d3ca1df8142
rasmussn <rasmussn@users.sourceforge.net>
2010-10-29 18:21:46 +0000

    First cut at OpenCL implementation of Lowe model simple cells.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2854 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/Simple.hpp
+++ src/layers/Simple.hpp
@@ -13,12 +13,19 @@
 namespace PV {
 
 class Simple: public PV::HyPerLayer {
+
 public:
    Simple(const char* name, HyPerCol * hc);
 
    virtual int recvSynapticInput(HyPerConn* conn, PVLayerCube* activity, int neighbor);
    virtual int reconstruct(HyPerConn* conn, PVLayerCube* activity);
    virtual int updateState(float time, float dt);
+
+protected:
+#ifdef PV_USE_OPENCL
+   virtual int initializeThreadData();
+   virtual int initializeThreadKernels();
+#endif
 
 };
 
