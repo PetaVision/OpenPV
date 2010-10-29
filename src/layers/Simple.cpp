854573e5e7c8d904819f9137081efedb8b8e7b23
rasmussn <rasmussn@users.sourceforge.net>
2010-10-29 18:22:08 +0000

    First cut at OpenCL implementation of Lowe model simple cells.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2855 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/Simple.cpp
+++ src/layers/Simple.cpp
@@ -15,6 +15,56 @@ Simple::Simple(const char* name, HyPerCol * hc)
    initialize(TypeSimple);
 }
 
+#ifdef PV_USE_OPENCL
+int Simple::initializeThreadData()
+{
+   int status = CL_SUCCESS;
+#ifdef IMPLEMENT_ME
+   // map layer buffers so that layer data can be initialized
+   //
+   pvdata_t * V = (pvdata_t *)   clBuffers.V->map(CL_MAP_WRITE);
+   pvdata_t * Vth = (pvdata_t *) clBuffers.Vth->map(CL_MAP_WRITE);
+
+   // initialize layer data
+   //
+   for (int k = 0; k < clayer->numNeurons; k++){
+      V[k] = V_REST;
+   }
+
+   for (int k = 0; k < clayer->numNeurons; k++){
+      Vth[k] = VTH_REST;
+   }
+
+   clBuffers.V->unmap(V);
+   clBuffers.Vth->unmap(Vth);
+#endif
+   return status;
+}
+
+int Simple::initializeThreadKernels()
+{
+   int status = CL_SUCCESS;
+
+   // create kernels
+   //
+   updatestate_kernel = parent->getCLDevice()->createKernel("LIF_updatestate.cl", "update_state");
+
+   int argid = 0;
+   status |= updatestate_kernel->setKernelArg(argid++, clBuffers.V);
+   status |= updatestate_kernel->setKernelArg(argid++, clBuffers.G_E);
+   status |= updatestate_kernel->setKernelArg(argid++, clBuffers.G_I);
+   status |= updatestate_kernel->setKernelArg(argid++, clBuffers.G_IB);
+   status |= updatestate_kernel->setKernelArg(argid++, clBuffers.phi);
+   status |= updatestate_kernel->setKernelArg(argid++, clBuffers.activity);
+   status |= updatestate_kernel->setKernelArg(argid++, clayer->loc.nx);
+   status |= updatestate_kernel->setKernelArg(argid++, clayer->loc.ny);
+   status |= updatestate_kernel->setKernelArg(argid++, clayer->numFeatures);
+   status |= updatestate_kernel->setKernelArg(argid++, clayer->loc.nPad);
+
+   return status;
+}
+#endif
+
 int Simple::recvSynapticInput(HyPerConn* conn, PVLayerCube* activity, int neighbor)
 {
    HyPerLayer::recvSynapticInput(conn,activity,neighbor);
