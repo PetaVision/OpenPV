8b5e94b183f1f48c281aacb968f293f894a705cc
rasmussn <rasmussn@users.sourceforge.net>
2010-11-09 19:32:37 +0000

    Added gettimeofday() timer for linux systems.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2872 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/utils/Timer.cpp
+++ src/utils/Timer.cpp
@@ -8,60 +8,84 @@
  */
 
 #include "Timer.hpp"
+#include <stdio.h>
 
-double mach_time_to_sec(uint64_t elapsed);
+#ifdef __APPLE__
+#  define USE_MACH_TIMER
+#endif
+
+#ifdef USE_MACH_TIMER
+//#  include <CoreServices/CoreServices.h>
+#  include <mach/mach.h>
+#  include <mach/mach_time.h>
+#else
+#  include <sys/time.h>
+#endif USE_MACH_TIMER
+
+
+/**
+ * Convert to milliseconds
+ */
+uint64_t get_cpu_time() {
+#ifdef USE_MACH_TIMER
+   return mach_absolute_time();
+#else
+   struct timeval tim;
+   //   struct rusage ru;
+   //   getrusage(RUSAGE_SELF, &ru);
+   //   tim = ru.ru_utime;
+   gettimeofday(&tim, NULL);
+   //printf("get_cpu_time: sec==%d usec==%d\n", tim.tv_sec, tim.tv_usec);
+   return ((uint64_t) tim.tv_sec)*1000000 + (uint64_t) tim.tv_usec;
+#endif
+}
+
+// Convert to milliseconds
+static double cpu_time_to_sec(uint64_t cpu_elapsed)
+{
+   double us = 0.0;
+#ifdef USE_MACH_TIMER
+   static mach_timebase_info_data_t  info;
+   mach_timebase_info(&info);
+   cpu_elapsed *= info.numer;
+   cpu_elapsed /= info.denom;
+   us = (double) (cpu_elapsed/1000);  // microseconds
+#else
+   us = (double) cpu_elapsed;
+#endif
+   return us/1000.0;
+}
+
 
 namespace PV {
 
 Timer::Timer()
 {
    rank = 0;
-   mach_start   = mach_absolute_time();
-   mach_end     = mach_start;
-   mach_elapsed = 0.0;
-}
-
-Timer::~Timer()
-{
+   time_start   = get_cpu_time();
+   time_end     = time_start;
+   time_elapsed = 0.0;
 }
 
 double Timer::start()
 {
-   return (mach_start = mach_absolute_time());
+   return (double) (time_start = get_cpu_time());
 }
 
 double Timer::stop()
 {
-   mach_end = mach_absolute_time();
-   mach_elapsed += mach_end - mach_start;
-   return mach_end;
+   time_end = get_cpu_time();
+   time_elapsed += time_end - time_start;
+   return (double) time_end;
 }
 
 double Timer::elapsed_time()
 {
    if (rank == 0) {
-      fprintf(stdout, "Mach processor cycle time == %f\n", (float) mach_time_to_sec(mach_elapsed));
+      fprintf(stdout, "processor cycle time == %f\n", (float) cpu_time_to_sec(time_elapsed));
       fflush(stdout);
    }
-   return mach_elapsed;
+   return (double) time_elapsed;
 }
    
 }  // namespace PV
-
-
-// Convert to milliseconds
-double mach_time_to_sec(uint64_t elapsed)
-{
-   double ms;
-   static mach_timebase_info_data_t  sTimebaseInfo;
-   
-   if ( sTimebaseInfo.denom == 0 ) {
-      // initialize (yuk, hope it isn't some stray value)
-      (void) mach_timebase_info(&sTimebaseInfo);
-   }
-   
-   ms = (double) (elapsed) / 1.0e9;
-   ms *= sTimebaseInfo.numer / sTimebaseInfo.denom;
-   
-   return ms;
-}
