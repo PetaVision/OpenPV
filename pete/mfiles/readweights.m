085f8f27117b313c9943722329fdd31784aec1ff
peteschultz <peteschultz@users.sourceforge.net>
2010-11-17 23:54:28 +0000

    The routine now reads minVal and maxVal from bytes 92-99 to return values in the correct range instead of in the range 0-255.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@2886 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: bc495b6a548f4212ac4c75af4302e9f3f1d072fa

--- pete/mfiles/readweights.m
+++ pete/mfiles/readweights.m
@@ -7,26 +7,40 @@ function W = readweights(weightfile)
 % W(i,j,m,n) is the weight of the (i,j)th location for feature m
 % and kernel patch n.
 
+filedata = dir(weightfile);
+if length(filedata) ~= 1
+    error('readweights:notonefile',...
+          'Path %s should expand to one file; in this case there are %d',...
+          length(filedata));
+end
+
+if filedata(1).bytes < 104
+    error('readweights:filetooshort',...
+          'File %s is too short to contain a valid header',weightfile);
+end%if numel
+
 fid = fopen(weightfile);
 if fid == -1
     error('readweights:cantopenfile','Can''t open %s',weightfile);
 end
 
+hdr = fread(fid, 20, 'int32');
+nxp = fread(fid, 1, 'int32');
+nyp = fread(fid, 1, 'int32');
+nfp = fread(fid, 1, 'int32');
+weight_min = fread(fid, 1, 'float32');
+weight_max = fread(fid, 1, 'float32');
+numpatches = fread(fid, 1, 'int32');
 A = fread(fid);
 fclose(fid);
 
-if numel(A)<104
-    error('readweights:filetooshort',...
-          'File %s is too short to contain a valid header',weightfile);
-end%if numel
+if isempty(nxp) || isempty(nyp) || isempty(nfp) || ...
+   isempty(weight_min) || isempty(weight_max) || isempty(numpatches)
+    error('readweights:badheader',...
+          'Unable to read header of file %s',weightfile);
+end
 
-nxp = [1 2^8 2^16 2^24]*A(81:84);
-nyp = [1 2^8 2^16 2^24]*A(85:88);
-nfp = [1 2^8 2^16 2^24]*A(89:92);
-
-numpatches = [1 2^8 2^16 2^24]*A(69:72);
-
-if numpatches*(nxp*nyp*nfp+4)+104 ~= numel(A);
+if numpatches*(nxp*nyp*nfp+4) ~= numel(A);
     error('readweights:badlength',...
           'The length of %s is inconsistent with its header',weightfile);
 end%if numpatches
@@ -34,9 +48,11 @@ end%if numpatches
 W = zeros(nxp, nyp, nfp, numpatches);
 
 for p=1:numpatches
-    baseindex = 104+(p-1)*(4+nxp*nyp*nfp)+4;
+    baseindex = (p-1)*(4+nxp*nyp*nfp)+4;
     R = reshape(A(baseindex+1:baseindex+nxp*nyp*nfp),[nfp,nxp,nyp]);
     for f=1:nfp
         W(:,:,f,p) = squeeze(R(f,:,:));
     end%for f
 end%for p
+
+W = W/255*(weight_max-weight_min)+weight_min;
