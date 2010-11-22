c31f96b7cea2db3569e7c149eb0196a72313be5d
garkenyon <garkenyon@users.sourceforge.net>
2010-11-22 18:02:09 +0000

    test of 2D Gaussian weight initialization
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@2891 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ tests/test_gauss2d.cpp
@@ -0,0 +1,76 @@
+/**
+ * This file tests weight initialization to a 2D Gaussian with sigma = 1.0 and normalized to 1.0
+ * Test compares HyPerConn to KernelConn,
+ * assumes kernelConn produces correct 2D Gaussian weights
+ *
+ */
+
+#undef DEBUG_PRINT
+
+#include "../src/layers/HyPerLayer.hpp"
+#include "../src/connections/HyPerConn.hpp"
+#include "../src/connections/KernelConn.hpp"
+#include "../src/layers/Example.hpp"
+#include "../src/io/io.h"
+#include <assert.h>
+
+using namespace PV;
+
+//const int numFeatures = 1;
+
+int main(int argc, char * argv[]) {
+	PV::HyPerCol * hc = new PV::HyPerCol("test_gauss2d column", argc, argv);
+	PV::Example * pre = new PV::Example("test_gauss2d pre", hc);
+	PV::Example * post = new PV::Example("test_gauss2d post", hc);
+	PV::HyPerConn * cHyPer = new HyPerConn("test_gauss2d hyperconn", hc, pre,
+			post, CHANNEL_EXC);
+	PV::KernelConn * cKernel = new KernelConn("test_gauss2d kernelconn", hc,
+			pre, post, CHANNEL_EXC);
+
+	const int axonID = 0;
+	int num_pre_extended = pre->clayer->numExtended;
+	assert(num_pre_extended == cHyPer->numWeightPatches(axonID));
+
+	for (int kPre = 0; kPre < num_pre_extended; kPre++) {
+
+		PVPatch * hyperPatch = cHyPer->getWeights(kPre, axonID);
+		PVPatch * kernelPatch = cKernel->getWeights(kPre, axonID);
+
+		int nk = hyperPatch->nf * hyperPatch->nx;
+		assert(nk == (kernelPatch->nf * kernelPatch->nx));
+		int ny = hyperPatch->ny;
+		assert(ny == kernelPatch->ny);
+		int sy = hyperPatch->sy;
+		assert(sy == kernelPatch->sy);
+		pvdata_t * hyperWeights = hyperPatch->data;
+		pvdata_t * kernelWeights = kernelPatch->data;
+		pvdata_t kernel_ratio = 0.0f;
+		pvdata_t hyper_ratio = 0.0f;
+		float test_cond = 0.0f;
+		for (int y = 0; y < ny; y++) {
+			for (int k = 0; k < nk; k++) {
+				kernel_ratio =
+						kernelWeights[0] != 0 ? kernelWeights[k]
+								/ kernelWeights[0] : 0.0;
+				hyper_ratio = hyperWeights[0] != 0 ? hyperWeights[k]
+						/ hyperWeights[0] : 0.0;
+				test_cond = fabs(kernel_ratio + hyper_ratio) > 0 ? fabs(
+						kernel_ratio - hyper_ratio) / fabs(kernel_ratio
+						+ hyper_ratio) : 0.0f;
+				if (test_cond > 0.01f) {
+					const char * cHyper_filename = "gauss2d_hyper.txt";
+					cHyPer->writeTextWeights(cHyper_filename, kPre);
+					const char * cKernel_filename = "gauss2d_kernel.txt";
+					cKernel->writeTextWeights(cKernel_filename, kPre);
+				}
+				assert(test_cond <= 0.01f);
+			}
+			// advance pointers in y
+			hyperWeights += sy;
+			kernelWeights += sy;
+		}
+	}
+
+	delete hc;
+	return 0;
+}
