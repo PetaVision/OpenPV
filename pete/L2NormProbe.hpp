efdaaf174f6467ceffe5f60959039e0d06100907
peteschultz <peteschultz@users.sourceforge.net>
2010-11-27 03:48:42 +0000

    L2NormProbe and SparsityTermProbe are derived classes of LayerFunctionProbe.  L2NormProbe sets the function to L2NormFunction and SparsityTermProbe sets it to SparsityTermFunction.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@2910 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: ff6c27ee6ece0366e85350c70ea95d3417f336a1

---
+++ pete/L2NormProbe.hpp
@@ -0,0 +1,27 @@
+/*
+ * L2NormProbe.hpp
+ *
+ *  Created on: Nov 19, 2010
+ *      Author: pschultz
+ */
+
+#ifndef L2NORMPROBE_HPP_
+#define L2NORMPROBE_HPP_
+
+#include "LayerFunctionProbe.hpp"
+#include "L2NormFunction.hpp"
+
+namespace PV {
+class L2NormProbe : public LayerFunctionProbe {
+public:
+	L2NormProbe(const char * msg);
+	L2NormProbe(const char * filename, const char * msg);
+	~L2NormProbe();
+    int outputState(float time, HyPerLayer * l);
+    pvdata_t evaluate();
+
+}; // end class L2NormProbe
+
+}  // end namespace PV
+
+#endif /* L2NORMPROBE_HPP_ */
