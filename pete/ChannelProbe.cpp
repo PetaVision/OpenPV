560d5ae3e3fb12455c7724a22eebdaca832467e8
peteschultz <peteschultz@users.sourceforge.net>
2010-11-27 03:52:23 +0000

    ChannelProbe is a subclass of LayerProbe that writes the contents of one phi channel to either stdout or a file.  There is one line per neuron, so these will be large text files.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@2911 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 470cd1616917a7f09541af1f8d419b3655534664

---
+++ pete/ChannelProbe.cpp
@@ -0,0 +1,30 @@
+/*
+ * ChannelProbe.cpp
+ *
+ *  Created on: Nov 22, 2010
+ *      Author: pschultz
+ */
+
+#include "ChannelProbe.hpp"
+
+namespace PV {
+
+ChannelProbe::ChannelProbe(ChannelType channel) : LayerProbe() {
+    pChannel = channel;
+}  // end ChannelProbe::ChannelProbe(ChannelType)
+
+ChannelProbe::ChannelProbe(const char * filename, ChannelType channel) : LayerProbe(filename){
+    pChannel = channel;
+}  // end ChannelProbe::ChannelProbe(const char *, ChannelType)
+
+int ChannelProbe::outputState(float time, HyPerLayer * l) {
+    pvdata_t * buf = l->getChannel(pChannel);
+    int n = l->getNumNeurons();
+    for( int k=0; k<n; k++) {
+        fprintf(fp, "Layer %s, channel %d, time %f, neuron %8d, value=%10.8f\n",
+        		l->getName(), (int) pChannel, time, k, buf[k]);
+    }
+    return EXIT_SUCCESS;
+}  // end ChannelProbe::outputState(float, HyPerLayer *)
+
+}  // end namespace PV
---
+++ pete/ChannelProbe.hpp
@@ -0,0 +1,31 @@
+/*
+ * ChannelProbe.hpp
+ *
+ *  Created on: Nov 22, 2010
+ *      Author: pschultz
+ */
+
+#ifndef CHANNELPROBE_HPP_
+#define CHANNELPROBE_HPP_
+
+#include "../PetaVision/src/io/LayerProbe.hpp"
+#include "../PetaVision/src/include/pv_types.h"
+#include "../PetaVision/src/layers/HyPerLayer.hpp"
+
+namespace PV {
+
+class ChannelProbe : public LayerProbe {
+public:
+    ChannelProbe(ChannelType channel);
+    ChannelProbe(const char * filename, ChannelType channel);
+
+    virtual int outputState(float time, HyPerLayer * l);
+protected:
+    ChannelType pChannel;
+
+}; // end of class ChannelProbe
+
+
+}  // end of namespace PV
+
+#endif /* CHANNELPROBE_HPP_ */
