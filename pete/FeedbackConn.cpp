83cb25acb922498a8803bd4aa82bdf696962dfbc
peteschultz <peteschultz@users.sourceforge.net>
2010-12-17 05:07:11 +0000

    Bug fixes in setPatchSize and transposeKernels
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@2979 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 240504dd18808bf03060768f6b45836799839bf9

--- pete/FeedbackConn.cpp
+++ pete/FeedbackConn.cpp
@@ -33,29 +33,38 @@ int FeedbackConn::initialize(const char * name, HyPerCol * hc,
 
 int FeedbackConn::setPatchSize(const char * filename) {
     int status = EXIT_SUCCESS;
+
+    int xscaleDiff = pre->getXScale() - post->getXScale();
+    // If feedforward conn is many-to-one, feedback conn is one-to-many.
+    // Then xscaleDiff > 0.
+    // Similarly, if feedforwardConn is one-to-many, xscaleDiff < 0.
+    int yscaleDiff = pre->getYScale() - post->getYScale();
+
     nxp = feedforwardConn->xPatchSize();
+    if(xscaleDiff > 0 ) {
+        nxp *= (int) powf( 2, xscaleDiff );
+    }
+    else if(xscaleDiff < 0) {
+        nxp /= (int) powf(2,-xscaleDiff);
+        assert(feedforwardConn->xPatchSize()==nxp*powf( 2, (float) (-xscaleDiff) ));
+    }
     nyp = feedforwardConn->yPatchSize();
+    if(yscaleDiff > 0 ) {
+        nyp *= (int) powf( 2, (float) yscaleDiff );
+    }
+    else if(yscaleDiff < 0) {
+        nyp /= (int) powf(2,-yscaleDiff);
+        assert(feedforwardConn->yPatchSize()==nyp*powf( 2, (float) (-yscaleDiff) ));
+    }
     nfp = post->getCLayer()->numFeatures;
 
-    if (filename != NULL) { // copied from HyPerConn::setPatchSize.  Would be better to modularize out the nxp, nyp, nfp calculations
-       int filetype, datatype;
-       double time = 0.0;
-       const PVLayerLoc loc = this->pre->clayer->loc;
+    assert( checkPatchSize(nyp, pre->getXScale(), post->getXScale(), 'x') ==
+            EXIT_SUCCESS );
 
-       int wgtParams[NUM_WGT_PARAMS];
-       int numWgtParams = NUM_WGT_PARAMS;
+    assert( checkPatchSize(nyp, pre->getYScale(), post->getYScale(), 'y') ==
+            EXIT_SUCCESS );
 
-       Communicator * comm = parent->icCommunicator();
-
-       status = pvp_read_header(filename, comm, &time, &filetype, &datatype, wgtParams, &numWgtParams);
-       if (status < 0) return status;
-
-       status = checkPVPFileHeader(comm, &loc, wgtParams, numWgtParams);
-       if (status < 0) return status;
-
-       // reconcile differences with inputParams
-       status = checkWeightsHeader(filename, wgtParams);
-    }
+    status = filename ? patchSizeFromFile(filename) : EXIT_SUCCESS;
     return status;
 }  // end of FeedbackConn::setPatchSize(const char *)
 
@@ -68,7 +77,6 @@ PVPatch ** FeedbackConn::initializeWeights(PVPatch ** patches, int numPatches,
 }  // end of FeedbackConn::initializeWeights
 
 int FeedbackConn::updateWeights(int axonID) {
-    printf("updateWeights %s\n", name);
     transposeKernels();
     return EXIT_SUCCESS;
 }  // end of FeedbackConn::updateWeights(int);
@@ -77,29 +85,76 @@ int FeedbackConn::transposeKernels() {
     // compute the transpose of feedforwardConn->kernelPatches and
     // store into this->kernelPatches
     // assume scale factors are 1 and that nxp, nyp are odd.
-    int numFFKernelPatches = feedforwardConn->numDataPatches(0);
+
+	int xscalediff = pre->getXScale()-post->getXScale(); // scalediff>0 means feedbackconn's post--that is, the forward conn's pre--has a higher neuron density
+	int yscalediff = pre->getYScale()-post->getYScale();
     int numFBKernelPatches = numDataPatches(0);
-    for( int kernelnumberFB=0; kernelnumberFB < numFBKernelPatches; kernelnumberFB++ ) {
-        PVPatch * kpFB = getKernelPatch(kernelnumberFB); //patches[0]?
-        int nxFB = kpFB->nx;
-        int nyFB = kpFB->ny;
-        int nfFB = kpFB->nf;
-        assert(numFFKernelPatches == nfFB);
-        for( int kyFB = 0; kyFB < nyFB; kyFB++ ) {
-            int kyFF = nyFB-1-kyFB;
-            for( int kxFB = 0; kxFB < nxFB; kxFB++ ) {
-                int kxFF = nxFB-1-kxFB;
-                for( int kfFB = 0; kfFB < nfFB; kfFB++ ) {
-                    int kfFF = kernelnumberFB;
-                    int kernelnumberFF = kfFB;
-                    PVPatch * kpFF = feedforwardConn->getKernelPatch(kernelnumberFF);
-                    int kIndexFF = kIndex(kxFF,kyFF,kfFF,kpFF->nx,kpFF->ny,kpFF->nf);
-                    int kIndexFB = kIndex(kxFB,kyFB,kfFB,nxFB,nyFB,nfFB);
-                    kpFB->data[kIndexFB] = kpFF->data[kIndexFF];
-                }
-            }
+    int numFFKernelPatches = feedforwardConn->numDataPatches(0);
+
+    if( xscalediff <= 0 && yscalediff <= 0) {
+        int xscaleq = (int) powf(2,-xscalediff);
+        int yscaleq = (int) powf(2,-yscalediff);
+
+        for( int kernelnumberFB = 0; kernelnumberFB < numFBKernelPatches; kernelnumberFB++ ) {
+            PVPatch * kpFB = getKernelPatch(kernelnumberFB);
+            int nfFB = kpFB->nf;
+       	    assert(numFFKernelPatches == nfFB);
+            int nxFB = kpFB->nx;
+            int nyFB = kpFB->ny;
+            for( int kyFB = 0; kyFB < nyFB; kyFB++ ) {
+        	    for( int kxFB = 0; kxFB < nxFB; kxFB++ ) {
+        		    for( int kfFB = 0; kfFB < nfFB; kfFB++ ) {
+                        int kIndexFB = kIndex(kxFB,kyFB,kfFB,nxFB,nyFB,nfFB);
+        			    int kernelnumberFF = kfFB;
+                        PVPatch * kpFF = feedforwardConn->getKernelPatch(kernelnumberFF);
+                   	    assert(numFBKernelPatches == kpFF->nf * xscaleq * yscaleq);
+                        int kfFF = featureIndex(kernelnumberFB, xscaleq, yscaleq, feedforwardConn->fPatchSize());
+                        int kxFFoffset = kxPos(kernelnumberFB, xscaleq, yscaleq, feedforwardConn->fPatchSize());
+                        int kxFF = (nxp - 1 - kxFB) * xscaleq + kxFFoffset;
+                        int kyFFoffset = kyPos(kernelnumberFB, xscaleq, yscaleq, feedforwardConn->fPatchSize());
+                        int kyFF = (nyp - 1 - kyFB) * yscaleq + kyFFoffset;
+                        int kIndexFF = kIndex(kxFF, kyFF, kfFF, kpFF->nx, kpFF->ny, kpFF->nf);
+                        // can the calls to kxPos, kyPos, featureIndex be replaced by one call to patchIndexToKernelIndex?
+                        kpFB->data[kIndexFB] = kpFF->data[kIndexFF];
+        		    }
+        		}
+        	}
         }
     }
+	else if( xscalediff > 0 && yscalediff > 0) {
+        int xscaleq = (int) powf(2,xscalediff);
+        int yscaleq = (int) powf(2,yscalediff);
+        for( int kernelnumberFB = 0; kernelnumberFB < numFBKernelPatches; kernelnumberFB++ ) {
+            PVPatch * kpFB = getKernelPatch(kernelnumberFB);
+            int nxFB = kpFB->nx;
+            int nyFB = kpFB->ny;
+            int nfFB = kpFB->nf;
+        	for( int kyFB = 0; kyFB < nyFB; kyFB++ ) {
+		        int precelloffsety = kyFB % yscaleq;
+       		    for( int kxFB = 0; kxFB < nxFB; kxFB++ ) {
+    		        int precelloffsetx = kxFB % xscaleq;
+        			for( int kfFB = 0; kfFB < nfFB; kfFB++ ) {
+        		        int kernelnumberFF = (precelloffsety*xscaleq + precelloffsetx)*nfFB + kfFB;
+                        PVPatch * kpFF = feedforwardConn->getKernelPatch(kernelnumberFF);
+        			    int kxFF = (nxp-kxFB-1)/xscaleq;
+        			    assert(kxFF >= 0 && kxFF < feedforwardConn->xPatchSize());
+        			    int kyFF = (nyp-kyFB-1)/yscaleq;
+        			    assert(kyFF >= 0 && kyFF < feedforwardConn->yPatchSize());
+        		        int kfFF = kernelnumberFB;
+        			    assert(kfFF >= 0 && kfFF < feedforwardConn->fPatchSize());
+        		        int kIndexFF = kIndex(kxFF, kyFF, kfFF, kpFF->nx, kpFF->ny, kpFF->nf);
+        		        int kIndexFB = kIndex(kxFB, kyFB, kfFB, nxFB, nyFB, nfFB);
+        		        kpFB->data[kIndexFB] = kpFF->data[kIndexFF];
+        			}
+        		}
+        	}
+        }
+	}
+	else {
+        fprintf(stderr,"xscalediff = %d, yscalediff = %d: this case not yet implemented.\n", xscalediff, yscalediff);
+        exit(1);
+    }
+
     return EXIT_SUCCESS;
 }  // FeedbackConn::transposeKernels()
 
