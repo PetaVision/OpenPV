05226ce257ff86d684f27928156c7b759733fc0c
peteschultz <peteschultz@users.sourceforge.net>
2010-12-17 05:20:08 +0000

    initialize and updateState reworked to inherit more and copy less code
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@2982 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 29739aca567bfeb8c1eef29c5f9395dc107e8000

--- pete/GenerativeLayer.cpp
+++ pete/GenerativeLayer.cpp
@@ -13,53 +13,30 @@
 namespace PV {
 
 GenerativeLayer::GenerativeLayer(const char * name, HyPerCol * hc) : V1(name, hc) {
-	initialize(TypeV1Simple);
+	initialize();
 }  // end of GenerativeLayer::GenerativeLayer(const char *, HyperCol *)
 
 GenerativeLayer::GenerativeLayer(const char * name, HyPerCol * hc, PVLayerType type) : V1(name, hc, type){
-    initialize(type);
+    initialize();
 }  // end of GenerativeLayer::GenerativeLayer(const char *, HyperCol *, PVLayerType *)
 
-int GenerativeLayer::initialize(PVLayerType type) {
+int GenerativeLayer::initialize() {
 	relaxation = parent->parameters()->value(name, "relaxation", 1.0);
 	return EXIT_SUCCESS;
-}  // end of GenerativeLayer::initialize(PVLayerType)
+}  // end of GenerativeLayer::initialize()
 
-int GenerativeLayer::updateState(float time, float dt) {
-	   pv_debug_info("[%d]: GenerativeLayer::updateState:", clayer->columnId);
+int GenerativeLayer::updateV() {
+   pvdata_t * V = getV();
+   pvdata_t ** phi = getCLayer()->phi;
+   pvdata_t * phiExc = phi[PHI_EXC];
+   pvdata_t * phiInh = phi[PHI_INH];
+   for( int k=0; k<getNumNeurons(); k++ ) {
+      V[k] += relaxation*(phiExc[k] - phiInh[k] - sparsitytermderivative(V[k]));
+      // Thresholding would go here
+   }
+   return EXIT_SUCCESS;
+}  // end of GenerativeLayer::updateV()
 
-	   // Update V using dV/dt = relaxationrate*(clayer->phi[0]-clayer->phi[1])
-	   // clayer->activity->data to new V
-	   // zero out clayer->phi[0] and clayer->phi[1]
-
-	   const int nx = clayer->loc.nx;
-	   const int ny = clayer->loc.ny;
-	   const int nf = clayer->numFeatures;
-	   const int marginWidth = clayer->loc.nPad;
-
-	   pvdata_t * V = getV();
-	   pvdata_t * phiExc   = clayer->phi[PHI_EXC];
-	   pvdata_t * phiInh   = clayer->phi[PHI_INH];
-	   pvdata_t * activity = clayer->activity->data;
-
-	   // make sure activity in border is zero
-	   //
-	   for (int k = 0; k < getNumExtended(); k++) {
-	      activity[k] = 0.0;
-	   }
-
-	   for (int k = 0; k < getNumNeurons(); k++) {
-	      int kex = kIndexExtended(k, nx, ny, nf, marginWidth);
-	      V[k] += relaxation*(phiExc[k] - phiInh[k] - sparsitytermderivative(V[k])); // essentially only change from V1
-	      activity[kex] = V[k];
-
-	      // reset accumulation buffers
-	      phiExc[k] = 0.0;
-	      phiInh[k] = 0.0;
-	   }
-
-	   return EXIT_SUCCESS;
-}  // end of GenerativeLayer::updateState(float, float)
 
 }  // end of namespace PV block
 
--- pete/GenerativeLayer.hpp
+++ pete/GenerativeLayer.hpp
@@ -17,12 +17,14 @@ class GenerativeLayer : public V1 {
 public:
 	GenerativeLayer(const char * name, HyPerCol * hc);
 	GenerativeLayer(const char * name, HyPerCol * hc, PVLayerType type);
-    int initialize(PVLayerType type);
+    int initialize();
 
     pvdata_t getRelaxation() {return relaxation;}
-    int updateState(float time, float dt);
     virtual pvdata_t sparsityterm(pvdata_t v) { return logf(1+v*v);}
     virtual pvdata_t sparsitytermderivative(pvdata_t v) { return 2.0*v/(1+v*v); }
+
+protected:
+    int updateV();
 
 private:
     pvdata_t relaxation; // V(new) = V(old) - relaxation*(gradient wrt V)
