5783ff73d927df550427c04145a8398262551897
rasmussn <rasmussn@users.sourceforge.net>
2010-12-21 22:17:50 +0000

    Started top down connection orientation.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3019 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/HMaxSimple.cpp
+++ src/layers/HMaxSimple.cpp
@@ -21,7 +21,41 @@ int HMaxSimple::recvSynapticInput(HyPerConn * conn, PVLayerCube * activity, int 
                  conn->preSynapticLayer()->clayer->layerId, clayer->layerId);
 
    // use implementation in base class
-   HyPerLayer::recvSynapticInput(conn, activity, neighbor);
+   //HyPerLayer::recvSynapticInput(conn, activity, neighbor);
+
+   PVLayer * lPre  = conn->preSynapticLayer() ->clayer;
+   PVLayer * lPost = conn->postSynapticLayer()->clayer;
+
+   const PVLayerLoc locPost = lPost->loc;
+
+   const int nxPost = locPost.nx;
+   const int nyPost = locPost.ny;
+   const int nfPost = lPost->numFeatures;
+
+   const int nxPreEx = lPre->loc.nx;
+   const int nyPreEx = lPre->loc.ny;
+   const int nfPre   = lPre->numFeatures;
+   const int nPadPre = lPre->loc.nPad;
+
+   const int aStrideX = nfPre;
+   const int aStrideY = nfPre * (nxPreEx + 2*nPadPre);
+
+   KernelConn * kConn = dynamic_cast<KernelConn*>(conn);
+
+#ifdef FORTRAN
+   int iKernel = 0;  // TODO - fix for multiple orientations
+   PVPatch * weights = kConn->getKernelPatch(iKernel);
+
+   pvdata_t * w = weights->data;
+   pvdata_t * V = lPost->V;
+
+   const int nxp = weights->nx;
+   const int nyp = weights->ny;
+
+   call convolve(nx)
+
+
+#endif
 
    return 0;
 }
