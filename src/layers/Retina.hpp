3a803826c77f17c36c0c35f5ac65822a9aeacae0
rasmussn <rasmussn@users.sourceforge.net>
2010-12-21 21:32:17 +0000

    Deleted some OBSOLETE code.  Added some OpenCL implementation in preparation for real stuff.  Actually mostly consistent use of #ifdef PV_USE_OPENCL to remove implementation.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3011 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/Retina.hpp
+++ src/layers/Retina.hpp
@@ -19,28 +19,26 @@ class Retina: public PV::HyPerLayer
 {
 public:
    Retina(const char * name, HyPerCol * hc);
-#ifdef OBSOLETE
-   Retina(const char * name, HyPerCol * hc, Image * img);
-   Retina(const char * name, HyPerCol * hc, const char * filename);
-#endif
 
    int initialize(PVLayerType type);
    int setParams(PVParams * params, fileread_params * p);
 
    virtual int updateState(float time, float dt);
+#ifdef PV_USE_OPENCL
+   virtual int updateStateOpenCL(float time, float dt);
+#endif
+
    virtual int writeState(const char * path, float time);
    virtual int spike(float time, float dt, float prev, float prob, float probStim, float * probSpike);
 
-#ifdef OBSOLETE
-   Image * getImage()  { return img; }
+protected:
+
+#ifdef PV_USE_OPENCL
+   virtual int initializeThreadData();
+   virtual int initializeThreadKernels();
 #endif
 
 private:
-
-#ifdef OBSOLETE
-   int updateImage(float time, float dt);
-   int copyFromImageBuffer();
-#endif
 
    // For input from a given source input layer, determine which
    // cells in this layer will respond to the input activity.
@@ -56,10 +54,6 @@ private:
 
    int calculateWeights(HyPerLayer * lSource, float * pos, float * vPos, float * vfWeights );
 
-#ifdef OBSOLETE
-   Image * img;        // image object
-   int fireOffPixels;  // controls firing of off (0) pixels
-#endif
 };
 
 } // namespace PV
