0aab253c7931216758938acfe56e8e878f3a58d5
rasmussn <rasmussn@users.sourceforge.net>
2010-12-27 21:43:15 +0000

    Added background noise.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3069 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/kernels/LIF_update_state.f90
+++ src/kernels/LIF_update_state.f90
@@ -1,3 +1,37 @@
+module OFP
+  type :: uint4
+     integer :: s0, s1, s2, s3
+  end type uint4
+
+contains
+
+   function local(A)
+      implicit none
+      real, target, dimension(:,:,:) :: A
+      real, pointer, dimension(:,:,:) :: local
+
+      local => A
+
+   end function local
+
+   function local_section(A, halo)
+      implicit none
+      real, target,  dimension(:,:,:) :: A
+      real, pointer, dimension(:,:,:) :: local_section
+      integer :: halo(6), lb(3), ub(3)
+
+      lb = lbound(A)
+      ub = ubound(A)
+
+      local_section => A( lb(1)+halo(1) : ub(1)-halo(2), &
+                          lb(2)+halo(3) : ub(2)-halo(4), &
+                          lb(3)+halo(5) : ub(3)-halo(6)  )
+
+   end function local_section
+
+end module OFP
+
+
 module LIF
 
    use, intrinsic :: ISO_C_BINDING
@@ -25,18 +59,32 @@ module LIF
       real(C_FLOAT) :: noiseAmpIB
    end type LIF_params
 
+   interface
+      pure elemental function cl_random_state(state)
+         use OFP
+         type(uint4), intent(in) :: state
+         type(uint4) :: cl_random_state
+      end function
+      pure elemental real function cl_random_prob(state)
+         use OFP
+         type(uint4), intent(in) :: state
+      end function
+   end interface
+
 contains
 
 !$OFP CL_KERNEL :: update_state
 subroutine update_state(time, dt, params,         &
-                        nf, nx, ny, nb,           &
+                        nf, nx, ny, nb, rnd,      &
                         V, Vth, G_E, G_I, G_IB,   &
                         phiExc, phiInh, phiInhB,  &
                         activity)
+   use OFP
    implicit none
    real, intent(in) :: time, dt
    type(LIF_Params), intent(in) :: params
    integer, intent(in) :: nf, nx, ny, nb
+   type(uint4), intent(inout) :: rnd(nf,nx,ny)
    real, intent(inout), dimension(nf,nx,ny) :: V, Vth, G_E, G_I, G_IB
    real, intent(inout), dimension(nf,nx,ny) :: phiExc, phiInh, phiInhB
    real, intent(inout), dimension(nf,nx+2*nb,ny+2*nb) :: activity
@@ -45,6 +93,7 @@ subroutine update_state(time, dt, params,         &
    real :: tau, tauE, tauI, tauIB, Vrest, VthRest, Vexc, Vinh, VinhB, tauVth, deltaVth
 
    real, parameter :: GMAX = 10.0
+   real :: dt_sec
 
    ! local param variables
    real, pointer, dimension(:,:,:) :: l_activ
@@ -73,7 +122,27 @@ subroutine update_state(time, dt, params,         &
    VthRest  = params%VthRest
    deltaVth = params%deltaVth
 
-!   call add_noise(l, dt)
+   ! add noise
+   !
+   dt_sec = .001 * dt   ! convert to seconds
+
+   where (cl_random_prob(rnd) < dt_sec*params%noiseFreqE)
+      rnd = cl_random_state(rnd)
+      phiExc = phiExc + params%noiseAmpE*cl_random_prob(rnd)
+      rnd = cl_random_state(rnd)
+   end where
+
+   where (cl_random_prob(rnd) < dt_sec*params%noiseFreqI)
+      rnd = cl_random_state(rnd)
+      phiInh = phiInh + params%noiseAmpI*cl_random_prob(rnd)
+      rnd = cl_random_state(rnd)
+   end where
+
+   where (cl_random_prob(rnd) < dt_sec*params%noiseFreqIB)
+      rnd = cl_random_state(rnd)
+      phiInhB = phiInhB + params%noiseAmpIB*cl_random_prob(rnd)
+      rnd = cl_random_state(rnd)
+   end where
 
    G_E  = phiExc  + G_E *exp(-dt/tauE)
    G_I  = phiInh  + G_I *exp(-dt/tauI )
@@ -102,7 +171,7 @@ subroutine update_state(time, dt, params,         &
    where (G_I  > GMAX) G_I  = GMAX
    where (G_IB > GMAX) G_IB = GMAX
 
-   l_activ => local(activity, [0,0,nb,nb,nb,nb])
+   l_activ => local_section(activity, [0,0,nb,nb,nb,nb])
 
    where (V > Vth)
       l_activ = 1.0
