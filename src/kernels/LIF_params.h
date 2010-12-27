e981bdff503bec7d86b84b81fe395936064f6900
rasmussn <rasmussn@users.sourceforge.net>
2010-12-27 21:40:38 +0000

    Initial version.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3066 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/kernels/LIF_params.h
@@ -0,0 +1,28 @@
+#ifndef LIF_PARAMS_H_
+#define LIF_PARAMS_H_
+
+typedef struct LIF_params_ {
+   float Vrest;
+   float Vexc;
+   float Vinh;
+   float VinhB;
+
+   float tau;
+   float tauE;
+   float tauI;
+   float tauIB;
+
+   float VthRest;
+   float tauVth;
+   float deltaVth;
+
+   float noiseFreqE;
+   float noiseAmpE;
+   float noiseFreqI;
+   float noiseAmpI;
+   float noiseFreqIB;
+   float noiseAmpIB;
+} LIF_params;
+
+#endif /* LIF_PARAMS_H_ */
+
