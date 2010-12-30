baed7559791efde7d988e373042c778ba8818fe0
rasmussn <rasmussn@users.sourceforge.net>
2010-12-30 07:35:53 +0000

    Initial version.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3087 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/utils/cl_random.h
@@ -0,0 +1,26 @@
+/*
+ * cl_random.h
+ *
+ *  Created on: Dec 30, 2010
+ *      Author: Craig Rasmussen
+ */
+
+#ifndef CL_RANDOM_H_
+#define CL_RANDOM_H_
+
+#include "../arch/opencl/pv_opencl.h"
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+uint4 * cl_random_init(size_t count);
+uint4   cl_random_state(uint4 state);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CL_RANDOM_H_ */
