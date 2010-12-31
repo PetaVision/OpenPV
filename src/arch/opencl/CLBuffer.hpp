024b28d2fa1c3756a01672b784ab125dd932b171
rasmussn <rasmussn@users.sourceforge.net>
2010-12-31 20:01:34 +0000

    Added overloaded copyTo/From methods to allow for waiting on events.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3098 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/arch/opencl/CLBuffer.hpp
+++ src/arch/opencl/CLBuffer.hpp
@@ -24,11 +24,20 @@ public:
             cl_map_flags flags, size_t size, void * host_ptr);
    virtual ~CLBuffer();
    
-   int copyToDevice  (void * host_ptr);
-   int copyFromDevice(void * host_ptr);
+   int copyToDevice  (void * host_ptr, unsigned int nWait, cl_event * evList, cl_event * ev);
+   int copyFromDevice(void * host_ptr, unsigned int nWait, cl_event * evList, cl_event * ev);
 
-   int copyToDevice()            {return copyToDevice(h_ptr);}
-   int copyFromDevice()          {return copyFromDevice(h_ptr);}
+   int copyToDevice  (cl_event * ev)  {return copyToDevice  (h_ptr, 0, NULL, ev);}
+   int copyFromDevice(cl_event * ev)  {return copyFromDevice(h_ptr, 0, NULL, ev);}
+
+   int copyToDevice(unsigned int nWait, cl_event * evList, cl_event * ev)
+      {
+         return copyToDevice(h_ptr, nWait, evList, ev);
+      }
+   int copyFromDevice(unsigned int nWait, cl_event * evList, cl_event * ev)
+      {
+         return copyFromDevice(h_ptr, nWait, evList, ev);
+      }
 
    void * map(cl_map_flags flags);
    int  unmap(void * mapped_ptr);
