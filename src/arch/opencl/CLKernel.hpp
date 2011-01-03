daf8a1b7d9b55cc3f621867e3f6406c3e91b8cdb
rasmussn <rasmussn@users.sourceforge.net>
2011-01-03 04:44:58 +0000

    Added an overloaded createKernel method so that an options string could be given to the OpenCL compiler. Improved overloading of setKernelArg and run methods.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3108 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/arch/opencl/CLKernel.hpp
+++ src/arch/opencl/CLKernel.hpp
@@ -16,10 +16,12 @@ namespace PV {
 class CLKernel {
 public:
    CLKernel(cl_context context, cl_command_queue commands, cl_device_id device,
-            const char * filename, const char * name);
+            const char * filename, const char * name, const char * options);
 
-   int setKernelArg(int argid, int arg);
-   int setKernelArg(int argid, float arg);
+   int setKernelArg(unsigned int arg_index, size_t arg_size, const void * arg_value);
+
+   int setKernelArg(int argid, int arg)         {return setKernelArg(argid, sizeof(int), &arg);}
+   int setKernelArg(int argid, float arg)       {return setKernelArg(argid, sizeof(float), &arg);}
    int setKernelArg(int argid, CLBuffer * buf);
    int setLocalArg(int argid, size_t size);
 
@@ -27,6 +29,16 @@ public:
          unsigned int nWait, cl_event * waitList, cl_event * ev);
    int run(size_t gWorkSizeX, size_t gWorkSizeY, size_t lWorkSizeX, size_t lWorkSizeY,
            unsigned int nWait, cl_event * waitList, cl_event * ev);
+
+   int run(size_t global_work_size, size_t local_work_size)
+      {
+         return run(global_work_size, local_work_size, 0, NULL, NULL);
+      }
+
+   int run(size_t gWorkSizeX, size_t gWorkSizeY, size_t lWorkSizeX, size_t lWorkSizeY)
+      {
+         return run(gWorkSizeX, gWorkSizeY, lWorkSizeX, lWorkSizeY, 0, NULL, NULL);
+      }
 
    // execution time in microseconds
    int get_execution_time()  { return elapsed; }
