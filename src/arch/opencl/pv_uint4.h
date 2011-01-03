56c3114804e8074ca133d84d25f5347600e23992
rasmussn <rasmussn@users.sourceforge.net>
2011-01-03 04:37:25 +0000

    Initial version.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3104 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/arch/opencl/pv_uint4.h
@@ -0,0 +1,15 @@
+/*
+ * pv_uint.h
+ *
+ *  Created on: Jan 2, 2011
+ *      Author: Craig Rasmussen
+ */
+
+#ifndef PV_UINT4_H_
+#define PV_UINT4_H_
+
+typedef struct uint4_ {
+   unsigned int s0, s1, s2, s3;
+} uint4;
+
+#endif /* PV_UINT4_H_ */
