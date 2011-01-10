c418719cb8a5e763e4f4a697f045c6a0dbb19eeb
peteschultz <peteschultz@users.sourceforge.net>
2011-01-10 23:17:14 +0000

    A connection probe to dump an entire KernelConn
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@3253 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 49e117e6290bf7d69346e3cf1b02f7e49faa91e9

---
+++ pete/KernelConnDumpProbe.cpp
@@ -0,0 +1,108 @@
+/*
+ * KernelConnProbe.cpp
+ *
+ *  Created on: Jan 10, 2011
+ *      Author: pschultz
+ */
+
+#include "KernelConnDumpProbe.hpp"
+
+namespace PV {
+
+KernelConnDumpProbe::KernelConnDumpProbe() {
+    textNotBinaryFlag = true;
+    stdoutNotFileFlag = true;
+    filenameFormatString = NULL;
+
+    initializeProbe();
+}
+
+KernelConnDumpProbe::KernelConnDumpProbe(const char * filenameformatstr) {
+    textNotBinaryFlag = false;
+    stdoutNotFileFlag = false;
+    filenameFormatString = (char *) malloc( ( strlen(filenameformatstr)+1 )*sizeof(char) );
+    strcpy(filenameFormatString, filenameformatstr);
+
+    initializeProbe();
+}
+
+KernelConnDumpProbe::KernelConnDumpProbe(const char * filenameformatstr, bool textNotBinary) {
+    textNotBinaryFlag = textNotBinary;
+    stdoutNotFileFlag = false;
+    filenameFormatString = (char *) malloc( ( strlen(filenameformatstr)+1 )*sizeof(char) );
+    strcpy(filenameFormatString, filenameformatstr);
+
+    initializeProbe();
+}
+
+KernelConnDumpProbe::~KernelConnDumpProbe() {
+    if( !stdoutNotFileFlag )
+        free( filenameFormatString );
+}
+
+int KernelConnDumpProbe::outputState(float time, HyPerConn * c) {
+    FILE * fp;
+    if( stdoutNotFileFlag ) {
+        fp = stdout;
+    }
+    else {
+        fp = fopen("/dev/null","w");
+        if( !fp ) return EXIT_FAILURE;
+        int len = fprintf(fp,filenameFormatString,time);
+        fclose(fp);
+        if( len <= 0 ) return EXIT_FAILURE;
+        char * filename = (char *) malloc( (len+1)*sizeof(char) );
+        if( !filename ) return EXIT_FAILURE;
+        sprintf(filename, filenameFormatString, time);
+        fp = fopen(filename,"w");
+        if( !filename ){ free(filename); return EXIT_FAILURE; }
+    }
+	int nfp = c->fPatchSize();
+	int nxp = c->xPatchSize();
+	int nyp = c->yPatchSize();
+    int numPatches = c->numDataPatches(0);
+    if( textNotBinaryFlag ) {
+        fprintf(fp, "time = %f", time);
+        fprintf(fp, "nfp=%d, nxp=%d, nyp=%d, numPatches=%d\n",nfp,nxp,nyp,numPatches);
+        for( int k=0; k<numPatches; k++) {
+            fprintf(fp, "Patch %d\n", k);
+            PVPatch * patch = ((KernelConn *) c)->getKernelPatch(k);
+            for( int y=0; y<nyp; y++ ) {
+                for( int x=0; x<nxp; x++ ) {
+                    fprintf(fp, "    x=%d, y=%d\n",x,y);
+                    for( int f=0; f<nfp; f++ ) {
+                        int idx = kIndex(x,y,f,nxp,nyp,nfp);
+                        pvdata_t value = patch->data[idx];
+                        fprintf(fp,"        feature %d: value=%g\n",f,value);
+                    }
+                }
+            }
+        }
+    }
+    else {
+        fwrite(&time,sizeof(time), (size_t) 1, fp);
+        fwrite(&nfp,sizeof(nfp), (size_t) 1, fp);
+        fwrite(&nxp,sizeof(nxp), (size_t) 1, fp);
+        fwrite(&nyp,sizeof(nyp), (size_t) 1, fp);
+        fwrite(&numPatches,sizeof(numPatches), (size_t) 1, fp);
+        for( int k=0; k<numPatches; k++) {
+        	PVPatch * patch = ((KernelConn *) c)->getKernelPatch(k);
+            fwrite(patch->data, sizeof(pvdata_t), (size_t) (nfp*nxp*nyp), fp);
+        }
+    }
+    if( !stdoutNotFileFlag ) {
+        fclose(fp);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int KernelConnDumpProbe::validateFormatString(const char * formatstr) {
+    return EXIT_SUCCESS;
+}
+
+int KernelConnDumpProbe::initializeProbe() {
+    return validateFormatString(filenameFormatString);
+}
+
+}  // end namespace PV
---
+++ pete/KernelConnDumpProbe.hpp
@@ -0,0 +1,39 @@
+/*
+ * KernelConnProbe.hpp
+ *
+ *  Created on: Jan 10, 2011
+ *      Author: pschultz
+ */
+
+#ifndef KERNELCONNDUMPPROBE_HPP_
+#define KERNELCONNDUMPPROBE_HPP_
+
+#include "../PetaVision/src/io/ConnectionProbe.hpp"
+#include "../PetaVision/src/connections/HyPerConn.hpp"
+#include "../PetaVision/src/connections/KernelConn.hpp"
+
+namespace PV {
+
+class KernelConnDumpProbe {
+public:
+   KernelConnDumpProbe();
+   KernelConnDumpProbe(const char * filenameformatstr);
+   KernelConnDumpProbe(const char * filenameformatstr, bool textNotBinary);
+   virtual ~KernelConnDumpProbe();
+
+   virtual int outputState(float time, HyPerConn * c);
+   bool getTextNotBinary() {return textNotBinaryFlag;}
+
+protected:
+   bool textNotBinaryFlag;
+   bool stdoutNotFileFlag;
+   char * filenameFormatString;
+
+   int validateFormatString(const char * filenameformatstr);
+
+   int initializeProbe();
+}; // end class KernelConnProbe
+
+}  // end namespace PV
+
+#endif /* KERNELCONNDUMPPROBE_HPP_ */
