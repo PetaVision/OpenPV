a573f1d7ceeb84fab3690527dd05de89c5dbb23e
peteschultz <peteschultz@users.sourceforge.net>
2011-01-19 19:55:19 +0000

    Moving classes GenerativeConn and FeedbackConn from sandbox to repository
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3303 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/connections/GenerativeConn.hpp
@@ -0,0 +1,49 @@
+/*
+ * GenerativeConn.hpp
+ *
+ *  Created on: Oct 27, 2010
+ *      Author: pschultz
+ */
+
+#ifndef GENERATIVECONN_HPP_
+#define GENERATIVECONN_HPP_
+
+#include <assert.h>
+#include "KernelConn.hpp"
+#include "../columns/HyPerCol.hpp"
+
+namespace PV {
+
+class GenerativeConn : public KernelConn {
+public:
+    GenerativeConn();
+    GenerativeConn(const char * name, HyPerCol * hc,
+        HyPerLayer * pre, HyPerLayer * post);
+    GenerativeConn(const char * name, HyPerCol * hc,
+        HyPerLayer * pre, HyPerLayer * post, ChannelType channel);
+    GenerativeConn(const char * name, HyPerCol * hc,
+        HyPerLayer * pre, HyPerLayer * post, ChannelType channel,
+            const char * filename);
+
+    int initialize_base();
+    int initialize(const char * name, HyPerCol * hc,
+            HyPerLayer * pre, HyPerLayer * post, ChannelType channel,
+            const char * filename);
+    int initialize(const char * name, HyPerCol * hc,
+            HyPerLayer * pre, HyPerLayer * post, ChannelType channel);
+    inline float getWeightUpdatePeriod() {return weightUpdatePeriod;}
+    inline float getNextWeightUpdate() { return nextWeightUpdate;}
+    inline float getRelaxation() { return relaxation; }
+    virtual int updateState(float time, float dt);
+    int updateWeights(int axonID);
+
+
+protected:
+    float weightUpdatePeriod;
+    float nextWeightUpdate;
+    float relaxation;
+};
+
+}  // end of block for namespace PV
+
+#endif /* GENERATIVECONN_HPP_ */
