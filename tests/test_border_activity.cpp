3d4ee95cceaafaa96c3c67e8a8f5ec85f03d634d
peteschultz <peteschultz@users.sourceforge.net>
2011-01-20 20:26:54 +0000

    Replace LIF layer with ANNLayer so that test works as intended.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3310 7c0444ca-5550-0410-9008-faecdbe78eb5

--- tests/test_border_activity.cpp
+++ tests/test_border_activity.cpp
@@ -13,7 +13,7 @@
 #include "../src/layers/Image.hpp"
 #include "../src/layers/Retina.hpp"
 #include "../src/connections/HyPerConn.hpp"
-#include "../src/layers/LIF.hpp"
+#include "../src/layers/ANNLayer.hpp"
 #include "../src/io/PointProbe.hpp"
 
 #include <assert.h>
@@ -38,9 +38,8 @@ int main(int argc, char * argv[])
    HyPerCol * hc = new HyPerCol("column", argc, argv, "..");
 
    Image * image   = new Image("test_border_activity image", hc, image_file);
-//   Retina * retina = new Retina("test_border_activity retina", hc, image);
    Retina * retina = new Retina("test_border_activity retina", hc);
-   LIF    * l1     = new LIF("test_border_activity layer", hc);
+   ANNLayer * l1     = new ANNLayer("test_border_activity layer", hc);
 
    new HyPerConn("test_border_activity connection 1", hc, image, retina, CHANNEL_EXC);
    new HyPerConn("test_border_activity connection 2", hc, retina, l1, CHANNEL_EXC);
