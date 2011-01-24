917588eeb6e857a14b5d9234fcda2a698995aba6
rasmussn <rasmussn@users.sourceforge.net>
2011-01-24 18:42:43 +0000

    Added WARNING message about changes to globalIndexFromLocal being copied to test_kg.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3324 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/utils/conversions.h
+++ src/utils/conversions.h
@@ -382,6 +382,9 @@ static inline int kIndexRestricted(int k_ex, int nx, int ny, int nf, int nb)
  * @nf
  */
 // TODO - put back in nx,ny,... so that it will vectorize with vector of kl's
+//
+// WARNING - If changed, copy changes to tests/test_kg.c. test_kg tests
+//           globalIndexFromLocal but without using MPI.
 static inline int globalIndexFromLocal(int kl, PVLayerLoc loc)
 {
 #if PV_USE_MPI
