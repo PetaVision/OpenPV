adef9dabd5ce8484e8486a9d82fa244320cce1dd
rasmussn <rasmussn@users.sourceforge.net>
2011-01-27 22:15:51 +0000

    Initial WORKING version.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@3382 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 93e8787c270b5746dcb21242124ff933509ba7d2

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -7,13 +7,10 @@
 
 #include <stdlib.h>
 
+#include "StringImage.hpp"
 #include <src/columns/HyPerCol.hpp>
-#include <src/io/LinearActivityProbe.hpp>
-#include <src/io/PointProbe.hpp>
 #include <src/layers/LIF.hpp>
-#include <src/layers/Retina.hpp>
 #include <src/connections/HyPerConn.hpp>
-#include <src/connections/CocircConn.hpp>
 
 using namespace PV;
 
@@ -23,22 +20,11 @@ int main(int argc, char* argv[])
    HyPerCol * hc = new HyPerCol("column", argc, argv);
 
    // create the layers
-   HyPerLayer * retina = new Retina("Retina", hc);
-   HyPerLayer * l1     = new LIF("L1", hc);
+   HyPerLayer * strings = new StringImage("Strings", hc);
+   HyPerLayer * complex = new LIF("Complex", hc);
 
    // connect the layers
-   new HyPerConn("Retina to L1", hc, retina, l1, CHANNEL_EXC);
-   new CocircConn("L1 to L1",    hc, l1,     l1, CHANNEL_EXC);
-
-   int locX = 39;
-   int locY = 31; // 53;
-   int locF = 0;
-
-   // add probes
-   LayerProbe * probe   = new LinearActivityProbe(hc, DimX, locY, locF);
-   LayerProbe * ptprobe = new PointProbe(locX, locY, locF, "L1:");
-
-   l1->insertProbe(probe);
+   new HyPerConn("Strings to Complex", hc, strings, complex, CHANNEL_EXC);
 
    // run the simulation
    hc->initFinish();
@@ -46,8 +32,6 @@ int main(int argc, char* argv[])
 
    /* clean up (HyPerCol owns layers and connections, don't delete them) */
    delete hc;
-   delete probe;
-   delete ptprobe;
 
    return 0;
 }
