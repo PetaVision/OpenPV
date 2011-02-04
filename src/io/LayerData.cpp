eaf9546809aed9eb4a5cd365e64b7e5d7fa3db05
marian_anghel <marian_anghel@users.sourceforge.net>
2011-02-04 22:55:30 +0000

    Initial version of a layer probe that writes a layer state variable to an output file.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3402 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/io/LayerData.cpp
@@ -0,0 +1,55 @@
+/*
+ * LayerData.cpp
+ *
+ *  Created on: Jan 31, 2011
+ *      Author: manghel
+ */
+
+#include "LayerData.hpp"
+
+namespace PV {
+
+//
+/*
+ * The default base constructor assign NULL to the file pointer
+ */
+LayerData::LayerData(const char * filename, HyPerCol * hc, HyPerLayer * l, pvdata_t * data, bool append)
+   : LayerProbe()
+{
+   this->data = data;
+   this->parent = hc;
+   this->append = append;
+   Communicator * comm = parent->icCommunicator();
+
+   this->fp = pvp_open_write_file(filename, comm, this->append);
+   if(!append){
+      bool contiguous = false;
+      bool extended   = false;
+      int status = 0;
+      PVLayerLoc * loc = & ((l->getCLayer())->loc);
+      float time = hc->simulationTime();
+      int numNeurons = l->getCLayer()->numNeurons;
+
+      status |= pvp_write_header(this->fp, comm, time, loc, PVP_FILE_TYPE,
+            PV_FLOAT_TYPE, sizeof(int), extended, contiguous, NUM_BIN_PARAMS, numNeurons);
+      assert(status == 0);
+   }
+
+}
+
+/**
+ * @time
+ * @l
+ */
+int LayerData::outputState(float time, HyPerLayer * l)
+{
+   Communicator * comm = parent->icCommunicator();
+   PVLayerLoc * loc = & ((l->getCLayer())->loc);
+   bool contiguous = false;
+   bool extended   = false;
+   int status = write(fp, comm, time, data, loc, PV_FLOAT_TYPE, extended, contiguous);
+   return status;
+}
+
+
+}
---
+++ src/io/LayerData.hpp
@@ -0,0 +1,38 @@
+/*
+ * LayerData.hpp
+ *
+ *  Created on: Jan 31, 2011
+ *      Author: manghel
+ */
+
+#ifndef LAYERDATA_HPP_
+#define LAYERDATA_HPP_
+
+#include "LayerProbe.hpp"
+#include "../columns/HyPerCol.hpp"
+#include "../include/pv_types.h"
+#include "../io/fileio.hpp"
+#include <assert.h>
+
+enum DataType{
+   VTH = 0;
+
+};
+namespace PV {
+
+class LayerData: public LayerProbe {
+public:
+   LayerData(const char * filename, HyPerCol * hc, HyPerLayer * l, pvdata_t * data, bool append);
+
+   virtual int outputState(float time, HyPerLayer * l) = 0;
+
+protected:
+   HyPerCol * parent;
+   const pvdata_t * data;
+   bool  append;
+
+};
+
+}
+
+#endif /* LAYERDATA_HPP_ */
