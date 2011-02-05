4ecbb3fcb4766e4ae09c8f35589aadbd232b8d8e
rasmussn <rasmussn@users.sourceforge.net>
2011-02-05 05:34:41 +0000

    Changed to inherit from PointProbe.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3405 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/io/PointLIFProbe.hpp
+++ src/io/PointLIFProbe.hpp
@@ -8,29 +8,18 @@
 #ifndef POINTLIFPROBE_HPP_
 #define POINTLIFPROBE_HPP_
 
-#include "LayerProbe.hpp"
+#include "PointProbe.hpp"
 
 namespace PV {
 
-class PointLIFProbe: public PV::LayerProbe {
+class PointLIFProbe: public PointProbe {
 public:
    PointLIFProbe(const char * filename, int xLoc, int yLoc, int fLoc, const char * msg);
    PointLIFProbe(int xLoc, int yLoc, int fLoc, const char * msg);
-   virtual ~PointLIFProbe();
 
    virtual int outputState(float time, HyPerLayer * l);
-
-   void setSparseOutput(bool flag) {sparseOutput = flag;}
-
-protected:
-   int xLoc;
-   int yLoc;
-   int fLoc;
-   char * msg;
-
-   bool sparseOutput;
 };
 
 }
 
-#endif /* POINTPROBE_HPP_ */
+#endif /* POINTLIFPROBE_HPP_ */
