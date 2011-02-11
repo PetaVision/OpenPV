126d899dff003baa559f168b66ac3acdf77130e7
rasmussn <rasmussn@users.sourceforge.net>
2011-02-11 17:44:56 +0000

    Added probes and changed naming convention for layers.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@3426 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 014a2b41a75e2b83e18e5ea78329240b26065972

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -11,6 +11,11 @@
 #include <src/columns/HyPerCol.hpp>
 #include <src/layers/LIF.hpp>
 #include <src/connections/HyPerConn.hpp>
+#include <src/io/PointProbe.hpp>
+#include <src/io/PointLIFProbe.hpp>
+#include <src/io/PointJunkProbe.hpp>
+#include <src/io/StatsProbe.hpp>
+#include <src/io/PostConnProbe.hpp>
 
 using namespace PV;
 
@@ -20,11 +25,25 @@ int main(int argc, char* argv[])
    HyPerCol * hc = new HyPerCol("column", argc, argv);
 
    // create the layers
-   HyPerLayer * strings = new StringImage("Strings", hc);
-   HyPerLayer * complex = new LIF("Complex", hc);
+   HyPerLayer * S1  = new StringImage("S1", hc);
+   HyPerLayer * C1  = new LIF("C1", hc);
+   HyPerLayer * C1i = new LIF("C1i", hc);
 
    // connect the layers
-   new HyPerConn("Strings to Complex", hc, strings, complex, CHANNEL_EXC);
+   HyPerConn * S1_to_C1  = new HyPerConn("S1 to C1",  hc, S1, C1,  CHANNEL_EXC);
+   HyPerConn * C1_to_C1i = new HyPerConn("C1 to C1i", hc, C1, C1i, CHANNEL_EXC);
+   HyPerConn * C1i_to_C1 = new HyPerConn("C1i to C1", hc, C1i, C1, CHANNEL_EXC);
+
+   // probes
+   //
+   const PVLayerLoc * loc = C1->getLayerLoc();
+   int x = (loc->nx + 2*loc->nb) / 2;
+   LayerProbe * lifProbe = new PointLIFProbe(x, 0, 0, "complex");
+   PostConnProbe * cProbe = new PostConnProbe(x, 0, 0);
+   cProbe->setOutputIndices(false);
+
+   //C1->insertProbe(lifProbe);
+   //S1_to_C1->insertProbe(cProbe);
 
    // run the simulation
    hc->initFinish();
