021d5be2e7db042f6522209f649f5c4ee2307c9e
rasmussn <rasmussn@users.sourceforge.net>
2011-02-14 16:52:38 +0000

    Still testing.  Added use of PointProbe for retina.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@3436 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: f7342b338ad9cdf8ba598678dd5a50c2147b2476

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -13,7 +13,6 @@
 #include <src/connections/HyPerConn.hpp>
 #include <src/io/PointProbe.hpp>
 #include <src/io/PointLIFProbe.hpp>
-#include <src/io/PointJunkProbe.hpp>
 #include <src/io/StatsProbe.hpp>
 #include <src/io/PostConnProbe.hpp>
 
@@ -31,17 +30,20 @@ int main(int argc, char* argv[])
 
    // connect the layers
    HyPerConn * S1_to_C1  = new HyPerConn("S1 to C1",  hc, S1, C1,  CHANNEL_EXC);
-   HyPerConn * C1_to_C1i = new HyPerConn("C1 to C1i", hc, C1, C1i, CHANNEL_EXC);
-   HyPerConn * C1i_to_C1 = new HyPerConn("C1i to C1", hc, C1i, C1, CHANNEL_EXC);
+//   HyPerConn * C1_to_C1i = new HyPerConn("C1 to C1i", hc, C1, C1i, CHANNEL_EXC);
+//   HyPerConn * C1i_to_C1 = new HyPerConn("C1i to C1", hc, C1i, C1, CHANNEL_INH);
 
    // probes
    //
    const PVLayerLoc * loc = C1->getLayerLoc();
    int x = (loc->nx + 2*loc->nb) / 2;
+   x = 0;
+   LayerProbe * lProbe   = new PointProbe(x, 0, 0, "simple");
    LayerProbe * lifProbe = new PointLIFProbe(x, 0, 0, "complex");
    PostConnProbe * cProbe = new PostConnProbe(x, 0, 0);
    cProbe->setOutputIndices(false);
 
+   S1->insertProbe(lProbe);
    //C1->insertProbe(lifProbe);
    //S1_to_C1->insertProbe(cProbe);
 
