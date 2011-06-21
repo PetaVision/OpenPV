610a541c11d2bafdfd8e84c594a651a8ffe9d89f
garkenyon <garkenyon@users.sourceforge.net>
2011-06-21 20:57:56 +0000

    new generic main
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@3755 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 08d81b95ab33827059876872ee3a748aec7311cb

--- geisler/pv.cpp
+++ geisler/pv.cpp
@@ -1,53 +1,10 @@
 /*
- * pv_ca.cpp
+ * pv.cpp
  *
- *  Created on: Apr 2, 2009
- *      Author: rasmussn
  */
 
-#include <stdlib.h>
+#include "../PetaVision2.1/src/columns/buildandrun.hpp"
 
-#include <src/columns/HyPerCol.hpp>
-#include <src/io/LinearActivityProbe.hpp>
-#include <src/io/PointProbe.hpp>
-#include <src/layers/Retina.hpp>
-#include <src/layers/LIF.hpp>
-#include <src/connections/HyPerConn.hpp>
-#include <src/connections/CocircConn.hpp>
-
-using namespace PV;
-
-int main_pv(int argc, char* argv[])
-{
-   // create the managing hypercolumn
-   HyPerCol * hc = new HyPerCol("column", argc, argv);
-
-   // create the layers
-   HyPerLayer * retina = new Retina("Retina", hc);
-   HyPerLayer * l1     = new LIF("L1", hc);
-
-   // connect the layers
-   new HyPerConn("Retina to L1", hc, retina, l1, CHANNEL_EXC);
-   new CocircConn("L1 to L1",    hc, l1,     l1, CHANNEL_EXC);
-
-   int locX = 39;
-   int locY = 31; // 53;
-   int locF = 0;
-
-   // add probes
-   LayerProbe * probe   = new LinearActivityProbe(hc, PV::DimX, locY, locF);
-   LayerProbe * ptprobe = new PointProbe(locX, locY, locF, "L1:");
-
-   l1->insertProbe(probe);
-
-   // run the simulation
-   hc->initFinish();
-   hc->run();
-
-   /* clean up (HyPerCol owns layers and connections, don't delete them) */
-   delete hc;
-   delete probe;
-   delete ptprobe;
-
-   return 0;
+int main(int argc, char * argv[]) {
+    return buildandrun(argc, argv)==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
 }
