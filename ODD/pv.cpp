e4ea1844576dcbcf39c83e46492a3ada65e2b5ad
garkenyon <garkenyon@users.sourceforge.net>
2011-07-13 18:47:30 +0000

    changed default reference to PetaVision (not PetaVision2.1)
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@3795 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 7e3e7f823629391ef340da938550b778a76988bd

--- ODD/pv.cpp
+++ ODD/pv.cpp
@@ -3,7 +3,7 @@
  *
  */
 
-#include "../PetaVision2.1/src/columns/buildandrun.hpp"
+#include "../PetaVision/src/columns/buildandrun.hpp"
 
 int main(int argc, char * argv[]) {
     return buildandrun(argc, argv)==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
