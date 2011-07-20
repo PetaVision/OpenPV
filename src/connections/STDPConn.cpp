96c112ffef2bdbbdf040ce4303c945b022cb1f61
rasmussn <rasmussn@users.sourceforge.net>
2011-07-20 02:48:14 +0000

    Modifications needed to implement STDPConn.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3803 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/connections/STDPConn.cpp
+++ src/connections/STDPConn.cpp
@@ -7,19 +7,21 @@
 
 #include "STDPConn.hpp"
 #include "../layers/LIF.hpp"
+#include "../io/io.h"
 #include <assert.h>
 
 namespace PV {
 
 STDPConn::STDPConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
-                   ChannelType channel) : HyPerConn(name, hc, pre, post, channel)
+                   ChannelType channel, const char * filename) : HyPerConn()
 {
-   initialize();
+   setParams(hc->parameters());
+   initialize(name, hc, pre, post, channel, filename);
 }
 
 STDPConn::~STDPConn()
 {
-   // TODO Auto-generated destructor stub
+   deleteWeights();
 }
 
 /*
@@ -27,7 +29,9 @@ STDPConn::~STDPConn()
  * to a pointer to a derived class (LIF). This way I do not need to define a virtual
  * function getWmax() in HyPerLayer which only returns a NULL pointer in the base class.
  */
-int STDPConn::initialize()
+int STDPConn::initialize(const char * name, HyPerCol * hc,
+                         HyPerLayer * pre, HyPerLayer * post,
+                         ChannelType channel, const char * filename)
 {
    // STDP parameters for modifying weights
    this->pIncr = NULL;
@@ -37,15 +41,16 @@ int STDPConn::initialize()
    this->tauLTP = 20;
    this->tauLTD = 20;
    this->dWMax = 0.1;
+   this->stdpFlag = true;
    this->localWmaxFlag = false;
 
-   int arbor = 0;
-   int numPatches = numWeightPatches(arbor);
-
-   pIncr = createWeights(NULL, numPatches, nxp, nyp, nfp);
-   assert(pIncr != NULL);
-   pDecr = pvcube_new(&post->getCLayer()->loc, post->getNumExtended());
-   assert(pDecr != NULL);
+   const int arbor = 0;
+   if (stdpFlag) {
+      pIncr = createWeights(NULL, numWeightPatches(arbor), nxp, nyp, nfp);
+      assert(pIncr != NULL);
+      pDecr = pvcube_new(&post->getCLayer()->loc, post->getNumExtended());
+      assert(pDecr != NULL);
+   }
 
    if (localWmaxFlag){
       LIF * LIF_layer = dynamic_cast<LIF *>(post);
@@ -56,6 +61,21 @@ int STDPConn::initialize()
       Wmax = NULL;
    }
 
+   // This needs to be called after pIncr is created
+   return HyPerConn::initialize(name, hc, pre, post, channel, filename);
+}
+
+int STDPConn::deleteWeights()
+{
+   if (stdpFlag) {
+      const int arbor = 0;
+      const int numPatches = numWeightPatches(arbor);
+      for (int k = 0; k < numPatches; k++) {
+         pvpatch_inplace_delete(pIncr[k]);
+      }
+      free(pIncr);
+      pvcube_delete(pDecr);
+   }
    return 0;
 }
 
@@ -72,8 +92,323 @@ int STDPConn::initializeThreadKernels()
 PVPatch * STDPConn::getPlasticityIncrement(int k, int arbor)
 {
    // a separate arbor/patch of plasticity for every neuron
-   return pIncr[k];
+   if (stdpFlag) {
+      return pIncr[k];
+   }
+   return NULL;
 }
+
+PVLayerCube * STDPConn::getPlasticityDecrement()
+{
+   return pDecr;
+}
+
+// set member variables specified by user
+int STDPConn::setParams(PVParams * filep)
+{
+   const char * name = getName();
+
+   stdpFlag = (bool) filep->value(name, "stdpFlag", (float) stdpFlag);
+   if (stdpFlag) {
+      ampLTP = filep->value(name, "ampLTP", ampLTP);
+      ampLTD = filep->value(name, "ampLTD", ampLTD);
+      tauLTP = filep->value(name, "tauLTP", tauLTP);
+      tauLTD = filep->value(name, "tauLTD", tauLTD);
+
+      dWMax = filep->value(name, "dWMax", dWMax);
+
+      // set params for rate dependent Wmax
+      localWmaxFlag = (bool) filep->value(name, "localWmaxFlag", (float) localWmaxFlag);
+   }
+
+   return 0;
+}
+
+int STDPConn::updateState(float time, float dt)
+{
+   update_timer->start();
+
+   if (stdpFlag) {
+      const float fac = ampLTD;
+      const float decay = expf(-dt / tauLTD);
+
+      //
+      // both pDecr and activity are extended regions (plus margins)
+      // to make processing them together simpler
+
+      const int nk = pDecr->numItems;
+      const float * a = post->getLayerData();
+      float * m = pDecr->data; // decrement (minus) variable
+
+      for (int k = 0; k < nk; k++) {
+         m[k] = decay * m[k] - fac * a[k];
+      }
+
+      const int axonId = 0;       // assume only one for now
+      updateWeights(axonId);
+   }
+   update_timer->stop();
+
+   const int axonId = 0;       // assume only one for now
+   return updateWeights(axonId);
+}
+
+/**
+ *  M (m or pDecr->data) is an extended post-layer variable
+ *
+ */
+int STDPConn::updateWeights(int axonId)
+{
+   // Steps:
+   // 1. Update pDecr (assume already done as it should only be done once)
+   // 2. update Psij (pIncr) for each synapse
+   // 3. update wij
+
+   const float dt = parent->getDeltaTime();
+   const float decayLTP = expf(-dt / tauLTP);
+
+   const int numExtended = pre->getNumExtended();
+   assert(numExtended == numWeightPatches(axonId));
+
+   const pvdata_t * preLayerData = pre->getLayerData();
+
+   // this stride is in extended space for post-synaptic activity and
+   // STDP decrement variable
+   const int postStrideY = post->getLayerLoc()->nf
+                         * (post->getLayerLoc()->nx + 2 * post->getLayerLoc()->nb);
+
+   for (int kPre = 0; kPre < numExtended; kPre++) {
+      PVAxonalArbor * arbor = axonalArbor(kPre, axonId);
+
+      const float preActivity = preLayerData[kPre];
+
+      PVPatch * pIncr   = arbor->plasticIncr;
+      PVPatch * w       = arbor->weights;
+      size_t postOffset = arbor->offset;
+
+      const float * postActivity = &post->getLayerData()[postOffset];
+      const float * M = &pDecr->data[postOffset];  // STDP decrement variable
+      float * P = pIncr->data;                     // STDP increment variable
+      float * W = w->data;
+
+      int nk  = pIncr->nf * pIncr->nx; // one line in x at a time
+      int ny  = pIncr->ny;
+      int sy  = pIncr->sy;
+
+      // TODO - unroll
+
+      // update Psij (pIncr variable)
+      // we are processing patches, one line in y at a time
+      for (int y = 0; y < ny; y++) {
+         pvpatch_update_plasticity_incr(nk, P + y * sy, preActivity, decayLTP, ampLTP);
+      }
+
+      if (localWmaxFlag) {
+         // update weights with local post-synaptic Wmax values
+         // Wmax lives in the restricted space - it is controlled
+         // by average rate in the post synaptic layer
+         float * Wmax_pointer = &Wmax[postOffset];
+         for (int y = 0; y < ny; y++) {
+            // TODO
+            pvpatch_update_weights_localWMax(nk,W,M,P,preActivity,postActivity,dWMax,wMin,Wmax_pointer);
+            //
+            // advance pointers in y
+            W += sy;
+            P += sy;
+
+            //
+            // postActivity, M are extended post-layer variables
+            //
+            postActivity += postStrideY;
+            M            += postStrideY;
+            Wmax_pointer += postStrideY;
+         }
+      } else {
+         // update weights
+         for (int y = 0; y < ny; y++) {
+            pvpatch_update_weights(nk, W, M, P, preActivity, postActivity, dWMax, wMin, wMax);
+            //
+            // advance pointers in y
+            W += sy;
+            P += sy;
+            //
+            // postActivity and M are extended layer
+            postActivity += postStrideY;
+            M += postStrideY;
+         }
+
+      }
+   }
+
+   return 0;
+}
+
+int STDPConn::outputState(float time, bool last)
+{
+   int status = HyPerConn::outputState(time, last);
+   if (status != PV_SUCCESS) return status;
+
+   if (stdpFlag != true) return status;
+
+   if (last) {
+      convertPreSynapticWeights(time);
+      status = writePostSynapticWeights(time, last);
+      assert(status == 0);
+   }
+   else if ( (time >= writeTime) && (writeStep >= 0) ) {
+      convertPreSynapticWeights(time);
+      status = writePostSynapticWeights(time, last);
+      assert(status == 0);
+   }
+
+   return status;
+}
+
+float STDPConn::maxWeight()
+{
+   float maxVal = 0.0;
+
+   if (localWmaxFlag) {
+      const int numExtended = post->getNumExtended();
+      for (int kPost = 0; kPost < numExtended; kPost++){
+         if(Wmax[kPost] > maxVal){
+            maxVal = Wmax[kPost];
+         }
+      }
+   } else {
+      maxVal = wMax;
+   }
+
+   return maxVal;
+}
+
+int STDPConn::writeTextWeightsExtra(FILE * fd, int k)
+{
+   if (stdpFlag) {
+      pv_text_write_patch(fd, pIncr[k]); // write the Ps variable
+   }
+   return 0;
+}
+
+int STDPConn::adjustAxonalPatches(PVAxonalArbor * arbor, int nxPatch, int nyPatch, int dx, int dy)
+{
+   int status = STDPConn::adjustAxonalPatches(arbor, nxPatch, nyPatch, dx, dy);
+
+   if (stdpFlag && status == PV_SUCCESS) {
+      pvpatch_adjust(arbor->plasticIncr, nxPatch, nyPatch, dx, dy);
+   }
+
+   return status;
+}
+
+#ifdef NOTYET
+void STDP_update_state_post(
+      const float dt,
+
+      const int nx,
+      const int ny,
+      const int nf,
+      const int nb,
+
+      const int nxp,
+      const int nyp,
+
+      STDP_params * params,
+
+      float * M,
+      float * Wmax,
+      float * Apost,
+      float * Rpost)
+{
+
+   int kex;
+#ifndef PV_USE_OPENCL
+   for (kex = 0; kex < nx*ny*nf; kex++) {
+#else
+   kex = get_global_id(0);
+#endif
+
+   //
+   // kernel (nonheader part) begins here
+   //
+
+   // update the decrement variable
+   //
+   M[kex] = decay * M[kex] - fac * Apost[kex];
+
+#ifndef PV_USE_OPENCL
+   }
+#endif
+
+}
+
+
+/**
+ * Loop over presynaptic extended layer.  Calculate pIncr, and weights.
+ */
+void STDP_update_state_pre(
+      const float time,
+      const float dt,
+
+      const int nx,
+      const int ny,
+      const int nf,
+      const int nb,
+
+      const int nxp,
+      const int nyp,
+
+      STDP_params * params,
+
+      float * M,
+      float * P,
+      float * W,
+      float * Wmax,
+      float * Apre,
+      float * Apost)
+{
+
+   int kex;
+
+   float m[NXP*NYP], aPost[NXP*NYP], wMax[NXP*NYP];
+
+#ifndef PV_USE_OPENCL
+   for (kex = 0; kex < nx*ny*nf; kex++) {
+#else
+   kex = get_global_id(0);
+#endif
+
+   //
+   // kernel (nonheader part) begins here
+   //
+
+   // update the increment variable
+   //
+   float aPre = Apre[kex];
+   float * p = P[kex*stride];
+
+   // copy into local variable
+   //
+
+   copy(m, M);
+   copy(aPost, Apost);
+   copy(wMax, Wmax);
+
+   // update the weights
+   //
+   for (int kp = 0; kp < nxp*nyp; kp++) {
+      p[kp] = decay * p[kp] + ltpAmp * aPre;
+      w[kp] += dWMax * (aPre * m[kp] + aPost[kp] * p[kp]);
+      w[kp] = w[kp] < wMin ? wMin : w[kp];
+      w[kp] = w[kp] > wMax ? wMax : w[kp];
+   }
+#ifndef PV_USE_OPENCL
+   }
+#endif
+
+}
+#endif // NOTYET - TODO
+
 
 } // End of namespace PV
 
