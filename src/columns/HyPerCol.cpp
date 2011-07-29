4bd382143ac68932af8e111cc3993180714d3df5
peteschultz <peteschultz@users.sourceforge.net>
2011-07-29 23:13:41 +0000

    Marked obsolete a HyPerCol constructor specifying a relative path to the working directory.  Use -w option on the command line instead.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3879 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/columns/HyPerCol.cpp
+++ src/columns/HyPerCol.cpp
@@ -31,6 +31,7 @@ HyPerCol::HyPerCol(const char * name, int argc, char * argv[])
    initialize(name, argc, argv);
 }
 
+#ifdef OBSOLETE // Marked obsolete Jul 29, 2011.  Use -w option in argc and argv instead of pv_path.
 HyPerCol::HyPerCol(const char * name, int argc, char * argv[], const char * pv_path)
          : warmStart(false), isInitialized(false)
 {
@@ -38,6 +39,7 @@ HyPerCol::HyPerCol(const char * name, int argc, char * argv[], const char * pv_p
    assert(strlen(path) + strlen(pv_path) < PV_PATH_MAX);
    sprintf(path, "%s/%s", path, pv_path);
 }
+#endif // OBSOLETE
 
 HyPerCol::~HyPerCol()
 {
