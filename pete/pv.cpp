aa9b0e34b0a32c896d2cf11459715c306f29feaf
peteschultz <peteschultz@users.sourceforge.net>
2011-07-29 23:08:05 +0000

    addcustom scans for ChannelProbe groups in params file instead of looking specifically for the layer "Retina"
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@3876 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: efc6989ee08ac865a3dab6475362c96cbb653dc4

--- pete/pv.cpp
+++ pete/pv.cpp
@@ -15,9 +15,9 @@ int addcustom(HyPerCol * hc, int argc, char * argv[]);
 
 int main(int argc, char * argv[]) {
 
-	int status;
+    int status;
 #ifdef MAIN_USES_ADDCUSTOM
-	status = buildandrun(argc, argv, &addcustom);
+    status = buildandrun(argc, argv, &addcustom);
 #else
     status = buildandrun(argc, argv);
 #endif // MAIN_USES_ADDCUSTOM
@@ -26,13 +26,35 @@ int main(int argc, char * argv[]) {
 
 #ifdef MAIN_USES_ADDCUSTOM
 int addcustom(HyPerCol * hc, int argc, char * argv[]) {
-    HyPerLayer * retina = hc->getLayer(1);
-
-    assert(!strcmp(retina->getName(), "Retina"));
-
-    ChannelProbe * retina_exc_channelprobe = new ChannelProbe("Retina_Exc_ChannelProbe.txt", hc, CHANNEL_EXC);
-    retina->insertProbe(retina_exc_channelprobe);
-
+    int status;
+    PVParams * params = hc->parameters();
+    int numGroups = params->numberOfGroups();
+    for(int n=0; n<numGroups; n++) {
+        const char * kw = params->groupKeywordFromIndex(n);
+        const char * name = params->groupNameFromIndex(n);
+        HyPerLayer * targetlayer;
+        const char * message;
+        const char * filename;
+        if( !strcmp( kw, "ChannelProbe") ) {
+            status = getLayerFunctionProbeParameters(name, kw, hc, &targetlayer, &message, &filename);
+            if(status != PV_SUCCESS) {
+                fprintf(stderr, "Skipping params group \"%s\"\n", name);
+                continue;
+            }
+            if(filename == NULL) {
+                fprintf(stderr, "ChannelProbe \"%s\": parameter filename must be set.  Skipping group.\n", name);
+                continue;
+            }
+            ChannelType channelCode;
+            int channelNo = params->value(name, "channelCode", -1);
+            if( decodeChannel( channelNo, &channelCode ) != PV_SUCCESS) {
+                fprintf(stderr, "%s \"%s\": parameter channelCode must be set.\n", kw, name);
+                continue;
+            }
+            ChannelProbe * addedProbe = new ChannelProbe(filename, hc, channelCode);
+            checknewobject((void *) addedProbe, kw, name);
+        }
+    }
     return PV_SUCCESS;
 }
 #endif // MAIN_USES_ADDCUSTOM
