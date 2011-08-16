af4fee1f18275d9d6edc77b0fa60ed02ef99cf27
garkenyon <garkenyon@users.sourceforge.net>
2011-08-16 23:45:54 +0000

    more routines for processing imageNet images.  includes procedure for calling grabcut openCV segmentation algorithm
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@3960 7c0444ca-5550-0410-9008-faecdbe78eb5

--- mlab/imageNet/imageNetResize2.m
+++ mlab/imageNet/imageNetResize2.m
@@ -13,15 +13,19 @@ function [tot_images ...
 	  std_height_landscape ...
 	  std_width_landscape ...
 	  tot_time] = ...
-      imageNetResize(imageNet_path, image_resize, object_list, grabcut_flag, num_procs)
+      imageNetResize2(imageNet_path, ...
+		      image_resize, ...
+		      object_list, ...
+		      image_type, ...
+		      grabcut_flag, ...
+		      num_procs, ...
+		      BB_only_flag)
 
   %% reformat directory of image net images to be of uniform size with
   %% mirror BCs for padding if necessary
   %% also produces masked images from bounding
   %% box data
-
-  begin_time = time();
-  more off
+  %% uses parcellfun to execute in parallel
 
   if nargin < 1 || ~exist(imageNet_path) || isempty(imageNet_path)
     imageNet_path = "~/Pictures/imageNet/";
@@ -30,37 +34,74 @@ function [tot_images ...
     image_resize = [256 360];
   endif
   if nargin < 3 || ~exist(object_list) || isempty(object_list)
-    object_list{1} = "cat";  %% could be a list?
+    object_list{1} = "cat"; 
   endif
   if nargin < 4 || ~exist(image_type) || isempty(image_type)
     image_type = ".png";  %% 
   endif
   if nargin < 5 || ~exist(grabcut_flag) || isempty(grabcut_flag)
-    grabcut_flag = 1;  %% 
+    grabcut_flag = 1;  %% uses openCV segmentation algorithm to focus bounding boxes
+  endif
+  if nargin < 6 || ~exist(num_procs) || isempty(num_procs)
+    num_procs = 4;  %% number of processors to use
+  endif
+  if nargin < 7 || ~exist(BB_only_flag) || isempty(BB_only_flag)
+    BB_only_flag = 1;  %% (-)1 -> only process images with(without) bounding boxes
   endif
 
   global NUM_FIGS
   NUM_FIGS = 0;
   
+  global VERBOSE_FLAG
+  if ~exist("VERBOSE_FLAG") || isempty(VERBOSE_FLAG)
+    VERBOSE_FLAG = 1;
+  endif
+
+  global UNAVAILABLE_INFO
+  UNAVAILABLE_INFO = [];
+
+  global STANDARD_DIR
+  global ANNOTATION_DIR
+  global MASKS_DIR
+  global TMP_DIR
+  global TMP_MASK_DIR
+  global IMAGE_RESIZE
+  global IMAGE_TYPE
+  global GRABCUT_FLAG
+  IMAGE_RESIZE = image_resize;
+  IMAGE_TYPE = image_type;
+  GRABCUT_FLAG = grabcut_flag;
+
+  begin_time = time();
+  more off
+
+  TMP_DIR = [imageNet_path, "tmp", filesep];
+  mkdir(TMP_DIR);
+
+  TMP_MASK_DIR = [imageNet_path, "tmpMask", filesep];
+  mkdir(TMP_MASK_DIR);
+
   matFiles_dir = ...
-      [imageNet_path, "matFiles", "/"];
+      [imageNet_path, "matFiles", filesep];
   mkdir(matFiles_dir); 
 
-  unavailable_info = [];
+  disp("");
+  disp(["num_procs = ", num2str(num_procs)]);
+
   num_objects = length(object_list);
   for i_object = 1 : num_objects
     object_name = object_list{i_object};
     disp(["object_name = ", object_name]);
     object_dir = ...
-	[ imageNet_path, "extract/", object_name, "/" ];
+	[ imageNet_path, "extract", filesep, object_name, filesep];
     if ~exist(object_dir, "dir")
       error(["object_dir does not exist: ", object_dir]);
     endif
     standard_parent_dir = ...
-	[ imageNet_path, "standard/", object_name, "/" ];
+	[ imageNet_path, "standard", filesep, object_name, filesep ];
     mkdir(standard_parent_dir);
     masks_parent_dir = ...
-	[ imageNet_path, "masks/", object_name, "/" ];
+	[ imageNet_path, "masks", filesep, object_name, filesep ];
     mkdir(masks_parent_dir);
     tot_images = zeros(1,num_objects);
     tot_masks = zeros(1,num_objects);
@@ -78,141 +119,112 @@ function [tot_images ...
     std_width_landscape = zeros(1,num_objects);
     
     %% imageNet sub directories are drawn from wordNet categories
-    subdir_struct = dir([object_dir,"*"]);
-    num_subdirs = length(subdir_struct);
+    subdir_pathnames = glob([object_dir, "*"]);
+    num_subdirs = length(subdir_pathnames);
     disp(["num_subdirs = ", num2str(num_subdirs)]);
-    for i_subdir = 12 : num_subdirs %% : fix(num_subdirs/2) %% 
-      if 0 %%~strcmp(subdir_struct(i_subdir).name(1),"n")
-	continue;
-      endif
+    subdir_foldernames = cellfun(@strFolderFromPath, subdir_pathnames, "UniformOutput", false);
+    for i_subdir = 1 : num_subdirs %% 
       disp(["i_subdir = ", num2str(i_subdir)]);
+      disp(["subdir_pathname = ", subdir_pathnames{i_subdir}]);
       images_path = ...
 	  [object_dir, ...
-	   subdir_struct(i_subdir).name, "/"];
+	   subdir_foldernames{i_subdir}, filesep];
       annotation_path = ...
 	  [object_dir, ...
-	   subdir_struct(i_subdir).name, "/", ...
-	   "Annotation", "/"];
+	   subdir_foldernames{i_subdir}, filesep, ...
+	   "Annotation", filesep];
       annotation_flag = exist(annotation_path, "dir");
+      if ~annotation_flag && BB_only_flag == 1
+	continue;
+      elseif annotation_flag && BB_only_flag == -1
+	continue;
+      endif
+      subdir_ids = [];
+      subdir_id = [];
+      num_subdir_ids = 0;
       if ~annotation_flag
-	warning(["annoation_path does not exist: ", annotation_path]);
+	if VERBOSE_FLAG
+	  warning(["annoation_path does not exist: ", annotation_path]);
+	endif
       else
-	local_dir = pwd;
-	try
-	  chdir(annotation_path);
-	  subdir_id = glob("n[0-9]*");
-	  subdir_id = subdir_id{1};
-	  chdir(local_dir);
-	catch
-	  chdir(local_dir);
-	end
+	  subdir_ids = glob([annotation_path, filesep, "n[0-9]*"]);
+	  num_subdir_ids = length(subdir_ids);
+	  subdir_id = subdir_ids{num_subdir_ids};
+	  base_id = strFolderFromPath(subdir_id);
+	  annotation_dir = [annotation_path, base_id, filesep];
       endif
       standard_dir = ...
 	  [standard_parent_dir, ...
-	   subdir_struct(i_subdir).name, "/"];
+	   subdir_foldernames{i_subdir}, filesep];
+      STANDARD_DIR = standard_dir;
       mkdir(standard_dir); %% does not clobber
       masks_dir = ...
 	  [masks_parent_dir, ...
-	   subdir_struct(i_subdir).name, "/"];
+	   subdir_foldernames{i_subdir}, filesep];
+      MASKS_DIR = masks_dir;
       mkdir(masks_dir); 
-      images_struct = dir([images_path, "n*.*"]);
-      num_images = length(images_struct);
-      disp(["num_images = ", num2str(num_images)]);
+
+      original_pathnames = glob([images_path, "n*.*"]);
+      num_images = length(original_pathnames);
+      if VERBOSE_FLAG
+	disp(["num_images = ", num2str(num_images)]);
+      endif
+      if annotation_flag
+	original_filenames = cellfun(@strFolderFromPath, original_pathnames, "UniformOutput", false);
+	base_filenames = cellfun(@strRemoveExtension, original_filenames, "UniformOutput", false);
+	xml_filenames = strcat(base_filenames, repmat(".xml", num_images, 1));
+	xml_pathnames = strcat(annotation_dir, xml_filenames);
+      else
+	xml_pathnames = cell(num_images,1);
+      endif
+
+      %%disp(["size(original_pathnames) = ", num2str(size(original_pathnames))]);
+      %%disp(["size(xml_pathnames) = ", num2str(size(xml_pathnames))]);
+      if num_procs > 1
+	[image_info] = ...
+	    parcellfun(num_procs, @imageNetStandardize, original_pathnames, xml_pathnames, ...
+		       "UniformOutput", false);
+      else
+	[image_info] = ...
+	    cellfun(@imageNetStandardize, original_pathnames, xml_pathnames, ...
+		       "UniformOutput", false);
+      endif
+
+      tot_discarded(i_object) = ...
+	  tot_discarded(i_object) + num_images;
       for i_image = 1 : num_images
-	tot_discarded(i_object) = tot_discarded(i_object) + 1;
-	disp(["i_image = ", num2str(i_image)]);
-	original_name = ...
-	    [images_path, images_struct(i_image).name];
-	disp(["original_name = ", original_name]);
-	base_ndx = strfind(images_struct(i_image).name, ".") - 1;
-	base_name = images_struct(i_image).name(1:base_ndx);
-	try
-	  [original_image, original_map, original_alpha] = imread(original_name);
-	catch 
-	  continue;
-	end
-	%% check if image unavaliable
-	original_info = imfinfo(original_name);
-	[unavailable_flag, unavailable_info] = ...
-	    imageNetUnavailableFLCKR(original_info, unavailable_info);
-	if unavailable_flag
+	if ~exist("image_info{i_image}") || isempty(image_info{i_image}) || image_info{i_image}.image_flag == 0
 	  continue;
 	endif
-	%%if (( original_info.Height < image_resize(1)/(256/194)) || ...
-	%%    (original_info.Width < image_resize(2)/(256/194)))
-	%%  continue;
-	%%endif
-	%% ignore transparancy channel: 4th "color")
-	if size(original_image,3) == 4
-	  original_image = original_image(:,:,1:3);
-	endif
-	if (strcmp( original_info.Format, "GIF" ) || ...
-	    strcmp( original_info.Format, "PNG" ))
-	endif
-	[pad_image] = ...
-	    imageNetPad(original_image, ...
-			original_info, ...
-			image_resize);
-	if isempty(pad_image)
-	  continue;
-	endif
-	standard_name = [standard_dir, base_name, image_type];
-	imwrite(pad_image, standard_name);
 	tot_images(1,i_object) = ...
-	    tot_images(1,i_object) + 1;
+	    tot_images(1,i_object) + image_info{i_image}.image_flag;
+	tot_masks(1,i_object) = ...
+	    tot_masks(1,i_object) + image_info{i_image}.mask_flag;
 	ave_height(1,i_object) = ...
-	    ave_height(1,i_object) + original_info.Height;
+	    ave_height(1,i_object) + image_info{i_image}.Height;
 	ave_width(1,i_object) = ...
-	    ave_width(1,i_object) + original_info.Width;
+	    ave_width(1,i_object) + image_info{i_image}.Width;
 	std_height(1,i_object) = ...
-	    std_height(1,i_object) + original_info.Height.^2;
+	    std_height(1,i_object) + image_info{i_image}.Height.^2;
 	std_width(1,i_object) = ...
-	    std_width(1,i_object) + original_info.Width.^2;
-	if original_info.Height < original_info.Width
+	    std_width(1,i_object) + image_info{i_image}.Width.^2;
+	if image_info{i_image}.Height < image_info{i_image}.Width
 	  tot_images_landscape(1,i_object) = ...
 	      tot_images_landscape(1,i_object) + 1;
 	  ave_height_landscape(1,i_object) = ...
-	      ave_height_landscape(1,i_object) + original_info.Height;
+	      ave_height_landscape(1,i_object) + image_info{i_image}.Height;
 	  ave_width_landscape(1,i_object) = ...
-	      ave_width_landscape(1,i_object) + original_info.Width;
+	      ave_width_landscape(1,i_object) + image_info{i_image}.Width;
 	  std_height_landscape(1,i_object) = ...
-	      std_height_landscape(1,i_object) + original_info.Height.^2;
+	      std_height_landscape(1,i_object) + image_info{i_image}.Height.^2;
 	  std_width_landscape(1,i_object) = ...
-	      std_width_landscape(1,i_object) + original_info.Width.^2;
+	      std_width_landscape(1,i_object) + image_info{i_image}.Width.^2;
 	endif
-	if annotation_flag
-	  xml_file = [annotation_path, ...
-		      subdir_id, "/", ...
-		      base_name, ".xml"];
-	  xml_flag = exist(xml_file, "file");
-	  if xml_flag
-	    %%keyboard
-	    disp(["xml_file = ", xml_file]);
-	    [mask_image, BB_mask, BB_scale_x, BB_scale_y] = ...
-		imageNetMaskBB(original_image, original_info, xml_file);
-	    if isempty(mask_image)
-	      continue;
-	    endif
-	    [pad_mask_image] = ...
-		imageNetPad(mask_image, ...
-			    original_info, ...
-			    image_resize);
-	    if isempty(pad_image)
-	      continue;
-	    endif
-	    mask_name = [masks_dir, base_name, ".png"];
-	    imwrite(pad_mask_image, mask_name);
-	    tot_masks(i_object) = tot_masks(i_object) + 1;
-	    if original_info.Height < original_info.Width
-	      tot_masks_landscape(i_object) = ...
-		  tot_masks_landscape(i_object) + 1;
-	    endif
-	    num_BB_mismatch(i_object) = ...
-		num_BB_mismatch(i_object) + ...
-		((BB_scale_x ~= 1) || (BB_scale_y ~= 1));	    
-	  endif
-	endif
+	num_BB_mismatch(i_object) = ...
+	    num_BB_mismatch(i_object) + ...
+	    ((image_info{i_image}.BB_scale_x ~= 1) || (image_info{i_image}.BB_scale_y ~= 1));	    
       endfor %% i_image
+
     endfor %% i_subdir
     
     tot_discarded(i_object) = ...
@@ -282,4 +294,4 @@ function [tot_images ...
   tot_time = end_time - begin_time;
   disp(["tot_time = ", num2str(tot_time)]);
 
-endfunction%% imageNetResize
+endfunction%% imageNetResize2
