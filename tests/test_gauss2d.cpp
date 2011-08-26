a15d011b53584f58e60c51c3540d25d0098ec5e2
peteschultz <peteschultz@users.sourceforge.net>
2011-08-26 20:01:22 +0000

    Changes to test_gauss2d, trying to identify when and where that test broke
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@4061 7c0444ca-5550-0410-9008-faecdbe78eb5

--- tests/test_gauss2d.cpp
+++ tests/test_gauss2d.cpp
@@ -22,23 +22,42 @@ int check_kernel_vs_hyper(HyPerConn * cHyPer, KernelConn * cKernel, int kPre,
 
 int main(int argc, char * argv[])
 {
-   PV::HyPerCol * hc = new PV::HyPerCol("test_gauss2d column", argc, argv);
+   char * cl_args[3];
+   cl_args[0] = strdup(argv[0]);
+   cl_args[1] = strdup("-p");
+   cl_args[2] = strdup("input/test_gauss2d.params");
+   PV::HyPerCol * hc = new PV::HyPerCol("test_gauss2d column", 3, cl_args);
    PV::Example * pre = new PV::Example("test_gauss2d pre", hc);
    PV::Example * post = new PV::Example("test_gauss2d post", hc);
-   PV::HyPerConn * cHyPer = new HyPerConn("test_gauss2d hyperconn", hc, pre,
-                                          post, CHANNEL_EXC);
-   PV::KernelConn * cKernel = new KernelConn("test_gauss2d kernelconn", hc,
-                                             pre, post, CHANNEL_EXC);
+/*
+   HyPerConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+             ChannelType channel, InitWeights *weightInit);
+ */
+   InitWeights * weightInitializer = new InitWeights();
+   PV::HyPerConn * cHyPer =
+         new HyPerConn("test_gauss2d hyperconn", hc, pre, post,
+                       CHANNEL_EXC, weightInitializer);
+   PV::KernelConn * cKernel =
+         new KernelConn("test_gauss2d kernelconn", hc, pre, post,
+                        CHANNEL_EXC, NULL, weightInitializer);
    PV::Example * pre2 = new PV::Example("test_gauss2d pre 2", hc);
    PV::Example * post2 = new PV::Example("test_gauss2d post 2", hc);
-   PV::HyPerConn * cHyPer1to2 = new HyPerConn("test_gauss2d hyperconn 1 to 2", hc, pre,
-                                              post2, CHANNEL_EXC);
-   PV::KernelConn * cKernel1to2 = new KernelConn("test_gauss2d kernelconn 1 to 2", hc,
-                                                 pre, post2, CHANNEL_EXC);
-   PV::HyPerConn * cHyPer2to1 = new HyPerConn("test_gauss2d hyperconn 2 to 1", hc, pre2,
-                                              post, CHANNEL_EXC);
-   PV::KernelConn * cKernel2to1 = new KernelConn("test_gauss2d kernelconn 2 to 1", hc,
-                                                 pre2, post, CHANNEL_EXC);
+   PV::HyPerConn * cHyPer1to2 =
+         new HyPerConn("test_gauss2d hyperconn 1 to 2", hc, pre, post2,
+                       CHANNEL_EXC, weightInitializer);
+   PV::KernelConn * cKernel1to2 =
+         new KernelConn("test_gauss2d kernelconn 1 to 2", hc, pre, post2,
+                        CHANNEL_EXC, NULL, weightInitializer);
+   PV::HyPerConn * cHyPer2to1 =
+         new HyPerConn("test_gauss2d hyperconn 2 to 1", hc, pre2, post,
+                       CHANNEL_EXC, weightInitializer);
+   PV::KernelConn * cKernel2to1 =
+         new KernelConn("test_gauss2d kernelconn 2 to 1", hc, pre2, post,
+                        CHANNEL_EXC, NULL, weightInitializer);
+
+   for( int c=0; c<hc->numberOfConnections(); c++ ) {
+      hc->getConnection(c)->writeWeights(0, true);
+   }
 
    const int axonID = 0;
    int num_pre_extended = pre->clayer->numExtended;
@@ -46,12 +65,12 @@ int main(int argc, char * argv[])
 
    int status = 0;
    for (int kPre = 0; kPre < num_pre_extended; kPre++) {
-      status = check_kernel_vs_hyper(cHyPer, cKernel, kPre, axonID);
-      assert(status==0);
-      status = check_kernel_vs_hyper(cHyPer1to2, cKernel1to2, kPre, axonID);
-      assert(status==0);
-      status = check_kernel_vs_hyper(cHyPer2to1, cKernel2to1, kPre, axonID);
-      assert(status==0);
+     status = check_kernel_vs_hyper(cHyPer, cKernel, kPre, axonID);
+     assert(status==0);
+     status = check_kernel_vs_hyper(cHyPer1to2, cKernel1to2, kPre, axonID);
+     assert(status==0);
+     status = check_kernel_vs_hyper(cHyPer2to1, cKernel2to1, kPre, axonID);
+     assert(status==0);
    }
 
    delete hc;
@@ -81,8 +100,8 @@ int check_kernel_vs_hyper(HyPerConn * cHyPer, KernelConn * cKernel, int kPre, in
             cHyPer->writeTextWeights(cHyper_filename, kPre);
             const char * cKernel_filename = "gauss2d_kernel.txt";
             cKernel->writeTextWeights(cKernel_filename, kPre);
+            exit(EXIT_FAILURE);
          }
-         assert(fabs(test_cond) <= 0.001f);
       }
       // advance pointers in y
       hyperWeights += sy;
