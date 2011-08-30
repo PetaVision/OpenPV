b81b9c20bf6a59f381ec9ff5f90e22cef02a74f4
garkenyon <garkenyon@users.sourceforge.net>
2011-08-30 18:10:39 +0000

    preliminary version
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@4076 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ MPITest/pv.cpp
@@ -0,0 +1,53 @@
+/*
+ * pv.cpp
+ *
+ */
+
+#include "../PetaVision/src/columns/buildandrun.hpp"
+#include "MPITestProbe.hpp"
+
+#define MAIN_USES_ADDCUSTOM
+
+#ifdef MAIN_USES_ADDCUSTOM
+int addcustom(HyPerCol * hc, int argc, char * argv[]);
+// addcustom is for adding objects not supported by build().
+#endif // MAIN_USES_ADDCUSTOM
+
+int main(int argc, char * argv[]) {
+
+    int status;
+#ifdef MAIN_USES_ADDCUSTOM
+    status = buildandrun(argc, argv, &addcustom);
+#else
+    status = buildandrun(argc, argv);
+#endif // MAIN_USES_ADDCUSTOM
+    return status==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+#ifdef MAIN_USES_ADDCUSTOM
+int addcustom(HyPerCol * hc, int argc, char * argv[]) {
+	int status;
+	PVParams * params = hc->parameters();
+	int numGroups = params->numberOfGroups();
+	for (int n = 0; n < numGroups; n++) {
+		const char * kw = params->groupKeywordFromIndex(n);
+		const char * name = params->groupNameFromIndex(n);
+		HyPerLayer * targetlayer;
+		const char * message;
+		const char * filename;
+		if (!strcmp(kw, "MPITestProbe")) {
+			status = getLayerFunctionProbeParameters(name, kw, hc, &targetlayer,
+					&message, &filename);
+			if (status != PV_SUCCESS) {
+				fprintf(stderr, "Skipping params group \"%s\"\n", name);
+				continue;
+			}
+			PVBufType buf_type = BufV;
+			MPITestProbe * addedProbe = new MPITestProbe(filename, hc, buf_type,
+					message);
+			checknewobject((void *) addedProbe, kw, name);
+		}
+	}
+	return PV_SUCCESS;
+}
+#endif // MAIN_USES_ADDCUSTOM
