eb70342af9f76425d7bebc4568935142ce9c1153
garkenyon <garkenyon@users.sourceforge.net>
2011-09-07 20:50:16 +0000

    layers for computing products over input cliques
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@4139 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/layers/CliqueLayer.hpp
@@ -0,0 +1,28 @@
+/*
+ * SigmaPiLayer.h
+ *
+ *  Created on: Sep 3, 2011
+ *      Author: gkenyon
+ */
+
+#ifndef SIGMAPILAYER_H_
+#define SIGMAPILAYER_H_
+
+#include "../columns/HyPerCol.hpp"
+#include "../connections/HyPerConn.hpp"
+#include "ANNLayer.hpp"
+
+
+namespace PV {
+
+class CliqueLayer: public PV::ANNLayer {
+public:
+
+   CliqueLayer(const char* name, HyPerCol * hc);
+   CliqueLayer(const char* name, HyPerCol * hc, PVLayerType type);
+   virtual int recvSynapticInput(HyPerConn * conn, PVLayerCube * cube, int neighbor);
+   virtual int updateState(float time, float dt);
+};
+
+} /* namespace PV */
+#endif /* SIGMAPILAYER_H_ */
