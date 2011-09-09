f74ce722d1df59819a9970bce392bcc6ab9020f7
garkenyon <garkenyon@users.sourceforge.net>
2011-09-09 20:10:13 +0000

    whoops, didn't mean to put these on the repository yet but since they're there, at least these compile (not functional yet).
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@4150 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/connections/CliqueConn.cpp
+++ src/connections/CliqueConn.cpp
@@ -9,15 +9,17 @@
 
 namespace PV {
 
-CliqueConn::CliqueConn()
-{
-   // TODO Auto-generated constructor stub
-
-}
-
-CliqueConn::~CliqueConn()
-{
-   // TODO Auto-generated destructor stub
-}
+CliqueConn::CliqueConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+            ChannelType channel, const char * filename) : KernelConn(name, hc, pre, post,
+                  channel, filename){};
+CliqueConn::CliqueConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+            ChannelType channel, const char * filename, InitWeights *weightInit) : KernelConn(name, hc, pre, post,
+                  channel, filename, weightInit){};
+CliqueConn::CliqueConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+            ChannelType channel) : KernelConn(name, hc, pre, post,
+                  channel){};
+int CliqueConn::updateState(float time, float dt){return PV_SUCCESS;};
+int CliqueConn::updateWeights(int arbor){return PV_SUCCESS;};
+PVPatch ** CliqueConn::normalizeWeights(PVPatch ** patches, int numPatches, int arborId){return PV_SUCCESS;};
 
 } /* namespace PV */
--- src/connections/CliqueConn.hpp
+++ src/connections/CliqueConn.hpp
@@ -12,7 +12,7 @@
 
 namespace PV {
 
-class CliqueConn: public PV::KernelConn {
+class CliqueConn: public KernelConn {
 public:
 
    CliqueConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
@@ -21,14 +21,11 @@ public:
                ChannelType channel, const char * filename, InitWeights *weightInit);
    CliqueConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
                ChannelType channel);
-   CliqueConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
-               ChannelType channel, InitWeights *weightInit);
-   virtual PVPatch ** initializeDefaultWeights(PVPatch ** patches, int numPatches);
    virtual int updateState(float time, float dt);
    virtual int updateWeights(int arbor);
    virtual PVPatch ** normalizeWeights(PVPatch ** patches, int numPatches, int arborId);
 
-};
+}; // class CliqueConn
 
 } /* namespace PV */
 #endif /* CLIQUECONN_HPP_ */
