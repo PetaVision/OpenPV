ab6ba6d293f5f9b08e81e033750d0b991ccdacb0
rasmussn <rasmussn@users.sourceforge.net>
2011-09-17 21:56:14 +0000

    Changes related to implementing OpenCL kernel for recvSynapticInput.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@4208 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/columns/DataStore.cpp
+++ src/columns/DataStore.cpp
@@ -2,12 +2,14 @@
  * DataStore.cpp
  *
  *  Created on: Sep 10, 2008
- *      Author: rasmussn
+ *      Author: Craig Rasmussen
  */
 
 #include "DataStore.hpp"
-#include <assert.h>
+#include "HyPerCol.hpp"
+#include "../include/pv_common.h"
 
+#include <assert.h>
 #include <stdlib.h>
 
 namespace PV
@@ -20,17 +22,30 @@ namespace PV
  */
 DataStore::DataStore(int numBuffers, size_t bufSize, int numLevels)
 {
-	this->curLevel = numLevels - 1;  // start at bottom, work up
-	this->bufSize = bufSize;
-	this->numLevels = numLevels;
-	this->numBuffers = numBuffers;
-	this->recvBuffers = (char*) calloc(numBuffers * numLevels * bufSize, sizeof(char));
-	assert(this->recvBuffers != NULL);
+   this->curLevel = numLevels - 1;  // start at bottom, work up
+   this->bufSize = bufSize;
+   this->numLevels = numLevels;
+   this->numBuffers = numBuffers;
+   this->recvBuffers = (char*) calloc(numBuffers * numLevels * bufSize, sizeof(char));
+   assert(this->recvBuffers != NULL);
 }
 
 DataStore::~DataStore()
 {
-	free(recvBuffers);
+#ifdef PV_USE_OPENCL
+   if (clRecvBuffers != NULL) delete clRecvBuffers;
+#endif
+
+   free(recvBuffers);
 }
+
+#ifdef PV_USE_OPENCL
+int DataStore::initializeThreadBuffers(HyPerCol * hc)
+{
+   const size_t size = numBuffers * numLevels * bufSize * sizeof(char);
+   clRecvBuffers = hc->getCLDevice()->createBuffer(CL_MEM_COPY_HOST_PTR, size, recvBuffers);
+   return PV_SUCCESS;
+}
+#endif
 
 }
--- src/columns/DataStore.hpp
+++ src/columns/DataStore.hpp
@@ -2,16 +2,23 @@
  * DataStore.h
  *
  *  Created on: Sep 10, 2008
- *      Author: rasmussn
+ *      Author: Craig Rasmussen
  */
 
 #ifndef DATASTORE_H_
 #define DATASTORE_H_
 
+#include "../include/pv_arch.h"
 #include <stdlib.h>
+
+#ifdef PV_USE_OPENCL
+#include "../arch/opencl/CLBuffer.hpp"
+#include "../arch/opencl/CLDevice.hpp"
+#endif
 
 namespace PV
 {
+class HyPerCol;
 
 class DataStore
 {
@@ -33,6 +40,8 @@ public:
          {return (recvBuffers + bufferId*numLevels*bufSize + levelIndex(level)*bufSize);}
    void* buffer(int bufferId)
          {return (recvBuffers + bufferId*numLevels*bufSize + curLevel*bufSize);}
+   size_t bufferOffset(int bufferId, int level=0)
+         {return (bufferId*numLevels*bufSize + levelIndex(level)*bufSize);}
 
 private:
    size_t bufSize;
@@ -40,6 +49,13 @@ private:
    int    numLevels;
    int    numBuffers;
    char*  recvBuffers;
+
+#ifdef PV_USE_OPENCL
+   CLBuffer * clRecvBuffers;
+public:
+   int initializeThreadBuffers(HyPerCol * hc);
+   CLBuffer * getCLBuffer()  {return clRecvBuffers;}
+#endif
 };
 
 } // NAMESPACE
