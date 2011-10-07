86893b972a2e237955202f3d63e2ff9b58a7bd63
garkenyon <garkenyon@users.sourceforge.net>
2011-10-07 23:52:39 +0000

    improve routines for processing darpa chips
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@4348 7c0444ca-5550-0410-9008-faecdbe78eb5

--- mlab/NeoVis2/padChipKernel.m
+++ mlab/NeoVis2/padChipKernel.m
@@ -8,6 +8,7 @@ function [status_info] = padChipKernel(target_pathname)
   global canny_struct
   global image_margin
   global pad_size
+  global cropped_dir
 
   global VERBOSE_FLAG
 
@@ -20,6 +21,8 @@ function [status_info] = padChipKernel(target_pathname)
     disp(["target_pathname = ", target_pathname]);
   endif
   target_filename = strFolderFromPath(target_pathname);
+  status_info.chipname = target_filename;
+  status_info.pathname = target_pathname;
   try
     image_color = ...
 	imread(target_pathname);
@@ -29,6 +32,53 @@ function [status_info] = padChipKernel(target_pathname)
   end
   status_info.target_flag = 1;
   image_gray = col2gray(image_color);
+  %%keyboard;
+
+  status_info.mean = mean(image_gray(:));
+  status_info.std = std(image_gray(:));
+
+  status_info.original_size = size(image_color);  
+
+  %% crop artifacts arising from image borders
+  border_artifact_thresh = 1.25;
+  status_info.num_border_artifact_top = 0;
+  while (mean(squeeze(image_gray(2,:))) - mean(squeeze(image_gray(1,:)))) / ...
+	(status_info.std + (status_info.std==0)) > ...
+	border_artifact_thresh
+    status_info.num_border_artifact_top = status_info.num_border_artifact_top + 1;
+    image_gray = image_gray(2:end,:);
+    image_color = image_color(2:end,:,:);
+  endwhile
+  status_info.num_border_artifact_bottom = 0;
+  while (mean(squeeze(image_gray(end-1,:))) - mean(squeeze(image_gray(end,:)))) / ...
+	(status_info.std + (status_info.std==0)) > ...
+	border_artifact_thresh
+    status_info.num_border_artifact_bottom = status_info.num_border_artifact_bottom + 1;
+    image_gray = image_gray(1:end-1,:);
+    image_color = image_gray(1:end-1,:,:);
+  endwhile
+  status_info.num_border_artifact_left = 0;
+  while (mean(squeeze(image_gray(:,2))) - mean(squeeze(image_gray(:,1)))) / ...
+	(status_info.std + (status_info.std==0)) > ...
+	border_artifact_thresh
+    status_info.num_border_artifact_left = status_info.num_border_artifact_left + 1;
+    image_gray = image_gray(:,2:end);
+    image_color = image_color(:,2:end,:);
+  endwhile
+  status_info.num_border_artifact_right = 0;
+  while (mean(squeeze(image_gray(:, end-1))) - mean(squeeze(image_gray(:,end)))) / ...
+	(status_info.std + (status_info.std==0)) > ...
+	border_artifact_thresh
+    status_info.num_border_artifact_right = status_info.num_border_artifact_right + 1;
+    image_gray = image_gray(:,1:end-1);
+    image_color = image_color(:,1:end-1,:);
+  endwhile
+  status_info.cropped_size = size(image_gray);
+  if any(status_info.cropped_size ~= status_info.original_size(1:2))
+    cropped_pathname = [cropped_dir, status_info.chipname];
+    imwrite(image_color, cropped_pathname);
+  endif
+
   extended_image_gray = addMirrorBC(image_gray, image_margin);
 
   if DoG_flag 
@@ -52,8 +102,8 @@ function [status_info] = padChipKernel(target_pathname)
       image_DoG_mirrorBC = addMirrorBC(image_DoG, mirror_width*i_blend);
       pad_DoG_mirror = padGray(image_DoG_mirrorBC, pad_size, DoG_gray_val);
       pad_DoG_blend = ...
-	  pad_DoG_blend * (1/2) + ...
-	  pad_DoG_mirror * (1/2);
+	  pad_DoG_blend * (i_blend/num_blend) + ...
+	  pad_DoG_mirror * ((num_blend-i_blend)/num_blend);
     endfor %% i_blend
 
     DoG_pathname = ...
@@ -85,8 +135,8 @@ function [status_info] = padChipKernel(target_pathname)
       image_canny_mirrorBC = addMirrorBC(image_canny, mirror_width*i_blend);
       pad_canny_mirror = padGray(image_canny_mirrorBC, pad_size, canny_gray_val);
       pad_canny_blend = ...
-	  pad_canny_blend * (1/2) + ...
-	  pad_canny_mirror * (1/2);
+	  pad_canny_blend * (i_blend/num_blend) + ...
+	  pad_canny_mirror * ((num_blend-i_blend)/num_blend);
     endfor %% i_blend
     
     canny_pathname = ...
--- mlab/NeoVis2/padChips.m
+++ mlab/NeoVis2/padChips.m
@@ -2,6 +2,17 @@
 function [tot_chips, ...
 	  tot_DoG, ...
 	  tot_canny, ...
+	  tot_cropped, ...
+	  tot_mean, ...
+	  tot_std, ...
+	  tot_border_artifact_top, ...
+	  tot_border_artifact_bottom, ...
+	  tot_border_artifact_left, ...
+	  tot_border_artifact_right, ...
+	  ave_original_size, ...
+	  ave_cropped_size, ...
+	  std_original_size, ...
+	  std_cropped_size, ...
 	  tot_time] = ...
       padChips(chip_path, ...
 	       object_name, ...
@@ -24,19 +35,20 @@ function [tot_chips, ...
   global canny_struct
   global image_margin
   global pad_size
+  global cropped_dir
 
   global VERBOSE_FLAG
   if ~exist("VERBOSE_FLAG") || isempty(VERBOSE_FLAG)
-    VERBOSE_FLAG = 0;
+    VERBOSE_FLAG, ...
   endif
  
   begin_time = time();
 
   if nargin < 1 || ~exist("chip_path") || isempty(chip_path)
-    chip_path = "~/Pictures/HeliChips/";
+    chip_path = "~/Pictures/HeliChips2/";
   endif
   if nargin < 2 || ~exist("object_name") || isempty(object_name)
-    object_name = "Car";  %% could be a list?
+    object_name =  "Car";  %%  "Helicopter"; %% "Container"; "Boat"; %% "Plane"; %% "Cyclist"; %% "Person"; %%  
   endif
   if nargin < 3 || ~exist("DoG_flag") || isempty(DoG_flag)
     DoG_flag = 1;  %% 
@@ -59,7 +71,7 @@ function [tot_chips, ...
     pad_size = [256 256];  %% 
   endif
   if nargin < 8 || ~exist("num_procs") || isempty(num_procs)
-    num_procs = 4;  %% 
+    num_procs = 6;  %% 
   endif
   
   setenv('GNUTERM', 'x11');
@@ -70,6 +82,19 @@ function [tot_chips, ...
   tot_targets = 0;
   tot_DoG = 0;
   tot_canny = 0;
+  tot_mean = 0;
+  tot_std = 0;
+  tot_border_artifact_top = 0;
+  tot_border_artifact_bottom = 0;
+  tot_border_artifact_left = 0;
+  tot_border_artifact_right = 0;
+  ave_original_size = zeros(1,3);
+  ave_cropped_size = zeros(1,2);
+  std_original_size = zeros(1,3);
+  std_cropped_size = zeros(1,2);
+  tot_cropped = 0;
+  cropped_list = {};
+  
 
   %% path to generic image processing routines
   img_proc_dir = "~/workspace-indigo/PetaVision/mlab/imgProc/";
@@ -80,6 +105,30 @@ function [tot_chips, ...
   addpath(str_kernel_dir);
 
   chip_dir = [chip_path, "chips", filesep];
+  if ~exist(chip_dir, "dir")
+    error(["~exist(chip_dir): ", chip_dir]);
+  endif
+  target_dir = ...
+      [chip_dir, object_name, filesep];  %%
+  if ~exist(target_dir, "dir")
+    error(["~exist(target_dir): ", target_dir]);
+  endif
+
+  cropped_path = [chip_path, "cropped", filesep];
+  mkdir(cropped_path);
+  cropped_dir = [cropped_path, object_name, filesep];
+  mkdir(cropped_dir);
+
+  log_path = [chip_path, "log", filesep];
+  mkdir(log_path);
+  log_dir = [log_path, object_name, filesep];
+  mkdir(log_dir);
+
+  list_path = [chip_path, "list", filesep];
+  mkdir(list_path);
+  list_dir = [list_path, object_name, filesep];
+  mkdir(list_dir);
+
   if DoG_flag
     DoG_folder = [chip_path, "DoG", filesep];
     mkdir(DoG_folder);
@@ -92,12 +141,14 @@ function [tot_chips, ...
     canny_dir = [canny_folder, object_name, filesep];
     mkdir(canny_dir);
   endif %% canny_flag
+  hist_folder = [chip_path, "hist", filesep];
+  mkdir(hist_folder);
+  hist_path = [hist_folder, object_name, filesep];
+  mkdir(hist_path);
 
   image_type = ".png";
   image_margin = 8;
 
-  target_dir = ...
-      [chip_dir, object_name, filesep];  %%
 
   target_path = ...
       [target_dir, '*', image_type];
@@ -106,6 +157,9 @@ function [tot_chips, ...
   disp(['num_chips = ', num2str(num_chips)]);
 
   tot_chips = tot_chips + num_chips;
+  mean_hist = zeros(1, tot_chips);
+  std_hist = zeros(1, tot_chips);
+  std_mean_hist = zeros(1, tot_chips);
     
   if num_procs > 1
     [status_info] = ...
@@ -119,15 +173,122 @@ function [tot_chips, ...
       tot_targets = tot_targets + status_info{i_chip}.target_flag;
       tot_DoG = tot_DoG + status_info{i_chip}.DoG_flag;
       tot_canny = tot_canny + status_info{i_chip}.canny_flag;
+      tot_mean = tot_mean + status_info{i_chip}.mean;
+      tot_std = tot_std + status_info{i_chip}.std;
+      mean_hist(i_chip) = status_info{i_chip}.mean;
+      std_hist(i_chip) = status_info{i_chip}.std;
+      std_mean_hist(i_chip) = std_hist(i_chip) / (mean_hist(i_chip) + (mean_hist(i_chip)==0));
+      tot_border_artifact_top = tot_border_artifact_top + status_info{i_chip}.num_border_artifact_top;
+      tot_border_artifact_bottom = tot_border_artifact_bottom + status_info{i_chip}.num_border_artifact_bottom;
+      tot_border_artifact_left = tot_border_artifact_left + status_info{i_chip}.num_border_artifact_left;
+      tot_border_artifact_right = tot_border_artifact_right + status_info{i_chip}.num_border_artifact_right;
+      ave_original_size = ave_original_size + status_info{i_chip}.original_size;
+      ave_cropped_size = ave_cropped_size + status_info{i_chip}.cropped_size;
+      std_original_size = std_original_size + (status_info{i_chip}.original_size).^2;
+      std_cropped_size = std_cropped_size + (status_info{i_chip}.cropped_size).^2;
+      if any(status_info{i_chip}.cropped_size ~= status_info{i_chip}.original_size(1:2))
+	tot_cropped = tot_cropped + 1;
+	cropped_list{tot_cropped} = status_info{i_chip}.chipname;
+      endif
    endfor %% i_chip
 
+   fig_list = [];
+   num_hist_bins = 20;
 
-  disp(["tot_chips = ", ...
-	num2str(tot_chips)]);
-  disp(["tot_DoG = ", ...
-	num2str(tot_DoG)]);
-  disp(["tot_canny = ", ...
-	num2str(tot_canny)]);
+   [hist_mean_count, hist_mean_bins] = ...
+       hist(mean_hist, num_hist_bins);
+   fh = figure;
+   fig_name = "hist_mean";
+   fig_path = [hist_path, fig_name];
+   set(fh, "Name", fig_name);
+   fig_list = [fig_list, fh];
+   bar(hist_mean_bins, hist_mean_count);
+   print(fh, fig_path, "-dpng");
+
+   [hist_std_count, hist_std_bins] = ...
+       hist(std_hist, num_hist_bins);
+   fh = figure;
+   fig_name = "hist_std";
+   fig_path = [hist_path, fig_name];
+   set(fh, "Name", fig_name);
+   fig_list = [fig_list, fh];
+   bar(hist_std_bins, hist_std_count);
+   print(fh, fig_path, "-dpng");
+   
+   [hist_std_mean_count, hist_std_mean_bins] = ...
+       hist(std_mean_hist, num_hist_bins);
+   fh = figure;
+   fig_name = "hist_std_mean";
+   fig_path = [hist_path, fig_name];
+   set(fh, "Name", fig_name);
+   fig_list = [fig_list, fh];
+   bar(hist_std_mean_bins, hist_std_mean_count);
+   print(fh, fig_path, "-dpng");
+   
+   ave_mean = tot_mean / tot_chips;
+   disp(["ave_mean = ", ...
+	 num2str(ave_mean)]);
+   ave_std = tot_std / tot_chips;
+   disp(["ave_std = ", ...
+	 num2str(ave_std)]);
+
+   ave_original_size =  ave_original_size / tot_chips;
+   disp(["ave_original_size = ", ...
+	 num2str(ave_original_size)]);
+   std_original_size =  sqrt((std_original_size / tot_chips) - ave_original_size.^2);
+   disp(["std_original_size = ", ...
+	 num2str(std_original_size)]);
+
+   ave_cropped_size =  ave_cropped_size / tot_chips;
+   disp(["ave_cropped_size = ", ...
+	 num2str(ave_cropped_size)]);
+   std_cropped_size =  sqrt((std_cropped_size / tot_chips) - ave_cropped_size.^2);
+   disp(["std_cropped_size = ", ...
+	 num2str(std_cropped_size)]);
+
+   disp(["tot_border_artifact_top = ", ...
+	 num2str(tot_border_artifact_top)]);
+   disp(["tot_border_artifact_bottom = ", ...
+	 num2str(tot_border_artifact_bottom)]);
+   disp(["tot_border_artifact_left = ", ...
+	 num2str(tot_border_artifact_left)]);
+   disp(["tot_border_artifact_right = ", ...
+	 num2str(tot_border_artifact_right)]);
+ 
+   disp(["tot_chips = ", ...
+	 num2str(tot_chips)]);
+   disp(["tot_DoG = ", ...
+	 num2str(tot_DoG)]);
+   disp(["tot_canny = ", ...
+	 num2str(tot_canny)]);
+   disp(["tot_cropped = ", ...
+	 num2str(tot_cropped)]);
+
+   log_filename = [log_dir, "log.txt"];
+   save("-ascii", ...
+	log_filename, ...
+	"tot_chips", ...
+	"tot_DoG", ...
+	"tot_canny", ...
+	"tot_cropped", ...
+	"tot_mean", ...
+	"tot_std", ...
+	"tot_border_artifact_top", ...
+	"tot_border_artifact_bottom", ...
+	"tot_border_artifact_left", ...
+	"tot_border_artifact_right", ...
+	"ave_original_size", ...
+	"ave_cropped_size", ...
+	"std_original_size", ...
+	"std_cropped_size", ...
+	"tot_time"); 
+
+   list_filename = [list_dir, "cropped_list.txt"];
+   fid = fopen(list_filename);
+   for i_crop = 1 : tot_cropped
+     fputs(fid, cropped_list{i_crop});
+   endfor
+   fclose(fid);
   
   end_time = time();
   tot_time = end_time - begin_time;
