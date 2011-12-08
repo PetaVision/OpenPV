d497e94fa895c02f10d9c332166da2c2f78e5f85
peteschultz <peteschultz@users.sourceforge.net>
2011-12-08 22:17:39 +0000

    A script to build all the systems tests, unit tests, and sandbox binaries
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@4624 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 60701d31b282f8c6378c571c1d68ebba52bf82c1

---
+++ pete/makeallprojects.bash
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+if test "${0%/*}" != "$0"
+then
+    cd "${0%/*}"
+fi
+cd ..
+wd=$PWD
+
+cd PetaVision/lib
+make clean
+make -j4 all
+cd $wd
+
+for k in $(ls | egrep -v PetaVision)
+do
+    cd $k/Debug
+    make clean
+    make -j4 all
+    cd $wd
+done
+
+cd PetaVision/tests
+make clean
+make -j4 all
+cd $wd
