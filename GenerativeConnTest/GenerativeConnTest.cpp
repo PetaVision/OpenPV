81be4fd69b42a8fb8004d8adbc52cff3c6512797
peteschultz <peteschultz@users.sourceforge.net>
2012-03-01 20:18:53 +0000

    Bugfix for GenerativeConnTest
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@4837 7c0444ca-5550-0410-9008-faecdbe78eb5

--- GenerativeConnTest/GenerativeConnTest.cpp
+++ GenerativeConnTest/GenerativeConnTest.cpp
@@ -135,8 +135,8 @@ int dumponeweight(GenerativeConn * conn) {
    int yScaleDiff = conn->postSynapticLayer()->getYScale() - conn->preSynapticLayer()->getYScale();
    float yFalloff = powf(2,yScaleDiff);
 
-   for( int p=0; p<conn->numDataPatches(); p++ ) {
-      pvdata_t * wgtData = conn->get_wData(0,p); // conn->getKernelPatch(0,p)->data;
+   for( int p=0; p<conn->getNumDataPatches(); p++ ) {
+      pvdata_t * wgtData = conn->get_wDataHead(0,p); // conn->getKernelPatch(0,p)->data;
       for( int f=0; f<nfp; f++ ) {
          for( int x=0; x<nxp; x++ ) {
             int xoffset = abs((int) floor((x-xcenter)*xFalloff));
