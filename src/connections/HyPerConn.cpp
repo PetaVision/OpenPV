35605a32ed1d8b83111bdd3bc1cab027c4fe2a11
peteschultz <peteschultz@users.sourceforge.net>
2012-03-02 22:21:50 +0000

    When createWeights calls allocWeights, the number of patches needs to be numDataPatches, not nPatches
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@4859 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/connections/HyPerConn.cpp
+++ src/connections/HyPerConn.cpp
@@ -311,11 +311,11 @@ int HyPerConn::constructWeights(const char * filename)
 
    //allocate weight patches and axonal arbors for each arbor
    for (int arborId=0;arborId<numAxonalArborLists;arborId++) {
-         wDataStart[arborId] = createWeights(wPatches, arborId);
-         assert(wPatches[arborId] != NULL);
-         if (shrinkPatches_flag || arborId == 0){
-            status |= adjustAxonalArbors(arborId);
-         }
+      wDataStart[arborId] = createWeights(wPatches, arborId);
+      assert(wPatches[arborId] != NULL);
+      if (shrinkPatches_flag || arborId == 0){
+         status |= adjustAxonalArbors(arborId);
+      }
    }  // arborId
 
    //initialize weights for patches:
@@ -1285,7 +1285,7 @@ pvdata_t * HyPerConn::createWeights(PVPatch *** patches, int nPatches, int nxPat
    }
 
    // allocate space for all weights at once (inplace), return pointer to beginning of weight array
-   pvdata_t * data_patches = allocWeights(patches, nPatches, nxPatch, nyPatch, nfPatch, axonId);
+   pvdata_t * data_patches = allocWeights(patches, getNumDataPatches(), nxPatch, nyPatch, nfPatch, axonId);
    return data_patches;
 }
 
