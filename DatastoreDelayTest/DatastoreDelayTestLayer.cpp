7110d3ce4e129582b6d15104ecc93b34abfe34f2
peteschultz <peteschultz@users.sourceforge.net>
2012-03-08 21:14:44 +0000

    Update to DatastoreDelayTestLayer to match recent changes to updateState/updateV
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@4897 7c0444ca-5550-0410-9008-faecdbe78eb5

--- DatastoreDelayTest/DatastoreDelayTestLayer.cpp
+++ DatastoreDelayTest/DatastoreDelayTestLayer.cpp
@@ -22,51 +22,111 @@ int DatastoreDelayTestLayer::initialize() {
    return PV_SUCCESS;
 }
 
-int DatastoreDelayTestLayer::updateV() {
+int DatastoreDelayTestLayer::updateState(float timef, float dt) {
    const PVLayerLoc * loc = getLayerLoc();
-   int nx = loc->nx;
-   int ny = loc->ny;
-   int nf = loc->nf;
-   if( inited ) {
+   return updateState(timef, dt, getNumNeurons(), getV(), getCLayer()->activity->data, loc->nx, loc->ny, loc->nf, loc->nb);
+}
+
+int DatastoreDelayTestLayer::updateState(float timef, float dt, int num_neurons, pvdata_t * V, pvdata_t * A, int nx, int ny, int nf, int nb) {
+   // updateV();
+   updateV_DatastoreDelayTestLayer(getLayerLoc(), &inited, getV(), parent->icCommunicator()->publisherStore(clayer->layerId)->numberOfLevels());
+   setActivity_HyPerLayer(num_neurons, V, A, nx, ny, nf, nb);
+   resetGSynBuffers();
+   updateActiveIndices();
+
+   return PV_SUCCESS;
+}
+
+int DatastoreDelayTestLayer::updateV_DatastoreDelayTestLayer(const PVLayerLoc * loc, bool * inited, pvdata_t * V, int period) {
+   if( *inited ) {
       // Rotate values by one row.
       // Move everything down one row; clobbering row 0 in the process
-      for( int y=ny-1; y>0; y-- ) {
-         for( int x=0; x<nx; x++ ) {
-            for( int f=0; f<nf; f++ ) {
-               pvdata_t * V = &getV()[kIndex(x,y,f,nx,ny,nf)];
-               (*V)--;
-               if( *V == 0 ) *V = period;
+      for( int y=loc->ny-1; y>0; y-- ) {
+         for( int x=0; x < loc->nx; x++ ) {
+            for( int f=0; f < loc->nf; f++ ) {
+               pvdata_t * V1 = &V[kIndex(x,y,f,loc->nx,loc->ny,loc->nf)];
+               (*V1)--;
+               if( *V1 == 0 ) *V1 = period;
             }
          }
       }
       // Finally, copy period-th row to zero-th row
-      for( int x=0; x<loc->nx; x++ ) {
-         for( int f=0; f<loc->nf; f++ ) {
-            getV()[kIndex(x,0,f,nx,ny,nf)] = getV()[kIndex(x,period,f,nx,ny,nf)];
+      for( int x=0; x < loc->nx; x++ ) {
+         for( int f=0; f < loc->nf; f++ ) {
+            V[kIndex(x,0,f,loc->nx,loc->ny,loc->nf)] = V[kIndex(x,period,f,loc->nx,loc->ny,loc->nf)];
          }
       }
+
    }
    else {
-      period = parent->icCommunicator()->publisherStore(clayer->layerId)->numberOfLevels();
-      if( nx < period ) {
-         if( parent->icCommunicator()->commRank() == 0 ) {
+      if( loc->ny < period ) {
+         int rank;
+         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+         if( rank == 0 ) {
             fflush(stdout);
-            fprintf(stderr, "DatastoreDelayTestLayer \"%s\": number of columns (%d) must be >= number of delay levels (%d).  Exiting.\n", name, getLayerLoc()->ny, period);
+            fprintf(stderr, "DatastoreDelayTestLayer: number of rows (%d) must be >= period (%d).  Exiting.\n", loc->ny, period);
          }
-         exit(EXIT_FAILURE);
+         abort();
       }
       int base = loc->ky0;
-      for( int x=0; x<nx; x++ ) {
-         for( int f=0; f<nf; f++ ) {
-            for( int row=0; row<ny; row++ ) {
-               getV()[kIndex(x,row,f,nx,ny,nf)] = (base+row) % period + 1;
+      for( int x=0; x < loc->nx; x++ ) {
+         for( int f=0; f < loc->nf; f++ ) {
+            for( int row=0; row < loc->ny; row++ ) {
+               V[kIndex(x,row,f,loc->nx,loc->ny,loc->nf)] = (base+row) % period + 1;
             }
          }
       }
-      inited = true;
+      *inited = true;
    }
    return PV_SUCCESS;
 }
+
+//int DatastoreDelayTestLayer::updateV() {
+//   const PVLayerLoc * loc = getLayerLoc();
+//   int nx = loc->nx;
+//   int ny = loc->ny;
+//   int nf = loc->nf;
+//   if( inited ) {
+//      // Rotate values by one row.
+//      // Move everything down one row; clobbering row 0 in the process
+//      for( int y=ny-1; y>0; y-- ) {
+//         for( int x=0; x<nx; x++ ) {
+//            for( int f=0; f<nf; f++ ) {
+//               pvdata_t * V = &getV()[kIndex(x,y,f,nx,ny,nf)];
+//               (*V)--;
+//               if( *V == 0 ) *V = period;
+//            }
+//         }
+//      }
+//      // Finally, copy period-th row to zero-th row
+//      for( int x=0; x<loc->nx; x++ ) {
+//         for( int f=0; f<loc->nf; f++ ) {
+//            getV()[kIndex(x,0,f,nx,ny,nf)] = getV()[kIndex(x,period,f,nx,ny,nf)];
+//         }
+//      }
+//   }
+//   else {
+//      period = parent->icCommunicator()->publisherStore(clayer->layerId)->numberOfLevels();
+//      if( nx < period ) {
+//         if( parent->icCommunicator()->commRank() == 0 ) {
+//            fflush(stdout);
+//            fprintf(stderr, "DatastoreDelayTestLayer \"%s\": number of columns (%d) must be >= number of delay levels (%d).  Exiting.\n", name, getLayerLoc()->ny, period);
+//         }
+//         exit(EXIT_FAILURE);
+//      }
+//      int base = loc->ky0;
+//      for( int x=0; x<nx; x++ ) {
+//         for( int f=0; f<nf; f++ ) {
+//            for( int row=0; row<ny; row++ ) {
+//               getV()[kIndex(x,row,f,nx,ny,nf)] = (base+row) % period + 1;
+//            }
+//         }
+//      }
+//      inited = true;
+//   }
+//   return PV_SUCCESS;
+// }
 
 }  // end of namespace PV block
 
--- DatastoreDelayTest/DatastoreDelayTestLayer.hpp
+++ DatastoreDelayTest/DatastoreDelayTestLayer.hpp
@@ -19,9 +19,13 @@ public:
    DatastoreDelayTestLayer(const char* name, HyPerCol * hc);
    virtual ~DatastoreDelayTestLayer();
 
-   virtual int updateV();
+   virtual int updateState(float timef, float dt);
+   // virtual int updateV();
 protected:
    int initialize();
+   int updateState(float timef, float dt, int numNeurons, pvdata_t * V, pvdata_t * A, int nx, int ny, int nf, int nb);
+
+   static int updateV_DatastoreDelayTestLayer(const PVLayerLoc * loc, bool * inited, pvdata_t * V, int period);
 
 protected:
    bool inited;
