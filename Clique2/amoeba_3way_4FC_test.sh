7570b47bbc68b75f98ec8c00227282b7a35e5478
garkenyon <garkenyon@users.sourceforge.net>
2012-04-25 23:10:04 +0000

    shell script
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5080 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 57238d59bffea1c6454cc7019f04836cc5770f89

---
+++ Clique2/amoeba_3way_4FC_test.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+time ./Debug/Clique2 -p /home/garkenyon/workspace-indigo/Clique2/input/amoeba/3way/4FC/target/001/amoeba_3way_4FC_target_001.params 1> /mnt/data/PetaVision/amoeba/3way/activity/4FC/target/001/amoeba_3way_4FC_target_001.log &
+time ./Debug/Clique2 -p /home/garkenyon/workspace-indigo/Clique2/input/noamoeba/3way/4FC/distractor/001/noamoeba_3way_4FC_distractor_001.params 1> /mnt/data/PetaVision/noamoeba/3way/activity/4FC/distractor/001/noamoeba_3way_4FC_distractor_001.log &
+
