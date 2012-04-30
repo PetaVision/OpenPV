8f67c0e61751ee6281cad993a751b7060e84f68b
garkenyon <garkenyon@users.sourceforge.net>
2012-04-30 16:26:40 +0000

    shell
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5098 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 06c54315696e766249f72e00f6407c5c7015f8cd

---
+++ Clique2/Heli_Training_Car_canny.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+time ./Debug/Clique2 -p ./input/Heli/Training/Car/canny/001/Heli_Training_Car_canny_001.params 1> /mnt/data/repo/neovision-programs-petavision/Heli/Training/activity/Car/canny/001/Heli_Training_Car_canny_001.log
