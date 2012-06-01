d257c472f2cab2b5e523a4e985201c3275df216b
garkenyon <garkenyon@users.sourceforge.net>
2012-06-01 20:14:50 +0000

    shell for complete set of training runs using sleep to space different stages.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5163 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: cce5bd949cc6a33e5026998ad3b61203a5c4b87c

--- Clique2/Heli_Formative.sh
+++ Clique2/Heli_Formative.sh
@@ -7,5 +7,5 @@ sleep 1200; bash ./Heli_Formative_Car3_canny.sh;
 sleep 1200; bash ./Heli_Formative_Car_distractor3_canny.sh;
 sleep 1200; bash ./Heli_Formative_Car4_canny.sh;
 sleep 1200; bash ./Heli_Formative_Car_distractor4_canny.sh;
-sleep 1200; bash ./Heli_Formative_Car4_canny.sh;
+sleep 1200; bash ./Heli_Formative_Car5_canny.sh;
 sleep 1200; bash ./Heli_Formative_Car_distractor5_canny.sh;
