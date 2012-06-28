b0d1c8da7f5c8b2fd26a4a72432fa39c2f509c2b
ruipcosta <ruipcosta@users.sourceforge.net>
2012-06-28 16:24:44 +0000

    - Update in the params file - New mlab analysis code for weights and activity
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5281 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: c2cd868f404fca871ac8de455c42af3ca9187cbe

---
+++ HyPerSTDP/analysis/pvp_plotActivity.m
@@ -0,0 +1,39 @@
+%Function for plotting activity from the STDP experiment
+
+clear all; close all; more off;
+addpath('/Users/ruicosta/Documents/workspace/HyPerSTDP/mlab/')
+
+output_path = '/Users/ruicosta/Documents/workspace/HyPerSTDP/output/';
+  
+
+filename = 'RetinaON.pvp';
+path = [output_path, filename];
+plot_on = 0;
+
+%1. Read file
+[data hdr]=readpvpfile(path, output_path, filename);
+
+
+% for f=1:size(data,1)
+%     nx=sqrt(size(data{f}.values{1},4));
+%     ny=sqrt(size(data{f}.values{1},4));
+%     
+% 	wm = zeros(hdr.nxp*nx, hdr.nyp*ny);
+% 	c = 1;
+% 	for x=0:(nx-1)
+% 	    for y=0:(ny-1)
+%             wm(x*hdr.nxp+1:x*hdr.nxp+hdr.nxp, y*hdr.nyp+1:y*hdr.nyp+hdr.nyp) = reshape(data{f}.values{1}(:,:,1,c), hdr.nxp, hdr.nyp);
+%             c=c+1;
+% 	    end%for
+% 	end%for
+% 
+%     if(plot_on)
+%         fig=figure;
+%         imagesc(wm);
+%         colormap(gray);
+%         title(['STDP Weights, t=' num2str(data{f}.time) 'ms']);
+%         colorbar
+%         pause
+%         close(fig);
+%     end    
+% end
