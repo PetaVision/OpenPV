965aadabf6ed10cf10d69e812c6332a9b45a4304
seancolin1 <seancolin1@users.sourceforge.net>
2012-07-20 17:38:42 +0000

    Minimization code needed for CMW phase distribution fitted with Von Mises distribution
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5350 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 08006e259810a0d557e5ea388916931f7ddeea6e

---
+++ Retina/analysis/mises.m
@@ -0,0 +1,19 @@
+# LS objective function
+function obj_value = mises(theta,y,x)
+  m = theta(1,:);	
+  k = theta(2,:);
+[zeroth,ierr]=besselj(0,k);
+     
+
+
+     if ierr ~= 0
+        ierr_st = num2str(ierr);
+        bessel_st = 'bessel call return';
+        print(strcat(ierr_st,bessel_st));
+     end %if
+
+     errors = y - (exp(k*cos(x-m)))/(2*pi*zeroth);   
+	obj_value = errors'*errors;
+   
+
+endfunction	
