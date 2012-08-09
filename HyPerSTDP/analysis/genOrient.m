544bff50c6e6e75db4aa0e08667b438803efece6
ruipcosta <ruipcosta@users.sourceforge.net>
2012-08-09 23:29:32 +0000

    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5424 7c0444ca-5550-0410-9008-faecdbe78eb5
    Former-commit-id: 909293d4477274f05b29b771d7a2143506f73772

--- HyPerSTDP/analysis/genOrient.m
+++ HyPerSTDP/analysis/genOrient.m
@@ -1,8 +1,8 @@
 d = 10
-max = 180;
+max = 360;
 im = imread('/Users/rcosta/Documents/workspace/HyPerSTDP/input/0deg.png');
 
-for r=0:max/10:max    
+for r=0:d:max    
     temp=imrotate(im,r,'nearest','crop');
     figure; imshow(temp);
     imwrite(temp,["/Users/rcosta/Documents/workspace/HyPerSTDP/input/orient/r_" num2str(r) "_deg.png"],'png');
