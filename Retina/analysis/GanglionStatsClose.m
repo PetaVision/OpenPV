8b981257de500bdaab14f825dab006e33e2be2df
seancolin1 <seancolin1@users.sourceforge.net>
2012-08-13 15:58:45 +0000

    Fixed so data is easier to see and new PV_readLayerStatsProbe is used
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5442 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: e0efe9ad4b28bcecfceec7963e3cffbcb4ba219d

--- Retina/analysis/GanglionStatsClose.m
+++ Retina/analysis/GanglionStatsClose.m
@@ -1,25 +1,25 @@
-function GanglionStatsClose(dirname)
-duration = 1000;
-[time,avgON]=PV_readLayerStatsProbe(dirname,"GanglionON",duration);
-[time,avgOFF]=PV_readLayerStatsProbe(dirname,"GanglionOFF",duration);
+
+[time,avgON]=pvp_readLayerStatsProbe("GanglionON");
+[time,avgOFF]=pvp_readLayerStatsProbe("GanglionOFF");
+
+rtime = 1:time;
+
+semilogy(rtime,avgON,"1",rtime,avgOFF,"3");
 
 
-semilogy(time,avgON,"1",time,avgOFF,"3");
-
-
-titlestring = ["Ganglion Spike Response (",dirname,") to 1/f  background with luminance of 128"] 
+titlestring = ["Ganglion Spike Response to 1/f  background with luminance of 128"]; 
 title(titlestring,"fontsize",15);
 
-t1=text (110,5,"ON cells");
-set(t1,'color',[1 0 0],"fontsize",20)
-t2=text (110,2,"OFF cells");
+t1=text (110,1.5,"ON cells");
+set(t1,'color',[1 0 0],"fontsize",20);
+t2=text (110,0.5,"OFF cells");
 set(t2,'color',[0 0 1],"fontsize",20);
-xlabel("time [msec]","fontsize",20);
+xlabel("time [msec]","fontsize",15);
 
-ylabel("Average Spike Frequency in Hz (/dt ms)","fontsize",20);
+ylabel("Average Spike Frequency in Hz (/dt ms)","fontsize",15);
 grid;
-axis([0,200,0.1,300]);
+axis([0 200 0.05 600]);
 ;
  
-outname = ["../../gjkunde/octave/GanglionStats_Close_",dirname,".pdf"]
+outname = ["../octave/GanglionStats_Close",".pdf"]
 print(outname,"-dpdf");
