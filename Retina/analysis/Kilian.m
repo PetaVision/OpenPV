afc21ed0b2b7e61e668cc72fc062c84f7771c070
seancolin1 <seancolin1@users.sourceforge.net>
2012-08-13 16:12:19 +0000

    Fixed averaged histogram (top plot) so adjusted and non-adjusted have same bin size and as a result, adjuted plot sums over trials. LGN cells now have their own vector, fb of CMWs is full width half-maximum for both Ganglion cells and LGN cell. Some other minor changes.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5449 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 727fc679252fa05a15386177f2133a08cae2fc81

--- Retina/analysis/Kilian.m
+++ Retina/analysis/Kilian.m
@@ -2,7 +2,7 @@
 
 function Kilian(run)
 
-% Making initial vectors
+%%---------------------------------------- MAKING INITIAL VECTORS AND GETTING DATA------------------------------------------------------------
 
 [time, Gin_A] = pvp_readPointLIFprobeRun(1,"GanglionON",{'A'});
 [time, N1_A] = pvp_readPointLIFprobeRun(1,"N1GanglionON",{'A'});
@@ -12,18 +12,30 @@ postime = 1:1:time/2;
 rtime = horzcat(negtime,postime);
 rtime = rtime./1000;
 rtime_all = zeros(run,time);
+rtime_LGN = zeros(run,time);
+
+negtimefine = -time/2+1:0.5:0;
+postimefine = 1:0.5:time/2;
+rtimefine = horzcat(-time/2+0.5,negtimefine,0.5,postimefine);
+rtimefine = rtimefine./1000;
 
 N_A = zeros (run,9,length(N1_A));
 G_A = zeros (run,length(Gin_A));
 Allsums = zeros (run,length(N1_A));
+Allsums_nDC =  zeros (run,length(N1_A));
+Allsums_hat = zeros (run,length(N1_A));
+LGN = zeros (run,length(Gin_A));
+LGN_nDC =  zeros (run,length(N1_A));
+LGN_hat = zeros (run,length(N1_A));
 SumT = zeros (1,length(N1_A));
 
-
-phase = zeros(1,run);
 freq = zeros(1,run);
+phase_G = zeros(1,run);
+phase_LGN = zeros(1,run);
+FWHMg = zeros(1,run);
+FWHMlgn = zeros(1,run);
 
-
-%%---------    reading in center and neighbour activities for "run" runs
+% Reading in center and neighbour activities for "run" runs
 for m = 1:run
 
 [time, Gin_A] = pvp_readPointLIFprobeRun(m,"GanglionON",{'A'});
@@ -49,17 +61,32 @@ N_A(m,9,:) = Gin_A;
 
 SumN = N1_A + N2_A + N3_A + N4_A + N5_A + N6_A + N7_A + N8_A+Gin_A;
 Allsums(m,:) = SumN;
-Allsums_nDC =  zeros (run,length(N1_A));
-Allsums_hat = zeros (run,length(N1_A));
+
+%----------------------------------------------------Find phase of Complex Morlet Wavelets for all G cells-------------------------------
 
 
-%Calculating phase of each trial
+%Getting inital values for calculating phase for CMWs of sum of G cells
 
 Allsums_nDC(m,:) = Allsums(m,:) - mean(Allsums(m,:));
 Allsums_hat(m,:) = fft(Allsums_nDC(m,:));
-phase(m) = angle(max(Allsums(m,:)));
 
+%ind = find(Allsums(m,:)); % Get length of phase for CMWs
+
+SumT = SumT + Allsums(m,:);
+
+end %for
+
+time = time/1000;
+
+% Determine frequency bandwidth
+
+for m = 1:run
 [imax,imaxindex] = max(abs(Allsums_hat(m,:)));
+FWHMg(m) = fwhm(abs(Allsums_hat(m,imaxindex-4:imaxindex+4)));
+if FWHMg(m) == 0
+   error(strcat('Trial',num2str(m),'full-width half maximum of ganglions equals zero'));
+end %if
+
 freq(m) = (imaxindex-1)/time;
 
 if freq(m) > 120
@@ -68,65 +95,250 @@ elseif  freq(m) < 40
    freq(m) = freq(m)*2;
 end %if
 
+%phase(m) = angle(max(Allsums(m,:))); % If you want to use phase from Fourier
 
-SumT = SumT + Allsums(m,:);
+% Set bandwith and center frequency parameters
 
-end %for
+fb = (1/(2*pi*FWHMg(m)))^2;
+fc = freq(m);
 
-time_sec = time/1000;
+% Set support and grid parameters
+
+lb = rtimefine(1);
+ub = rtimefine(length(rtimefine));
+n = time*2000;
+
+ind = find(Allsums(m,1:length(Allsums(m,:))/2));
+wav = zeros(length(ind),2*length(N1_A));
+
+ for j = 1:length(ind)
+
+    displ = (time/2-0.001*ind(j));
+    wav(j,:) = cmorwavf(lb,ub,n,fb,fc,displ);
+
+% Finding phase of each CMW across trials
+
+    phase_all(m,j) = angle(wav(j,length(rtimefine)/2));
+
+  end %for j wav
+
+ rtime_all(m,:) = rtime;
+
+% Phase shift
+
+x = -pi:pi/10:pi;
+
+xx = -pi+pi/20:pi/10:pi;
+
+phase_dist = histc(phase_all(m,:),x); % Histogram of phases
+phase_dist = (phase_dist./sum(phase_dist))*(10/pi); % Normalize phase_dist
+phase_dist = phase_dist(1:length(phase_dist)-1);
+
+% Find minimized Von Mises Distribution for phase_dist
+
+theta = [0,1];
+control = {1000000; 0; 1};
+[theta, obj_value, iterations, convergence] = bfgsmin("mises", {theta, phase_dist', xx'}, control);
+
+convergence
+
+mu = theta(1,:);
+k = theta(2,:);
+[zeroth,ierr]=besseli(0,k);
+     
+     if ierr ~= 0
+        ierr_st = num2str(ierr);
+        bessel_st = 'bessel call return';
+        print(strcat(ierr_st,bessel_st));
+     end %if
+
+phase_G(m) = mu;
+
+ind_long = find(Allsums(m,:));
+
+   for j = 1:length(ind_long)
+      rtime_all(m,ind_long(j)) = rtime_all(m,ind_long(j)) + phase_G(m)/(2*pi*freq(m));
+   end %for j
+
+end %for %run
+
+
+
+% Find LGN cell data
+
+for m = 1:run
+
+    for k = 1:8 
+
+        for i = 1:time*1000
+                         
+              if (N_A(m,k,i)~=0) && (G_A(m,i)~=0)
+
+                 LGN(m,i) = 1;
+           
+              end %if
+
+        end %for indices
+    
+    end %for neighbours
+
+end %for run
+
+%----------------------------------------------------Find phase of Complex Morlet Wavelets for LGN cell-------------------------------
+
+
+%Getting inital values for calculating phase for CMWs of LGN cell
+
+for m = 1:run
+
+LGN_nDC(m,:) = LGN(m,:) - mean(LGN(m,:));
+LGN_hat(m,:) = fft(LGN_nDC(m,:));
+
+
+%ind = find(LGN(m,1:length(LGN(m,:))/2)); % Get length of phase for CMWs
+
+[imax,imaxindex] = max(abs(LGN_hat(m,1:length(LGN_hat(m,:))/2)));
+freq(m) = (imaxindex-1)/time;
+
+if freq(m) > 120
+   freq(m) = freq(m)/2;
+elseif  freq(m) < 40
+   freq(m) = freq(m)*2;
+end %if
+
+%phase(m) = angle(max(LGN_hat(m,:))); % If you want to use phase from Fourier
+
+% Calculate frequency bandwidth
+
+FWHMlgn(m) = fwhm(abs(LGN_hat(m,imaxindex-4:imaxindex+4)));
+
+if FWHMlgn(m) == 0
+   error(strcat('Trial',num2str(m),'full-width half maximum of LGN equals zero'));
+end %if
+
+% Set bandwith and center frequency parameters
+
+fb = (1/(FWHMlgn(m)*2*pi))^2;
+fc = freq(m);
+
+% Set support and grid parameters
+
+lb = rtimefine(1);
+ub = rtimefine(length(rtimefine));
+n = time*2000;
+
+ind = find(LGN(m,1:length(LGN(m,:))/2));
+wav = zeros(length(ind),2*length(N1_A));
+
+ for j = 1:length(ind)
+
+    displ = (time/2-0.001*ind(j));
+    wav(j,:) = cmorwavf(lb,ub,n,fb,fc,displ);
+
+% Finding phase of each CMW across trials
+
+    phase_LGNs(m,j) = angle(wav(j,length(rtimefine)/2));
+
+  end %for j wav
+
+% Phase shift
+
+x = -pi:pi/10:pi;
+
+xx = -pi+pi/20:pi/10:pi;
+
+phase_dist = histc(phase_LGNs(m,:),x); % Histogram of phases
+phase_dist = (phase_dist./sum(phase_dist))*(10/pi); % Normalize phase_dist
+phase_dist = phase_dist(1:length(phase_dist)-1);
+
+% Find minimized Von Mises Distribution for phase_dist
+
+theta = [0,1];
+control = {1000000; 0; 1};
+[theta, obj_value, iterations, convergence] = bfgsmin("mises", {theta, phase_dist', xx'}, control);
+
+convergence
+
+mu = theta(1,:);
+k = theta(2,:);
+[zeroth,ierr]=besseli(0,k);
+     
+     if ierr ~= 0
+        ierr_st = num2str(ierr);
+        bessel_st = 'bessel call return';
+        print(strcat(ierr_st,bessel_st));
+     end %if
+
+phase_LGN(m) = mu;
+
+ rtime_LGN(m,:) = rtime;
+
+ind_long = find(LGN(m,:));
+
+% Phase shift
+
+   for j = 1:length(ind_long)
+      rtime_LGN(m,ind_long(j)) = rtime_LGN(m,ind_long(j)) + phase_LGN(m)/(2*pi*freq(m));
+   end %for j
+
+end %for %run
+
+
+% Line params
+
+linelength = 0.4;
+linewidth = 0.3;
+
+%%--------------------------------------------------------NON-ADJUSTED PLOTTING------------------------------------------------------------
+
+
+figure(1);
+clf;
+
+
+%-----------------------------------------------------Averaged Spike rate histogram-------------------------------------------------------
 
 % Averaged spike rate by dividing by number of trials
 
 SumT = SumT./run;
 
-LineLength = 0.2;
-
-clf;
-
-figure(1);
-
-%Bar plot of sum of center Ganglion cell activity
-
 top = subplot(3,1,1);
 
-bar(rtime,SumT,'facecolor','r','edgecolor','r');
+%bar(rtime,SumT,'facecolor','r','edgecolor','r');
+rtimep5 = rtime - 0.0005;
+plot(rtimep5,SumT,'r','LineWidth',1.5);
 
-axis([-time_sec/2 time_sec/2 0 1.5*max(SumT)])
-
-title('Activity of LGN Cell and Neighbouring Ganglion Cells');
+axis([-time/2-time/50 time/2+time/50 0 1.5*max(SumT)])
+title('Non-Phase Adjusted Activity of LGN Cell and Neighbouring Ganglion Cells');
 ylabel('Avg. Spike Rate (1/s)');
 
 set(gca,'XTickLabel',[]);
 
 set(top,'pos',[0.13,0.64,0.774,0.258]);
 
-%Line plot of center Ganglion cell activity
+%------------------------------------------------Line plot of LGN cell activity--------------------------------------------------
 
 middle = subplot(3,1,2);
 
 for m = 1:run
 
-    for j = 1:8 
-
-        for i = 1:time
+        for i = 1:time*1000
                          
-              if (N_A(m,j,i)~=0) && (G_A(m,i)~=0)
+              if LGN(m,i) ~= 0
 
                  x = [rtime(i),rtime(i)];
 
-                 y = [m-LineLength,m+LineLength];
+                 y = [m-linelength,m+linelength];
 
-                 line(x,y,'color','r');
+                 line(x,y,'color','r','LineWidth',linewidth+0.1);
       
               end %if
 
-        end %for %i
-
-    end %for j        
+        end %for %i       
 
 end %for %m
 
-axis([-time_sec/2 time_sec/2 0.1 max(m)*1.5]);
+axis([-time/2-time/50 time/2+time/50 0.1 max(m)*1.5]);
 set(gca,'YTick',[0:max(m)]);
 set(middle,'pos',[0.13,0.38,0.774,0.258]);
 
@@ -135,29 +347,23 @@ set(gca,'XTickLabel',[]);
 
 ylabel('Trial #'); 
 
-%Make line plot of Ganglion neighbours activity
+%------------------------------------------------Line plot of all Ganglion cells activity-----------------------------------------------------
 
 bottom = subplot(3,1,3);
 
 for m = 1:run
 
-rtime_all(m,:) = rtime;
+for k = 1:9
 
-rtime_all(m,:) = rtime_all(m,:);
+    for i = 1:time*1000
 
-%- phase(m)/(1000*2*pi*freq(m));
+       if N_A(m,k,i) ~= 0
 
-for j = 1:9
+         x = [rtime(i)-0.0004+0.0001*k,rtime(i)-0.0004+0.0001*k];
 
-    for i = 1:time
+         y = [m*N_A(m,k,i)-linelength,m*N_A(m,k,i)+linelength];
 
-       if N_A(m,j,i) ~= 0
-
-         x = [rtime_all(m,i)-0.0004+0.00002*j,rtime_all(m,i)-0.0004+0.00002*j];
-
-         y = [m*N_A(m,j,i)-LineLength,m*N_A(m,j,i)+LineLength];
-
-         line(x,y,'color','b');
+         line(x,y,'color','b','LineWidth',linewidth);
 
        end %if
 
@@ -165,20 +371,158 @@ for j = 1:9
 
 end %for %cells
 
-axis([-time_sec/2 time_sec/2 0.1 run*1.5]);
+axis([-time/2-time/50 time/2+time/50 0.1 run*1.5]);
 xlabel('Time (s)');
 ylabel('Trial #');
 
 end %for %runs
 
-%pos = get(bottom,'pos');
-
-%disp(pos);
-
-%pos(2) = pos(2)+0.01;
 set(gca,'YTick',[0:run]);
 set(bottom,'pos',[0.13,0.11958,0.774,0.258]);
 
-xneg = -time_sec/2:0.1:0;
-xpos = 0:0.1:time_sec/2;
+xneg = -time/2:0.1:0;
+xpos = 0:0.1:time/2;
+set(gca,'XTick',horzcat(xneg,xpos));
+
+
+%%----------------------------------------------------------ADJUSTED PLOTTING----------------------------------------------------------------
+
+figure(2);
+clf;
+
+%-----------------------------------------------------Adjusted Average Spike Rate----------------------------------------------------------
+
+% Make vector of rtime values from all phase shifts
+
+rtime_diff = rtime;
+
+for m = 1:run
+    for i = 1:time*1000
+        if rtime_all(m,i) ~= rtime(i)
+           rtime_diff(length(rtime_diff)+1) = rtime_all(m,i);
+        end %if
+    end %for indices
+end %for runs
+
+SumT_diff = zeros(1,length(rtime_diff));
+
+% Sum over trials
+
+% If you want individual cells to affect height
+
+for m = 1:run
+    for ib = 1:length(rtime_diff)
+        for i = 1:length(rtime)
+            if rtime_diff(ib) == rtime_all(m,i);
+               SumT_diff(ib) = SumT_diff(ib) + Allsums(m,i);
+            end % if
+        end %for rtime_all
+    end %for rtime_diff
+end %for runs
+
+% Find histogram values and adjust them for num. cells spiking
+
+shift_count = histc(rtime_diff(length(rtime)+1:length(rtime_diff)),rtime);
+
+[rtime_shift,locs] = sort(rtime_diff);
+for ix = 1:length(locs)
+SumT_shift(ix) = SumT_diff(locs(ix));
+end %for sort rtime values accordingly
+
+indhist = find(shift_count);
+indSumT_shift = find(SumT_shift);
+start = 1;
+
+for j = 1:length(indhist)
+	  heights(j) = sum(SumT_shift(indSumT_shift(start:start-1+shift_count(indhist(j)))));
+    start = start + shift_count(indhist(j));
+    shift_count(indhist(j)) = (shift_count(indhist(j))/shift_count(indhist(j)))*heights(j);
+end %for nz shift_count
+
+shift_count = shift_count./run;
+
+%-----------------------------------------------------Averaged Spike Rate Histogram-------------------------------------------------------
+
+% Averaged spike rate by dividing by number of trials
+
+top = subplot(3,1,1);
+plot(rtimep5,shift_count,'r','LineWidth',1.5);
+
+%plot(rtime_shift,SumT_shift,'r','LineWidth',1.5);
+
+axis([-time/2-time/50 time/2+time/50 0 1.5*max(SumT)])
+
+title('Phase Adjusted Activity of LGN Cell and Neighbouring Ganglion Cells');
+ylabel('Avg. Spike Rate (1/s)');
+
+set(gca,'XTickLabel',[]);
+
+set(top,'pos',[0.13,0.64,0.774,0.258]);
+
+%------------------------------------------------Line plot of LGN cell activity--------------------------------------------------
+
+middle = subplot(3,1,2);
+
+for m = 1:run
+
+        for i = 1:time*1000
+                         
+              if LGN(m,i) ~= 0
+
+                 x = [rtime_LGN(m,i),rtime_LGN(m,i)];
+
+                 y = [m-linelength,m+linelength];
+
+                 line(x,y,'color','r','LineWidth',linewidth+0.1);
+      
+              end %if
+
+        end %for %i       
+
+end %for %m
+
+axis([-time/2-time/50 time/2+time/50 0.1 max(m)*1.5]);
+set(gca,'YTick',[0:max(m)]);
+set(middle,'pos',[0.13,0.38,0.774,0.258]);
+
+set(gca,'XTickLabel',[]);
+
+
+ylabel('Trial #'); 
+
+%--------------------------------------------Line plot of all Ganglion cells activity------------------------------------------------------
+
+bottom = subplot(3,1,3);
+
+for m = 1:run
+
+for k = 1:9
+
+    for i = 1:time*1000
+
+       if N_A(m,k,i) ~= 0
+
+         x = [rtime_all(m,i)-0.0004+0.0001*k,rtime_all(m,i)-0.0004+0.0001*k];
+
+         y = [m*N_A(m,k,i)-linelength,m*N_A(m,k,i)+linelength];
+
+         line(x,y,'color','b','LineWidth',linewidth);
+
+       end %if
+
+     end %for %time
+
+end %for %cells
+
+axis([-time/2-time/50 time/2+time/50 0.1 run*1.5]);
+xlabel('Time (s)');
+ylabel('Trial #');
+
+end %for %runs
+
+set(gca,'YTick',[0:run]);
+set(bottom,'pos',[0.13,0.11958,0.774,0.258]);
+
+xneg = -time/2:0.1:0;
+xpos = 0:0.1:time/2;
 set(gca,'XTick',horzcat(xneg,xpos));
