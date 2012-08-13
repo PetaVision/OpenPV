823831eef1d53582115b0dbc7370fe9b612f224e
seancolin1 <seancolin1@users.sourceforge.net>
2012-08-13 15:53:13 +0000

    Fixed so data is easier to see and new pvp_readPointLIFProbe is used
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5432 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: da752c75b23ce96cf1af1983eefdc7971dabc315

--- Retina/analysis/ConeCalibration_nfb.m
+++ Retina/analysis/ConeCalibration_nfb.m
@@ -1,27 +1,33 @@
-[time,Cone_V_B] =PV_readPointLIFprobe("ConeB",200);
-[time,Cone_V_02]=PV_readPointLIFprobe("Cone02",200);
-[time,Cone_V_04]=PV_readPointLIFprobe("Cone04",200);
-[time,Cone_V_06]=PV_readPointLIFprobe("Cone06",200);
-[time,Cone_V_08]=PV_readPointLIFprobe("Cone08",200);
-[time,Cone_V_10]=PV_readPointLIFprobe("Cone10",200);
+
+[time,Cone_V_B] =pvp_readPointLIFprobe("ConeB",{'V'});
+[time,Cone_V_02]=pvp_readPointLIFprobe("Cone02",{'V'});
+[time,Cone_V_04]=pvp_readPointLIFprobe("Cone04",{'V'});
+[time,Cone_V_06]=pvp_readPointLIFprobe("Cone06",{'V'});
+[time,Cone_V_08]=pvp_readPointLIFprobe("Cone08",{'V'});
+[time,Cone_V_10]=pvp_readPointLIFprobe("Cone10",{'V'});
+
+rtime = 1:time;
+
 plot \
-    (time,Cone_V_B,"0",time,Cone_V_02,"1",time,Cone_V_04,"2",time,Cone_V_06,"3",time,Cone_V_08,"4",time,Cone_V_10,"5");
+(rtime,Cone_V_B,"0",rtime,Cone_V_02-5,"1",rtime,Cone_V_04-10,"2",rtime,Cone_V_06-15,"3",rtime,Cone_V_08-20,"4",rtime,Cone_V_10-25,"5");
+
+n = 2;
 
 title("Cone Calibration Target: -55 to -40 mV");
-t1=text (100,-56,"Black");
+t1=text (100,Cone_V_B(100)-n,"Black");
 set(t1,'color',[0 0 0]);
-t2=text (100,-52,"20%");
+t2=text (100,Cone_V_02(100)-5-n,"20%");
 set(t2,'color',[1 0 0]);
-t3=text (100,-49,"40%");
+t3=text (100,Cone_V_04(100)-10-n,"40%");
 set(t3,'color',[0 1 0]);
-t4=text (100,-46,"60%");
-set(t4,'color',[0 0 1])
-t5=text (100,-43,"80%");
+t4=text (100,Cone_V_06(100)-15-n,"60%");
+set(t4,'color',[0 0 1]);
+t5=text (100,Cone_V_08(100)-20-n,"80%");
 set(t5,'color',[1 0 1]);
-t5=text (100,-41,"100%");
+t5=text (100,Cone_V_10(100)-25-n,"100%");
 set(t5,'color',[0 1 1]);
 xlabel("time [msec]");
 ylabel("Membrane Potential [mV]");
-text(40,-37,"tau = 10 msec, strength = 0.1475, no feedback");
+text(40,-80,"tau = 10 msec, strength = 0.1475, no feedback");
 
 
