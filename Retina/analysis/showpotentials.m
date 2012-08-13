8c75c361e78b533b5f990c6cc75cc42c5ea8a55b
seancolin1 <seancolin1@users.sourceforge.net>
2012-08-13 16:25:47 +0000

    No longer specific to gjkunde
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5456 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 6d3c7ff16cc236bee35f38f54fca32e0adc35a01

--- Retina/analysis/showpotentials.m
+++ Retina/analysis/showpotentials.m
@@ -1,6 +1,6 @@
 figure
 colormap("gray")
-PAAON_V5 = readpvpfile("../../gjkunde/graystart/Checkpoint5/PAAmacrineON_V.pvp");
+PAAON_V5 = readpvpfile("../graystart/Checkpoint5/PAAmacrineON_V.pvp");
 PAA = PAAON_V5{1}.values;
 imagesc(PAA)
 colorbar
@@ -8,7 +8,7 @@ mean(PAA(:))
 std(PAA(:))
 figure
 colormap("gray")
-GanglionON_V5 = readpvpfile("../../gjkunde/graystart/Checkpoint5/GanglionON_V.pvp");
+GanglionON_V5 = readpvpfile("../graystart/Checkpoint5/GanglionON_V.pvp");
 Ganglion = GanglionON_V5{1}.values;
 imagesc(Ganglion)
 colorbar
