91314d0d133783ecc29a3f10cf4830583a398f48
seancolin1 <seancolin1@users.sourceforge.net>
2012-08-13 15:59:17 +0000

    Fixed so data is easier to see and new PV_readLayerStatsProbe is used
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5443 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: d24080dd0b00e43fe61703472b47aeac8868ac5d

--- Retina/analysis/GanglionXCorrelationActivity.m
+++ Retina/analysis/GanglionXCorrelationActivity.m
@@ -1,35 +1,68 @@
-duration = 600;
-[time,Ganglion_RU1] =PV_readPointLIFprobeA("GanglionRU1",duration);
-[time,Ganglion_RD1] =PV_readPointLIFprobeA("GanglionRD1",duration);
-[time,Ganglion_RU2] =PV_readPointLIFprobeA("GanglionRU2",duration);
-[time,Ganglion_RD2] =PV_readPointLIFprobeA("GanglionRD2",duration);
-[time,Ganglion_RU1l] =PV_readPointLIFprobeA("GanglionRU1l",duration);
-[time,Ganglion_RD1l] =PV_readPointLIFprobeA("GanglionRD1l",duration);
-[time,Ganglion_RU2l] =PV_readPointLIFprobeA("GanglionRU2l",duration);
-[time,Ganglion_RD2l] =PV_readPointLIFprobeA("GanglionRD2l",duration);
-[time,Ganglion_RU1r] =PV_readPointLIFprobeA("GanglionRU1r",duration);
-[time,Ganglion_RD1r] =PV_readPointLIFprobeA("GanglionRD1r",duration);
-[time,Ganglion_RU2r] =PV_readPointLIFprobeA("GanglionRU2r",duration);
-[time,Ganglion_RD2r] =PV_readPointLIFprobeA("GanglionRD2r",duration);
 
-gu1  = Ganglion_RU1(100:500) % only the stimulated time frame
-gd1  = Ganglion_RD1(100:500) % only the stimulated time frame
-gu2  = Ganglion_RU2(100:500) % only the stimulated time frame
-gd2  = Ganglion_RD2(100:500) % only the stimulated time frame
+[time,GanglionON_RU1] =pvp_readPointLIFprobe("GanglionONRU1",{'A'});
+[time,GanglionON_RD1] =pvp_readPointLIFprobe("GanglionONRD1",{'A'});
+[time,GanglionON_RU2] =pvp_readPointLIFprobe("GanglionONRU2",{'A'});
+[time,GanglionON_RD2] =pvp_readPointLIFprobe("GanglionONRD2",{'A'});
+[time,GanglionON_RU1l] =pvp_readPointLIFprobe("GanglionONRU1l",{'A'});
+[time,GanglionON_RD1l] =pvp_readPointLIFprobe("GanglionONRD1l",{'A'});
+[time,GanglionON_RU2l] =pvp_readPointLIFprobe("GanglionONRU2l",{'A'});
+[time,GanglionON_RD2l] =pvp_readPointLIFprobe("GanglionONRD2l",{'A'});
+[time,GanglionON_RU1r] =pvp_readPointLIFprobe("GanglionONRU1r",{'A'});
+[time,GanglionON_RD1r] =pvp_readPointLIFprobe("GanglionONRD1r",{'A'});
+[time,GanglionON_RU2r] =pvp_readPointLIFprobe("GanglionONRU2r",{'A'});
+[time,GanglionON_RD2r] =pvp_readPointLIFprobe("GanglionONRD2r",{'A'});
 
-gu1l  = Ganglion_RU1l(100:500) % only the stimulated time frame
-gd1l  = Ganglion_RD1l(100:500) % only the stimulated time frame
-gu2l  = Ganglion_RU2l(100:500) % only the stimulated time frame
-gd2l  = Ganglion_RD2l(100:500) % only the stimulated time frame
+[time,GanglionOFF_RU1] =pvp_readPointLIFprobe("GanglionOFFRU1",{'A'});
+[time,GanglionOFF_RD1] =pvp_readPointLIFprobe("GanglionOFFRD1",{'A'});
+[time,GanglionOFF_RU2] =pvp_readPointLIFprobe("GanglionOFFRU2",{'A'});
+[time,GanglionOFF_RD2] =pvp_readPointLIFprobe("GanglionOFFRD2",{'A'});
+[time,GanglionOFF_RU1l] =pvp_readPointLIFprobe("GanglionOFFRU1l",{'A'});
+[time,GanglionOFF_RD1l] =pvp_readPointLIFprobe("GanglionOFFRD1l",{'A'});
+[time,GanglionOFF_RU2l] =pvp_readPointLIFprobe("GanglionOFFRU2l",{'A'});
+[time,GanglionOFF_RD2l] =pvp_readPointLIFprobe("GanglionOFFRD2l",{'A'});
+[time,GanglionOFF_RU1r] =pvp_readPointLIFprobe("GanglionOFFRU1r",{'A'});
+[time,GanglionOFF_RD1r] =pvp_readPointLIFprobe("GanglionOFFRD1r",{'A'});
+[time,GanglionOFF_RU2r] =pvp_readPointLIFprobe("GanglionOFFRU2r",{'A'});
+[time,GanglionOFF_RD2r] =pvp_readPointLIFprobe("GanglionOFFRD2r",{'A'});
 
-gu1r  = Ganglion_RU1r(100:500) % only the stimulated time frame
-gd1r  = Ganglion_RD1r(100:500) % only the stimulated time frame
-gu2r  = Ganglion_RU2r(100:500) % only the stimulated time frame
-gd2r  = Ganglion_RD2r(100:500) % only the stimulated time frame
+%ON
+
+guON1  = GanglionON_RU1(100:500) % only the stimulated time frame
+gdON1  = GanglionON_RD1(100:500) % only the stimulated time frame
+guON2  = GanglionON_RU2(100:500) % only the stimulated time frame
+gdON2  = GanglionON_RD2(100:500) % only the stimulated time frame
+
+guON1l  = GanglionON_RU1l(100:500) % only the stimulated time frame
+gdON1l  = GanglionON_RD1l(100:500) % only the stimulated time frame
+guON2l  = GanglionON_RU2l(100:500) % only the stimulated time frame
+gdON2l  = GanglionON_RD2l(100:500) % only the stimulated time frame
+
+guON1r  = GanglionON_RU1r(100:500) % only the stimulated time frame
+gdON1r  = GanglionON_RD1r(100:500) % only the stimulated time frame
+guON2r  = GanglionON_RU2r(100:500) % only the stimulated time frame
+gdON2r  = GanglionON_RD2r(100:500) % only the stimulated time frame
+
+%OFF
+
+guOFF1  = GanglionOFF_RU1(100:500) % only the stimulated time frame
+gdOFF1  = GanglionOFF_RD1(100:500) % only the stimulated time frame
+guOFF2  = GanglionOFF_RU2(100:500) % only the stimulated time frame
+gdOFF2  = GanglionOFF_RD2(100:500) % only the stimulated time frame
+
+guOFF1l  = GanglionOFF_RU1l(100:500) % only the stimulated time frame
+gdOFF1l  = GanglionOFF_RD1l(100:500) % only the stimulated time frame
+guOFF2l  = GanglionOFF_RU2l(100:500) % only the stimulated time frame
+gdOFF2l  = GanglionOFF_RD2l(100:500) % only the stimulated time frame
+
+guOFF1r  = GanglionOFF_RU1r(100:500) % only the stimulated time frame
+gdOFF1r  = GanglionOFF_RD1r(100:500) % only the stimulated time frame
+guOFF2r  = GanglionOFF_RU2r(100:500) % only the stimulated time frame
+gdOFF2r  = GanglionOFF_RD2r(100:500) % only the stimulated time frame
+
 
 clf;
 xrange=50;
-[R,lag] = xcorr(gu1,gu1,'unbiased'); lagt=lag',plot(lagt,R,"1"),xlim([-1.*xrange,xrange]),grid;hold on;
-[R,lag] = xcorr(gu1,gu1l,'unbiased'); lagt=lag',plot(lagt,R,"2",xlim([-1.*xrange,xrange]));
-[R,lag] = xcorr(gu1,gu1r,'unbiased'); lagt=lag',plot(lagt,R,"3"),xlim([-1.*xrange,xrange]);
+[R,lag] = xcorr(guON1,guOFF1,'unbiased'); lagt=lag',plot(lagt,R,"1"),xlim([-1.*xrange,xrange]),grid;hold on;
+[R,lag] = xcorr(guON1,guON1l,'unbiased'); lagt=lag',plot(lagt,R,"2",xlim([-1.*xrange,xrange]));
+[R,lag] = xcorr(guON1,guON1r,'unbiased'); lagt=lag',plot(lagt,R,"3"),xlim([-1.*xrange,xrange]);
 hold off;
