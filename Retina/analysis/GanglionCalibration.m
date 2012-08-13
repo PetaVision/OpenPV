088018de146870716ade076459dac16887d79f5e
seancolin1 <seancolin1@users.sourceforge.net>
2012-08-13 15:55:31 +0000

    Fixed so data is easier to see and new pvp_readPointLIFProbe is used
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5437 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 501300795c00bdc71c3fabfa0354766b5a8b7af3

--- Retina/analysis/GanglionCalibration.m
+++ Retina/analysis/GanglionCalibration.m
@@ -1,61 +1,51 @@
-function GanglionCalibration(duration = 1000,filename)
-[time,Horizontal_V_10]=PV_readPointLIFprobe("Horizontal",duration);
-[time,Bipolar_V_10]=PV_readPointLIFprobe("BipolarON",duration);
-[time,Ganglion_V_10,Ganglion_A_10,Ganglion_T_10]=PV_readPointLIFprobeVAT("GanglionON",duration);
-[time,Amacrine_V_10,Amacrine_A_10,Amacrine_T_10]=PV_readPointLIFprobeVAT("PAAmacrineON",duration);
-[time,Ganglion_E_10,Ganglion_I_10,Ganglion_IB_10]=PV_readPointLIFprobeG("GanglionON",duration);
 
-figure
+[time,Horizontal_V_10]=pvp_readPointLIFprobe("Horizontal",{'V'});
+[time,Bipolar_V_10]=pvp_readPointLIFprobe("BipolarON",{'V'});
+[time,Ganglion_V_10,Ganglion_A_10,Ganglion_T_10]=pvp_readPointLIFprobe("GanglionON",{'V','A','Vth'});
+[time,Amacrine_V_10,Amacrine_A_10,Amacrine_T_10]=pvp_readPointLIFprobe("PAAmacrineON",{'V','A','Vth'});
+[time,Ganglion_E_10,Ganglion_I_10,Ganglion_IB_10]=pvp_readPointLIFprobe("GanglionON",{'G_E','G_I','G_IB'});
 
-plot \
-    (time,Horizontal_V_10,"2",time,Bipolar_V_10,"3",time,Ganglion_V_10,"4",time,Ganglion_T_10,"4",time,Amacrine_V_10,"5",time,Amacrine_T_10,"5",time,Ganglion_A_10*5-40,"4",time,Amacrine_A_10*5-45,"5",time,Ganglion_E_10*10-80,"4",Ganglion_I_10*10-85,"4",Ganglion_IB_10*10-90,"4");
+rtime = 1:time;
 
-title("Ganglion ON for center");
-t4=text (time(1)-25,-55,"Bipolar");
-set(t4,'color',[0 0 1])
-t5=text (time(1)-25,-60,"Ganglion");
-set(t5,'color',[1 0 1]);
-t5=text (time(1)-25,-65,"Amacrine");
-set(t5,'color',[0 1 1]);
+
+
+subplot(1,2,1);
+plot(rtime,Ganglion_V_10,"4",rtime,Ganglion_T_10+4,"4",rtime,Ganglion_A_10*5-40,"4",rtime,Ganglion_E_10*10-40,"4",rtime,Ganglion_I_10*10-90,"4",rtime,Ganglion_IB_10*10-90,"4");
+axis([0 500 -95 -25]);
+title("Ganglion ON Properties for center");
 xlabel("time [msec]");
 ylabel("Membrane Potential [mV]");
+t1=text (100, Ganglion_V_10(100)-19, "Membrane Potential");
+set(t1,'color',[1 0 1]);
+t2=text (100,Ganglion_T_10(100)+2,"mV Threshold");
+set(t2,'color',[1 0 1]);
+t3=text (100,Ganglion_A_10(100)*5-42,"Activity");
+set(t3,'color',[1 0 1]);
+
+subplot(1,2,2);
+plot(rtime,Horizontal_V_10,"2",rtime,Bipolar_V_10,"3",rtime,Amacrine_V_10,"5",rtime,Amacrine_T_10+15,"5",rtime,Amacrine_A_10*5-20,"5");
+%(rtime,Horizontal_V_10-90,"2",rtime,Bipolar_V_10,"3",rtime,Ganglion_V_10-80,"4",rtime,Ganglion_T_10-70,"4",rtime,Amacrine_V_10,"5",rtime,Amacrine_T_10,"5",rtime,Ganglion_A_10*5-40,"4",rtime,Amacrine_A_10*5-45,"5",rtime,Ganglion_E_10*10-80,"4",rtime,Ganglion_I_10*10-85,"4",rtime,Ganglion_IB_10*10-90,"4");
+
+title("Other cells for Ganglion ON center");
+xlabel("time [msec]");
+ylabel("Membrane Potential [mV]");
+t1=text (70,Amacrine_A_10(100)*5-30,"Activity of Amacrine");
+set(t1,'color',[0 1 1]);
+t2=text (70,Amacrine_T_10(100)+7,"mV Threshold of Amacrine");
+set(t2,'color',[0 1 1]);
+t3=text (70,Amacrine_V_10(100)-10,"mV of Amacrine");
+set(t3,'color',[0 1 1]);
+t4=text (70,Bipolar_V_10(100)-10,"mV of Bipolar");
+set(t4,'color',[0 0 1]);
+t5=text (70,Horizontal_V_10(100)-10,"mV of Horizontal");
+set(t5,'color',[0 1 0]);
 grid;
 
 
 
-outname = [filename,"ON.pdf"]
-print(outname,"-dpdf");
-outname = [filename,"ON.jpeg"]
-print(outname,"-djpeg");
+%outname = [filename,"ON.pdf"]
+%print(outname,"-dpdf");
+%outname = [filename,"ON.jpeg"]
+%print(outname,"-djpeg");
 
 
-[time,Horizontal_V_10]=PV_readPointLIFprobe("Horizontal",duration);
-[time,Bipolar_V_10]=PV_readPointLIFprobe("BipolarOFF",duration);
-[time,Ganglion_V_10,Ganglion_A_10,Ganglion_T_10]=PV_readPointLIFprobeVAT("GanglionOFF",duration);
-[time,Amacrine_V_10,Amacrine_A_10,Amacrine_T_10]=PV_readPointLIFprobeVAT("PAAmacrineOFF",duration);
-[time,Ganglion_E_10,Ganglion_I_10,Ganglion_IB_10]=PV_readPointLIFprobeG("GanglionOFF",duration);
-
-
-
-figure
-
-
-plot \
-    (time,Horizontal_V_10,"2",time,Bipolar_V_10,"3",time,Ganglion_V_10,"4",time,Ganglion_T_10,"4",time,Amacrine_V_10,"5",time,Amacrine_T_10,"5",time,Ganglion_A_10*5-40,"4",time,Amacrine_A_10*5-45,"5",time,Ganglion_E_10*10-80,"4",Ganglion_I_10*10-85,"4",Ganglion_IB_10*10-90,"4");
-
-title("Ganglion OFF for center");
-t4=text (time(1)-25,-55,"Bipolar");
-set(t4,'color',[0 0 1])
-t5=text (time(1)-25,-60,"Ganglion");
-set(t5,'color',[1 0 1]);
-t5=text (time(1)-25,-65,"Amacrine");
-set(t5,'color',[0 1 1]);
-xlabel("time [msec]");
-ylabel("Membrane Potential [mV]");
-grid;
-
-outname = [filename,"OFF.pdf"]
-print(outname,"-dpdf");
-
-outname = [filename,"OFF.jpeg"]
-print(outname,"-djpeg");
