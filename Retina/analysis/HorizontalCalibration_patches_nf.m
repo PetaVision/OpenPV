0893852270252c9413184651f170dd89274f1f5f
seancolin1 <seancolin1@users.sourceforge.net>
2012-08-13 16:04:03 +0000

    Fixed so data is easier to see and new pvp_readPointLIFProbe is used
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5446 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 43b42eba43afac835205a6dc9c51b7a932ffbfba

--- Retina/analysis/HorizontalCalibration_patches_nf.m
+++ Retina/analysis/HorizontalCalibration_patches_nf.m
@@ -1,29 +1,34 @@
-duration = 400;
-[time,Horizontal_V_B] =PV_readPointLIFprobe("HorizontalB",duration);
-[time,Horizontal_V_P1]=PV_readPointLIFprobe("HorizontalP1",duration);
-[time,Horizontal_V_P3]=PV_readPointLIFprobe("HorizontalP3",duration);
-[time,Horizontal_V_P5]=PV_readPointLIFprobe("HorizontalP5",duration);
-[time,Horizontal_V_P7]=PV_readPointLIFprobe("HorizontalP7",duration);
-[time,Horizontal_V_P9]=PV_readPointLIFprobe("HorizontalP9",duration);
+
+[time,Horizontal_V_B]=pvp_readPointLIFprobe("HorizontalB",{'V'});
+[time,Horizontal_V_P1]=pvp_readPointLIFprobe("HorizontalP1",{'V'});
+[time,Horizontal_V_P3]=pvp_readPointLIFprobe("HorizontalP3",{'V'});
+[time,Horizontal_V_P5]=pvp_readPointLIFprobe("HorizontalP5",{'V'});
+[time,Horizontal_V_P7]=pvp_readPointLIFprobe("HorizontalP7",{'V'});
+[time,Horizontal_V_P9]=pvp_readPointLIFprobe("HorizontalP9",{'V'});
+
+rtime = 1:time;
+
 plot \
-    (time,Horizontal_V_B,"0",time,Horizontal_V_P1,"1",time,Horizontal_V_P3,"2",time,Horizontal_V_P5,"3",time,Horizontal_V_P7,"4",time,Horizontal_V_P9,"5");
+    (rtime,Horizontal_V_B,"0",rtime,Horizontal_V_P1-30,"1",rtime,Horizontal_V_P3-60,"2",rtime,Horizontal_V_P5-90,"3",rtime,Horizontal_V_P7-120,"4",rtime,Horizontal_V_P9-150,"5");
+
+n = 8;
 
 title("Horizontal Patchsize Response");
-t1=text (50,-54,"Black");
+t1=text (80,Horizontal_V_B(100)-10,"Black");
 set(t1,'color',[0 0 0]);
-t2=text (50,-50,"1x1");
+t2=text (80,Horizontal_V_P1(100)-30-n,"1x1");
 set(t2,'color',[1 0 0]);
-t3=text (50,-46,"3x3");
+t3=text (80,Horizontal_V_P3(100)-60-n,"3x3");
 set(t3,'color',[0 1 0]);
-t4=text (50,-42,"5x5");
+t4=text (80,Horizontal_V_P5(100)-90-n,"5x5");
 set(t4,'color',[0 0 1])
-t5=text (50,-38,"7x7");
+t5=text (80,Horizontal_V_P7(100)-120-n,"7x7");
 set(t5,'color',[1 0 1]);
-t5=text (50,-34,"9x9");
-set(t5,'color',[0 1 1]);
+t6=text (80,Horizontal_V_P9(100)-150-n,"9x9");
+set(t6,'color',[0 1 1]);
 xlabel("time [msec]");
 ylabel("Membrane Potential [mV]");
-axis([0,400,-60,-48]);
+axis([0 500 -350 -50]);
 grid;
 
 
