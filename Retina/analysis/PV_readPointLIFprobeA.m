2cddd59d3c091b08689998277a0df2c59f9b21a0
seancolin1 <seancolin1@users.sourceforge.net>
2012-08-13 16:21:50 +0000

    Implemented new method for reading files
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5454 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: a6da47d68ae9e5ff89fed9cfcb2268eb698408b4

--- Retina/analysis/PV_readPointLIFprobeA.m
+++ Retina/analysis/PV_readPointLIFprobeA.m
@@ -1,9 +1,10 @@
 
-function [time,A] = PV_readPointLIFprobeA(filename,nsteps)
+function [time,V] = PV_readPointLIFprobeA(filename,nsteps)
 
-filename = ["../../gjkunde/output/pt",filename,".txt"]
+filename = ["../output/pt",filename,".txt"]
 myfile = fopen(filename,"r");
 if myfile == -1
+  error('File not found');
   return;
 endif
 
@@ -14,7 +15,7 @@ G_IB = [];
 V    = [];
 Vth  = [];
 A    = [];
-
+location = [];
 
 time = zeros(nsteps, 1);
 G_E  = zeros(nsteps, 1);
@@ -23,21 +24,80 @@ G_IB = zeros(nsteps, 1);
 V    = zeros(nsteps, 1);
 Vth  = zeros(nsteps, 1);
 A    = zeros(nsteps, 1);
+location = zeros(nsteps, 1);
 
+  for i_step = 1:nsteps
+     line_str = fgets(myfile);
 
+     name_pos = index(line_str, ":");
+     name_tmp = line_str(1:name_pos-1);
 
-for i_step = 1:nsteps
-    name         = fscanf(myfile, '%s', 1);
-    time(i_step) = fscanf(myfile, ' t=%f', 1);
-    location     = fscanf(myfile, ' k=%i', 1);
-    G_E(i_step)  = fscanf(myfile, ' G_E=%f', 1);
-    G_I(i_step)  = fscanf(myfile, ' G_I=%f', 1);
-    G_IB(i_step) = fscanf(myfile, ' G_IB=%f', 1);
-    V(i_step)    = fscanf(myfile, ' V=%f', 1);
-    Vth(i_step)  = fscanf(myfile, ' Vth=%f', 1);
-    R            = fscanf(myfile, ' R= %f', 1);
-    A(i_step)    = fscanf(myfile, ' a=%f\n', 1);
+     time_begin_ndx = index(line_str, "t=");
+     time_end_ndx = index(line_str, "k=");
+     time_tmp_str = line_str(time_begin_ndx+2:time_end_ndx-1);
+     time(i_step) = str2double(time_tmp_str);
+    
+     location_begin_ndx = index(line_str, "k=");
+     location_end_ndx = index(line_str, "G_E=");
+     location_tmp_str = line_str(location_begin_ndx+2:location_end_ndx-1);
+     location(i_step) = str2double(location_tmp_str);
+
+     G_E_begin_ndx = index(line_str, "G_E=");
+     G_E_end_ndx = index(line_str, "G_I=");
+     G_E_tmp_str = line_str(G_E_begin_ndx+5:G_E_end_ndx-1);
+     G_E(i_step) = str2double(G_E_tmp_str);
+
+     G_I_begin_ndx = index(line_str, "G_I=");
+     G_I_end_ndx = index(line_str, "G_IB=");
+     G_I_tmp_str = line_str(G_I_begin_ndx+5:G_I_end_ndx-1);
+     G_I(i_step) = str2double(G_I_tmp_str);
+
+     G_IB_begin_ndx = index(line_str, "G_IB=");
+     G_IB_end_ndx = index(line_str, "V=");
+     G_IB_tmp_str = line_str(G_IB_begin_ndx+6:G_IB_end_ndx-1);
+     G_IB(i_step) = str2double(G_IB_tmp_str);
+   
+     V_begin_ndx = index(line_str, "V=");
+     V_end_ndx = index(line_str, "Vth=");
+     V_tmp_str = line_str(V_begin_ndx+2:V_end_ndx-1);
+     V(i_step) = str2double(V_tmp_str);
+
+     Vth_begin_ndx = index(line_str, "Vth=");
+     Vth_end_ndx = index(line_str, "a=");
+     Vth_tmp_str = line_str(Vth_begin_ndx+4:Vth_end_ndx-2);
+     Vth(i_step) = str2double(Vth_tmp_str);
+        
+     A_begin_ndx = index(line_str, "a=");
+     A_tmp_str = line_str(A_begin_ndx+2:84);
+     A(i_step) = str2double(A_tmp_str);
+
 end
+
+
+%BB = zeros(nsteps,8);
+%BB(:,1) = time(i_step);
+%BB(:,2) = location(i_step);
+%BB(:,3) = G_E(i_step);
+%BB(:,4) = G_I(i_step);
+%BB(:,5) = G_IB(i_step);
+%BB(:,6) = V(i_step);
+%BB(:,7) = Vth(i_step);
+%BB(:,8) = A(i_step);
+
+%disp(BB);
+
+%name = fscanf(myfile, "%s", 1)
+%time = fscanf(myfile," t=%3.1f", 1)
+%location = fscanf(myfile, " k=%i", 1)
+%G_E  = fscanf(myfile, " G_E=%5.3f", 1)
+%G_I  = fscanf(myfile, " G_I=%5.3f", 1)
+%G_IB = fscanf(myfile, " G_IB=%5.3f", 1)
+%V    = fscanf(myfile, " V=%6.3f", 1)
+%Vth  = fscanf(myfile, " Vth=%10.3f", 1)
+%R            = fscanf(myfile, " R= %f", 1)
+%A_tmp    = fscanf(myfile, " a=%f\n", 1)
+
+
 
 fclose(myfile);
 
