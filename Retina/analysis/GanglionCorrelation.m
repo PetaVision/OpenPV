228377fe875f1e24468d61ba96f5786ef19e663a
seancolin1 <seancolin1@users.sourceforge.net>
2012-08-13 15:56:05 +0000

    Fixed so data is easier to see and new pvp_readPointLIFProbe is used
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5439 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 3a0c35ab1e233ed88321a798dec016d9693230df

--- Retina/analysis/GanglionCorrelation.m
+++ Retina/analysis/GanglionCorrelation.m
@@ -1,8 +1,8 @@
-duration = 600;
-[time,Ganglion_RU1] =PV_readPointLIFprobe("GanglionRU1",duration);
-[time,Ganglion_RD1] =PV_readPointLIFprobe("GanglionRD1",duration);
-[time,Ganglion_RU2] =PV_readPointLIFprobe("GanglionRU2",duration);
-[time,Ganglion_RD2] =PV_readPointLIFprobe("GanglionRD2",duration);
+
+[time,Ganglion_RU1]=pvp_readPointLIFprobe("GanglionRU1",{'V'});
+[time,Ganglion_RD1]=pvp_readPointLIFprobe("GanglionRD1",{'V'});
+[time,Ganglion_RU2]=pvp_readPointLIFprobe("GanglionRU2",{'V'});
+[time,Ganglion_RD2]=pvp_readPointLIFprobe("GanglionRD2",{'V'});
 size time;
 S1 = (Ganglion_RU1-Ganglion_RD1);
 S2 = (Ganglion_RU2-Ganglion_RD2);
@@ -11,8 +11,10 @@ D  = (Ganglion_RD1-Ganglion_RD2);
 
 %[R,lag] = xcorr(Ganglion_RU1,Ganglion_RU1);
 
+rtime = 1:time;
+
 plot \
-    (time,Ganglion_RU1,"1",time,Ganglion_RD1-50,"2",time,Ganglion_RU2-100,"3",time,Ganglion_RD2-150,"4",time,S1-85,"5",time,S2-185,"5",U-250,"0",time,D-300,"0");
+    (rtime,Ganglion_RU1,"1",rtime,Ganglion_RD1-50,"2",rtime,Ganglion_RU2-100,"3",rtime,Ganglion_RD2-150,"4",rtime,S1-85,"5",rtime,S2-185,"5",U-250,"0",rtime,D-300,"0");
 
 title("Ganglion Rectangle Correlations");
 
@@ -28,4 +30,4 @@ xlabel("time [msec]");
 ylabel("Membrane Potential [mV]");
 grid;
 %%axis([0,600,-350,-40]);
-print -dgif ../../gjkunde/octave/GanglionCorrelations.gif
+print -dgif ../octave/GanglionCorrelations.gif
