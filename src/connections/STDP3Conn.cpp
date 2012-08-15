4391bb70fb0b3e9088f278c6f6d6e213c9a8a251
ruipcosta <ruipcosta@users.sourceforge.net>
2012-08-15 23:54:58 +0000

    Online implementation of minimalist triplet STDP learning rule.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@5476 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/connections/STDP3Conn.cpp
@@ -0,0 +1,367 @@
+/*
+ * Triplet-based STDP with online implementation (the minimal version)
+ * (see more at: Pfister et al. 2006)
+ *
+ *  Created on: Jul 24, 2012
+ *      Author: Rui P. Costa
+ *
+ */
+
+#include "STDP3Conn.hpp"
+#include "../layers/LIF.hpp"
+#include "../io/io.h"
+#include <assert.h>
+#include <math.h>
+
+namespace PV {
+
+STDP3Conn::STDP3Conn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+                   const char * filename, bool stdpFlag,
+                   InitWeights *weightInit) : HyPerConn()
+{
+   initialize_base();
+   initialize(name, hc, pre, post, filename, stdpFlag, weightInit);
+}
+
+STDP3Conn::~STDP3Conn()
+{
+   deleteWeights();
+}
+
+int STDP3Conn::initialize_base() {
+   // Default STDP parameters for modifying weights; defaults are overridden in setParams().
+   // this->dwPatches = NULL;
+   this->post_tr = NULL;
+   this->ampLTP = 0.0065;
+   this->ampLTD = 0.0071;
+   this->tauLTP = 16.8;
+   this->tauLTD = 33.7;
+   this->tauY = 114;
+   this->dWMax = 1;
+   // TODO: Set the default values for wMin and wMax? Or are they already set somewhere?
+   return PV_SUCCESS;
+}
+
+int STDP3Conn::initialize(const char * name, HyPerCol * hc,
+                         HyPerLayer * pre, HyPerLayer * post,
+                         const char * filename, bool stdpFlag, InitWeights *weightInit)
+{
+   this->stdpFlag = stdpFlag; //needs to be before call to HyPerConn::initialize since it calls overridden methods that depend on stdpFlag being set.
+   int status = HyPerConn::initialize(name, hc, pre, post, filename, weightInit);
+
+   status |= setParams(hc->parameters()); // needs to be called after HyPerConn::initialize since it depends on post being set
+   status |= initPlasticityPatches();
+
+   return status;
+}
+
+int STDP3Conn::initPlasticityPatches()
+{
+   if (!stdpFlag) return PV_SUCCESS;
+
+   int status = HyPerConn::initPlasticityPatches();
+   assert(status == 0);
+   //const int arbor = 0;
+   //const int numAxons = numberOfAxonalArborLists();
+
+//   dwPatches = createWeights(NULL, numWeightPatches(), nxp, nyp, nfp, 0);
+   post_tr = pvcube_new(&post->getCLayer()->loc, post->getNumExtended());
+   post2_tr = pvcube_new(&post->getCLayer()->loc, post->getNumExtended());
+   pre_tr = pvcube_new(&pre->getCLayer()->loc, pre->getNumExtended());
+   assert(post_tr != NULL);
+   assert(post2_tr != NULL);
+   assert(pre_tr != NULL);
+
+   return PV_SUCCESS;
+}
+
+int STDP3Conn::deleteWeights()
+{
+   if (stdpFlag) {
+      //dwPatches belongs to HyPerConn, so it is deleted in HyPerConn::deleteWeights()
+      // const int numPatches = numWeightPatches();
+      // const int numAxons = numberOfAxonalArborLists();
+      // for (int n = 0; n < numAxons; n++) {
+      //    for (int k = 0; k < numPatches; k++) {
+      //       pvpatch_inplace_delete(dwPatches[n][k]);
+      //    }
+      //    free(dwPatches[n]);
+      // }
+      // free(dwPatches);
+      pvcube_delete(post_tr);
+      post_tr = NULL;
+   }
+   return 0;
+}
+
+int STDP3Conn::initializeThreadBuffers()
+{
+   return 0;
+}
+
+int STDP3Conn::initializeThreadKernels()
+{
+   return 0;
+}
+
+PVLayerCube * STDP3Conn::getPlasticityDecrement()
+{
+   return post_tr;
+}
+
+// set member variables specified by user
+/*
+ * Using a dynamic_cast operator to convert (downcast) a pointer to a base class (HyPerLayer)
+ * to a pointer to a derived class (LIF). This way I do not need to define a virtual
+ * function getWmax() in HyPerLayer which only returns a NULL pointer in the base class.
+ */
+int STDP3Conn::setParams(PVParams * params)
+{
+   // stdpFlag is now set by constructor
+   // stdpFlag = (bool) filep->value(getName(), "stdpFlag", (float) stdpFlag);
+   HyPerConn::setParams(params);
+
+   if (stdpFlag) {
+      ampLTP = params->value(getName(), "ampLTP", ampLTP);
+      ampLTD = params->value(getName(), "ampLTD", ampLTD);
+      tauLTP = params->value(getName(), "tauLTP", tauLTP);
+      tauLTD = params->value(getName(), "tauLTD", tauLTD);
+      tauY = params->value(getName(), "tauY", tauY);
+
+      wMax = params->value(getName(), "wMax", wMax);
+      wMin = params->value(getName(), "wMin", wMin);
+      dWMax = params->value(getName(), "dWMax", dWMax);
+
+   }
+
+   return 0;
+}
+
+/**
+ * First function to be executed
+ * Updates the postsynaptic trace and calls the updateWeights function
+ */
+int STDP3Conn::updateState(float time, float dt)
+{
+   update_timer->start();
+
+   int status=0;
+   if (stdpFlag) {
+
+      //const int axonId = 0;       // assume only one for now
+      for(int axonId = 0; axonId<numberOfAxonalArborLists(); axonId++) {
+         status=updateWeights(axonId);
+      }
+   }
+   update_timer->stop();
+
+   //const int axonId = 0;       // assume only one for now
+   //return updateWeights(axonId);
+   return status;
+}
+
+
+
+/**
+ *  STDP online implementation
+ *  (see more at: Pfister et al. 2008)
+ *
+ */
+int STDP3Conn::updateWeights(int axonId)
+{
+   // Steps:
+   // 1. Update post_tr
+   // 2. Update pre_tr
+   // 3. Update w_ij
+
+   const float dt = parent->getDeltaTime();
+   const float decayLTP = exp(-dt / tauLTP);
+   const float decayLTD = exp(-dt / tauLTD);
+   const float decayY = exp(-dt / tauY);
+   const int nkpre = pre->getNumExtended();
+   assert(nkpre == getNumWeightPatches());
+   const pvdata_t * preLayerData = pre->getLayerData();
+   const pvdata_t * aPost = post->getLayerData();
+
+   pvdata_t aPre;
+   //PVPatch * w;
+
+   pvdata_t * post_tr_m;
+   pvdata_t * post2_tr_m;
+   pvdata_t * pre_tr_m; // Presynaptic trace matrix
+   pvdata_t * W;
+   int nk, ny;
+
+   const int nkPost = post_tr->numItems;
+   post_tr_m = post_tr->data; // Postsynaptic trace matrix
+   post2_tr_m = post2_tr->data; // Postsynaptic trace matrix
+
+
+   // 1. Updates the postsynaptic traces
+   for (int kPost = 0; kPost < nkPost; kPost++) {
+             //post_tr_m[kPost] = (decayLTD * post_tr_m[kPost] + aPost[kPost]) > 1? 1 : (decayLTD * post_tr_m[kPost] + aPost[kPost]);
+            post_tr_m[kPost] = decayLTD * post_tr_m[kPost] + aPost[kPost];
+            post2_tr_m[kPost] = decayY * post2_tr_m[kPost] + aPost[kPost];
+   }
+
+   // this stride is in extended space for post-synaptic activity and STDP decrement variable
+   const int postStrideY = post->getLayerLoc()->nf * (post->getLayerLoc()->nx + 2 * post->getLayerLoc()->nb);
+   //FIXME: In the first iteration post is -70!!
+
+   for (int kPre = 0; kPre < nkpre; kPre++) {
+
+         aPre = preLayerData[kPre];
+         PVPatch * w = getWeights(kPre, axonId); //Get weights in form of a patch (nx,ny,nf), TODO: what's the role of the offset?
+         size_t postOffset = getAPostOffset(kPre, axonId); //Gets start index for postsynaptic vectors given presynaptic neuron kPre
+
+         aPost = &post->getLayerData()[postOffset]; //Gets postsynaptic activity
+         post_tr_m = &(post_tr->data[postOffset]);  // STDP decrement variable
+         post2_tr_m = &(post_tr->data[postOffset]);  // STDP y decrement variable
+         //pre_tr_m = get_dwData(axonId, kPre);        // STDP increment variable
+         pre_tr_m = &(pre_tr->data[kPre]);
+         W = get_wData(axonId, kPre); // w->data;
+
+         nk  = nfp * w->nx; // one line in x at a time
+         ny  = w->ny;
+
+         // 2. Updates the presynaptic trace
+         //pre_tr_m[0] = (decayLTP * pre_tr_m[0] + aPre) > 1? 1 : (decayLTP * pre_tr_m[0] + aPre);
+         pre_tr_m[0] = decayLTP * pre_tr_m[0] + aPre;
+
+         //3. Update weights
+         for (int y = 0; y < ny; y++) {
+               for (int k = 0; k < nk; k++) {
+                  // The next statement allows some synapses to "die".
+                  if (W[k] < WEIGHT_MIN_VALUE) continue;
+
+                   W[k] += dWMax * (-ampLTD*aPre * post_tr_m[k] + ampLTP * aPost[k] * (pre_tr_m[0]*post2_tr_m[k]));
+
+                   W[k] = W[k] < wMin ? wMin : W[k];
+                   W[k] = W[k] > wMax ? wMax : W[k];
+
+               }
+
+            // advance pointers in y
+            W += syp; //FIXME: W += nk
+            //pre_tr_m += syp; //FIXME: pre_tr_m += syp;
+
+            // postActivity and post trace are extended layer
+            aPost += postStrideY; //TODO: is this really in the extended space?
+            post_tr_m += postStrideY;
+            post2_tr_m += postStrideY;
+         }
+
+      }
+
+
+
+//   for (int kPre = 0; kPre < nkpre; kPre++) {
+//      w = getWeights(kPre, axonId);
+//
+//      pre_tr_m = get_dwData(axonId, kPre);  // STDP increment variable (presynaptic trace), Note: It uses dwData as the presynaptic trace variable, FIXME: Use an internal variable similar to post_tr?
+//      W = get_wData(axonId, kPre); // w->data; TODO: is this diff from w?
+//      aPre = preLayerData[kPre];
+//
+//      // 1. Updates the presynaptic trace
+//      pre_tr_m[kPre] = decayLTP * pre_tr_m[kPre] + aPre;
+//
+//      for (int kPost = 0; kPost < nkPost; kPost++) {
+//
+//
+//
+//       // 3. Update weights w_ij
+//       if (W[kPre] > WEIGHT_MIN_VALUE){
+//        W[kPre] += dWMax * (-ampLTD*aPre * post_tr_m[kPre] + ampLTP * aPost[kPost] * pre_tr_m[kPre]);
+//        W[kPre] = W[kPre] < wMin ? wMin : W[kPre];
+//        W[kPre] = W[kPre] > wMax ? wMax : W[kPre];
+//       }
+//      }
+   //}
+
+   return 0;
+}
+
+
+int STDP3Conn::pvpatch_update_plasticity_incr(int nk, float * RESTRICT p,
+                                   float aPre, float decay, float ltpAmp)
+{
+   int k;
+   for (k = 0; k < nk; k++) {
+      p[k] = decay * p[k] + ltpAmp * aPre;
+   }
+   return 0;
+}
+
+
+int STDP3Conn::pvpatch_update_weights(int nk, float * RESTRICT w, const float * RESTRICT m,
+                           const float * RESTRICT p, float aPre,
+                           const float * RESTRICT aPost, float dWMax, float wMin, float wMax)
+{
+   int k;
+   for (k = 0; k < nk; k++) {
+      // The next statement allows some synapses to "die".
+      // TODO - check to see if its faster to not use branching
+      if (w[k] < WEIGHT_MIN_VALUE) continue;
+       w[k] += dWMax * (aPre * m[k] + aPost[k] * p[k]);
+       w[k] = w[k] < wMin ? wMin : w[k];
+       w[k] = w[k] > wMax ? wMax : w[k];
+   }
+   return 0;
+}
+
+
+int STDP3Conn::outputState(float timef, bool last)
+{
+   int status;
+
+   if (last) {
+         printf("Writing last STDP weights..%f\n",timef);
+         convertPreSynapticWeights(timef);
+         status = writePostSynapticWeights(timef, last);
+         assert(status == 0);
+      }else if ( (timef >= writeTime) && (writeStep >= 0) ) {
+         //writeTime += writeStep; Done in HyperConn
+         convertPreSynapticWeights(timef);
+         status = writePostSynapticWeights(timef, false);
+         assert(status == 0);
+
+         // append to output file after original open
+         //ioAppend = true;
+      }
+   status = HyPerConn::outputState(timef, last);
+
+   if (status != PV_SUCCESS) return status;
+
+
+//   if (stdpFlag != true) return status;
+//
+//   if (last) {
+//      convertPreSynapticWeights(time);
+//      status = writePostSynapticWeights(time, last);
+//      assert(status == PV_SUCCESS);
+//   }
+//   else if ( (time >= writeTime) && (writeStep >= 0) ) {
+//
+//      convertPreSynapticWeights(time);
+//      status = writePostSynapticWeights(time, last);
+//      assert(status == PV_SUCCESS);
+//   }
+
+   return status;
+}
+
+float STDP3Conn::maxWeight(int arborID)
+{
+   return wMax;
+}
+
+int STDP3Conn::writeTextWeightsExtra(FILE * fd, int k, int arborID)
+{
+   if (stdpFlag) {
+      pv_text_write_patch(fd, getWeights(k, arborID), get_dwData(arborID, k), nfp, sxp, syp, sfp); // write the Ps variable
+   }
+   return 0;
+}
+
+} // End of namespace PV
+
---
+++ src/connections/STDP3Conn.hpp
@@ -0,0 +1,84 @@
+/*
+ * 3STDPConn.hpp
+ *
+ *  Created on: July 24, 2012
+ *      Author: Rui P. Costa
+ */
+
+#ifndef STDPCONN_HPP_
+#define STDPCONN_HPP_
+
+#include "HyPerConn.hpp"
+#include "../include/default_params.h"
+#include <stdio.h>
+
+namespace PV {
+
+class STDP3Conn : HyPerConn {
+public:
+   STDP3Conn();
+   STDP3Conn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+               const char * filename=NULL, bool stdpFlag=true,
+               InitWeights *weightInit=NULL);
+   virtual ~STDP3Conn();
+
+   int setParams(PVParams * params);
+
+   virtual int initializeThreadBuffers();
+   virtual int initializeThreadKernels();
+
+   // virtual int deleteWeights(); // Changed to a private method.  Should not be virtual since it's called from the destructor.
+
+   virtual float maxWeight(int arborId = 0);
+
+   virtual int updateState(float time, float dt);
+   virtual int updateWeights(int axonId);
+   virtual int outputState(float time, bool last=false);
+   virtual int writeTextWeightsExtra(FILE * fd, int k, int arborID);
+
+   virtual PVLayerCube * getPlasticityDecrement();
+
+protected:
+
+   int initialize_base();
+   int initialize(const char * name, HyPerCol * hc,
+                     HyPerLayer * pre, HyPerLayer * post,
+                     const char * filename, bool stdpFlag, InitWeights *weightInit);
+   virtual int initPlasticityPatches();
+
+   PVLayerCube    * post_tr;      // plasticity decrement variable for postsynaptic layer
+   PVLayerCube    * post2_tr;      // second plasticity decrement variable for postsynaptic layer
+   PVLayerCube    * pre_tr;      // plasticity increment variable for presynaptic layer
+#ifdef OBSOLETE_STDP
+   PVPatch       *** dwPatches;      // list of stdp patches Psij variable
+#endif
+
+   bool       stdpFlag;         // presence of spike timing dependent plasticity
+
+   int pvpatch_update_plasticity_incr(int nk, float * RESTRICT p,
+                                      float aj, float decay, float fac);
+   int pvpatch_update_weights(int nk, float * RESTRICT w, const float * RESTRICT m,
+                              const float * RESTRICT p, float aPre,
+                              const float * RESTRICT aPost, float dWmax, float wMin, float wMax);
+#ifdef OBSOLETE
+   int pvpatch_update_weights_localWMax(int nk, float * RESTRICT w, const float * RESTRICT m,
+                              const float * RESTRICT p, float aPre,
+                              const float * RESTRICT aPost, float dWMax, float wMin, float * RESTRICT Wmax);
+#endif // OBSOLETE
+
+   // STDP parameters for modifying weights
+   float ampLTP; // long term potentiation amplitude
+   float ampLTD; // long term depression amplitude
+   float tauLTP;
+   float tauLTD;
+   float tauY;
+   float dWMax;
+
+private:
+   int deleteWeights();
+
+};
+
+}
+
+#endif /* STDPCONN_HPP_ */
