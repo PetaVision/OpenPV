2f457da7b343e0c95940a0f1457fa8d8085e9551
peteschultz <peteschultz@users.sourceforge.net>
2012-10-11 19:13:42 +0000

    First draft of OjaKernelConn
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@5866 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ src/connections/OjaKernelConn.cpp
@@ -0,0 +1,186 @@
+/*
+ * OjaKernelConn.cpp
+ *
+ *  Created on: Oct 10, 2012
+ *      Author: pschultz
+ */
+
+#include "OjaKernelConn.hpp"
+
+namespace PV {
+OjaKernelConn::OjaKernelConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+                             const char * filename, InitWeights *weightInit) {
+   initialize_base();
+   initialize(name, hc, pre, post, filename, weightInit);
+}
+
+OjaKernelConn::OjaKernelConn()
+{
+   initialize_base();
+}
+
+OjaKernelConn::~OjaKernelConn() {
+   free(inputFiringRate[0]); inputFiringRate[0] = NULL;
+   free(inputFiringRate); inputFiringRate = NULL;
+   free(outputFiringRate); outputFiringRate = NULL;
+}
+
+int OjaKernelConn::initialize_base() {
+   return PV_SUCCESS;
+}
+
+int OjaKernelConn::initialize(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+      const char * filename, InitWeights *weightInit) {
+   int status = KernelConn::initialize(name, hc, pre, post, filename, weightInit);
+   readIntegrationTime();
+   readInputTargetRate();
+   readOutputTargetRate();
+   readIntegrationTime();
+
+   int numarbors = numberOfAxonalArborLists();
+   int n_pre_ext = getNumWeightPatches();
+   int n_post = post->getNumNeurons();
+   inputFiringRate = (pvdata_t **) calloc(numarbors, sizeof(pvdata_t *));
+   if (inputFiringRate == NULL) {
+      fprintf(stderr, "OjaKernelConn::initialize error for layer \"%s\": unable to allocate memory for input firing rates\n", name);
+      abort();
+   }
+   inputFiringRate[0] = (pvdata_t *) calloc(n_pre_ext*numarbors, sizeof(pvdata_t));
+   if (inputFiringRate[0] == NULL) {
+      fprintf(stderr, "OjaKernelConn::initialize error for layer \"%s\": unable to allocate memory for input firing rates\n", name);
+      abort();
+   }
+   for (int arbor=1; arbor<numarbors; arbor++) {
+      inputFiringRate[arbor] = inputFiringRate[0] + arbor*n_post;
+   }
+   outputFiringRate = (pvdata_t *) calloc(n_post, sizeof(pvdata_t *));
+   if (outputFiringRate == NULL) {
+      fprintf(stderr, "OjaKernelConn::initialize error for layer \"%s\": unable to allocate memory for output firing rates\n", name);
+      abort();
+   }
+   return status;
+}
+
+int OjaKernelConn::updateState(float timef, float dt) {
+   float decayfactor = expf(-parent->getDeltaTime()/integrationTime);
+
+
+   // Update output firing rate
+   const PVLayerLoc * postloc = post->getLayerLoc();
+   for (int kpost=0; kpost<post->getNumNeurons(); kpost++) {
+      int kpostext = kIndexExtended(kpost, postloc->nx, postloc->ny, postloc->nf, postloc->nb);
+      outputFiringRate[kpost] = decayfactor * (outputFiringRate[kpost] + post->getActivity()[kpostext]/integrationTime);
+   }
+
+   // HyPerConn::updateState calls update_dW and updateWeights; we override update_dW but there is no need to override updateWeights
+   int status = HyPerConn::updateState(timef, dt);
+   return status;
+}
+
+int OjaKernelConn::update_dW(int axonId) {
+   int status = PV_SUCCESS;
+   assert(axonId>=0 && axonId<this->numberOfAxonalArborLists());
+
+   // Update input firing rate
+   pvdata_t * input_rate = inputFiringRate[axonId];
+   float decayfactor = expf(-parent->getDeltaTime()/integrationTime);
+   for (int kex=0; kex<getNumWeightPatches(); kex++) {
+      input_rate[kex] = decayfactor * (input_rate[kex] + pre->getLayerData(getDelay(axonId))[kex]/integrationTime);
+   }
+
+   // Update weights
+   int sya = (post->getLayerLoc()->nf * (post->getLayerLoc()->nx + 2*post->getLayerLoc()->nb));
+
+   for (int kex=0; kex<getNumWeightPatches(); kex++) {
+      PVPatch * weights = getWeights(kex,axonId);
+      size_t offset = getAPostOffset(kex, axonId);
+      int ny = weights->ny;
+      int nk = weights->nx * nfp;
+      pvdata_t * dwdata = get_dwData(axonId, kex);
+      const pvdata_t * wdata = get_wData(axonId, kex);
+      int lineoffsetw = 0;
+      int lineoffseta = 0;
+      for( int y=0; y<ny; y++ ) {
+         for( int k=0; k<nk; k++ ) {
+            dwdata[lineoffsetw + k] += (input_rate[kex] - wdata[lineoffsetw+k])*outputFiringRate[offset+lineoffseta+k];
+         }
+         lineoffsetw += syp;
+         lineoffseta += sya;
+      }
+   }
+   // Multiply by dt*learningRate, normalize by dividing by inputTargetRate*outputTargetRate, and average by dividing by (numNeurons/numKernels)
+   int numKernelIndices = getNumDataPatches();
+   int divisor = pre->getNumNeurons()/numKernelIndices;
+   assert( divisor*numKernelIndices == pre->getNumNeurons() );
+   float scalefactor = parent->getDeltaTime()*getLearningRate()/(getInputTargetRate()*getOutputTargetRate())/((float) divisor);
+   for( int kernelindex=0; kernelindex<numKernelIndices; kernelindex++ ) {
+      int numpatchitems = nxp*nyp*nfp;
+      pvdata_t * dwpatchdata = get_dwDataHead(axonId,kernelindex);
+      for( int n=0; n<numpatchitems; n++ ) {
+         dwpatchdata[n] *= scalefactor;
+      }
+   }
+   // Averaging across processes is done in reduceKernels().
+
+   return status;
+}
+
+int OjaKernelConn::checkpointRead(const char * cpDir, float* timef) {
+   int status = KernelConn::checkpointRead(cpDir, timef);
+   char filename[PV_PATH_MAX];
+   int chars_needed;
+   chars_needed = snprintf(filename, PV_PATH_MAX, "%s/%s_outputFiringRate.pvp", cpDir, name);
+   if (chars_needed>=PV_PATH_MAX) {
+      if (parent->columnId()==0) {
+         fprintf(stderr, "OjaKernelConn::checkpointWrite error: Path \"%s/%s_outputFiringRate.pvp\" is too long.\n", cpDir, name);
+         abort();
+      }
+   }
+   double timed;
+   HyPerLayer::readBufferFile(filename, parent->icCommunicator(), &timed, outputFiringRate, 1, /*extended*/false, /*contiguous*/false, post->getLayerLoc());
+
+   const PVLayerLoc * preloc = pre->getLayerLoc();
+   chars_needed = snprintf(filename, PV_PATH_MAX, "%s/%s_inputFiringRate.pvp", cpDir, name);
+   assert(chars_needed<PV_PATH_MAX);
+   HyPerLayer::readBufferFile(filename, parent->icCommunicator(), &timed, inputFiringRate[0], numberOfAxonalArborLists(), /*extended*/true, /*contiguous*/false, preloc);
+
+   // Apply mirror boundary conditions
+
+   // Exchange borders
+   MPI_Datatype * mpi_datatype = Communicator::newDatatypes(preloc);
+   PVLayerCube cube;
+   memcpy(&cube.loc, preloc, sizeof(PVLayerLoc));
+   cube.numItems = pre->getNumExtended();
+   cube.size = sizeof(pvdata_t)*cube.numItems + sizeof(PVLayerCube); // Should be okay even though cube's data is not in place, since the mirrorTo functions don't use the size field
+   for (int arbor=0; arbor<numberOfAxonalArborLists(); arbor++) {
+      cube.data = inputFiringRate[0];
+      if ( pre->useMirrorBCs() ) {
+         for (int borderId = 1; borderId < NUM_NEIGHBORHOOD; borderId++){
+            pre->mirrorInteriorToBorder(borderId, &cube, &cube);
+         }
+      }
+      parent->icCommunicator()->exchange(inputFiringRate[0], mpi_datatype, preloc);
+   }
+   Communicator::freeDatatypes(mpi_datatype);
+
+   return status;
+}
+int OjaKernelConn::checkpointWrite(const char * cpDir) {
+   int status = KernelConn::checkpointWrite(cpDir);
+   char filename[PV_PATH_MAX];
+   int chars_needed;
+   chars_needed = snprintf(filename, PV_PATH_MAX, "%s/%s_outputFiringRate.pvp", cpDir, name);
+   if (chars_needed>=PV_PATH_MAX) {
+      if (parent->columnId()==0) {
+         fprintf(stderr, "OjaKernelConn::checkpointWrite error: Path \"%s/%s_outputFiringRate.pvp\" is too long.\n", cpDir, name);
+         abort();
+      }
+   }
+   HyPerLayer::writeBufferFile(filename, parent->icCommunicator(), (double) parent->simulationTime(), outputFiringRate, 1, /*extended*/ false, /*contiguous*/ false, post->getLayerLoc());
+   chars_needed = snprintf(filename, PV_PATH_MAX, "%s/%s_inputFiringRate.pvp", cpDir, name);
+   assert(chars_needed<PV_PATH_MAX);
+   HyPerLayer::writeBufferFile(filename, parent->icCommunicator(), (double) parent->simulationTime(), inputFiringRate[0], numberOfAxonalArborLists(), /*extended*/ true, /*contiguous*/ false, pre->getLayerLoc());
+   return status;
+}
+
+} /* namespace PV */
---
+++ src/connections/OjaKernelConn.hpp
@@ -0,0 +1,63 @@
+/*
+ * OjaKernelConn.h
+ *
+ *  Created on: Oct 10, 2012
+ *      Author: pschultz
+ */
+
+#ifndef OJAKERNELCONN_H_
+#define OJAKERNELCONN_H_
+
+#include "KernelConn.hpp"
+
+namespace PV {
+
+class OjaKernelConn: public PV::KernelConn {
+
+// Methods
+public:
+   // Public constructor, called when creating a new OjaKernelConn object
+   OjaKernelConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+                 const char * filename = NULL, InitWeights *weightInit = NULL);
+
+   virtual ~OjaKernelConn();
+
+   virtual int checkpointRead(const char * cpDir, float* timef);
+   virtual int checkpointWrite(const char * cpDir);
+
+   virtual int updateState(float timef, float dt);
+
+   // Public get-methods
+   float getLearningRate() {return learningRate;}
+   float getInputTargetRate() {return inputTargetRate;}
+   float getOutputTargetRate() {return outputTargetRate;}
+   float getIntegrationTime() {return integrationTime;}
+
+protected:
+   OjaKernelConn(); // Called by derived classes' constructors
+   int initialize(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+                  const char * filename, InitWeights *weightInit);
+   virtual int update_dW(int axonId);
+
+   // Load member variables from params.  Virtual so that derived classes can deactivate a param if it isn't needed.
+   virtual float readLearningRate() {return getParent()->parameters()->value(name, "learningRate", 1.0);}
+   virtual float readInputTargetRate() {return getParent()->parameters()->value(name, "inputTargetRate", 1.0);}
+   virtual float readOutputTargetRate() {return getParent()->parameters()->value(name, "outputTargetRate", 1.0);}
+   virtual float readIntegrationTime() {return getParent()->parameters()->value(name, "integrationTime", 1.0);}
+
+private:
+   int initialize_base();
+
+// Member variables
+protected:
+   float learningRate;
+   float inputTargetRate;
+   float outputTargetRate;
+   float integrationTime; // \tau_{\hbox{\it Oja}}
+   pvdata_t ** inputFiringRate; // inputFiringRate[arbor][patchIndex]
+   pvdata_t * outputFiringRate; // outputFiringRate[output neuron (in restricted space)]
+
+};
+
+} /* namespace PV */
+#endif /* OJAKERNELCONN_H_ */
