a2b359c7b39c551c876486a9f480944f5b100642
kepatel <kepatel@users.sourceforge.net>
2012-10-15 17:45:10 +0000

    LCA Connection
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@5901 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/connections/LCAConn.cpp
+++ src/connections/LCAConn.cpp
@@ -1,10 +1,31 @@
 /*
  * LCAConn.cpp
  *
- *  Created on: Sep 26, 2012
- *      Author: slundquist
+ *  Created on: Oct 8, 2012
+ *      Author: kpatel
  */
 
+#include "LCAConn.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+namespace PV {
+  
+  LCAConn::LCAConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+		   const char * filename, InitWeights *weightInit, Movie * auxLayer) 
+  {
+    KernelConn::initialize_base();
+    KernelConn::initialize(name, hc, pre, post, filename, weightInit);
+    layerOfInterest = auxLayer;
+  }
+
+  pvdata_t LCAConn::updateRule_dW(pvdata_t pre, pvdata_t post)
+  {
+    pvdata_t input = *(layerOfInterest->getImageBuffer());
+    return pre*(post-input);
+  }
+
+}
 
 
--- src/connections/LCAConn.hpp
+++ src/connections/LCAConn.hpp
@@ -1,29 +1,35 @@
 /*
  * LCAConn.hpp
  *
- *  Created on: Sep 26, 2012
- *      Author: slundquist
+ *  Created on: Oct 8, 2012
+ *      Author: kpatel
  */
 
 #ifndef LCACONN_HPP_
 #define LCACONN_HPP_
 
-#include "HyPerConn.hpp"
-#include "../include/default_params.h"
-#include <stdio.h>
+#include "KernelConn.hpp"
+#include "../include/pv_common.h"
+#include "../layers/Movie.hpp"
 
 namespace PV {
 
-class LCAConn : HyPerConn {
-   LCAConn();
-   LCAConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
-            const char * filename=NULL, bool stdpFlag=true,
-            InitWeights *weightInit=NULL);
-   virtual ~LCAConn();
+class LCAConn : public PV::KernelConn {
 
-   int setParams(PVParams * params);
+public:
+  LCAConn(const char * name, HyPerCol * hc, HyPerLayer * pre, HyPerLayer * post,
+	  const char * filename=NULL, InitWeights *weightInit=NULL, Movie * auxLayer=NULL);
+  
+protected:
+  int initialize(const char * name, HyPerCol * hc,
+		 HyPerLayer * pre, HyPerLayer * post,
+		 const char * filename,
+		 InitWeights *weightInit=NULL,
+		 Movie * auxLayer=NULL);
+  pvdata_t updateRule_dW(pvdata_t pre, pvdata_t post);
+  int updateState(float timef, float dt);
+  Movie * layerOfInterest;
 };
 
-
-
+}
 #endif /* LCACONN_HPP_ */
