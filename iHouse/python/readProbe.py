d6fde781b1fdeef61375300c84ad61e626df7e51
slundquist <slundquist@users.sourceforge.net>
2012-10-16 22:35:49 +0000

    Added functionality to allow for multiple plots for multiple probe data
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@5936 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 91ac8ccabd0acd5e4a6e1f88c54ef3bd7fd98887

--- iHouse/python/readProbe.py
+++ iHouse/python/readProbe.py
@@ -1,43 +1,57 @@
-import matplotlib.pyplot as plt
-from numpy import array, dot
+from matplotlib.pyplot import plot, legend, show
+from numpy import array, dot, arange, mean, polyfit, ndarray, std
 from collections import OrderedDict
 
-filename = "/Users/slundquist/Desktop/LCALIF_31_31_0.txt"
+#filename = "/Users/slundquist/Desktop/LCALIF_31_31_0.txt"
+filename = "/Users/slundquist/Desktop/retONtoLif.txt"
 
 #Values for range of frames
 all = True; #All values
 startVal = 0
 endVal = 100
 
-t = []
-#Data that is to be pulled out from the probe
-data = {
-      'G_E':           [],
-      'G_I':           [],
-      'G_IB':          [],
-      'Vadpt':        [],
-      'V':             [],
-      'Vth':           [],
-      'a':             [],
-      'integratedSpikeCount': []
-}
+numBins = 2
 
-#Which data points needs to be plotted
-plotData = OrderedDict()
-plotData['V'] = 1
-plotData['Vth'] = 1
-plotData['Vadpt'] = 1
-plotData['a'] = 10
-plotData['integratedSpikeCount'] = 1
+#Data structure for scale, and data array to store all the data
+data = OrderedDict()
+#Made time for data
+#TIME MUST EXIST AND BE FIRST IN THIS LIST
+data['t']                     = []
+
+#data['V']                    = []
+#data['Vth']                  = []
+#data['Vadpt']                = []
+#data['a']                    = []
+#data['integratedSpikeCount'] = []
+
+#data['weights*']             = []
+data['prOjaTr15']             = []
+data['prOjaTr*']              = []
+data['prStdpTr*']             = []
+data['poIntTr']               = []
+
+
+def splitLine(line):
+   #Split line by =
+   lineSp = line.split("=")
+   #Split further by spaces
+   lineSp = [x.split() for x in lineSp]
+   #Combine into one list
+   lineSp = [a for x in lineSp for a in x]
+   #Remove first element, which is probe name
+   lineSp = lineSp[1:]
+   #Group elements into tuples
+   lineSp = zip(*[lineSp[i::2] for i in range(2)])
+   return lineSp
 
 
 f = open(filename, 'r')
 lines = f.readlines()
 f.close()
 
-if startVal < 0:
+if startVal < 0 and not all :
    print "Start value must be above 0"
-if endVal >= len(lines):
+if endVal >= len(lines) and not all:
    print "End value must be below the total time"
 
 if (all):
@@ -45,26 +59,86 @@ if (all):
 else:
    timeRange = range(startVal, endVal)
 
-#Made time for data
-data['t'] = [];
 
-for t in timeRange:
-   line = lines[t]
-   for key in data.keys():
-      tok = key + '='
-      listSp = line.rsplit(tok)
-      strVal = listSp[1]
-      ind = strVal[1:].find(' ');
-      if ind == -1:
-         data[key].append(float(strVal[:len(strVal) - 1]))
+bounds = {}
+stds = {}
+
+linesSp = [splitLine(line) for line in lines]
+
+for key in data.keys():
+   if key[len(key)-1] == "*":
+      tok = key[:len(key) - 1]
+   else:
+      tok = key
+   #Grab the value if element is the same as token
+   if key[len(key)-1] == "*":
+      allVals = [[float(x[1]) for x in lineSp if x[0][:min(len(x[0]), len(tok))] == tok] for lineSp in linesSp]
+      minVal = min(min(allVals))
+      maxVal = max(max(allVals))
+      #Grab boundary points based on bins
+      step = (maxVal - minVal) / float(numBins)
+      if step == 0:
+         print key + " Min and Max are same value, defaulting to one bin"
+         #Put in all one list, should be only one element in list
+         data[key] = [mean(x) for x in allVals]
       else:
-         data[key].append(float(strVal[:ind + 1]))
+         boundList = list(arange(minVal, maxVal, step))
+         boundList.append(maxVal)
+         #Split data into another array based on bound list for each val in timestep
+         tempVals = [[[a for a in val if a > boundList[i] and a <= boundList[i+1]]for i in range(len(boundList) - 1)] for val in allVals]
+         #Find best line of fit
+         xVals = {}
+         yVals = {}
+         #Allocate arrays for dictionaries
+         for i in range(numBins):
+            xVals[i] = []
+            yVals[i] = []
+
+         #Iterate through everything to get data points for line of best fit
+         for time in range(len(tempVals)):
+            for i, bins in enumerate(tempVals[time]):
+               if len(bins) != 0:
+                  yVals[i].extend(bins)
+                  xVals[i].extend([data['t'][time] for i in range(len(bins))])
+         #Find line of best fit, one for each bin
+         #Output is polynomial value, as such: (slope, yintercept)
+         data[key] = [polyfit(xVals[i], yVals[i], 1) if len(yVals[i]) != 0 else array([]) for i in range(numBins)]
+         #Calculate standard deviation
+         stds[key] = [std(yVals[i]) if len(yVals[i]) != 0 else array([]) for i in range(numBins)]
+         bounds[key] = boundList
+   else:
+      data[key] = [float(x[1]) for lineSp in linesSp for x in lineSp if len(x[0]) == len(tok) and x[0][:len(tok)] == tok]
+
+   #Save bounds
+
+#for t in timeRange:
+#   line = lines[t]
+#   for key in data.keys():
+#      tok = key + '='
+#      listSp = line.rsplit(tok)
+#      strVal = listSp[1]
+#      ind = strVal[1:].find(' ');
+#      if ind == -1:
+#         data[key].append(float(strVal[:len(strVal) - 1]))
+#      else:
+#         data[key].append(float(strVal[:ind + 1]))
 
 time = array(data['t'])
-for key in plotData.keys():
-   plotMe = array(data[key])
-   plotMe = dot(plotMe, plotData[key])
-   plt.plot(time, plotMe, label=key)
+for key in data.keys():
+   if key == 't':
+      continue
+   if key[len(key)-1] == "*":
+      if type(data[key][1]) is ndarray:
+         for i in range(numBins):
+            if(len(data[key][i]) != 0):
+               plotMe = dot(time, data[key][i][0]) + data[key][i][1]
+               plot(time, plotMe, label=key + ' bin:(' + str(bounds[key][i]) + ',' + str(bounds[key][i+1]) + ')' + 'std:' + str(stds[key][i]))
+      else:
+         plotMe = array(data[key])
+         plot(time, plotMe, label=key)
+   else:
+      plotMe = array(data[key])
+      plot(time, plotMe, label=key)
 
-plt.legend(loc=3)
-plt.show()
+legend(bbox_to_anchor=(0., 1.02, 1., .102), ncol = 2, mode="expand", borderaxespad=0.,loc=3)
+show()
