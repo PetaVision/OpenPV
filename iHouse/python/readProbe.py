c1da866b0372a5266f0c9e7a501096721e017ab7
dpaiton <dpaiton@users.sourceforge.net>
2012-10-19 19:31:15 +0000

    Major speed up by not duplicating lists.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@6018 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: ea7fa919702650f7b3ed185abe5f233b91203498

--- iHouse/python/readProbe.py
+++ iHouse/python/readProbe.py
@@ -18,15 +18,15 @@ def splitLine(line):
    lineSp = zip(*[lineSp[i::2] for i in range(2)])
    return lineSp
 
-filename = "/Users/slundquist/Desktop/ptLIF.txt"
+#filename = "/Users/slundquist/Desktop/ptLIF.txt"
 #filename = "/Users/slundquist/Desktop/retONtoLif.txt"
-#filename = "/Users/dpaiton/Documents/Work/LANL/workspace/iHouse/checkpoints/Checkpoint3000000/retONtoLif.txt"
+filename = "/Users/dpaiton/Documents/Work/LANL/workspace/iHouse/checkpoints/Checkpoint3000000/retONtoLif.txt"
 
 #Values for range of frames
 all_lines = False #All values if True
-startTime = 2900000
+startTime = 2475000
 #End must be under number of lines in file
-endTime   = 3000000
+endTime   = 2625000
 
 numTCBins   = 2  #number of bins for time course plot
 numHistBins = -1 #number of bins for histogram of weights (-1 means no histogram)
@@ -37,15 +37,42 @@ data = OrderedDict()
 #TIME MUST EXIST AND BE FIRST IN THIS LIST
 data['t']                     = []
 
-data['V']                    = []
-data['Vth']                  = []
-data['a']                    = []
+#data['V']                    = []
+#data['Vth']                  = []
+#data['a']                    = []
 
-##data['weights*']             = []
-#data['prOjaTr15']             = []
 #data['prOjaTr*']              = []
 #data['prStdpTr*']             = []
 #data['poIntTr']               = []
+#data['poOjaTr']               = []
+#data['poStdpTr']              = []
+data['weight*']               = []
+data['weight0']               = []
+data['weight1']               = []
+data['weight2']               = []
+data['weight3']               = []
+data['weight4']               = []
+data['weight5']               = []
+data['weight6']               = []
+data['weight7']               = []
+data['weight8']               = []
+data['weight9']               = []
+data['weight10']              = []
+data['weight11']              = []
+data['weight12']              = []
+data['weight13']              = []
+data['weight14']              = []
+data['weight15']              = []
+data['weight16']              = []
+data['weight17']              = []
+data['weight18']              = []
+data['weight19']              = []
+data['weight20']              = []
+data['weight21']              = []
+data['weight22']              = []
+data['weight23']              = []
+data['weight24']              = []
+#data['ampLTD']                = []
 
 print "readProbe: Reading file..."
 f = open(filename, 'r')
@@ -57,49 +84,56 @@ else:
     fileStartTime = float(firstLineSplit[0][1])
     if startTime < fileStartTime: #can't start from a time earlier than the first time
         startTime = fileStartTime
-    if startTime != fileStartTime: #start time is somewhere past the beginning of the file
-        timeOffset = startTime - fileStartTime #now we know how many lines forward we need to go
-        lineLength = len(firstLine)
-        f.seek(lineLength*timeOffset,0)
-        line = f.readline() #this possibly(probably?) does not start at the beginning of the line
-        line = f.readline() #is definitely a full line
-        currentTime = float(splitLine(line)[0][1])
-        while currentTime != startTime:
-            if currentTime > startTime: #shouldn't have to go very far back, assuming that the length of the first line approximately matches the average line length
-                tempTime = currentTime #to make sure we actually back up
-                numBack = 3 #should allow you to back up a single line.
-                while tempTime >= currentTime:
+    timeOffset = startTime - fileStartTime #now we know how many lines forward we need to go
+    lineLength = len(firstLine)
+    f.seek(lineLength*timeOffset,0)
+    line = f.readline() #this possibly(probably?) does not start at the beginning of the line
+    line = f.readline() #is definitely a full line
+    currentTime = float(splitLine(line)[0][1])
+    while currentTime != startTime:
+        if currentTime > startTime: #shouldn't have to go very far back, assuming that the length of the first line approximately matches the average line length
+            tempTime = currentTime #to make sure we actually back up
+            numBack = 3 #should allow you to back up a single line.
+            while tempTime >= currentTime:
+                if (-len(line)*numBack < 0):
+                    f.seek(0,0)
+                    line = f.readline() #might not be full line
+                else:
                     f.seek(-len(line)*numBack,1) #back up an extra line because you have to jump forward to be sure you have a whole line
                     line = f.readline() #might not be full line
                     line = f.readline()
-                    tempTime = float(splitLine(line)[0][1])
-                    numBack += 1
-                currentTime = tempTime
-            else: #currentTime < startTime
-                line = f.readline()
-                currentTime = float(splitLine(line)[0][1])
-        #Should be at the right spot in the file now
-        lines = []
-        lines.append(line) #put the first line where it belongs
-        while True:
+                tempTime = float(splitLine(line)[0][1])
+                numBack += 1
+            currentTime = tempTime
+        else: #currentTime < startTime
             line = f.readline()
-            if len(line) == 0: #make sure endTime is not too far
-                if endTime > currentTime:
-                    print "readProbe: WARNING: Your endTime is greater than the max time in the file. Stopping at time "+str(currentTime)
-                break
-            currentTime += 1
-            lines.append(line)
+            currentTime = float(splitLine(line)[0][1])
+    #Should be at the right spot in the file now
+    lines = []
+    lines.append(line) #put the first line where it belongs
+    while True:
+        if currentTime < endTime:
+            line = f.readline()
+        else:
+            break
+        if len(line) == 0: #make sure endTime is not too far
+            if endTime > currentTime:
+                print "readProbe: WARNING: Your endTime is greater than the max time in the file. Stopping at time "+str(currentTime)
+            break
+        currentTime += 1
+        lines.append(line)
 f.close()
 
 print "readProbe: Formatting file into data structure..."
 bounds = {}
 stds = {}
-linesSp = [splitLine(line) for line in lines]
+lines = [splitLine(line) for line in lines]
 
 print "readProbe: Parsing Keys..."
 doHist = 0
 #Loop through all keys given by user
 for key in data.keys():
+    workingLines = lines[:]
     print "readProbe: -Formatting key: '" + key + "'"
     #Get key value, without the * if it is there
     if key[len(key)-1] == "*":
@@ -114,11 +148,15 @@ for key in data.keys():
             doHist = 0
         print "readProbe: --Key occurs in multiple instances per line, computing bin edge values..."
         #Get all instance values in all time steps for given key
-        #allVals is list of lists - [time][vals]
+        #workingLines is now a list of lists - [time][vals]
         #  num vals in each time step should equal the pre patch size
-        allVals = [[float(x[1]) for x in lineSp if x[0][:min(len(x[0]), len(tok))] == tok] for lineSp in linesSp]
-        minVal = min(min(allVals)) #max of all vals across all time
-        maxVal = max(max(allVals)) #min of all vals across all time
+        workingLines = [[float(x[1]) for x in lineSp if x[0][:min(len(x[0]), len(tok))] == tok] for lineSp in workingLines]
+        if doHist:
+            allVals = [[float(x[1]) for x in lineSp if x[0][:min(len(x[0]), len(tok))] == tok] for lineSp in lines]
+        minVal = min(min(workingLines)) #max of all vals across all time
+        maxVal = max(max(workingLines)) #min of all vals across all time
+        if doHist:
+            maxWeight = maxVal
         step = (maxVal - minVal) / float(numTCBins)
         if doHist:
             stepHist = (maxVal - minVal) / float(numHistBins)
@@ -129,24 +167,26 @@ for key in data.keys():
         if step == 0:
             print "readProbe: --" + key + " Min and Max equivelant, defaulting to one bin."
             #data refers to list of all values in time. mean() will reduce number of instances to 1
-            data[key] = [mean(x) for x in allVals]
+            data[key] = [mean(x) for x in workingLines]
             if doHist:
                 numHistBins = 1
         else:
             boundList = list(arange(minVal, maxVal, step))
             boundList.append(maxVal)
             #Split data into another array based on bound list for each val in timestep
-            tempVals = [[[a for a in val if a > boundList[i] and a <= boundList[i+1]]for i in range(len(boundList) - 1)] for val in allVals]
+            workingLines = [[[a for a in val if a > boundList[i] and a <= boundList[i+1]]for i in range(len(boundList) - 1)] for val in workingLines]
             if doHist:
                 print "readProbe: ---Computing histogram information..."
                 boundListHist = list(arange(minVal, maxVal, stepHist))
                 boundListHist.append(maxVal)
                 tempValsHist = [[len([a for a in val if a > boundListHist[i] and a <= boundListHist[i+1]]) for i in range(len(boundListHist) - 1)] for val in allVals]
+                del allVals #will not free until garbage collection
                 counts = zeros(numHistBins)
-                ##TODO: make list comprehension
+                ##TODO: make list comprehension and negate need for tempValsHist list
                 for time in range(len(tempValsHist)):
                     for iBin in range(numHistBins):
                         counts[iBin] += tempValsHist[time][iBin]
+                del tempValsHist #will not free until garbage collection
 
             print "readProbe: --Computing line of best fit..."
             #Find best line of fit
@@ -158,8 +198,8 @@ for key in data.keys():
                 yVals[i] = []
 
             #Iterate through everything to get data points for line of best fit
-            for time in range(len(tempVals)):
-                for i, bins in enumerate(tempVals[time]):
+            for time in range(len(workingLines)):
+                for i, bins in enumerate(workingLines[time]):
                     if len(bins) != 0:
                         yVals[i].extend(bins)
                         xVals[i].extend([data['t'][time] for i in range(len(bins))])
@@ -171,7 +211,7 @@ for key in data.keys():
             stds[key] = [std(yVals[i]) if len(yVals[i]) != 0 else array([]) for i in range(numTCBins)]
             bounds[key] = boundList
     else:
-        data[key] = [float(x[1]) for lineSp in linesSp for x in lineSp if len(x[0]) == len(tok) and x[0][:len(tok)] == tok]
+        data[key] = [float(x[1]) for lineSp in workingLines for x in lineSp if len(x[0]) == len(tok) and x[0][:len(tok)] == tok]
     print "readProbe: -Done formatting '"+key+"'."
 
 print "readProbe: Done parsing keys."
@@ -186,20 +226,23 @@ for key in data.keys():
             for i in range(numTCBins):
                 if(len(data[key][i]) != 0):
                     plotMe = time * data[key][i][0] + data[key][i][1]
-                plot(time, plotMe, label=key + ' bin:(' + str(bounds[key][i]) + ',' + str(bounds[key][i+1]) + ')' + 'std:' + str(stds[key][i]))
+                    plot(time, plotMe, label=key + ' bin:(' + str(bounds[key][i]) + ',' + str(bounds[key][i+1]) + ')' + 'std:' + str(stds[key][i]))
         else:
             plotMe = array(data[key])
-        plot(time, plotMe, label=key)
+            plot(time, plotMe, label=key)
     else:
-        plotMe = array(data[key])
-        plot(time, plotMe, label=key)
+        if key == 'poIntTr':
+            plotMe = array(data[key]) - 1
+        else:
+            plotMe = array(data[key])
+        if len(plotMe) != 0:
+            plot(time, plotMe, label=key)
 legend()#bbox_to_anchor=(0., 1.02, 1., .102), ncol = 2, mode="expand", borderaxespad=0.,loc=3)
 tight_layout()
 
 if doHist:
     print "readProbe: Creating histogram plot..."
     fig1 = figure(1)
-    maxWeight = max(max(max(tempVals))) #TODO: is there a better way to do this?
     xVals = arange(0,maxWeight,maxWeight/len(counts))
     bar(range(len(counts)),counts)
     xticks(arange(20),xVals,rotation='vertical')
