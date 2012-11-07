f7a55e91e61e18e1b2dda57cc4d859f8690eba18
dpaiton <dpaiton@users.sourceforge.net>
2012-11-07 22:44:30 +0000

    changed spacing
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@6320 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: c7c44e8e3628108fc1d39c5c9b42199fcf29b570

--- iHouse/mlab/reconstruct.m
+++ iHouse/mlab/reconstruct.m
@@ -19,8 +19,8 @@ function [outMat] = reconstruct(activityIndex, onWeightValues, offWeightValues, 
       [aIx aIy] = ind2sub([columnSizeX columnSizeY], activityIndex(activityi) + 1); 
 
       %Calculate what process this is in
-      procXi = floor((aIx - 1)/sizeX) + 1;
-      procYi = floor((aIy - 1)/sizeY) + 1;
+      procXi  = floor((aIx - 1)/sizeX) + 1;
+      procYi  = floor((aIy - 1)/sizeY) + 1;
 
       %If the spiking activity is not in the allowed area
       if isempty(find(marginIndex == (activityIndex(activityi) + 1)))
@@ -28,25 +28,25 @@ function [outMat] = reconstruct(activityIndex, onWeightValues, offWeightValues, 
       end
 
       %Convert postsynaptic cell to center of presynaptic cell
-      preIx = floor((aIx - 1)/postNxScale) + 1;
-      preIy = floor((aIy - 1)/postNyScale) + 1;
+      preIx   = floor((aIx - 1)/postNxScale) + 1;
+      preIy   = floor((aIy - 1)/postNyScale) + 1;
 
       %Get bounds for out
-      yStart = preIy - floor(patchSizeX/2);
-      yEnd = preIy + floor(patchSizeY/2);
-      xStart = preIx - floor(patchSizeX/2);
-      xEnd = preIx + floor(patchSizeY/2);
+      yStart  = preIy - floor(patchSizeX/2);
+      yEnd    = preIy + floor(patchSizeY/2);
+      xStart  = preIx - floor(patchSizeX/2);
+      xEnd    = preIx + floor(patchSizeY/2);
 
       %get index based on what quaderant
       newIndX = mod((aIx - 1), sizeX) + 1;
       newIndY = mod((aIy - 1), sizeY) + 1;
-      newInd = sub2ind([sizeX sizeY], newIndX, newIndY);
+      newInd  = sub2ind([sizeX sizeY], newIndX, newIndY);
 
       for nfi = 1:numFeatures %Number of features
          %Set out
          outMat(yStart:yEnd, xStart:xEnd) += ... 
-         onWeightValues{procXi, procYi, arborId}(:, :, nfi, newInd)' - ...
-         offWeightValues{procXi, procYi, arborId}(:, :, nfi, newInd)';
+           onWeightValues{procXi, procYi, arborId}(:, :, nfi, newInd)' - ...
+           offWeightValues{procXi, procYi, arborId}(:, :, nfi, newInd)';
       end
    end
 end
