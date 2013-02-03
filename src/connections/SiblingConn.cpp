5f13177c2147049121d3c581e0512f3b0849894c
garkenyon <garkenyon@users.sourceforge.net>
2013-02-03 18:42:58 +0000

    bug fixes in SiblingConn related to shmget
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@6571 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/connections/SiblingConn.cpp
+++ src/connections/SiblingConn.cpp
@@ -44,85 +44,118 @@ void SiblingConn::setSiblingConn(SiblingConn *sibling_conn){
    siblingConn = sibling_conn;
 }
 
-int SiblingConn::normalizeFamily()
-{
-   // normalize all arbors individuqlly relative to siblings
-   const int num_kernels = getNumDataPatches();
-   double sum_local = 0.0;
-   double sum_sibling = 0.0;
-   for (int kArbor = 0; kArbor < this->numberOfAxonalArborLists(); kArbor++) {
+int SiblingConn::normalizeFamily() {
+	// normalize all arbors individuqlly relative to siblings
+	// insert MPI_Barrier before callng SiblingConn::normalizeFamily to ensure all processors have finished self normalization
+	const int num_kernels = getNumDataPatches();
+	double sum_local = 0.0;
+	double sum_sibling = 0.0;
+	for (int kArbor = 0; kArbor < this->numberOfAxonalArborLists(); kArbor++) {
 #ifdef USE_SHMGET
-   if (shmget_flag && !shmget_owner[kArbor]){
-         continue;
-      }
+		if (shmget_flag && (!shmget_owner[kArbor] || !siblingConn->getShmgetOwner(kArbor))) {
+			continue;
+		}
 #endif
-      for (int kPatch = 0; kPatch < num_kernels; kPatch++) {
-         // PVPatch * localWPatch = getWeights(kPatch,kArbor); // this->getKernelPatch(kArbor, kPatch);
-         // pvdata_t * myWeights = myWpatch->data;
-         pvdata_t * localWeights = get_wDataHead(kArbor, kPatch);
-         assert(localWeights != NULL);
-         // PVPatch * siblingWpatch = siblingConn->getKernelPatch(kArbor, kPatch);
-         // pvdata_t * siblingWeights = siblingWpatch->data;
+		for (int kPatch = 0; kPatch < num_kernels; kPatch++) {
+			// PVPatch * localWPatch = getWeights(kPatch,kArbor); // this->getKernelPatch(kArbor, kPatch);
+			// pvdata_t * myWeights = myWpatch->data;
+			pvdata_t * localWeights = get_wDataHead(kArbor, kPatch);
+			assert(localWeights != NULL);
+			// PVPatch * siblingWpatch = siblingConn->getKernelPatch(kArbor, kPatch);
+			// pvdata_t * siblingWeights = siblingWpatch->data;
 #ifdef USE_SHMGET
-         volatile pvdata_t * siblingWeights = siblingConn->get_wDataHead(kArbor, kPatch);
+			volatile pvdata_t * siblingWeights = siblingConn->get_wDataHead(
+					kArbor, kPatch);
 #else
-         pvdata_t * siblingWeights = siblingConn->get_wDataHead(kArbor, kPatch);
+			pvdata_t * siblingWeights = siblingConn->get_wDataHead(kArbor, kPatch);
 #endif
-         assert(siblingWeights != NULL);
-         const int nx = nxp; // localWPatch->nx;
-         const int ny = nyp; // localWPatch->ny;
-         const int nf = nfp;
-         const int sy = syp;
-         for (int ky = 0; ky < ny; ky++) {
-            for (int iWeight = 0; iWeight < nf * nx; iWeight++) {
-               pvdata_t norm_denom = fabs(siblingWeights[iWeight]) + fabs(localWeights[iWeight]);
-               norm_denom = (norm_denom != 0.0f) ? norm_denom : 1.0f;
-               localWeights[iWeight] /= norm_denom;
-               siblingWeights[iWeight] /= norm_denom;
-               sum_local += localWeights[iWeight];
-               sum_sibling += siblingWeights[iWeight];
-            }
-            localWeights += sy;
-            siblingWeights += sy;
-         }
-      } // kPatch < numPatches
-   } // kArbor
-   // scale local weights so that average = sibling average
-   float scale_factor = sum_local != 0 ? fabs(sum_sibling) / fabs(sum_local) : 1;
-   for (int kArbor = 0; kArbor < this->numberOfAxonalArborLists(); kArbor++) {
+			assert(siblingWeights != NULL);
+			const int nx = nxp; // localWPatch->nx;
+			const int ny = nyp; // localWPatch->ny;
+			const int nf = nfp;
+			const int sy = syp;
+			for (int ky = 0; ky < ny; ky++) {
+				for (int iWeight = 0; iWeight < nf * nx; iWeight++) {
+					pvdata_t norm_denom = fabs(siblingWeights[iWeight])
+							+ fabs(localWeights[iWeight]);
+					norm_denom = (norm_denom != 0.0f) ? norm_denom : 1.0f;
 #ifdef USE_SHMGET
-         volatile pvdata_t * localWeights = this->get_wDataStart(kArbor);
+					if (shmget_flag){
+						if (shmget_owner[kArbor]) {
+							localWeights[iWeight] /= norm_denom;
+						}
+					}
+					else{
+						localWeights[iWeight] /= norm_denom;
+					}
 #else
-         pvdata_t * localWeights = this->get_wDataStart(kArbor);
+					localWeights[iWeight] /= norm_denom;
 #endif
-      for (int iWeight = 0; iWeight < nxp * nyp * nfp; iWeight++){
-         localWeights[iWeight] *= scale_factor;
-      }
-   }
-   return PV_BREAK;
+#ifdef USE_SHMGET
+					if (shmget_flag){
+						if (siblingConn->getShmgetOwner(kArbor)) {
+							siblingWeights[iWeight] /= norm_denom;
+						}
+					}
+					else{
+						siblingWeights[iWeight] /= norm_denom;
+					}
+#else
+					siblingWeights[iWeight] /= norm_denom;
+#endif
+#ifdef PV_USE_MPI
+#ifdef USE_SHMGET
+		// insert barrier to ensure that all processes have finished updating weights before computing sum
+					MPI_Barrier(parent->icCommunicator()->communicator());
+#endif
+#endif
+					sum_local += localWeights[iWeight];
+					sum_sibling += siblingWeights[iWeight];
+				}
+				localWeights += sy;
+				siblingWeights += sy;
+			}
+		} // kPatch < numPatches
+	} // kArbor
+	  // scale local weights so that average = sibling average
+	float scale_factor =
+			sum_local != 0 ? fabs(sum_sibling) / fabs(sum_local) : 1;
+	for (int kArbor = 0; kArbor < this->numberOfAxonalArborLists(); kArbor++) {
+#ifdef USE_SHMGET
+		if (shmget_flag && !shmget_owner[kArbor]) {
+			continue;
+		}
+		volatile pvdata_t * localWeights = this->get_wDataStart(kArbor);
+#else
+		pvdata_t * localWeights = this->get_wDataStart(kArbor);
+#endif
+		for (int iWeight = 0; iWeight < nxp * nyp * nfp; iWeight++) {
+			localWeights[iWeight] *= scale_factor;
+		}
+	}
+	return PV_BREAK;
 } // normalizeFamily
 
-int SiblingConn::normalizeWeights(PVPatch ** patches, pvdata_t ** dataStart, int numPatches, int arborId)
-{
-   int status = PV_SUCCESS;
+int SiblingConn::normalizeWeights(PVPatch ** patches, pvdata_t ** dataStart,
+		int numPatches, int arborId) {
+	int status = PV_SUCCESS;
 
-   // individually normalize each arbor for self and sibling
-//   if (this->numberOfAxonalArborLists() > 1) {
-//      assert(this->normalizeArborsIndividually);
-//   }
-   status = NoSelfKernelConn::normalizeWeights(patches, dataStart, numPatches, arborId);  // parent class should return PV_BREAK
-   assert( (status == PV_SUCCESS) || (status == PV_BREAK) );
+	status = NoSelfKernelConn::normalizeWeights(patches, dataStart, numPatches,
+			arborId);  // parent class should return PV_BREAK
+	assert( (status == PV_SUCCESS) || (status == PV_BREAK));
 
-   if ((siblingConn != NULL) && (siblingConn->getIsNormalized())){
-      status = this->normalizeFamily();
-      assert( (status == PV_SUCCESS) || (status == PV_BREAK) );
-//      status = HyPerConn::normalizeWeights(NULL, dataStart, numPatches, arborId);  // parent class should return PV_BREAK
-//      assert( (status == PV_SUCCESS) || (status == PV_BREAK) );
-//      status = siblingConn->HyPerConn::normalizeWeights(NULL, siblingConn->get_wDataStart(), numPatches, arborId);  // parent class should return PV_BREAK
-//      assert( (status == PV_SUCCESS) || (status == PV_BREAK) );
-   }
+	if ((siblingConn != NULL) && (siblingConn->getIsNormalized())) {
+#ifdef PV_USE_MPI
+#ifdef USE_SHMGET
+		// insert barrier to ensure that all processes have finished individual normalization before starting family normalization
+		MPI_Barrier(parent->icCommunicator()->communicator());
+#endif
+#endif
+		status = this->normalizeFamily();
+		assert( (status == PV_SUCCESS) || (status == PV_BREAK));
+	}
 
-   return PV_BREAK;
+	return PV_BREAK;
 } // normalizeWeights
 
 
