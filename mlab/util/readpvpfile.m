e9da523506771d298b13e898d1d2e3dba3663c68
garkenyon <garkenyon@users.sourceforge.net>
2013-03-08 22:11:10 +0000

    added start_frame for only reading between start_frame and num_frames as input parameters
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@6730 7c0444ca-5550-0410-9008-faecdbe78eb5

--- mlab/util/readpvpfile.m
+++ mlab/util/readpvpfile.m
@@ -1,4 +1,4 @@
-function [data,hdr] = readpvpfile(filename,progressperiod, num_frames)
+function [data,hdr] = readpvpfile(filename,progressperiod, num_frames, start_frame)
 % Usage:[data,hdr] = readpvpfile(filename)
 % filename is a pvp file (any type)
 % data is a cell array containing the data.
@@ -6,6 +6,11 @@ function [data,hdr] = readpvpfile(filename,progressperiod, num_frames)
 %     Each element is a struct containing the fields 'time' and 'values'
 %     For activities, values is an nx-by-ny-by-nf array.
 %     For weights, values is a cell array, each element is an nxp-by-nyp-by-nfp array.
+
+%% start_frame allows the user to only read frames from some starting point
+if nargin < 4 || ~exist("start_frame") || isempty(start_frame)
+  start_frame = 1;
+end
 
 filedata = dir(filename);
 if length(filedata) ~= 1
@@ -34,12 +39,7 @@ switch hdr.filetype
         framesize = hdr.recordsize*hdr.numrecords;
         numframes = (filedata(1).bytes - hdr.headersize)/framesize;
     case 2 % PVP_ACT_FILE_TYPE % Compressed for spiking
-        if (~exist('num_frames','var') || isempty(num_frames))
-            numframes = hdr.nbands;
-        else
-            numframes = num_frames;
-        end%if
-        %%numframes = hdr.nbands;
+        numframes = hdr.nbands;
         % framesize is variable
     case 3 % PVP_WGT_FILE_TYPE % HyPerConns that aren't KernelConns
         framesize = hdr.recordsize*hdr.numrecords+hdr.headersize;
@@ -47,18 +47,18 @@ switch hdr.filetype
     case 4 % PVP_NONSPIKING_ACT_FILE_TYPE
         nxprocs = hdr.nxGlobal/hdr.nx;
         nyprocs = hdr.nyGlobal/hdr.ny;
-        % framesize = hdr.recordsize*hdr.datasize*nxprocs*nyprocs+8;
-        if (~exist('num_frames','var') || isempty(num_frames))
-            numframes = hdr.nbands;
-        else
-            numframes = num_frames;
-        end%if
+        framesize = hdr.recordsize*hdr.datasize*nxprocs*nyprocs+8;
+        numframes = hdr.nbands;
     case 5 % PVP_KERNEL_FILE_TYPE
         framesize = hdr.recordsize*hdr.nbands+hdr.headersize;
         numframes = filedata(1).bytes/framesize;
     otherwise
         errorident = 'readpvpfile:badfiletype';
         errorstring = sprintf('readpvpfile:File %s has unrecognized file type %d',filename,hdr.filetype);
+end
+%% allow user to override value of numframes
+if (exist("num_frames","var") && ~isempty(num_frames))
+    numframes = num_frames;
 end
 
 if isempty(errorstring)
@@ -69,7 +69,7 @@ if isempty(errorstring)
 end
 
 if isempty(errorstring)
-    data = cell(numframes,1);
+    data = cell(numframes-start_frame+1,1);
     switch hdr.datatype
         case 1 % PV_BYTE_TYPE
             precision = 'uchar';
@@ -82,45 +82,56 @@ if isempty(errorstring)
         case 1 % PVP_FILE_TYPE, used in HyPerCol::exitRunLoop
             numvalues = hdr.recordsize/hdr.datasize;
             for f=1:numframes
-                data{f} = struct('time',hdr.time,'values',[]);
-                data{f}.time = hdr.time;
-                Y = zeros(numvalues,hdr.numrecords);
-                for r=1:hdr.numrecords
-                    Y(:,r) = fread(fid, numvalues, precision);
-                end
-                Z = zeros(hdr.nxGlobal,hdr.nyGlobal,hdr.nf);
-                r=0;
-                for y=1:hdr.nyprocs
-                    yidx = (1:hdr.ny)+(y-1)*hdr.ny;
-                    for x=1:hdr.nxprocs
-                        xidx = (1:hdr.nx)+(x-1)*hdr.nx;
-                        r = r+1;
-                        for feature=1:hdr.nf
-                            Z(xidx,yidx,feature) = reshape(Y(feature:hdr.nf:hdr.nx*hdr.ny*hdr.nf,r),hdr.nx,hdr.ny);
-                        end
-                    end
-                end
-                data{f}.values = Z;
-                if exist('progressperiod','var')
-                    if ~mod(f,progressperiod)
+	      data_tmp = struct('time',hdr.time,'values',[]);
+	      data_tmp.time = hdr.time;
+	      Y = zeros(numvalues,hdr.numrecords);
+              for r=1:hdr.numrecords
+		     Y(:,r) = fread(fid, numvalues, precision);
+              end
+	      if f < 1
+	      	 continue;
+	      end
+	      Z = zeros(hdr.nxGlobal,hdr.nyGlobal,hdr.nf);
+	      r=0;
+	      for y=1:hdr.nyprocs
+		  yidx = (1:hdr.ny)+(y-1)*hdr.ny;
+		  for x=1:hdr.nxprocs
+                       xidx = (1:hdr.nx)+(x-1)*hdr.nx;
+                       r = r+1;
+                       for feature=1:hdr.nf
+                          Z(xidx,yidx,feature) = reshape(Y(feature:hdr.nf:hdr.nx*hdr.ny*hdr.nf,r),hdr.nx,hdr.ny);
+                       end
+                   end
+               end %% num_procs
+               data_tmp.values = Z;
+               if exist('progressperiod','var')
+                  if ~mod(f,progressperiod)
                         fprintf(1,'File %s: frame %d of %d\n',filename, f, numframes);
                         fflush(1);
-                    end
-                end
-            end
+                  end
+               end
+	       if f < start_frame
+	      	   continue;
+	      	end
+	       data{f - start_frame + 1} = data_tmp;
+            end  %% num_frames
         case 2 % PVP_ACT_FILE_TYPE % Compressed for spiking
             for f=1:numframes
-                data{f} = struct('time',0,'values',[]);
-                data{f}.time = fread(fid,1,'float64');
+                data_tmp = struct('time',0,'values',[]);
+                data_tmp.time = fread(fid,1,'float64');
                 numactive = fread(fid,1,'uint32');
-                data{f}.values = fread(fid,numactive,'uint32');
+                data_tmp.values = fread(fid,numactive,'uint32');
                 if exist('progressperiod','var')
                     if ~mod(f,progressperiod)
                         fprintf(1,'File %s: frame %d of %d\n',filename, f, numframes);
                         fflush(1);
                     end
                 end
-            end
+	 	if f < start_frame
+	      	   continue;
+	      	end
+		data{f - start_frame + 1} = data_tmp;
+            end %% num_frames
         case 3 % PVP_WGT_FILE_TYPE
             fseek(fid,0,'bof');
             for f=1:numframes
@@ -139,18 +150,26 @@ if isempty(errorstring)
                 if numextra > 0
                     hdr.additional = fread(fid,numextra,'int32');
                 end
+<<<<<<< .mine
+                data_tmp = struct('time',hdr.time,'values',[],'nx',[],'ny',[],'offset',[]);
+                data_tmp.values = cell(hdr.nxprocs,hdr.nyprocs,hdr.nbands);
+                data_tmp.nx = cell(hdr.nxprocs,hdr.nyprocs,hdr.nbands);
+                data_tmp.ny = cell(hdr.nxprocs,hdr.nyprocs,hdr.nbands);
+                data_tmp.offset = cell(hdr.nxprocs,hdr.nyprocs,hdr.nbands);                
+=======
                 data{f} = struct('time',hdr.time,'values',[],'nx',[],'ny',[],'offset',[]);
                 data{f}.values = cell(hdr.nxprocs,hdr.nyprocs,hdr.nbands);
                 data{f}.nx = cell(hdr.nxprocs,hdr.nyprocs,hdr.nbands);
                 data{f}.ny = cell(hdr.nxprocs,hdr.nyprocs,hdr.nbands);
                 data{f}.offset = cell(hdr.nxprocs,hdr.nyprocs,hdr.nbands);
+>>>>>>> .r6729
                 for arbor=1:hdr.nbands
                     for y=1:hdr.nyprocs
                         for x=1:hdr.nxprocs
                             cellindex = sub2ind([hdr.nxprocs hdr.nyprocs hdr.nbands],x,y,arbor);
                             % octave has trouble with multidim cell arrays
                             patchesperproc = hdr.numPatches/(hdr.nxprocs*hdr.nyprocs);
-                            data{f}.values{cellindex} = nan(hdr.nxp,hdr.nyp,hdr.nfp,patchesperproc);
+                            data_tmp.values{cellindex} = nan(hdr.nxp,hdr.nyp,hdr.nfp,patchesperproc);
                             for p=1:patchesperproc
                                 patchnx = fread(fid,1,'uint16');
                                 patchny = fread(fid,1,'uint16');
@@ -159,13 +178,13 @@ if isempty(errorstring)
                                 tempdata = reshape(Z(1:hdr.nfp*hdr.nxp*hdr.nyp),hdr.nfp,hdr.nxp,hdr.nyp);
                                 tempdata = permute(tempdata,[2 3 1]);
                                 % Need to move shrunken patches
-                                data{f}.values{cellindex}(:,:,:,p) = tempdata;
-                                data{f}.nx{cellindex} = patchnx;
-                                data{f}.ny{cellindex} = patchny;
-                                data{f}.offset{cellindex} = patchoffset;
+                                data_tmp.values{cellindex}(:,:,:,p) = tempdata;
+                                data_tmp.nx{cellindex} = patchnx;
+                                data_tmp.ny{cellindex} = patchny;
+                                data_tmp.offset{cellindex} = patchoffset;
                             end
                             if hdr.datatype==1 % byte-type.  If float-type, no rescaling took place.
-                                data{f}.values{cellindex} = data{f}.values{1}/255*(hdr.wMax-hdr.wMin)+hdr.wMin;
+                                data_tmp.values{cellindex} = data_tmp.values{1}/255*(hdr.wMax-hdr.wMin)+hdr.wMin;
                             elseif hdr.datatype ~= 3
                                 error('readpvpfile:baddatatype',...
                                     'Weight file type requires hdr.datatype of 1 or 3; received %d',...
@@ -173,18 +192,28 @@ if isempty(errorstring)
                             end
                         end
                     end
+                end  %% arbor
+                if exist('progressperiod','var')
+                    if ~mod(f,progressperiod)
+                        fprintf(1,'File %s: frame %d of %d\n',filename, f, numframes);
+                        fflush(1);
+                    end
                 end
-            end
+	 	if f < start_frame
+	      	   continue;
+	      	end
+                data{f-start_frame+1} = data_tmp;
+            end  %% num_frames
         case 4 % PVP_NONSPIKING_ACT_FILE_TYPE
             for f=1:numframes
-                data{f} = struct('time',0,'values',zeros(hdr.nxGlobal,hdr.nyGlobal,hdr.nf));
-                data{f}.time = fread(fid,1,'float64');
+                data_tmp = struct('time',0,'values',zeros(hdr.nxGlobal,hdr.nyGlobal,hdr.nf));
+                data_tmp.time = fread(fid,1,'float64');
                 for y=1:nyprocs
                     yidx = (1:hdr.ny)+(y-1)*hdr.ny;
                     for x=1:nxprocs
                         xidx = (1:hdr.nx)+(x-1)*hdr.nx;
                         Z = fread(fid,hdr.recordsize,precision);
-                        data{f}.values(xidx,yidx,:) = permute(reshape(Z,hdr.nf,hdr.nx,hdr.ny),[2 3 1]);
+                        data_tmp.values(xidx,yidx,:) = permute(reshape(Z,hdr.nf,hdr.nx,hdr.ny),[2 3 1]);
                     end
                 end
                 if exist('progressperiod','var')
@@ -193,7 +222,11 @@ if isempty(errorstring)
                         fflush(1);
                     end
                 end
-            end
+	 	if f < start_frame
+	      	   continue;
+	      	end
+                data{f-start_frame+1} = data_tmp;
+	    end %% numframes
         case 5 % PVP_KERNEL_FILE_TYPE
             fseek(fid,0,'bof'); % there's a header in every frame, unlike other file types
             % So go back to the beginning and read the header in each frame.
@@ -213,12 +246,12 @@ if isempty(errorstring)
                 if numextra > 0
                     hdr.additional = fread(fid,numextra,'int32');
                 end
-                data{f} = struct('time',hdr.time,'values',[]);
-                data{f}.values = cell(1,1,hdr.nbands);
+                data_tmp = struct('time',hdr.time,'values',[]);
+                data_tmp.values = cell(1,1,hdr.nbands);
                 for arbor=1:hdr.nbands
                     cellindex = sub2ind([1 1 hdr.nbands],1,1,arbor);
                     % octave has trouble with multidim cell arrays
-                    data{f}.values{cellindex} = nan(hdr.nxp,hdr.nyp,hdr.nfp,hdr.numPatches);
+                    data_tmp.values{cellindex} = nan(hdr.nxp,hdr.nyp,hdr.nfp,hdr.numPatches);
                     for p=1:hdr.numPatches
                         fread(fid,1,'uint16'); % patch->nx
                         fread(fid,1,'uint16'); % patch->ny
@@ -227,10 +260,10 @@ if isempty(errorstring)
                         tempdata = reshape(Z,hdr.nfp,hdr.nxp,hdr.nyp);
                         tempdata = permute(tempdata,[2 3 1]);
                         
-                        data{f}.values{cellindex}(:,:,:,p) = tempdata;
+                        data_tmp.values{cellindex}(:,:,:,p) = tempdata;
                     end
                     if hdr.datatype==1 % byte-type.  If float-type, no rescaling took place.
-                        data{f}.values{cellindex} = data{f}.values{1}/255*(hdr.wMax-hdr.wMin)+hdr.wMin;
+                        data_tmp.values{cellindex} = data_tmp.values{1}/255*(hdr.wMax-hdr.wMin)+hdr.wMin;
                     elseif hdr.datatype ~= 3
                         error('readpvpfile:baddatatype',...
                             'Weight file type requires hdr.datatype of 1 or 3; received %d',...
@@ -243,7 +276,11 @@ if isempty(errorstring)
                         fflush(1);
                     end
                 end
-            end
+	 	if f < start_frame
+	      	   continue;
+	      	end
+                data{f-start_frame+1} = data_tmp;
+	    end %% num_frames
         otherwise
             assert(0); % This possibility should have been weeded out above
     end
