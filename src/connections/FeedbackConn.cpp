0bdc972fb212d89166969aac901dd962d32a7ff2
peteschultz <peteschultz@users.sourceforge.net>
2013-03-21 22:35:31 +0000

    Added readPatchSize and readNfp to FeedbackConn
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@6762 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/connections/FeedbackConn.cpp
+++ src/connections/FeedbackConn.cpp
@@ -32,41 +32,53 @@ int FeedbackConn::initialize(const char * name, HyPerCol *hc, KernelConn * ffcon
    return PV_SUCCESS;
 }
 
+int FeedbackConn::readPatchSize(PVParams * params) {
+   int status = PV_SUCCESS;
+
+   int xscaleDiff = pre->getXScale() - post->getXScale();
+   // If feedforward conn is many-to-one, feedback conn is one-to-many.
+   // Then xscaleDiff > 0.
+   // Similarly, if feedforwardConn is one-to-many, xscaleDiff < 0.
+   int yscaleDiff = pre->getYScale() - post->getYScale();
+
+   nxp = feedforwardConn->xPatchSize();
+   if(xscaleDiff > 0 ) {
+       nxp *= (int) powf( 2, xscaleDiff );
+   }
+   else if(xscaleDiff < 0) {
+       nxp /= (int) powf(2,-xscaleDiff);
+       assert(feedforwardConn->xPatchSize()==nxp*powf( 2, (float) (-xscaleDiff) ));
+   }
+   nyp = feedforwardConn->yPatchSize();
+   if(yscaleDiff > 0 ) {
+       nyp *= (int) powf( 2, (float) yscaleDiff );
+   }
+   else if(yscaleDiff < 0) {
+       nyp /= (int) powf(2,-yscaleDiff);
+       assert(feedforwardConn->yPatchSize()==nyp*powf( 2, (float) (-yscaleDiff) ));
+   }
+   assert( checkPatchSize(nxp, pre->getXScale(), post->getXScale(), 'x') ==
+           PV_SUCCESS );
+   assert( checkPatchSize(nyp, pre->getYScale(), post->getYScale(), 'y') ==
+           PV_SUCCESS );
+   return status;
+}
+
+int FeedbackConn::readNfp(PVParams * params) {
+   nfp = post->getLayerLoc()->nf;
+   assert(feedforwardConn && nfp==feedforwardConn->preSynapticLayer()->getLayerLoc()->nf);
+   return PV_SUCCESS;
+}
+
 int FeedbackConn::setPatchSize(const char * filename) {
-    int status = PV_SUCCESS;
-
-    int xscaleDiff = pre->getXScale() - post->getXScale();
-    // If feedforward conn is many-to-one, feedback conn is one-to-many.
-    // Then xscaleDiff > 0.
-    // Similarly, if feedforwardConn is one-to-many, xscaleDiff < 0.
-    int yscaleDiff = pre->getYScale() - post->getYScale();
-
-    nxp = feedforwardConn->xPatchSize();
-    if(xscaleDiff > 0 ) {
-        nxp *= (int) powf( 2, xscaleDiff );
-    }
-    else if(xscaleDiff < 0) {
-        nxp /= (int) powf(2,-xscaleDiff);
-        assert(feedforwardConn->xPatchSize()==nxp*powf( 2, (float) (-xscaleDiff) ));
-    }
-    nyp = feedforwardConn->yPatchSize();
-    if(yscaleDiff > 0 ) {
-        nyp *= (int) powf( 2, (float) yscaleDiff );
-    }
-    else if(yscaleDiff < 0) {
-        nyp /= (int) powf(2,-yscaleDiff);
-        assert(feedforwardConn->yPatchSize()==nyp*powf( 2, (float) (-yscaleDiff) ));
-    }
-    nfp = post->getLayerLoc()->nf;
-
-    assert( checkPatchSize(nyp, pre->getXScale(), post->getXScale(), 'x') ==
-            PV_SUCCESS );
-
-    assert( checkPatchSize(nyp, pre->getYScale(), post->getYScale(), 'y') ==
-            PV_SUCCESS );
-
-    status = filename ? patchSizeFromFile(filename) : PV_SUCCESS;
-    return status;
+   int status = PV_SUCCESS;
+   if( filename != NULL ) {
+      PVParams * inputParams = parent->parameters();
+      bool useListOfArborFiles = inputParams->value(name, "useListOfArborFiles", false)!=0;
+      bool combineWeightFiles = inputParams->value(name, "combineWeightFiles", false)!=0;
+      if( !useListOfArborFiles && !combineWeightFiles) status = patchSizeFromFile(filename);
+   }
+   return status;
 }  // end of FeedbackConn::setPatchSize(const char *)
 
 PVPatch *** FeedbackConn::initializeWeights(PVPatch *** arbors, pvdata_t ** dataStart, int numPatches,
