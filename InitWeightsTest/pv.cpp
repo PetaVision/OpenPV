141c86436aabf030888e1ffd6797d54634b59075
peteschultz <peteschultz@users.sourceforge.net>
2013-04-09 19:41:46 +0000

    Change to InitWeightsTest.  Previously, params file contained only HyPerConn and KernelConn objects, and then the addcustom hook into buildandrun would loop through all connections and add a corresponding HyperConnDebugInitWeights or KernelConnDebugInitWeights connection.  The disadvantage of this method is that corresponding HyPerConn and HyperConnDebugInitWeights connections had the same name and would clobber each other during checkpointing.
    Now, the HyperConnDebugInitWeights and KernelConnDebugInitWeights objects must be specified in the params file, and we use the customgroups hook to buildandrun to add these objects.
    
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@6838 7c0444ca-5550-0410-9008-faecdbe78eb5

--- InitWeightsTest/pv.cpp
+++ InitWeightsTest/pv.cpp
@@ -8,78 +8,62 @@
 #include "KernelConnDebugInitWeights.hpp"
 #include "InitWeightTestProbe.hpp"
 
-int addcustom(HyPerCol * hc, int argc, char * argv[]);
-// addcustom is for adding objects not understood by build().
+// customgroups is for adding objects not understood by build().
+void * customgroups(const char * keyword, const char * name, HyPerCol * hc);
 
 int main(int argc, char * argv[]) {
-   return buildandrun(argc, argv, &addcustom)==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
+   return buildandrun(argc, argv, NULL, NULL, &customgroups)==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
-int addcustom(HyPerCol * hc, int argc, char * argv[]) {
-
-   int numOfConns=hc->numberOfConnections();
-   int rank=hc->icCommunicator()->commRank();
-   for(int i=0; i<numOfConns;i++){
-      HyPerConn *tempConnPtr = hc->getConnection(i);
-      const char * weightInitTypeStr = hc->parameters()->stringValue(tempConnPtr->getName(), "weightInitType");
-      ParameterGroup *grp =hc->parameters()->group(tempConnPtr->getName());
-
-      if( rank==0 ) {
-         printf("connection %s\n", tempConnPtr->getName());
-         printf("weightInitType %s\n", weightInitTypeStr);
-         printf("keyword %s\n", grp->getGroupKeyword());
-         char newName[100];
-         strncpy(newName, tempConnPtr->getName(), 94);
-         strncat(newName, "_copy", 5);
-         printf("new conn name %s\n", newName);
+void * customgroups(const char * keyword, const char * name, HyPerCol * hc) {
+   bool errorFound = false;
+   void * newobject = NULL;
+   if (!strcmp( keyword, "HyperConnDebugInitWeights") ) {
+      HyPerLayer * pre = NULL;
+      HyPerLayer * post = NULL;
+      getPreAndPostLayers(name, hc, &pre, &post);
+      const char * copiedConnName = hc->parameters()->stringValue(name, "copiedConn");
+      HyPerConn * copiedConn = hc->getConnFromName(copiedConnName);
+      newobject = new HyperConnDebugInitWeights(name, hc, pre, post, copiedConn);
+      if( !newobject ) {
+          fprintf(stderr, "Group \"%s\": Unable to create HyperConnDebugInitWeights\n", name);
+          errorFound = true;
       }
-
-      HyPerLayer * preLayer = tempConnPtr->getPre();
-      HyPerLayer * postLayer = tempConnPtr->getPost();
-      // PVParams * params = hc->parameters();
-      // ChannelType channelType = CHANNEL_INH;
-      //int channelNo = (int) params->value(tempConnPtr->getName(), "channelCode", -1);
-      HyPerConn * newConn;
-      if( !strcmp(grp->getGroupKeyword(), "HyPerConn") ) {
-         newConn = new HyperConnDebugInitWeights(tempConnPtr->getName(), hc, preLayer, postLayer, tempConnPtr);
-      }
-      else if( !strcmp(grp->getGroupKeyword(), "KernelConn") ) {
-         newConn = new KernelConnDebugInitWeights(tempConnPtr->getName(), hc, preLayer, postLayer, tempConnPtr);
-      }
-
    }
-
-   PVParams * params = hc->parameters();
-   int status;
-   int numGroups = params->numberOfGroups();
-   for (int n = 0; n < numGroups; n++) {
-      const char * kw = params->groupKeywordFromIndex(n);
-      const char * name = params->groupNameFromIndex(n);
+   else if (!strcmp(keyword, "KernelConnDebugInitWeights") ) {
+      HyPerLayer * pre = NULL;
+      HyPerLayer * post = NULL;
+      getPreAndPostLayers(name, hc, &pre, &post);
+      const char * copiedConnName = hc->parameters()->stringValue(name, "copiedConn");
+      HyPerConn * copiedConn = hc->getConnFromName(copiedConnName);
+      newobject = new KernelConnDebugInitWeights(name, hc, pre, post, copiedConn);
+      if( !newobject ) {
+          fprintf(stderr, "Group \"%s\": Unable to create KernelConnDebugInitWeights\n", name);
+          errorFound = true;
+      }
+   }
+   else if (!strcmp(keyword, "InitWeightTestProbe")) {
       HyPerLayer * targetlayer;
       char * message = NULL;
       const char * filename;
-      InitWeightTestProbe * addedProbe;
-      if (!strcmp(kw, "InitWeightTestProbe")) {
-         status = getLayerFunctionProbeParameters(name, kw, hc, &targetlayer,
-               &message, &filename);
-         if (status != PV_SUCCESS) {
-            fprintf(stderr, "Skipping params group \"%s\"\n", name);
-            continue;
-         }
-         if( filename ) {
-            addedProbe =  new InitWeightTestProbe(filename, targetlayer, message);
-         }
-         else {
-            addedProbe =  new InitWeightTestProbe(targetlayer, message);
-         }
-         free(message); message=NULL;
-         if( !addedProbe ) {
-            fprintf(stderr, "Group \"%s\": Unable to create probe\n", name);
-         }
-         assert(targetlayer);
-         checknewobject((void *) addedProbe, kw, name, hc);
+      int status = getLayerFunctionProbeParameters(name, keyword, hc, &targetlayer,
+            &message, &filename);
+      if (status != PV_SUCCESS) {
+         fprintf(stderr, "Skipping params group \"%s\"\n", name);
+         return NULL;
       }
+      if( filename ) {
+         newobject =  new InitWeightTestProbe(filename, targetlayer, message);
+      }
+      else {
+         newobject =  new InitWeightTestProbe(targetlayer, message);
+      }
+      free(message); message=NULL;
+      if( !newobject ) {
+         fprintf(stderr, "Group \"%s\": Unable to create probe\n", name);
+      }
+      assert(targetlayer);
+      checknewobject(newobject, keyword, name, hc);
    }
-
-   return PV_SUCCESS;
+   return newobject;
 }
