f917b26a0563513a05eb9fcfa975790d88b8df51
garkenyon <garkenyon@users.sourceforge.net>
2013-04-16 19:13:45 +0000

    shell
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@6873 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 62f452ee078a01243abc667a43fe2ea1398c4e3d

--- HyPerHLCA/shell/mpirun_vine_shared.sh
+++ HyPerHLCA/shell/mpirun_vine_shared.sh
@@ -3,7 +3,7 @@ MPIHOME=/usr/lib64/openmpi
 MPIRUN=${MPIHOME}/bin/mpirun
 BASEDIR=/home/gkenyon/workspace/HyPerHLCA2
 COMMAND="${BASEDIR}/Debug/HyPerHLCA2 -p ${BASEDIR}/input/HyPerHLCA_vine.params -rows 6 -columns 5"
-LOGDIR=/nh/compneuro/Data/vine/LCA/2013_01_31
+LOGDIR=/nh/compneuro/Data/vine/LCA/2013_01_31/output
 LOGFILE=${LOGDIR}/LCA_vine_2013_01_31.log
 mkdir -p ${LOGDIR}
 time ${MPIRUN} --byslot -np 30 ${COMMAND} 1> ${LOGFILE}
