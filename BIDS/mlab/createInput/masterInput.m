a9be81ffcab025d9adcefe0a9e70d030f155bcda
dpaiton <dpaiton@users.sourceforge.net>
2013-04-29 00:36:35 +0000

    tried to fix script so it uses less memory
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@6973 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: c9878611c8e1377788db7fcc2c29ed01f19a8fff

--- BIDS/mlab/createInput/masterInput.m
+++ BIDS/mlab/createInput/masterInput.m
@@ -2,20 +2,23 @@ clear all;
 close all;
 addpath('./k-Wave Toolbox');
 
-DIM = [1024, 1024, 0];   % [X, Y, t=0] This will be a 1280x1280m grid for the simulation (0.4 px/m).
-dx = 0.625;              % [m]
-dy = dx;                 % [m]
-BETA = -1;               % 0 is gaussian white, -1 is pink, -2 is Brownian
+SIMULATION_FILENAME = './simulation_output.mat';
+NOISE_FILENAME      = './noise_output.mat';
+
+DIM  = [1024, 1024, 0]; % [X, Y, t=0] This will be a 2560x2560m (640*4) grid for the simulation (2.5 m/px).
+dx   = 2.5;             % [m]
+dy   = dx;              % [m]
+BETA = -1;              % 0 is gaussian white, -1 is pink, -2 is Brownian
 
 %Sine wave params
 % peak vehicle frequency
-WAVE_FREQUENCY = 125;    %Hz
-WAVE_STRENGTH = 0.11;    %Pa (75dB SBL)
+WAVE_FREQUENCY = 125;   %Hz
+WAVE_STRENGTH  = 0.11;  %Pa (75dB SBL)
 
 %Time properties
-SOURCE_VEL  = 8.9408;    % [m/s] = 20 mph
-TIME_LENGTH = 120;       % [s]
-dt = 10e-3;              % [s] - 1ms
+SOURCE_VEL  = 8.9408;   % [m/s] = 20 mph
+TIME_LENGTH = 240;      % [s]
+dt          = 10e-3;    % [s] - 1ms
 
 %Medium properties
 % pure tone through air at 20 deg C, 30 perc humidity, 4000ft elevation (0.8755 bars, 0.864 ATM)
@@ -47,6 +50,14 @@ all_wave = bsxfun(@times, orig_drop, all_wave); %%TODO: This might be cheating -
 [Y, X, Z] = size(all_wave);
 DIM(3) = Z*2;
 
+%Scale wave
+range_wave = max(all_wave(:)) - min(all_wave(:));
+new_wave = all_wave ./ range_wave;
+long_wave = zeros(DIM);
+long_wave(:,:,DIM(3)/2:end) = new_wave;
+save(SIMULATION_FILENAME,'long_wave','-v7.3');
+clearvars -except DIM BETA NOISE_SCALE NOISE_FILENAME OUTPUT_DIR
+
 disp('MasterScript: Creating noise...');
 all_noise = spatialPattern(DIM, BETA);
 
@@ -54,30 +65,37 @@ all_noise = spatialPattern(DIM, BETA);
 std_noise = std(all_noise(:));
 new_noise = all_noise ./ std_noise;
 new_noise = new_noise .* NOISE_SCALE;
+save(NOISE_FILENAME,'new_noise','-v7.3');
+clearvars -except DIM OUTPUT_DIR
 
-%Scale wave
-range_wave = max(all_wave(:)) - min(all_wave(:));
-new_wave = all_wave ./ range_wave;
-long_wave = zeros(DIM);
-long_wave(:,:,DIM(3)/2:end) = new_wave;
-new_input = long_wave + new_noise;
+long_wave  = matfile(SIMULATION_FILENAME);
+new_noise = matfile(NOISE_FILENAME);
 
-%Scale input for imwrite
-scale = (max([abs(max(new_input(:))) abs(min(new_input(:)))]) * 2);
-scaled_input = new_input ./ scale;
-scaled_input = scaled_input .* 255; 
-scaled_input = scaled_input + 128;
-scaled_input = uint8(floor(scaled_input));
-
+disp('MasterScript: Scaling input...');
+scale_time = zeros(DIM(3));
+for i = 1:DIM(3)
+    new_input = long_wave(:,:,i) + new_noise(:,:,i);
+    scale_time(i) = (max([abs(max(new_input(:))) abs(min(new_input(:)))]) * 2);
+end
 
 disp('MasterScript: Writing files...');
 h = waitbar(0, 'Writing files...');
 for i = 1:DIM(3)
-   %imwrite(scaled_input(:,:,i),[OUTPUT_DIR,'/scaled_input_',num2str(i),'.jpg']);
-   frame_str = sprintf('%03d',i);
+   
+    %Add simulation output to noise
+    new_input = long_wave(:,:,i) + new_noise(:,:,i);
 
-   imwrite(abs(scaled_input(:, :, i)),[OUTPUT_DIR,'/input_',frame_str,'.jpg']);
-   waitbar(i/DIM(3));
+    %Scale input for imwrite
+    scaled_input = new_input ./ scale(i);
+    scaled_input = scaled_input .* 255; 
+    scaled_input = scaled_input + 128;
+    scaled_input = uint8(floor(scaled_input));
+
+    %imwrite(scaled_input(:,:,i),[OUTPUT_DIR,'/scaled_input_',num2str(i),'.jpg']);
+    frame_str = sprintf('%03d',i);
+
+    imwrite(abs(scaled_input(:, :)),[OUTPUT_DIR,'/input_',frame_str,'.jpg']);
+    waitbar(i/DIM(3));
 end
 close(h);
 
