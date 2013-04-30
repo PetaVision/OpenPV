873770b1a83af5792158ccd51c737a20e4cb5c01
slundquist <slundquist@users.sourceforge.net>
2013-04-30 16:58:19 +0000

    Made changes to environment figure creator
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@6985 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 55b92b52d7103436cdefae12d0dfbd455d8188f6

--- BIDS/mlab/bidsEnvironmentCreator.py
+++ BIDS/mlab/bidsEnvironmentCreator.py
@@ -1,26 +1,71 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import os
+from math import floor
+from scipy.misc import imsave
 
-JITTER = False
+#Params
+#Node radius is how big to make the dot.
+#Radius of 1 means it's expanded to a square that's 3 by 3, with orig dot in center
+#Note: there is a change for nodes to overlap in the image
+nodeRadius = 1
 
-world = np.zeros((256, 256));
-nodex = range(2, 256, 4)
-nodey = range(2, 256, 4)
+jitterVal = 9
+worldSize = (256, 256) #y by x
+bidsSize = (20, 20) #y by x, number of bids nodes in world
+outputDir = "./envFigs/"
+filename = "envFig_"
+ext = ".png"
 
-#Jitter
-if JITTER:
-    jnode = [(y+random.randint(-3, 3), x+random.randint(-3, 3)) for y in nodey for x in nodex]
-    jnode = [(y, x) for (y, x) in jnode if y>=0 and y<256 and x>=0 and x<256]
-else:
-    jnode = [(y, x) for y in nodey for x in nodex]
+def makeFig(jitterVal, worldSize, bidsSize, nodeRadius, outputFilename):
+    world = np.zeros(worldSize);
+    spacing = (float(worldSize[0])/bidsSize[0], float(worldSize[1])/bidsSize[1])
+
+    #Make sure radius isn't going to make node go off screen without jitter
+    if floor(float(spacing[0])/2) <= nodeRadius:
+        print "Radius too big, max val", floor(float(spacing[0]/2)) - 1
+        return
+    #Start spacing halfway spacing
+    nodes = (list(np.arange(float(spacing[0])/2, worldSize[0], spacing[0])),
+             list(np.arange(float(spacing[1])/2, worldSize[1], spacing[1])))
+
+    #Jitter
+    jnode = [(floor(y+random.randint(-1 * jitterVal, jitterVal)), floor(x+random.randint(-1 * jitterVal, jitterVal))) for y in nodes[0] for x in nodes[1]]
+    #Make sure that boundary conditions consider node radius as well.
+    #A node will only be shown if the node in it's entirety is in bounds
+    jnode = [(y, x) for (y, x) in jnode if y - nodeRadius>=0 and y + nodeRadius<worldSize[0] and x - nodeRadius>=0 and x + nodeRadius<worldSize[1]]
+
+    #if JITTER:
+    #    jnode = [(floor(y+random.randint(-1 * jitterVal, jitterVal)), floor(x+random.randint(-1 * jitterVal, jitterVal))) for y in nodes[0] for x in nodes[1]]
+    #    jnode = [(y, x) for (y, x) in jnode if y>=0 and y<worldSize[0] and x>=0 and x<worldSize[1]]
+    #else:
+    #    jnode = [(floor(y), floor(x)) for y in nodes[0] for x in nodes[1]]
 
 
-for coor in jnode:
-    world[coor[0]][coor[1]] = 1
+    for coor in jnode:
+        for curRadius in range(nodeRadius + 1):
+            #Square
+            for horL in range(int(coor[1]-curRadius), int(coor[1]+curRadius+1)):
+                #Top line
+                world[coor[0]-curRadius][horL] = 1
+                #Bot line
+                world[coor[0]+curRadius][horL] = 1
+            for vertL in range(int(coor[0]-curRadius), int(coor[0]+curRadius+1)):
+                #Left line
+                world[vertL][coor[1]-curRadius] = 1
+                #Right line
+                world[vertL][coor[1]+curRadius] = 1
 
-plt.gray()
 
-plt.imshow(world)
-plt.show()
+    #save img
+    imsave(outputFilename, world)
 
+
+if not os.path.exists(outputDir):
+    os.makedirs(outputDir)
+
+#No jitter
+makeFig(0, worldSize, bidsSize, nodeRadius, outputDir + filename + "nj" + ext)
+#jitter
+makeFig(jitterVal, worldSize, bidsSize, nodeRadius, outputDir + filename + "j" + ext)
