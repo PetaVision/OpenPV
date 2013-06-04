a707181b64a9caab74e33947bea55fc21b2b194c
garkenyon <garkenyon@users.sourceforge.net>
2013-06-04 15:14:32 +0000

    scripts
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@7187 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 306eaa869f6ee97d91e0b9777333633ad542ed89

--- HyPerHLCA/shell/mpirun_vine_anterior.sh
+++ HyPerHLCA/shell/mpirun_vine_anterior.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 MPIHOME=/usr/lib64/openmpi
 MPIRUN=${MPIHOME}/bin/mpirun
-MPIARGS="--mca bta tcp,self"
 BASEDIR=/home/gkenyon/workspace/HyPerHLCA2
-COMMAND="${BASEDIR}/Debug/HyPerHLCA2 -p ${BASEDIR}/input/HyPerHLCA_vine.params -rows 6 -columns 10"
+COMMAND="${BASEDIR}/Debug/HyPerHLCA2 -p ${BASEDIR}/input/HyPerHLCA_vine.params -rows 12 -columns 10"
 LOGDIR=/nh/compneuro/Data/vine/LCA/2013_01_31/output_16x16x1024_Overlap_lambda_05X2
 LOGFILE=${LOGDIR}/LCA_vine_2013_01_31.log
 mkdir -p ${LOGDIR}
 touch ${LOGFILE}
 echo ${LOGFILE}
-#time ${MPIRUN} --byslot -np 60 ${MPIARGS} ${COMMAND} &> ${LOGFILE}
-time ${MPIRUN} -np 60 --mca bta tcp,self ${COMMAND} &> ${LOGFILE}
+time ${MPIRUN} --bynode --hostfile ~/.mpi_hosts -np 120 ${COMMAND} &> ${LOGFILE}
