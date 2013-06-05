00c3c2700a1f28807c06fdc893b7641b082133e9
dpaiton <dpaiton@users.sourceforge.net>
2013-06-05 11:58:43 +0000

    Moved probe to PetaVision src
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@7208 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 91f7e995ac4b7b006ae29fee633368d6206f3e76

--- textAnalysis/main.cpp
+++ textAnalysis/main.cpp
@@ -4,7 +4,6 @@
  */
 
 #include "../PetaVision/src/columns/buildandrun.hpp"
-#include "TextStreamProbe.hpp"
 
 #define MAIN_USES_CUSTOMGROUPS
 
@@ -26,35 +25,5 @@ int main(int argc, char * argv[]) {
 
 #ifdef MAIN_USES_CUSTOMGROUPS
 void * addcustomgroup(const char * keyword, const char * groupname, HyPerCol * hc) {
-    int status;
-    LayerProbe * addedProbe = NULL;
-    HyPerLayer * targetlayer;
-    char * message = NULL;
-    const char * filename;
-    if( !strcmp( keyword, "TextStreamProbe") ) {
-        status = getLayerFunctionProbeParameters(groupname, keyword, hc, &targetlayer,
-                &message, &filename);
-        if (status != PV_SUCCESS) {
-            fprintf(stderr, "Error reading params group \"%s\"\n", groupname);
-            return addedProbe;
-        }
-        int display_period = hc->parameters()->value(groupname, "displayPeriod", 1);
-        if( filename ) {
-            addedProbe =  new TextStreamProbe(filename, targetlayer, display_period);
-        }
-        else {
-            addedProbe =  new TextStreamProbe(NULL, targetlayer, display_period);
-        }
-        free(message); message=NULL; // message was alloc'ed in getLayerFunctionProbeParameters call
-        if( !addedProbe ) {
-            fprintf(stderr, "Group \"%s\": Unable to create %s\n", groupname, keyword);
-        }
-        assert(targetlayer);
-        checknewobject((void *) addedProbe, keyword, groupname, hc);
-        return addedProbe;
-    }
-    assert(!addedProbe);
-    fprintf(stderr, "Unrecognized params keyword \"%s\"\n", keyword);
-    return addedProbe;
 }
 #endif // MAIN_USE_CUSTOMGROUPS
