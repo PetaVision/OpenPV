22de567d597f687d9b2b6ca141c0fe4f5f879231
garkenyon <garkenyon@users.sourceforge.net>
2013-07-08 21:24:29 +0000

    LCA params and scripts
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@7310 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 5d5efa04d96f36e277d8c9211f3ac2d7723982b8

--- HyPerHLCA/shell/mpirun_KITTI.sh
+++ HyPerHLCA/shell/mpirun_KITTI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MPIHOME=/usr/lib64/openmpi
 MPIRUN=${MPIHOME}/bin/mpirun
-MPIARGS="--mca bta tcp,self"
+MPIARGS=""##"--mca bta tcp,self"
 BASEDIR=/home/gkenyon/workspace/HyPerHLCA2
 COMMAND="${BASEDIR}/Debug/HyPerHLCA2 -p ${BASEDIR}/input/HyPerHLCA_KITTI.params -rows 5 -columns 12"
 LOGDIR=/nh/compneuro/Data/KITTI/LCA/2011_09_26_drive_0005_sync/
@@ -9,5 +9,5 @@ LOGFILE=${LOGDIR}/KITTI_LCA_2011_09_26_drive_0005_sync.log
 mkdir -p ${LOGDIR}
 touch ${LOGFILE}
 echo ${LOGFILE}
-#time ${MPIRUN} --byslot -np 60 ${MPIARGS} ${COMMAND} &> ${LOGFILE}
-time ${MPIRUN} -np 60 --mca btl tcp,self ${COMMAND} &> ${LOGFILE}
+time ${MPIRUN} --byslot -np 60 ${COMMAND} &> ${LOGFILE}
+#time ${MPIRUN} -np 60 --mca btl tcp,self ${COMMAND} &> ${LOGFILE}
