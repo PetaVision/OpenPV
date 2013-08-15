9ad6bbf3f4d68b3c36dc8e61f2734344b0cc143e
peteschultz <peteschultz@users.sourceforge.net>
2013-08-15 00:24:38 +0000

    L2NormFunction now uses double-precision arithmetic internally and then converts to pv_data at the end.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@7532 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/io/L2NormFunction.cpp
+++ src/io/L2NormFunction.cpp
@@ -13,7 +13,7 @@ L2NormFunction::L2NormFunction(const char * name) : LayerFunction(name) {
 }  // end L2NormFunction::L2NormFunction
 
 pvdata_t L2NormFunction::evaluateLocal(float time, HyPerLayer * l) {
-    pvdata_t l2norm = 0;
+    double l2norm = 0;
     pvdata_t * activityBuffer = l->getCLayer()->activity->data;
     int numNeurons = l->getNumNeurons();
     const int nx = l->getLayerLoc()->nx;
@@ -22,10 +22,10 @@ pvdata_t L2NormFunction::evaluateLocal(float time, HyPerLayer * l) {
     const int nb = l->getLayerLoc()->nb;
     for(int n=0; n<numNeurons; n++) {
         int nex = kIndexExtended(n, nx, ny, nf, nb);
-    	pvdata_t v = activityBuffer[nex];
+    	double v = (double) activityBuffer[nex];
         l2norm += v*v;
     }
-    return 0.5*l2norm;
+    return (pvdata_t) (0.5*l2norm);
 }  // end L2NormFunction::evaluate(float, HyPerLayer *)
 
 }  // end namespace PV
