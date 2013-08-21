ec595e77993e8bacfb8f35d1e6c23079d55d528d
garkenyon <garkenyon@users.sourceforge.net>
2013-08-21 22:48:26 +0000

    mpi shell scripts
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@7567 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: b6676f6c8c13a03ba855a74afeb8f4768d521c51

--- HyPerHLCA/shell/mpirun_vine_color_deep_anterior.sh
+++ HyPerHLCA/shell/mpirun_vine_color_deep_anterior.sh
@@ -3,8 +3,8 @@ MPIHOME=/usr/lib64/openmpi
 MPIRUN=${MPIHOME}/bin/mpirun
 BASEDIR=/home/gkenyon/workspace/HyPerHLCA2
 COMMAND="${BASEDIR}/Debug/HyPerHLCA2 -p ${BASEDIR}/input/HyPerHLCA_vine_color_deep.params -rows 12 -columns 10"
-LOGDIR=/nh/compneuro/Data/vine/LCA/2013_01_31/output_12x12x512_lambda_05X2_color_deep
-LOGFILE=${LOGDIR}/LCA_vine_12x12x512_lambda_05X2_color_deep.log
+LOGDIR=/nh/compneuro/Data/vine/LCA/2013_01_29/output_12x12x128_lambda_05X2_color_deep
+LOGFILE=${LOGDIR}/LCA_vine_12x12x128_lambda_05X2_color_deep.log
 mkdir -p ${LOGDIR}
 touch ${LOGFILE}
 echo ${LOGFILE}
--- HyPerHLCA/shell/mpirun_vine_voluptuosa.sh
+++ HyPerHLCA/shell/mpirun_vine_voluptuosa.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 MPIHOME=/usr/lib64/openmpi
 MPIRUN=${MPIHOME}/bin/mpirun
-MPIARGS="--mca bta tcp,self"
+MPIARGS="--mca btl tcp,self"
 BASEDIR=/home/gkenyon/workspace/HyPerHLCA2
-COMMAND="${BASEDIR}/Debug/HyPerHLCA2 -p ${BASEDIR}/input/HyPerHLCA_vine.params -rows 6 -columns 10"
-LOGDIR=/nh/compneuro/Data/vine/LCA/2013_01_31/output_16x16x1024_Overlap_lambda_05X2
-LOGFILE=${LOGDIR}/LCA_vine_2013_01_31.log
+COMMAND="${BASEDIR}/Debug/HyPerHLCA2 -p ${BASEDIR}/input/HyPerHLCA_vine_color_noPulvinar.params -rows 6 -columns 10"
+LOGDIR=/nh/compneuro/Data/vine/LCA/2013_01_31/output_12x12x128_lambda_05X2_color_noPulvinar
+LOGFILE=${LOGDIR}/LCA_vine_2013_01_31_12x12x128_lambda_05X2_color_noPulvinar.log
 mkdir -p ${LOGDIR}
 touch ${LOGFILE}
 echo ${LOGFILE}
 #time ${MPIRUN} --byslot -np 60 ${MPIARGS} ${COMMAND} &> ${LOGFILE}
-time ${MPIRUN} -np 60 --mca bta tcp,self ${COMMAND} &> ${LOGFILE}
+time ${MPIRUN} -np 60 --mca btl tcp,self ${COMMAND} &> ${LOGFILE}
