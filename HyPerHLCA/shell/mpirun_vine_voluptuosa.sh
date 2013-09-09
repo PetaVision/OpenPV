4449a08d85a2ff1fda02e5de27d2adc4e60d9512
garkenyon <garkenyon@users.sourceforge.net>
2013-09-09 17:41:06 +0000

    LCA input params and scripts
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@7645 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 12fa8727ee345eb6498c4104a8d6a3923c8ffb7d

--- HyPerHLCA/shell/mpirun_vine_voluptuosa.sh
+++ HyPerHLCA/shell/mpirun_vine_voluptuosa.sh
@@ -3,9 +3,9 @@ MPIHOME=/usr/lib64/openmpi
 MPIRUN=${MPIHOME}/bin/mpirun
 MPIARGS="--mca btl tcp,self"
 BASEDIR=/home/gkenyon/workspace/HyPerHLCA2
-COMMAND="${BASEDIR}/Debug/HyPerHLCA2 -p ${BASEDIR}/input/HyPerHLCA_vine_color_noPulvinar.params -rows 6 -columns 10"
-LOGDIR=/nh/compneuro/Data/vine/LCA/2013_01_31/output_12x12x128_lambda_05X2_color_noPulvinar
-LOGFILE=${LOGDIR}/LCA_vine_2013_01_31_12x12x128_lambda_05X2_color_noPulvinar.log
+COMMAND="${BASEDIR}/Debug/HyPerHLCA2 -p ${BASEDIR}/input/HyPerHLCA_vine_noPulvinar.params -rows 6 -columns 10"
+LOGDIR=/nh/compneuro/Data/vine/LCA/2013_01_31/output_2013_01_31_12x12x128_lambda_05X2_noPulvinar
+LOGFILE=${LOGDIR}/LCA_vine_2013_01_31_12x12x128_lambda_05X2_noPulvinar.log
 mkdir -p ${LOGDIR}
 touch ${LOGFILE}
 echo ${LOGFILE}
