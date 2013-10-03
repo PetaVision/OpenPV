e2e9054765206a50d575958a1d3ff5b28be9473f
dpaiton <dpaiton@users.sourceforge.net>
2013-10-03 20:34:53 +0000

    fixes to speed up the run
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@7773 7c0444ca-5550-0410-9008-faecdbe78eb5

--- mlab/HyPerLCA/computeFiringStats.m
+++ mlab/HyPerLCA/computeFiringStats.m
@@ -15,8 +15,19 @@
 
 %function [statsStruct] = computeFiringStats(pvp_filename, output_dir)
 
-    pvp_filename = '~/Documents/Workspace/HyPerHLCA/output/a3_V1.pvp';
-    output_dir = '~/Documents/Workspace/HyPerHLCA/output/mlab';
+    workspace_path = '/home/dpaiton/workspace';
+    addpath([workspace_path,filesep,'PetaVision/mlab/util']);
+
+    %pvp_filename = '~/Documents/Workspace/HyPerHLCA/output/a3_V1.pvp';
+    %output_dir = '~/Documents/Workspace/HyPerHLCA/output/mlab';
+
+    pvp_filename   = '/nh/compneuro/Data/vine/LCA/2013_02_01/output_2013_02_01_12x12x128_lambda_05X2_deep/a5_V1.pvp';
+    output_dir     = '/nh/compneuro/Data/vine/LCA/2013_02_01/output_2013_02_01_12x12x128_lambda_05X2_deep/correlation_stats/';
+    progressperiod = 100;
+    num_frames     = 500;
+    start_frame    = 10000000;
+    skip_frame     = 1;
+
 
 
     %%Variable input
@@ -26,7 +37,7 @@
 
     %%Input PVP file
     if exist(pvp_filename,'file')
-       [data,hdr] = readpvpfile(pvp_filename);
+       [data,hdr] = readpvpfile(pvp_filename,progressperiod,num_frames,start_frame,skip_frame);
     else
         error(['computeFiringStats: ~exist(pvp_filename,"file") in pvp file: ', pvp_filename]);
     end
@@ -41,18 +52,20 @@
     if exist('data','var') && exist('hdr','var')
         times    = zeros(numPvFrames,1); 
         full_mat = zeros(hdr.ny,hdr.nx,hdr.nf,numPvFrames);
+        for frame = 1 : 1 : numPvFrames %%Assumes PVP file has contiguous frames
+            times(frame) = squeeze(data{frame}.time);
+            active_ndx   = squeeze(data{frame}.values);
+            vec_mat      = full(sparse(active_ndx+1,1,1,N,1,N)); %%Column vector. PetaVision increments in order: nf, nx, ny
+            rs_mat       = reshape(vec_mat,hdr.nf,hdr.nx,hdr.ny);
+
+            full_mat(:,:,:,frame) = permute(rs_mat,[3 2 1]); %%Matrix is now [ny, nx, nf]
+        end
+
+        disp(['computeFiringStats: Output correlation mat files to ',output_dir])
+
         for y = 1 : 1 : hdr.ny
             for x = 1 : 1 : hdr.nx
-                for frame = 1 : 1 : numPvFrames %%Assumes PVP file has contiguous frames
-                    times(frame) = squeeze(data{frame}.time);
-                    active_ndx   = squeeze(data{frame}.values);
-                    vec_mat      = full(sparse(active_ndx+1,1,1,N,1,N)); %%Column vector. PetaVision increments in order: nf, nx, ny
-                    rs_mat       = reshape(vec_mat,hdr.nf,hdr.nx,hdr.ny);
-
-                    full_mat(:,:,:,frame) = permute(rs_mat,[3 2 1]); %%Matrix is now [ny, nx, nf]
-                end
-
-                [co,lags] = xcorr(squeeze(full_mat(1,1,:,:)));
+                [co,lags] = xcorr(squeeze(full_mat(yx1,:,:)));
 
                 correlations.corrs = co;
                 correlations.lags = lags;
