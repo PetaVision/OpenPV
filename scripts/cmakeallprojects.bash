c86fac61c3e5f75698c8ee7ca91ad2f7781d3028
peteschultz <peteschultz@users.sourceforge.net>
2013-10-07 21:23:35 +0000

    A bash script to run cmake on PetaVision and then all projects in the directory containing PetaVision
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@7787 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ scripts/cmakeallprojects.bash
@@ -0,0 +1,92 @@
+#! /usr/bin/env bash
+
+# If called from a directory other than PetaVision/scripts, change to PetaVision/scripts
+if test "${0%/*}" != "$0"
+then
+    cd "${0%/*}"
+fi
+cd ../.. # We should now be in the eclipse workspace directory
+wd=$PWD
+
+fails=""
+nomakefile=""
+
+# Building PetaVision does not automatically build the parser files created by flex/bison
+echo ; echo ======== Building io/parser ========
+cd PetaVision/src/io/parser
+make all
+if test "$?" -ne 0
+then
+    fails="$fails io/parser"
+fi
+cd $wd
+
+# PetaVision must be compiled before any projects that depend on it
+echo ; echo ======== Building PetaVision ========
+cd PetaVision
+rm -rf CMakeFiles
+cmake -DCMAKE_C_COMPILER=openmpicc -DCMAKE_CXX_COMPILER=openmpic++ -DMPI_C_COMPILER=openmpicc -DMPI_CXX_COMPILER=openmpic++ -DCMAKE_BUILD_TYPE=Debug
+make -j4 all
+if test "$?" -ne 0
+then
+    echo "PetaVision failed to build."
+    exit 1;
+fi
+cd $wd
+
+# The PetaVision/tools directory has the source code for the command-line tool readpvpheader
+echo ; echo ======== Building PetaVision tools ========
+cd PetaVision/tools
+make
+if test "$?" -ne 0
+then
+    fails="$fails PetaVision/tools"
+fi
+cd $wd
+
+projectlist=$(ls -F | egrep '/$' | sed -e '1,$s/\///' | egrep -v '^(CMakeFiles|PetaVision)')
+
+# Compile each project in workspace directory except PetaVision
+for k in $projectlist
+do
+    echo ; echo ======== Building $(basename $k) ========
+    if test -f $k/CMakeLists.txt
+    then
+        hascmake=1
+        cd $k
+        rm -rf CMakeFiles
+        cmake -DCMAKE_C_COMPILER=openmpicc -DCMAKE_CXX_COMPILER=openmpic++ -DMPI_C_COMPILER=openmpicc -DMPI_CXX_COMPILER=openmpic++ -DCMAKE_BUILD_TYPE=Debug -DPV_WRKSPC_DIR=../       
+        make clean
+        make -j4 all
+        if test "$?" -ne 0
+        then
+            fails="$fails $k"
+        fi
+    else
+        nocmakefile="$nocmakefile $k"
+    fi
+    cd $wd
+done
+
+# Compile the unit tests
+echo ; echo ======== Building PetaVision/tests ========
+cd PetaVision/tests
+make clean
+make -j4 all
+if test "$?" -ne 0
+then
+    fails="$fails tests"
+fi
+cd $wd
+echo cd $wd
+
+if test -n "$fails"
+then
+    echo "The following projects failed to build:$fails"
+else
+    echo "All builds succeeded."
+fi
+if test -n "$nomakefile"
+then
+    echo "The following projects have no CMakeLists.txt:$nocmakefile"
+fi
