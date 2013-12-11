7007702fb2caf08a2ee787b918c09d261d5539f7
dpaiton <dpaiton@users.sourceforge.net>
2013-12-11 21:09:58 +0000

    lots of v1 param files...
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@7941 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: c7da81915c8ac57a0d0c0c090928edf2e13ec68b

--- HyPerHLCA/shell/mpirun_CIFAR.sh
+++ HyPerHLCA/shell/mpirun_CIFAR.sh
@@ -3,11 +3,11 @@ MPIHOME=/usr/lib64/openmpi
 MPIRUN=${MPIHOME}/bin/mpirun
 
 ## FOR VOLUPTUOSA
-MPIARGS="--mca bta tcp,self"
+#MPIARGS="--mca bta tcp,self"
 
 NUMPROCS="4"
-BASEDIR=/home/dpaiton/workspace/HyPerHLCA2
-COMMAND="${BASEDIR}/Debug/HyPerHLCA2 -p ${BASEDIR}/input/HyPerHLCA_vine_lateral.params -rows 2 -columns 2"
+BASEDIR=/home/dpaiton/workspace/HyPerHLCA
+COMMAND="${BASEDIR}/Debug/HyPerHLCA -p ${BASEDIR}/input/HyPerHLCA_CIFAR_RGB.params -rows 2 -columns 2"
 LOGDIR=/nh/compneuro/Data/Cifar/LCA/data_driven/pass0
 LOGFILE=${LOGDIR}/LCA_Deep_CIFAR.log
 mkdir -p ${LOGDIR}
