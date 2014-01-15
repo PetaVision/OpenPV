36be4c59b2788acba32720e887ffd2722dfe2cb4
slundquist <slundquist@users.sourceforge.net>
2014-01-15 17:16:01 +0000

    Added system test for binning layer
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8065 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ BinningLayerTest/src/BinningTestLayer.hpp
@@ -0,0 +1,19 @@
+#ifndef BINNINGTESTLAYER_HPP_ 
+#define BINNINGTESTLAYER_HPP_
+
+#include <layers/BinningLayer.hpp>
+
+namespace PV {
+
+class BinningTestLayer: public PV::BinningLayer{
+public:
+	BinningTestLayer(const char* name, HyPerCol * hc);
+
+protected:
+   int updateState(double timef, double dt);
+
+private:
+};
+
+} /* namespace PV */
+#endif
