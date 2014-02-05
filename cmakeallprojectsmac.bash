28e013e3501b7515818de1e94a42eefeedecfe2b
peteschultz <peteschultz@users.sourceforge.net>
2014-02-05 00:02:26 +0000

    Fix to cmakeallprojectsmac.bash since bash script isn't recognizing cmakemac alias
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8128 7c0444ca-5550-0410-9008-faecdbe78eb5

--- cmakeallprojectsmac.bash
+++ cmakeallprojectsmac.bash
@@ -1,11 +1,17 @@
 #! /bin/bash
-alias cmakemac="cmake CMakeLists.txt -DCMAKE_C_COMPILER=openmpicc \
-                                     -DCMAKE_CXX_COMPILER=openmpic++ \
-                                     -DMPI_C_COMPILER=openmpicc \
-                                     -DMPI_CXX_COMPILER=openmpic++ \
-                                     -DMPI_HOME=/opt/local"
+# # Can you not use alias command inside a bash script?
+#alias cmakemac="cmake CMakeLists.txt -DCMAKE_C_COMPILER=openmpicc \
+#                                     -DCMAKE_CXX_COMPILER=openmpic++ \
+#                                     -DMPI_C_COMPILER=openmpicc \
+#                                     -DMPI_CXX_COMPILER=openmpic++ \
+#                                     -DMPI_HOME=/opt/local"
 for proj in $(ls */CMakeLists.txt | xargs -n 1 dirname)
 do
-    (cd $proj && cmakemac -DCMAKE_BUILD_TYPE=Debug -DPV_WRKSPC_DIR=../.. &&
+    (cd $proj && cmake -DCMAKE_C_COMPILER=openmpicc \
+                       -DCMAKE_CXX_COMPILER=openmpic++ \
+                       -DMPI_C_COMPILER=openmpicc \
+                       -DMPI_CXX_COMPILER=openmpic++ \
+                       -DMPI_HOME=/opt/local \
+                       -DCMAKE_BUILD_TYPE=Debug -DPV_WRKSPC_DIR=../.. &&
                  make clean && make)
 done
