b5d84da0a563776f2c8fe3de7e1a20cb43a81c33
peteschultz <peteschultz@users.sourceforge.net>
2014-02-12 19:21:16 +0000

    Add $PWD to PV_WRKSPC_DIR argument to avoid relative path warnings
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8149 7c0444ca-5550-0410-9008-faecdbe78eb5

--- cmakeallprojectslinux.bash
+++ cmakeallprojectslinux.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
 for proj in $(ls */CMakeLists.txt | xargs -n 1 dirname)
 do
-    (cd $proj && cmakemac -DCMAKE_BUILD_TYPE=Debug -DPV_WRKSPC_DIR=../..)
+    (cd $proj && cmakemac -DCMAKE_BUILD_TYPE=Debug -DPV_WRKSPC_DIR=$PWD/../..)
 done
--- cmakeallprojectsmac.bash
+++ cmakeallprojectsmac.bash
@@ -1,17 +1,14 @@
 #! /bin/bash
-# # Can you not use alias command inside a bash script?
-#alias cmakemac="cmake CMakeLists.txt -DCMAKE_C_COMPILER=openmpicc \
-#                                     -DCMAKE_CXX_COMPILER=openmpic++ \
-#                                     -DMPI_C_COMPILER=openmpicc \
-#                                     -DMPI_CXX_COMPILER=openmpic++ \
-#                                     -DMPI_HOME=/opt/local"
+OPENMPIC=openmpicc
+OPENMPICXX=openmpic++
+MPI_HOME=/opt/local
 for proj in $(ls */CMakeLists.txt | xargs -n 1 dirname)
 do
-    (cd $proj && cmake -DCMAKE_C_COMPILER=openmpicc \
-                       -DCMAKE_CXX_COMPILER=openmpic++ \
-                       -DMPI_C_COMPILER=openmpicc \
-                       -DMPI_CXX_COMPILER=openmpic++ \
+    (cd $proj && cmake -DCMAKE_C_COMPILER=$OPENMPIC \
+                       -DCMAKE_CXX_COMPILER=$OPENMPICXX \
+                       -DMPI_C_COMPILER=$OPENMPIC \
+                       -DMPI_CXX_COMPILER=$OPENMPICXX \
                        -DMPI_HOME=/opt/local \
-                       -DCMAKE_BUILD_TYPE=Debug -DPV_WRKSPC_DIR=../.. &&
+                       -DCMAKE_BUILD_TYPE=Debug -DPV_WRKSPC_DIR=$PWD/../.. &&
                  make clean && make)
 done
