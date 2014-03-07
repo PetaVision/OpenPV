4cdb7deaf569b39770689cb6be9ec0a43ed062ef
wshainin <wshainin@users.sourceforge.net>
2014-03-07 21:05:27 +0000

    Latest script changes; combines multi-scale reconstructions and normalizes
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@8217 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 93eeb2e3ca3208f66e8100ee3fa8b647f07ddbd7

--- MaskLCA/scripts/combineRecon.m
+++ MaskLCA/scripts/combineRecon.m
@@ -1,13 +1,20 @@
-%% Combines the reconstructed images from the three layers of the stack and places
-%%    all reconstructions from a set in a new folder corresponding to the timestep
+%% -Combines the reconstructed images from the three layers of the stack
+%% -Combines shuffled images if runType is "stack"
+%% -Places all reconstructions from a set in a new folder corresponding to the timestep
 %% Recon_S2 prefix: Reconstruction from the stride of 2
 %% Recon_S4 prefix: Reconstruction from the stride of 4
 %% Recon_S8 prefix: Reconstruction from the stride of 8
 %% Recon_XA prefix: Linear combination of all reconstructions
 %% Recon_XN prefix: Normalized linear combination
 
-function combineRecon
-recon_path         = '/nh/compneuro/Data/vine/LCA/2013_01_24_2013_02_01/output_stack_vine/Recon/';
+function combineRecon%%(run_type)
+  %%if strcmp(run_type, "stack" | nargin == 0)
+     recon_path         = '/nh/compneuro/Data/vine/LCA/2013_01_24_2013_02_01/output_stack_vine/Recon/';
+     global shuffle     = 0;
+ %% elseif strcmp(run_type, "shuffle")
+ %%    recon_path         = '/nh/compneuro/Data/vine/LCA/2013_01_24_2013_02_01/output_stack_vine_shuffle/Recon/';
+ %%    global shuffle     = 1;
+ %% end
 global inputs_file = '/nh/compneuro/Data/vine/list/2013_01_24_2013_02_01/fileList_2013_01_24_2013_02_01.txt';
 global dP = 40;
        nP = 16;
@@ -29,7 +36,7 @@ end
 function [img] = comb(in)  %% Input cell contains three full paths to each image in a set
 global inputs_file
 global dP
-
+global shuffle
 im1 = imread(in{1,1});
 im2 = imread(in{2,1});
 im3 = imread(in{3,1});
