7f4f5ed24ff1c22c2a788cbeefe08696e5648de8
slundquist <slundquist@users.sourceforge.net>
2014-03-13 21:52:42 +0000

    Fixed cmake lists in all sandboxes to allow for user specified petavision directory
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8235 7c0444ca-5550-0410-9008-faecdbe78eb5

--- cmakeallprojectslinux.bash
+++ cmakeallprojectslinux.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
 for proj in $(ls */CMakeLists.txt | xargs -n 1 dirname)
 do
-    (cd $proj && cmakemac -DCMAKE_BUILD_TYPE=Debug -DPV_WRKSPC_DIR=$PWD/../..)
+    (cd $proj && cmake -DCMAKE_BUILD_TYPE=Debug -DPV_WRKSPC_DIR=$PWD/../..)
 done
