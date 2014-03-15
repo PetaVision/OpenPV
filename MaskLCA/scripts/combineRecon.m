8670a7d70394ab28f727f30469acf10b7a1b8957
wshainin <wshainin@users.sourceforge.net>
2014-03-15 00:10:39 +0000

    Fixed bugs in script
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@8250 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 6d96cd00aa7b010c27aa2af5cdd40cd22a8a2fc7

--- MaskLCA/scripts/combineRecon.m
+++ MaskLCA/scripts/combineRecon.m
@@ -7,59 +7,94 @@
 %% Recon_XA prefix: Linear combination of all reconstructions
 %% Recon_XN prefix: Normalized linear combination
 
-function combineRecon%%(run_type)
-  %%if strcmp(run_type, "stack" | nargin == 0)
-     recon_path         = '/nh/compneuro/Data/vine/LCA/2013_01_24_2013_02_01/output_stack_vine/Recon/';
-     global shuffle     = 0;
- %% elseif strcmp(run_type, "shuffle")
- %%    recon_path         = '/nh/compneuro/Data/vine/LCA/2013_01_24_2013_02_01/output_stack_vine_shuffle/Recon/';
- %%    global shuffle     = 1;
- %% end
-global inputs_file = '/nh/compneuro/Data/vine/list/2013_01_24_2013_02_01/fileList_2013_01_24_2013_02_01.txt';
-global dP = 40;
-       nP = 16;
+function combineRecon(run_type, source_orig)
+   global inputs_file = '/nh/compneuro/Data/vine/list/2013_01_24_2013_02_01/fileList_2013_01_24_2013_02_01.txt';
+   global disp_period = 40;
+   num_procs = 16;
+   num_layers = 3;
+   if nargin < 2
+      global s_o = 0;
+   else
+      global s_o = source_orig;
+   end
+   if nargin == 0 || strcmp(run_type, "stack")
+      recon_path         = '/nh/compneuro/Data/vine/LCA/2013_01_24_2013_02_01/output_stack_vine/Recon/'
+      global shuffle     = 0;
+      dir_list   = glob([recon_path,'*.png']);
+      num_images = numel(dir_list);
+      num_recon  = num_images/num_layers;
+   elseif strcmp(run_type, "shuffle")
+      recon_path         = '/nh/compneuro/Data/vine/LCA/2013_01_24_2013_02_01/output_stack_vine_shuffle/Recon/'
+      global shuffle     = 1;
+      dir_list   = glob([recon_path,'*.png']);
+      num_images = numel(dir_list);
+      num_recon  = (num_images/num_layers)/2;
+   end
 
-dir_list   = glob([recon_path,'*.png']);
-num_images = numel(dir_list);
-num_recon  = num_images/3;
-
-for i=1:num_recon
-   img_comp{1,1} = dir_list{i};
-   img_comp{2,1} = dir_list{i+num_recon};
-   img_comp{3,1} = dir_list{i+2*num_recon};
-   img_sets{i,1} = img_comp;
-end
-out = parcellfun(nP, @comb, img_sets,'UniformOutput',false);
-%%out = cellfun(@comb, img_sets,'UniformOutput',false);
+   for i=1:num_recon 
+      for j=1:num_layers
+         img_comp{j,1} = dir_list{i + (j-1)*num_recon};
+         if shuffle
+            img_comp{j+num_layers,1} = dir_list{i + num_images/2 + (j-1)*num_recon};
+         end
+      end
+      img_sets{i,1} = img_comp;
+   end
+   
+   out = parcellfun(num_procs, @comb, img_sets,'UniformOutput',false);
+   %%out = cellfun(@comb, img_sets,'UniformOutput',false);
 end
 
 function [img] = comb(in)  %% Input cell contains three full paths to each image in a set
-global inputs_file
-global dP
-global shuffle
-im1 = imread(in{1,1});
-im2 = imread(in{2,1});
-im3 = imread(in{3,1});
-img = imlincomb((1/3),im1,(1/3),im2,(1/3),im3);
-%%img = (im1*(1/3)+im2*(1/3)+im3*(1/3)); %% For older Octave image packages without imlincomb()
+   global inputs_file
+   global disp_period
+   global shuffle
+   global s_o
 
-[path,name,ext] = fileparts(in{1});
-img_ID          = regexp(name,'_\d+','match'){1}(2:end);
-set_path        = [path,filesep,img_ID];
-in_img_idx      = ceil(str2double(img_ID)/dP); 
-in_img_dir      = textread(inputs_file, '%s', 2, 'headerlines', in_img_idx-2);
-%% The recon image names are not consistently reflecting the correct timestep
+   %% Probably make the following dynamic, not hard-coded. Lazyass
+   im1 = imread(in{1,1});
+   im2 = imread(in{2,1});
+   im3 = imread(in{3,1});
+   %%img = imlincomb((1/3),im1,(1/3),im2,(1/3),im3);
+   img = (im1*(1/3)+im2*(1/3)+im3*(1/3)); %% For older Octave image packages without imlincomb()
+   
+   if shuffle
+      shuf_im1 = imread(in{4,1});
+      shuf_im2 = imread(in{5,1});
+      shuf_im3 = imread(in{6,1});
+      %%shuf_img = imlincomb((1/3),shuf_im1,(1/3),shuf_im2,(1/3),shuf_im3);
+      shuf_img = (shuf_im1*(1/3)+shuf_im2*(1/3)+shuf_im3*(1/3)); %% For older Octave image packages without imlincomb()
+   end
+   
+   [path,name,ext] = fileparts(in{1});
+   img_ID          = regexp(name,'_\d+','match'){1}(2:end);
+   set_path        = [path,filesep,img_ID,filesep];
+   mkdir(set_path);
 
-mkdir(set_path);
-copyfile(in_img_dir{1}, set_path); 
-copyfile(in_img_dir{2}, set_path); 
-movefile(in{1},         set_path);
-movefile(in{2},         set_path);
-movefile(in{3},         set_path);
+   if s_o
+      %% TODO: The recon image names are not consistently reflecting the correct timestep. Fix this. Allow for skipped frames
+      %%in_img_idx      = ceil(str2double(img_ID)/disp_period); 
+      %%in_img_dir      = textread(inputs_file, '%s', 2, 'headerlines', in_img_idx-2);
+      %% copyfile(in_img_dir{1}, set_path); 
+      %% copyfile(in_img_dir{2}, set_path); 
+   end
 
-imwrite(img, [set_path, filesep, 'Recon_XA_', img_ID, ext]);
+   movefile(in{1}, set_path);
+   movefile(in{2}, set_path);
+   movefile(in{3}, set_path);
 
-img=(img-double(min(img(:))))*(255/((double(max(img(:)))-double(min(img(:))))+(double((max(img(:)))-double(min(img(:))))==0)));
+   imwrite(img, [set_path, 'Recon_XA_', img_ID, ext]);
+   img=(img-double(min(img(:))))*(255/((double(max(img(:)))-double(min(img(:))))+(double((max(img(:)))-double(min(img(:))))==0)));
+   imwrite(img, [set_path, 'Recon_XN_', img_ID, ext]);
+   
+   if shuffle
+      movefile(in{4}, set_path);
+      movefile(in{5}, set_path);
+      movefile(in{6}, set_path);
+      
+      imwrite(shuf_img, [set_path, 'Shuffle_Recon_XA_', img_ID, ext]);
+      shuf_img=(shuf_img-double(min(shuf_img(:))))*(255/((double(max(shuf_img(:)))-double(min(shuf_img(:))))+(double((max(shuf_img(:)))-double(min(shuf_img(:))))==0)));
+      imwrite(shuf_img, [set_path, 'Shuffle_Recon_XN_', img_ID, ext]);
+   end
 
-imwrite(img, [set_path, filesep, 'Recon_XN_', img_ID, ext]);
 end
