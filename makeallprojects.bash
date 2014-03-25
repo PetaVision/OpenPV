dcb6ff2679add5cead984af53f786c86c9b886d9
peteschultz <peteschultz@users.sourceforge.net>
2014-03-25 17:38:10 +0000

    In PVSystemsTest directory, adds a "-DPV_DIR=" option to makeallprojects, cmakeallprojects and UnitTests/Makefile
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8297 7c0444ca-5550-0410-9008-faecdbe78eb5

--- makeallprojects.bash
+++ makeallprojects.bash
@@ -5,5 +5,16 @@ do
 done
 if test -d UnitTests
 then
-    (cd UnitTests && make clean && make)
+    PV_DIR=$PWD/../PetaVision
+    if test -n "$(echo "$1" | egrep '^-DPV_DIR=')"
+    then
+        PV_DIR="$(echo "$1" | sed -e '1,$s/^-DPV_DIR=//')"
+        if test -z "$(echo "$PV_DIR" | egrep '^/')"
+        then
+            PV_DIR="$PWD/$PV_DIR"
+        fi
+    fi
+    echo "Compiling UnitTests with PetaVision directory set to"
+    echo "$PV_DIR"
+    (cd UnitTests && make clean && make PV_DIR=$PV_DIR)
 fi
