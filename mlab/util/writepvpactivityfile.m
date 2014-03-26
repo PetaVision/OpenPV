ffd3445cb5a9820b28fef4a4478894cfcbb1c897
peteschultz <peteschultz@users.sourceforge.net>
2014-03-26 19:11:40 +0000

    Bugfixes to writepvpactivityfile.m
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@8303 7c0444ca-5550-0410-9008-faecdbe78eb5

--- mlab/util/writepvpactivityfile.m
+++ mlab/util/writepvpactivityfile.m
@@ -22,38 +22,40 @@ function writepvpactivityfile(filename, data)
        error('writepvpactivityfile:datanotcell', 'data must be a cell array, one element per frame');
    end%if
    
+   if isempty(data)
+       error('writepvpactivityfile:dataempty', 'data must have at least one frame');
+   end%if
+   
    fid = fopen(filename, 'w');
    if fid < 0
        error('writepvpactivityfile:fopenerror', 'unable to open %s', filename);
    end%if
    
    errorpresent = 0;
-   hdr = zeros(26,1);
+   hdr = zeros(20,1);
+   [nx ny nf] = size(data{1}.values);
+   hdr(1)  = 80;       % Number of bytes in header
+   hdr(2)  = 20;       % Number of 4-byte values in header
+   hdr(3)  = 4;        % File type for non-sparse activity pvp files 
+   hdr(4)  = nx;       % nx
+   hdr(5)  = ny;       % ny
+   hdr(6)  = nf;       % nf
+   hdr(7)  = 1;        % Number of records
+   hdr(8)  = nx*ny*nf; % Record size, the size of the layer in bytes 
+   hdr(9)  = 4;        % Data size: floats are four bytes
+   hdr(10) = 3;        % Types are 1=byte, 2=int, 3=float.  Type 1 is for compressed weights; type 2 isn't used for activity files
+   hdr(11) = 1;        % Number of processes in x-direction; no longer used
+   hdr(12) = 1;        % Number of processes in y-direction; no longer used
+   hdr(13) = nx;       % Presynaptic nxGlobal
+   hdr(14) = ny;       % Presynaptic nyGlobal
+   hdr(15) = 0;        % kx0, no longer used
+   hdr(16) = 0;        % ky0, no longer used
+   hdr(17) = 0;        % Presynaptic nb, not relevant for activity files
+   hdr(18) = numel(data); % number of frames 
+   hdr(19:20) = typecast(double(data{1}.time),'uint32'); % timestamp
+   fwrite(fid,hdr,'uint32');
    for frameno=1:size(data)
-       [nx ny nf] = size(data{frameno}.values);
-       amax = max(data{frameno}.values(:));
-       amin = min(data{frameno}.values(:));
-       % Each frame has its own header
-       hdr(1)  = 80;       % Number of bytes in header
-       hdr(2)  = 20;       % Number of 4-byte values in header
-       hdr(3)  = 4;        % File type for non-sparse activity pvp files 
-       hdr(4)  = nx;       % nx
-       hdr(5)  = ny;       % ny
-       hdr(6)  = nf;       % nf
-       hdr(7)  = 1;        % Number of records
-       hdr(8)  = nx*ny*nf; % Record size, the size of the layer in bytes 
-       hdr(9)  = 4;        % Data size: floats are four bytes
-       hdr(10) = 3;        % Types are 1=byte, 2=int, 3=float.  Type 1 is for compressed weights; type 2 isn't used for weight files
-       hdr(11) = 1;        % Number of processes in x-direction; no longer used
-       hdr(12) = 1;        % Number of processes in y-direction; no longer used
-       hdr(13) = nx;       % Presynaptic nxGlobal
-       hdr(14) = ny;       % Presynaptic nyGlobal
-       hdr(15) = 0;        % kx0, no longer used
-       hdr(16) = 0;        % ky0, no longer used
-       hdr(17) = 1;       % Presynaptic nb, not relevant for activity files
-       hdr(18) = length(data); % number of frames 
-       hdr(19:20) = typecast(double(data{frameno}.time),'uint32'); % timestamp
-       fwrite(fid,hdr,'uint32');
+       fwrite(fid,data{frameno}.time,'double');
        fwrite(fid,permute(data{frameno}.values(:,:,:),[3 1 2]),'single');
    end%for
    
