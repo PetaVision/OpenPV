7876872b54d778199f830a65421f8839dabddfb4
peteschultz <peteschultz@users.sourceforge.net>
2014-03-27 17:16:38 +0000

    Update to GenericSystemTest to use RequireAllZeroActivityProbe
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8316 7c0444ca-5550-0410-9008-faecdbe78eb5

--- GenericSystemTest/src/main.cpp
+++ GenericSystemTest/src/main.cpp
@@ -5,8 +5,11 @@
 
 
 #include <columns/buildandrun.hpp>
+#include <io/RequireAllZeroActivityProbe.hpp>
 
 #define MAIN_USES_CUSTOMGROUPS
+
+int customexit(HyPerCol * hc, int argc, char * argv[]);
 
 #ifdef MAIN_USES_CUSTOMGROUPS
 void * customgroup(const char * name, const char * groupname, HyPerCol * hc);
@@ -17,11 +20,36 @@ int main(int argc, char * argv[]) {
 
    int status;
 #ifdef MAIN_USES_CUSTOMGROUPS
-   status = buildandrun(argc, argv, NULL, NULL, &customgroup);
+   status = buildandrun(argc, argv, NULL, &customexit, &customgroup);
 #else
-   status = buildandrun(argc, argv);
+   status = buildandrun(argc, argv, NULL, &customexit);
 #endif // MAIN_USES_CUSTOMGROUPS
    return status==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+int customexit(HyPerCol * hc, int argc, char * argv[]) {
+   const char * targetLayerName = "comparison";
+   HyPerLayer * layer = hc->getLayerFromName(targetLayerName);
+   LayerProbe * probe = NULL;
+   int np = layer->getNumProbes();
+   for (int p=0; p<np; p++) {
+      if (!strcmp(layer->getProbe(p)->getProbeName(), "comparison_test")) {
+         probe = layer->getProbe(p);
+         break;
+      }
+   }
+   RequireAllZeroActivityProbe * allzeroProbe = dynamic_cast<RequireAllZeroActivityProbe *>(probe);
+   assert(allzeroProbe);
+   if (allzeroProbe->getNonzeroFound()) {
+      if (hc->columnId()==0) {
+         double t = allzeroProbe->getNonzeroTime();
+         fprintf(stderr, "%s \"%s\" had at least one nonzero activity value, beginning at time %f\n",
+               hc->parameters()->groupKeywordFromName(targetLayerName), targetLayerName, t);
+      }
+      MPI_Barrier(hc->icCommunicator()->communicator());
+      exit(EXIT_FAILURE);
+   }
+   return PV_SUCCESS;
 }
 
 #ifdef MAIN_USES_CUSTOMGROUPS
