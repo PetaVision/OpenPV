c7163dbbd4b4c76d9136e70e1f4e50552515e0a8
peteschultz <peteschultz@users.sourceforge.net>
2014-03-29 01:35:38 +0000

    Converting test_border_activity, test_cocirc, and test_gauss2d to systems tests in the PVSystemTest directory
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8341 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ test_border_activity/src/test_border_activity.cpp
@@ -0,0 +1,99 @@
+/**
+ * test_border_activity.cpp
+ *
+ *  Created on: Feb 11, 2010
+ *      Author: Craig Rasmussen
+ *
+ * This file tests activity near extended borders.  With mirror boundary
+ * conditions a uniform retinal input should produce uniform output across
+ * the post-synaptic layer.
+ */
+
+#include "../src/columns/HyPerCol.hpp"
+#include "../src/layers/Image.hpp"
+#include "../src/layers/Retina.hpp"
+#include "../src/connections/HyPerConn.hpp"
+#include "../src/layers/ANNLayer.hpp"
+#include "../src/io/PointProbe.hpp"
+#include "../src/weightinit/InitUniformWeights.hpp"
+
+#include <assert.h>
+
+#undef DEBUG_OUTPUT
+
+// The activity in L1 given a 7x7 weight patch,
+// with all weights initialized to 1.
+//
+#define ARGC 3
+#define UNIFORM_ACTIVITY_VALUE 49
+
+using namespace PV;
+
+int check_activity(HyPerLayer * l);
+
+int main(int argc, char * argv[])
+{
+   int status = 0;
+
+   char * cl_args[ARGC+1];
+   cl_args[0] = strdup(argv[0]);
+   cl_args[1] = strdup("-p");
+   cl_args[2] = strdup("input/test_border_activity.params");
+   cl_args[ARGC] = NULL;
+   HyPerCol * hc = new HyPerCol("column", ARGC, cl_args);
+   for( int k=0; k<ARGC; k++ )
+   {
+      free(cl_args[k]);
+   }
+
+   const char * imageLayerName = "test_border_activity image";
+   const char * retinaLayerName = "test_border_activity retina";
+   const char * l1LayerName = "test_border_activity layer";
+
+   Image * image   = new Image(imageLayerName, hc); assert(image);
+   Retina * retina = new Retina(retinaLayerName, hc);           assert(retina);
+   ANNLayer * l1     = new ANNLayer(l1LayerName, hc);           assert(l1);
+
+   HyPerConn * conn1 = new HyPerConn("test_border_activity connection 1", hc);
+   assert(conn1);
+   HyPerConn * conn2 = new HyPerConn("test_border_activity connection 2", hc);
+   assert(conn2);
+   
+#ifdef DEBUG_OUTPUT
+   PointProbe * p1 = new PointProbe( 0,  0,  0, "L1 (0,0,0):");
+   PointProbe * p2 = new PointProbe(32, 32, 32, "L1 (32,32,0):");
+   l1->insertProbe(p1);
+   l1->insertProbe(p2);
+#endif
+
+   // run the simulation
+   hc->run();
+
+   status = check_activity(l1);
+
+   delete hc;
+   return status;
+}
+
+int check_activity(HyPerLayer * l)
+{
+   int status = 0;
+
+   const int nx = l->clayer->loc.nx;
+   const int ny = l->clayer->loc.ny;
+   const int nf = l->clayer->loc.nf;
+
+   const int nk = l->clayer->numNeurons;
+   assert(nk == nx*ny*nf);
+
+   for (int k = 0; k < nk; k++) {
+      int a = (int) l->clayer->activity->data[k];
+      if (a != UNIFORM_ACTIVITY_VALUE) {
+         status = -1;
+         fprintf(stderr, "ERROR: test_border_activity: activity==%d != %d\n",
+                 a, UNIFORM_ACTIVITY_VALUE);
+         return status;
+      }
+   }
+   return status;
+}
---
+++ test_gauss2d/src/test_gauss2d.cpp
@@ -0,0 +1,143 @@
+/**
+ * This file tests weight initialization to a 2D Gaussian with sigma = 1.0 and normalized to 1.0
+ * Test compares HyPerConn to KernelConn,
+ * assumes kernelConn produces correct 2D Gaussian weights
+ *
+ */
+
+#undef DEBUG_PRINT
+
+#include "../src/layers/HyPerLayer.hpp"
+#include "../src/connections/HyPerConn.hpp"
+#include "../src/connections/KernelConn.hpp"
+#include "../src/io/io.h"
+#include <assert.h>
+
+#include "Example.hpp"
+
+using namespace PV;
+
+int check_kernel_vs_hyper(HyPerConn * cHyPer, KernelConn * cKernel, int kPre,
+		int axonID);
+
+int main(int argc, char * argv[])
+{
+   char * cl_args[4];
+   cl_args[0] = strdup(argv[0]);
+   cl_args[1] = strdup("-p");
+   cl_args[2] = strdup("input/test_gauss2d.params");
+   cl_args[3] = NULL;
+   const char * pre_layer_name = "test_gauss2d pre";
+   const char * post_layer_name = "test_gauss2d post";
+   const char * pre2_layer_name = "test_gauss2d pre 2";
+   const char * post2_layer_name = "test_gauss2d post 2";
+   PV::HyPerCol * hc = new PV::HyPerCol("test_gauss2d column", 3, cl_args);
+   PV::Example * pre = new PV::Example(pre_layer_name, hc);
+   assert(pre);
+   PV::Example * post = new PV::Example(post_layer_name, hc);
+   assert(post);
+
+   
+   PV::HyPerConn * cHyPer = new HyPerConn("test_gauss2d hyperconn", hc);
+
+   PV::KernelConn * cKernel = new KernelConn("test_gauss2d kernelconn", hc);
+
+   PV::Example * pre2 = new PV::Example(pre2_layer_name, hc);
+   assert(pre2);
+   PV::Example * post2 = new PV::Example(post2_layer_name, hc);
+   assert(post2);
+
+   PV::HyPerConn * cHyPer1to2 =
+         new HyPerConn("test_gauss2d hyperconn 1 to 2", hc);
+   assert(cHyPer1to2);
+
+   PV::KernelConn * cKernel1to2 =
+         new KernelConn("test_gauss2d kernelconn 1 to 2", hc);
+   assert(cKernel1to2);
+
+   PV::HyPerConn * cHyPer2to1 =
+         new HyPerConn("test_gauss2d hyperconn 2 to 1", hc);
+   assert(cHyPer2to1);
+
+   PV::KernelConn * cKernel2to1 =
+         new KernelConn("test_gauss2d kernelconn 2 to 1", hc);
+   assert(cKernel2to1);
+   
+   int status = 0;
+
+   for (int l=0; l<hc->numberOfLayers(); l++) {
+      status = hc->getLayer(l)->communicateInitInfo();
+      assert(status==PV_SUCCESS);
+   }
+   for (int c=0; c<hc->numberOfConnections(); c++) {
+      status = hc->getConnection(c)->communicateInitInfo();
+      assert(status==PV_SUCCESS);
+   }
+   for (int l=0; l<hc->numberOfLayers(); l++) {
+      status = hc->getLayer(l)->allocateDataStructures();
+      assert(status==PV_SUCCESS);
+   }
+   for( int c=0; c<hc->numberOfConnections(); c++ ) {
+      HyPerConn * conn = hc->getConnection(c);
+      conn->allocateDataStructures();
+      conn->writeWeights(0, true);
+   }
+
+   const int axonID = 0;
+   int num_pre_extended = pre->clayer->numExtended;
+   assert(num_pre_extended == cHyPer->getNumWeightPatches());
+
+   for (int kPre = 0; kPre < num_pre_extended; kPre++) {
+     //printf("testing testing 1 2 3...\n");
+     status = check_kernel_vs_hyper(cHyPer, cKernel, kPre, axonID);
+     assert(status==0);
+     status = check_kernel_vs_hyper(cHyPer1to2, cKernel1to2, kPre, axonID);
+     assert(status==0);
+     status = check_kernel_vs_hyper(cHyPer2to1, cKernel2to1, kPre, axonID);
+     assert(status==0);
+   }
+
+   delete hc;
+   return 0;
+}
+
+int check_kernel_vs_hyper(HyPerConn * cHyPer, KernelConn * cKernel, int kPre, int axonID)
+{
+   int status = 0;
+   PVPatch * hyperPatch = cHyPer->getWeights(kPre, axonID);
+   PVPatch * kernelPatch = cKernel->getWeights(kPre, axonID);
+   int hyPerDataIndex = cHyPer->patchIndexToDataIndex(kPre);
+   int kernelDataIndex = cKernel->patchIndexToDataIndex(kPre);
+
+   int nk = cHyPer->fPatchSize() * (int) hyperPatch->nx; // hyperPatch->nf * hyperPatch->nx;
+   assert(nk == (cKernel->fPatchSize() * (int) kernelPatch->nx));// assert(nk == (kernelPatch->nf * kernelPatch->nx));
+   int ny = hyperPatch->ny;
+   assert(ny == kernelPatch->ny);
+   int sy = cHyPer->yPatchStride(); // hyperPatch->sy;
+   assert(sy == cKernel->yPatchStride()); // assert(sy == kernelPatch->sy);
+   pvdata_t * hyperWeights = cHyPer->get_wData(axonID, hyPerDataIndex); // hyperPatch->data;
+   pvdata_t * kernelWeights = cKernel->get_wDataHead(axonID, kernelDataIndex)+hyperPatch->offset; // kernelPatch->data;
+   float test_cond = 0.0f;
+   for (int y = 0; y < ny; y++) {
+      for (int k = 0; k < nk; k++) {
+         test_cond = kernelWeights[k] - hyperWeights[k];
+         if (fabs(test_cond) > 0.001f) {
+            printf("y %d\n", y);
+            printf("k %d\n", k);
+            printf("kernelweight %f\n", kernelWeights[k]);
+            printf("hyperWeights %f\n", hyperWeights[k]);
+            const char * cHyper_filename = "gauss2d_hyper.txt";
+            cHyPer->writeTextWeights(cHyper_filename, kPre);
+            const char * cKernel_filename = "gauss2d_kernel.txt";
+            cKernel->writeTextWeights(cKernel_filename, kPre);
+            status=1;
+            //exit(EXIT_FAILURE);
+         }
+      }
+      // advance pointers in y
+      hyperWeights += sy;
+      kernelWeights += sy;
+   }
+   return status;
+}
+
