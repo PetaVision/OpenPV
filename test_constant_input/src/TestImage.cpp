593f89789e4f2424769f30775cbab13d3c81402b
peteschultz <peteschultz@users.sourceforge.net>
2014-04-03 02:22:30 +0000

    Converting unit tests in the trunk/tests directory to individual projects in the PVSystemTests directory
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8369 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ test_constant_input/src/TestImage.cpp
@@ -0,0 +1,65 @@
+/*
+ * TestImage.cpp
+ *
+ *  Created on: Mar 19, 2010
+ *      Author: Craig Rasmussen
+ */
+
+#include "TestImage.hpp"
+
+namespace PV {
+
+TestImage::TestImage() {
+   initialize_base();
+}
+
+TestImage::TestImage(const char * name, HyPerCol * hc) {
+   initialize_base();
+   initialize(name, hc);
+}
+
+TestImage::~TestImage() {
+}
+
+int TestImage::initialize_base() {
+   return PV_SUCCESS;
+}
+
+int TestImage::initialize(const char * name, HyPerCol * hc) {
+   return HyPerLayer::initialize(name, hc);
+}
+
+int TestImage::ioParamsFillGroup(enum ParamsIOFlag ioFlag) {
+   int status = HyPerLayer::ioParamsFillGroup(ioFlag);
+   ioParam_constantVal(ioFlag);
+   return status;
+}
+
+void TestImage::ioParam_InitVType(enum ParamsIOFlag ioFlag) {
+   parent->parameters()->handleUnnecessaryParameter(name, "InitVType");
+   return;
+}
+
+void TestImage::ioParam_constantVal(enum ParamsIOFlag ioFlag) {
+   parent->ioParamValue(ioFlag, name, "constantVal", &val, (pvdata_t) 1);
+}
+
+int TestImage::allocateV() {
+   assert(getV()==NULL);
+   return PV_SUCCESS;
+}
+
+int TestImage::initializeActivity() {
+   for (int k=0; k<getNumNeurons(); k++) {
+      const PVLayerLoc * loc = getLayerLoc();
+      int kExt = kIndexExtended(k,loc->nx,loc->ny,loc->nf,loc->nb);
+      getActivity()[kExt] = val;
+   }
+   return PV_SUCCESS;
+}
+
+int TestImage::updateState(double timed, double dt) {
+   return PV_SUCCESS;
+}
+
+} // namespace PV
---
+++ test_post_weights/src/Example.hpp
@@ -0,0 +1,36 @@
+/*
+ * Example.hpp
+ *
+ *  Created on: Oct 19, 2008
+ *      Author: rasmussn
+ */
+
+#ifndef EXAMPLE_HPP_
+#define EXAMPLE_HPP_
+
+#include <layers/HyPerLayer.hpp>
+
+namespace PV
+{
+
+class Example: public PV::HyPerLayer
+{
+public:
+   Example(const char* name, HyPerCol * hc);
+
+#ifdef PV_USE_OPENCL
+   virtual int initializeThreadBuffers(const char * kernelName);
+   virtual int initializeThreadKernels(const char * kernelName);
+#endif
+
+   virtual int recvSynapticInput(HyPerConn* conn, const PVLayerCube* activity, int neighbor);
+   virtual int updateState(double time, double dt);
+
+   virtual int initFinish(int colId, int colRow, int colCol);
+
+   virtual int outputState(double timef, bool last=false);
+};
+
+}
+
+#endif /* EXAMPLE_HPP_ */
