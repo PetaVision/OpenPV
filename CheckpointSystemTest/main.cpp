a22ed8d328861b990b588c0f4701b6796b656b3b
slundquist <slundquist@users.sourceforge.net>
2014-05-21 20:43:35 +0000

    Added a run all system tests with threads script, and fixed a few system tests to pass with threads.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8524 7c0444ca-5550-0410-9008-faecdbe78eb5

--- CheckpointSystemTest/main.cpp
+++ CheckpointSystemTest/main.cpp
@@ -18,13 +18,38 @@ int main(int argc, char * argv[]) {
    int rank;
    bool argerr = false;
    int reqrtn = 0;
-   if (argc > 2) argerr = 2;
-   else if (argc == 2) {
-      argerr = strcmp(argv[1], "--require-return");
-      reqrtn = 1;
+   int threading = 0;
+
+   if (argc > 1){
+      for(int i = 1; i < argc; i++){
+         std::cout << "i: " << i << "argc: " << argc << "\n";
+         //Allowed arguments are -t and --require-return
+         if(strcmp(argv[i], "-t") == 0){
+            if(i+1 >= argc || argv[i+1][0] == '-'){
+               //Do nothing
+            }
+            else{
+               int numthreads = atoi(argv[i+1]);
+               if(numthreads != 4){
+                  std::cout << "Hardcoding to 4 threads\n";
+               }
+               i++;
+            }
+            threading = 2;
+            argerr |= false;
+         }
+         else if(strcmp(argv[i], "--require-return") == 0){
+            reqrtn = 1;
+            argerr |= false;
+         }
+         else{
+            //error
+            argerr = true;
+         }
+      }
    }
    if (argerr) {
-      fprintf(stderr, "%s: run without input arguments (except for --require-return); the necessary arguments are hardcoded.\n", argv[0]);
+      fprintf(stderr, "%s: run without input arguments (except for --require-return or -t); the necessary arguments are hardcoded.\n", argv[0]);
       exit(EXIT_FAILURE);
    }
 #ifdef PV_USE_MPI
@@ -48,14 +73,29 @@ int main(int argc, char * argv[]) {
 
    int status;
    assert(reqrtn==0 || reqrtn==1);
-   int cl_argc = 3+reqrtn;
+   assert(threading == 0 || threading == 2);
+   int cl_argc = 3+reqrtn+threading;
    char * cl_args[cl_argc];
    cl_args[0] = strdup(argv[0]);
    cl_args[1] = strdup("-p");
    cl_args[2] = strdup("input/CheckpointParameters1.params");
-   if (reqrtn) {
+   if(threading && reqrtn){
+      assert(cl_argc==6);
+      cl_args[3] = strdup("-t");
+      cl_args[4] = strdup("4");
+      cl_args[5] = strdup("--require-return");
+      std::cout << "Requiring Return and Threading!\n";
+   }
+   else if (threading) {
+      assert(cl_argc==5);
+      cl_args[3] = strdup("-t");
+      cl_args[4] = strdup("4");
+      std::cout << "Threading!\n";
+   }
+   else if (reqrtn){
       assert(cl_argc==4);
       cl_args[3] = strdup("--require-return");
+      std::cout << "Requiring Return!\n";
    }
    status = buildandrun(cl_argc, cl_args, NULL, NULL, &customgroup);
    if( status != PV_SUCCESS ) {
