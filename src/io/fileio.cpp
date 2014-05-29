7cbec7043d5130c79564a5b0536ce2089a32ae4c
slundquist <slundquist@users.sourceforge.net>
2014-05-29 20:16:49 +0000

    Flushing position file for every write
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@8545 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/io/fileio.cpp
+++ src/io/fileio.cpp
@@ -1316,6 +1316,7 @@ int writeActivitySparse(PV_Stream * pvstream, PV_Stream * posstream, Communicato
       //Write to seperate file this current file position
       long filepos = pvstream->filepos;
       status = (PV_fwrite(&filepos, sizeof(long), 1, posstream) != 1);
+      fflush(posstream->fp);
       if (status != 0) {
          fprintf(stderr, "[%2d]: writeActivitySparse: failed in fwrite(&filepos), filepos==%ld\n",
                  comm->commRank(), filepos);
