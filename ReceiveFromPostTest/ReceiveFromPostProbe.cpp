d180dd58ac4012f42ef4f3e14f1ebe3146460c4a
slundquist <slundquist@users.sourceforge.net>
2014-05-30 18:34:58 +0000

    Changed the orig connection to be different from the transpose
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8546 7c0444ca-5550-0410-9008-faecdbe78eb5

--- ReceiveFromPostTest/ReceiveFromPostProbe.cpp
+++ ReceiveFromPostTest/ReceiveFromPostProbe.cpp
@@ -32,16 +32,15 @@ int ReceiveFromPostProbe::outputState(double timed){
    const PVLayerLoc * loc = getTargetLayer()->getLayerLoc();
    int numExtNeurons = getTargetLayer()->getNumExtended();
    const pvdata_t * A = getTargetLayer()->getLayerData();
-   std::cout << "nx: " << loc->nxGlobal << " ny: " << loc->nyGlobal << " nf: " << loc->nf << " nb: " << loc->nb << "\n";
    for (int i = 0; i < numExtNeurons; i++){
-      //if(A[i] >= 1e-5){
+      //if(fabs(A[i]) >= 1e-6){
       //   int xpos = kxPos(i, loc->nx+2*loc->nb, loc->ny+2*loc->nb, loc->nf);
       //   int ypos = kyPos(i, loc->nx+2*loc->nb, loc->ny+2*loc->nb, loc->nf);
       //   int fpos = featureIndex(i, loc->nx+2*loc->nb, loc->ny+2*loc->nb, loc->nf);
       //   std::cout << "[" << xpos << "," << ypos << "," << fpos << "] = " << A[i] << "\n";
       //}
       //For roundoff errors
-      assert(abs(A[i]) < 1e-5);
+      assert(fabs(A[i]) < 1e-6);
    }
    return status;
 }
