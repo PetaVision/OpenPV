938b8f9e484dfe215136088b26670b7c772ba585
peteschultz <peteschultz@users.sourceforge.net>
2014-08-01 20:06:49 +0000

    New system test, ConnectionRestartTest, checks the initializeFromCheckpointFlag param for connections.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8744 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ ConnectionRestartTest/src/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * pv.cpp
+ *
+ */
+
+
+#include <columns/buildandrun.hpp>
+
+int customexit(HyPerCol * hc, int argc, char ** argv);
+
+int main(int argc, char * argv[]) {
+
+   int status;
+   status = buildandrun(argc, argv, NULL, &customexit, NULL);
+   return status==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+int customexit(HyPerCol * hc, int argc, char ** argv) {
+   HyPerConn * initializeFromInitWeightsConn = hc->getConnFromName("initializeFromInitWeights");
+   // There must be a connection named initializeFromInitWeights. It should have a single weight with value 1
+   assert(initializeFromInitWeightsConn);
+   assert(initializeFromInitWeightsConn->xPatchSize()==1);
+   assert(initializeFromInitWeightsConn->yPatchSize()==1);
+   assert(initializeFromInitWeightsConn->fPatchSize()==1);
+   assert(initializeFromInitWeightsConn->numberOfAxonalArborLists()==1);
+   assert(initializeFromInitWeightsConn->get_wData(0,0)[0] == 1.0f);
+
+   // There must be a connection named initializeFromCheckpoint.  It should have a single weight with value 2
+   HyPerConn * initializeFromCheckpointConn = hc->getConnFromName("initializeFromCheckpoint");
+   assert(initializeFromCheckpointConn);
+   assert(initializeFromCheckpointConn->xPatchSize()==1);
+   assert(initializeFromCheckpointConn->yPatchSize()==1);
+   assert(initializeFromCheckpointConn->fPatchSize()==1);
+   assert(initializeFromCheckpointConn->numberOfAxonalArborLists()==1);
+   assert(initializeFromCheckpointConn->get_wData(0,0)[0] == 2.0f);
+   return PV_SUCCESS;
+}
