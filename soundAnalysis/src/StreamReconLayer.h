5968b453b84de0ba245f7c940a799f84ba579e8b
mohitdubey <mohitdubey@users.sourceforge.net>
2014-08-15 13:28:34 +0000

    stream recon
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@8774 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 84e0e218078f4e635d98335624d63dde4ceffb5c

---
+++ soundAnalysis/src/StreamReconLayer.h
@@ -0,0 +1,36 @@
+//
+//  StreamReconLayer.h
+//  
+//
+//  Created by Brohit's Mac 5054128794 on 7/29/14.
+//
+//
+
+#ifndef STREAMRECONLAYER_H_
+#define STREAMRECONLAYER_H_
+
+#include <iostream>
+#include <layers/HyPerLayer.hpp>
+
+
+namespace PV {
+    
+    class StreamReconLayer : public HyPerLayer{
+    public:
+        StreamReconLayer(const char * name, HyPerCol * hc);
+        virtual ~StreamReconLayer();
+        
+    protected:
+        StreamReconLayer();
+        virtual int updateState (double time, double dt);
+        
+    private:
+        int initialize_base();
+        int bufferLevel;
+        
+    }; // end of class streamreconlayer
+    
+}  // end namespace PV
+
+
+#endif /* defined(STREAMRECONLAYER_H_) */
