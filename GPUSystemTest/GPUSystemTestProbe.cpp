493c4203967cb709a60aed1ecc1228061c58ed45
slundquist <slundquist@users.sourceforge.net>
2014-08-28 18:42:35 +0000

    Added a new gpu system test for gpu stuff
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@8827 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ GPUSystemTest/GPUSystemTestProbe.cpp
@@ -0,0 +1,49 @@
+/*
+ * receiveFromPostProbe.cpp
+ * Author: slundquist
+ */
+
+#include "GPUSystemTestProbe.hpp"
+#include <include/pv_arch.h>
+#include <layers/HyPerLayer.hpp>
+#include <assert.h>
+#include <string.h>
+
+namespace PV {
+GPUSystemTestProbe::GPUSystemTestProbe(const char * probeName, HyPerCol * hc)
+   : StatsProbe()
+{
+   initGPUSystemTestProbe_base();
+   initGPUSystemTestProbe(probeName, hc);
+}
+
+int GPUSystemTestProbe::initGPUSystemTestProbe_base() { return PV_SUCCESS; }
+
+int GPUSystemTestProbe::initGPUSystemTestProbe(const char * probeName, HyPerCol * hc) {
+   return initStatsProbe(probeName, hc);
+}
+
+void GPUSystemTestProbe::ioParam_buffer(enum ParamsIOFlag ioFlag) {
+   requireType(BufActivity);
+}
+
+int GPUSystemTestProbe::outputState(double timed){
+   int status = StatsProbe::outputState(timed);
+   const PVLayerLoc * loc = getTargetLayer()->getLayerLoc();
+   int numExtNeurons = getTargetLayer()->getNumExtended();
+   const pvdata_t * A = getTargetLayer()->getLayerData();
+   std::cout.precision(15);
+   for (int i = 0; i < numExtNeurons; i++){
+      if(fabs(A[i]) != 0){
+         int xpos = kxPos(i, loc->nx+2*loc->nb, loc->ny+2*loc->nb, loc->nf);
+         int ypos = kyPos(i, loc->nx+2*loc->nb, loc->ny+2*loc->nb, loc->nf);
+         int fpos = featureIndex(i, loc->nx+2*loc->nb, loc->ny+2*loc->nb, loc->nf);
+         //std::cout << "[" << xpos << "," << ypos << "," << fpos << "] = " << std::fixed << A[i] << "\n";
+      }
+      //For roundoff errors
+      //assert(fabs(A[i]) < 1e-6);
+   }
+   return status;
+}
+
+}
---
+++ GPUSystemTest/GPUSystemTestProbe.hpp
@@ -0,0 +1,28 @@
+/*
+ * GPUSystemTestProbe.hpp
+ * Author: slundquist
+ */
+
+#ifndef GPUSYSTEMTESTPROBE_HPP_ 
+#define GPUSYSTEMTESTPROBE_HPP_
+#include <io/StatsProbe.hpp>
+
+namespace PV{
+
+class GPUSystemTestProbe : public PV::StatsProbe{
+public:
+   GPUSystemTestProbe(const char * probeName, HyPerCol * hc);
+
+   virtual int outputState(double timed);
+
+protected:
+   int initGPUSystemTestProbe(const char * probeName, HyPerCol * hc);
+   void ioParam_buffer(enum ParamsIOFlag ioFlag);
+
+private:
+   int initGPUSystemTestProbe_base();
+
+};
+
+}
+#endif 
