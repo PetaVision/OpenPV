6b605bb5da92e33ebcc73d60080599eb303818fb
slundquist <slundquist@users.sourceforge.net>
2014-09-10 19:34:55 +0000

    Testing coalessed global memory access to local memory of pre activity
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@8889 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/cudakernels/CudaRecvPost.cu
+++ src/cudakernels/CudaRecvPost.cu
@@ -6,127 +6,113 @@ namespace PVCuda{
 
 //Kernel code
 __global__
-void HyPerLayer_recv_post(
-   recv_post_params params
-){
+void HyPerLayer_recv_post(recv_post_params params){
+   ////Shared memory buffers are declared
+   extern __shared__ char sharedMem[];
+   __shared__ float* preBuffer;
+   __shared__ float* postBuffer;
+   //__shared__ long* localStartSourceExt;
+   preBuffer = (float*)sharedMem;
+   postBuffer = (float*)(&(preBuffer[params.preBufNum]));
 
-      ////Shared memory buffers are declared
-      extern __shared__ char sharedMem[];
-      __shared__ float* postBuffer;
-      //__shared__ float* preBuffer;
-      //__shared__ long* localStartSourceExt;
-      postBuffer = (float*)sharedMem;
-      //preBuffer = (float*)(&postBuffer[params.postBufNum]);
-      //__shared__ long localStartSourceExt;
-      //localStartSourceExt = (long*)(&preBuffer[params.preBufNum]);
+   //postBuffer = (float*)sharedMem;
+   //preBuffer = (float*)(&postBuffer[params.postBufNum]);
+   //__shared__ long localStartSourceExt;
+   //localStartSourceExt = (long*)(&preBuffer[params.preBufNum]);
 
-      //Ordered this way because threads vary fastest in x, then y, then z
-      //Mapped to petavision order of f, x, and y
-      int localF = blockDim.x;
-      int localX = blockDim.y;
-      int localY = blockDim.z;
+   //Ordered this way because threads vary fastest in x, then y, then z
+   //Mapped to petavision order of f, x, and y
+   int localF = blockDim.x;
+   int localX = blockDim.y;
+   int localY = blockDim.z;
+   
+   int localFIndex = threadIdx.x;
+   int localXIndex = threadIdx.y;
+   int localYIndex = threadIdx.z;
+
+   int fTargetRes = (blockIdx.x * blockDim.x) + threadIdx.x;
+   int xTargetRes = (blockIdx.y * blockDim.y) + threadIdx.y;
+   int yTargetRes = (blockIdx.z * blockDim.z) + threadIdx.z;
+
+   ////Calculate kTargetRes based on x, y, and f
+   int kTargetRes = kIndex(xTargetRes, yTargetRes, fTargetRes, params.nxRes, params.nyRes, params.nf);
+
+   //Change restricted to extended post neuron
+   int kTargetExt = kIndexExtended(kTargetRes, params.nxRes, params.nyRes, params.nf, params.nblt, params.nbrt, params.nbdn, params.nbup);
+
+   int kernelIndex;
+   if(params.sharedWeights == 1){
+      kernelIndex = params.patch2datalookuptable[kTargetExt];
+   }
+   else{
+      kernelIndex = kTargetExt;
+   }
+   int wIdx = kernelIndex * params.nxp * params.nyp * params.nfp;
+
+   //Get top left most neuron in the group
+   __shared__ long localStartSourceExt;
+   if(localXIndex == 0 && localYIndex == 0 && localFIndex == 0){
+      localStartSourceExt = params.startSourceExtBuf[kTargetRes];
+   }
+
+   long startSourceExt = params.startSourceExtBuf[kTargetRes];
+
+   int localIndex = kIndex(localXIndex, localYIndex, localFIndex, localX, localY, localF);
+
+   postBuffer[localIndex] = 0;
       
-      int localFIndex = threadIdx.x;
-      int localXIndex = threadIdx.y;
-      int localYIndex = threadIdx.z;
+   int numXfBuffer = params.localBufSizeX * params.nfp;
 
-      int fTargetRes = (blockIdx.x * blockDim.x) + threadIdx.x;
-      int xTargetRes = (blockIdx.y * blockDim.y) + threadIdx.y;
-      int yTargetRes = (blockIdx.z * blockDim.z) + threadIdx.z;
+   int xOffset = localXIndex * params.preToPostScaleX;
+   //int yOffset = localYIndex * params.preToPostScaleY;
 
-      ////Calculate kTargetRes based on x, y, and f
-      int kTargetRes = kIndex(xTargetRes, yTargetRes, fTargetRes, params.nxRes, params.nyRes, params.nf);
-
-      //int kTargetRes = blockIdx.x;
-
-      //Change restricted to extended post neuron
-      int kTargetExt = kIndexExtended(kTargetRes, params.nxRes, params.nyRes, params.nf, params.nblt, params.nbrt, params.nbdn, params.nbup);
-
-      int kernelIndex;
-      if(params.sharedWeights == 1){
-         kernelIndex = params.patch2datalookuptable[kTargetExt];
-      }
-      else{
-         kernelIndex = kTargetExt;
-      }
-      int wIdx = kernelIndex * params.nxp * params.nyp * params.nfp;
-
-      //Get top left most neuron in the group
-      //if(localXIndex == 0 && localYIndex == 0 && localFIndex == 0){
-      //   localStartSourceExt = params.startSourceExtBuf[kTargetRes];
-      //}
-
-      long startSourceExt = params.startSourceExtBuf[kTargetRes];
-
-      int localIndex = kIndex(localXIndex, localYIndex, localFIndex, localX, localY, localF);
-
-      postBuffer[localIndex] = 0;
-         
-      //int numXfBuffer = params.localBufSizeX * params.nfp;
-
-      //////Match preBuffer to indPreData, need to find x and y offsets
-      //
-      ////TODO find a better size of pre buffer to see if you can do multiple lines at a time
-      //for(int ky = 0; ky < params.localBufSizeY ; ky++){
-      //   float* preDataY = &(params.preData[*localStartSourceExt + ky * params.sy]);
-      //   float* preBufferY = &(preBuffer[ky * params.localBufSizeX * params.nfp]);
-      //   work_group_copy(preBufferY, preDataY, numXfBuffer, warpSize, localIndex); 
-      //}
-
-      //Wait for shared memory loads
-      __syncthreads();
-
-      //int xOffset = localXIndex * params.preToPostScaleX;
-      //int yOffset = localYIndex * params.preToPostScaleY;
+   //Wait for shared memory loads
+   __syncthreads();
 
 
-      for(int ky = 0; ky < params.nyp; ky++){
+   for(int ky = 0; ky < params.nyp; ky++){
 
-
-         //Copy global to local, do this with all threads
-         //This function has thread sync in it
-         //Find total number of threads working on this copy
-         
-         //int numThreads = localX * localY * localF;
-         //int numThreadsPart = numThreads < warpSize ? numThreads : warpSize;
-         //float* preDataY = &(params.preData[*localStartSourceExt + ky * params.sy]);
-         //if(localIndex < numThreadsPart){
-         //   for(int i = localIndex; i < numXfBuffer; i+= numThreadsPart){
-         //      preBuffer[i] = preDataY[i];
-         //   }
-         //}
-         //__syncthreads();
-
-         //work_group_copy(preBuffer, &(params.preData[localStartSourceExt + ky * params.sy]), numXfBuffer, warpSize, localIndex);
-         //__syncthreads();
-
-         float* activityY = &(params.preData[startSourceExt + ky * params.sy]);
-         //float* activityY = &(preBuffer[xOffset * params.nfp]);
-         //float* activityY = &(preBuffer[(ky+yOffset) * params.localBufSizeX * params.nfp + xOffset*params.nfp]);
-
-         float* weightY = &(params.weights[wIdx + ky * params.syp]);
-         //pvpatch_accumulate_from_post(numPerStride, postAddr, activityY, weightY, dt_factor, (void*)0);
-
-         //Summing into post buffer indexed by localIndex
-         int k;
-         for (k = 0; k < params.numPerStride; k++) {
-            //if(dataActivityY[k] != activityY[k]){
+      //Copy global to local, do this with all threads
+      //This function has thread sync in it
+      //Find total number of threads working on this copy
+      if(localIndex < warpSize){
+         for(int i = localIndex; i < numXfBuffer; i+= warpSize){
+            //if(ky == 5 && i == 47){
             //   printf("Break here\n");
             //}
-            //float weightVal = tex1Dfetch(texReference, (wIdx + ky*syp) + k);
-            //float weightVal = weights[wIdx+ky*syp+k];
-            //*gSynPatchPos += activityY[k]*weightY[k]*params.dt_factor;
-            postBuffer[localIndex] += activityY[k]*weightY[k]*params.dt_factor;
-         //   params.postGsyn[kTargetRes] += activityY[k]*weightY[k]*params.dt_factor;
-            //*postAddr += activityY[k]*weightVal*dt_factor;
+            preBuffer[i] = params.preData[localStartSourceExt + ky * params.sy + i];
          }
       }
-
-      ////Barrier to make sure the work group's postbuffer is set
       __syncthreads();
 
-      ////Sum into global memory
-      params.postGsyn[kTargetRes] += postBuffer[localIndex];
+      //float* activityY = &(params.preData[startSourceExt + ky * params.sy]);
+      float* activityY = &(preBuffer[xOffset * params.nfp]);
+      //float* activityY = &(preBuffer[(ky+yOffset) * params.localBufSizeX * params.nfp + xOffset*params.nfp]);
+
+      float* weightY = &(params.weights[wIdx + ky * params.syp]);
+      //pvpatch_accumulate_from_post(numPerStride, postAddr, activityY, weightY, dt_factor, (void*)0);
+
+      //Summing into post buffer indexed by localIndex
+      int k;
+      for (k = 0; k < params.numPerStride; k++) {
+         //if(activityY[k] != preBuffer[xOffset * params.nfp + k]){
+         //   printf("Break here\n");
+         //}
+         //float weightVal = tex1Dfetch(texReference, (wIdx + ky*syp) + k);
+         //float weightVal = weights[wIdx+ky*syp+k];
+         //*gSynPatchPos += activityY[k]*weightY[k]*params.dt_factor;
+         postBuffer[localIndex] += activityY[k]*weightY[k]*params.dt_factor;
+      //   params.postGsyn[kTargetRes] += activityY[k]*weightY[k]*params.dt_factor;
+         //*postAddr += activityY[k]*weightVal*dt_factor;
+      }
+      __syncthreads();
+   }
+
+   ////Barrier to make sure the work group's postbuffer is set
+   __syncthreads();
+
+   ////Sum into global memory
+   params.postGsyn[kTargetRes] += postBuffer[localIndex];
 }
 
 
@@ -204,10 +190,10 @@ void CudaRecvPost::setArgs(
 
 int CudaRecvPost::run(){
 
-   params.preBufNum = params.localBufSizeY * params.localBufSizeX * params.nfp;
+   params.preBufNum = params.localBufSizeX * params.nfp;
    params.postBufNum = block_size.x * block_size.y * block_size.z;
-   //size_t sharedSize = sizeof(float) * (params.preBufNum + params.postBufNum);
-   size_t sharedSize = sizeof(float) * params.postBufNum;
+   size_t sharedSize = sizeof(float) * (params.preBufNum + params.postBufNum);
+   //size_t sharedSize = sizeof(float) * params.postBufNum;
    //printf("preBufNum: %d  postBufNum: %d\n", params.preBufNum, params.postBufNum);
 
    if(sharedSize > device->get_local_mem()){
