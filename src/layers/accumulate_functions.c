2afd6c07f970c237a6b82719765323dba1212eef
peteschultz <peteschultz@users.sourceforge.net>
2014-09-19 20:45:27 +0000

    Removing long-obsolete code from accumulate_functions.c
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@8930 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/accumulate_functions.c
+++ src/layers/accumulate_functions.c
@@ -63,35 +63,6 @@ int pvpatch_accumulate2(int nk, float* RESTRICT v, float a, pvwdata_t* RESTRICT 
    return err;
 }
 
-#ifdef OBSOLETE // Marked obsolete Aug 21, 2013.  Use cl_random instead of pv_random
-int pvpatch_accumulate_stochastic(int nk, float* RESTRICT v, float a, pvwdata_t* RESTRICT w, void * auxPtr)
-{
-   int k;
-   long along = (long) (a*pv_random_max());
-   int err = 0;
-   float accumval = 0;
-   for (k = 0; k < nk; k++) {
-      accumval = (pv_random()<along)*w[k];
-//#ifdef PV_USE_OPENMP_THREADS
-//#pragma omp atomic
-//#endif
-      v[k] += accumval;
-   }
-   return err;
-}
-
-int pvpatch_accumulate_stochastic_from_post(int nk, float * RESTRICT v, float * RESTRICT a, pvwdata_t * RESTRICT w, float dt_factor, void * auxPtr) {
-   int status = 0;
-   int k;
-   float dv = 0.0f;
-   for (k = 0; k < nk; k++) {
-      long along = (long) (a[k]*pv_random_max());
-      dv = pv_random()<along ? dv + a[k]*w[k] : 0.0f;
-   }
-   *v = *v + dt_factor*dv;
-   return status;
-}
-#else
 int pvpatch_accumulate_stochastic(int nk, float* RESTRICT v, float a, pvwdata_t* RESTRICT w, void * auxPtr)
 {
    uint4 * rng = (uint4 *) auxPtr;
@@ -123,36 +94,6 @@ int pvpatch_accumulate_stochastic_from_post(int nk, float * RESTRICT v, float * 
    *v = *v + dt_factor*dv;
    return status;
 }
-#endif // OBSOLETE
-
-#ifdef OBSOLETE // Marked obsolete Aug 19, 2013.  Nobody calls pvpatch_max and whatever WTACompressedLayer was, it's not in the code now.
-// Used by WTACompressedLayer
-int pvpatch_max(int nk, float * RESTRICT v, float a, pvwdata_t * RESTRICT w, int feature, int * RESTRICT maxloc) {
-   int k;
-   int err = 0;
-   for (k = 0; k < nk; k++) {
-//#ifdef PV_USE_OPENMP_THREADS
-//#pragma omp critical
-//      {
-//#endif
-
-      float prod = a*w[k];
-      if (prod!=0 && v[k] == prod) {
-         err = 1;
-      }
-      if (v[k] < prod) {
-         v[k] = prod;
-         maxloc[k] = feature;
-      }
-//#ifdef PV_USE_OPENMP_THREADS
-//#pragma omp critical
-//      }
-//#endif
-
-   }
-   return err;
-}
-#endif
 
 int pvpatch_max_pooling(int nk, float* RESTRICT v, float a, pvwdata_t* RESTRICT w, void * auxPtr)
 {
--- src/layers/accumulate_functions.h
+++ src/layers/accumulate_functions.h
@@ -4,7 +4,6 @@
 #ifndef CL_KERNEL_INCLUDE
 #include "../include/pv_types.h"
 #include "../include/pv_common.h"
-//#include "../arch/opencl/pv_uint4.h"
 #include "../utils/cl_random.h"
 #endif
 
@@ -14,9 +13,6 @@ extern "C" {
 int pvpatch_accumulate(int nk, float * v, float a, pvwdata_t * w, void * auxPtr);
 int pvpatch_accumulate2(int nk, float* RESTRICT v, float a, pvwdata_t* RESTRICT w, float* RESTRICT m);
 int pvpatch_accumulate_stochastic(int nk, float* RESTRICT v, float a, pvwdata_t* RESTRICT w, void * auxPtr);
-#ifdef OBSOLETE // Marked obsolete Aug 19, 2013.  Nobody calls pvpatch_max and whatever WTACompressedLayer was, it's not in the code now.
-int pvpatch_max(int nk, float * RESTRICT v, float a, float * RESTRICT w, int feature, int * RESTRICT maxloc);
-#endif // OBSOLETE
 int pvpatch_max_pooling(int nk, float* RESTRICT v, float a, pvwdata_t* RESTRICT w, void * auxPtr);
 
 int pvpatch_accumulate_from_post(int nk, float *v, float *a, pvwdata_t *w, float dt_factor, void * auxPtr);
