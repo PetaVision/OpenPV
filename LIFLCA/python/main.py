f4c6a79847ec17f83476e7a7c13c802e6bb564ae
dpaiton <dpaiton@users.sourceforge.net>
2014-12-02 22:18:38 +0000

    set up to test on weight file
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@9171 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 8ecf814f0dfc5bf4591509ee51fcfaa317ce28a7

--- LIFLCA/python/main.py
+++ LIFLCA/python/main.py
@@ -9,6 +9,7 @@ lib_path = os.path.abspath('/Users/dpaiton/Documents/workspace/PetaVision/plab')
 sys.path.append(lib_path)
 import pvAnalysis as pv
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Problem 1 setup:
@@ -27,16 +28,41 @@ import numpy as np
 
 # File Locations
 output_dir = '/Users/dpaiton/Documents/workspace/LIFLCA/output/LCA/'
-layer   = 'a2_L1.pvp'
-weights = 'checkpoints/Checkpoint23400/L1_to_Residual_W.pvp'
+l1_layer   = 'a2_L1.pvp'
+err_layer  = 'a1_Residual.pvp'
+weights    = 'w1_L1_to_Residual.pvp'
+#weights    = 'checkpoints/Checkpoint23400/L1_to_Residual_W.pvp'
 
-# Open file
-activityFile = open(output_dir + layer,'rb')
+# Open files
+l1_activityFile  = open(output_dir + l1_layer,'rb')
+err_activityFile = open(output_dir + err_layer,'rb')
+weightsFile      = open(output_dir + weights,'rb')
 
 progressPeriod  = 1
 startFrame      = 0
-lastFrame       = 3
+lastFrame       = -1  # -1 for all
 skipFrames      = 1
 
-(outStruct,hdr) = pv.get_pvp_data(activityFile,progressPeriod,lastFrame,startFrame,skipFrames)
+# outStruct has fields "time" and "values"
+
+#print 'L1:'
+#(L1_outStruct,L1_hdr)   = pv.get_pvp_data(l1_activityFile,progressPeriod,lastFrame,startFrame,skipFrames)
+
+# divide the L2 norm of the residual by the L2 of the input to the
+# residiual (i.e the image) to get % error
+#print 'Err:'
+#(err_outStruct,err_hdr) = pv.get_pvp_data(err_activityFile,progressPeriod,lastFrame,startFrame,skipFrames)
+
+#Recon error?
+# ABS gives distance from 0. 
+# Averaging over the 512x512 array gives err per frame
+#plt.plot(np.average(np.average(np.abs(err_outStruct["values"]),2),2))
+#plt.show()
+
+print 'Weights:'
+(weights_outStruct,weights_hdr) = pv.get_pvp_data(weightsFile,progressPeriod,lastFrame,startFrame,skipFrames)
+
+#l1_activityFile.close()
+#err_activityFile.close()
+weightsFile.close
 
