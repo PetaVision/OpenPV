68c81ff476290797a8ec5107cb81966ab4993003
dpaiton <dpaiton@users.sourceforge.net>
2014-12-05 22:16:20 +0000

    Initial commit
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@9198 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ plab/plotWeights.py
@@ -0,0 +1,9 @@
+#########################################
+##  plotWeights.py
+##  Written by Dylan Paiton, William Shainin
+##  Dec 5, 2014
+##  
+##  Input is PetaVision weight PVP file
+##  Produces weight plot and makes file if requested
+##
+#########################################
