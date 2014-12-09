2d8536a0e96b02aa7f186c5c8949535805dac51c
slundquist <slundquist@users.sourceforge.net>
2014-12-09 19:42:16 +0000

    Fixed an issue with returning 64 bit address from c++. Added conditionals for operating system
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@9220 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/bindings/pyPV.py
+++ src/bindings/pyPV.py
@@ -1,19 +1,32 @@
 from ctypes import *
 import sys
-lib = cdll.LoadLibrary('../../lib/libpv.so')
-mpi =  CDLL('libmpi.so', RTLD_GLOBAL)
 
 class pyHyPerCol(object):
    def __init__(self, argc, argv):
-      self.obj = lib.pvBuild(argc, argv)
+      #If use linux
+      if sys.platform == "linux" or sys.platform == "linux2":
+         self.lib = cdll.LoadLibrary('../../lib/libpv.so')
+         mpi =  CDLL('libmpi.so', RTLD_GLOBAL)
+      elif sys.platform == "darwin":
+         #If use mac
+         self.lib = cdll.LoadLibrary('../../lib/libpv.dylib')
+         self.lib.pvBuild.restype = POINTER(c_char_p)
+         mpi =  CDLL('libmpi.dylib', RTLD_GLOBAL)
+      elif sys.platform == "win32":
+         print "Windows not supported"
+         sys.exit()
+      else:
+         print "Operating system", sys.platform, "not known"
+         sys.exit()
+      self.hc = self.lib.pvBuild(argc, argv)
 
    def run(self):
-      return lib.pvRun(self.obj)
+      return self.lib.pvRun(self.hc)
 
 
-#Test script
+#Test scripti
 if __name__ == "__main__":
    c_argc = 4
-   c_argv = (c_char_p * c_argc)("pv", "-p", "/home/slundquist/workspace/PVSystemTests/BasicSystemTest/input/BasicSystemTestAbsolute.params", "-t")
+   c_argv = (c_char_p * (c_argc+1))("pv", "-p", "input/BasicSystemTest.params", "-t", None)
    pvObj = pyHyPerCol(c_argc, c_argv)
    pvObj.run()
