439a5c9edd687e8630ea74bbde4b730c0e536973
dpaiton <dpaiton@users.sourceforge.net>
2014-12-10 18:40:29 +0000

    Weights are restructured & plot, but they seem to be ordered incorrectly.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@9228 7c0444ca-5550-0410-9008-faecdbe78eb5

--- plab/plotWeights.py
+++ plab/plotWeights.py
@@ -14,15 +14,46 @@
 ##
 #########################################
 
+#TODO: Can we plan out the imports better so they are only imported once & when needed?
+#      What is proper importing protocol?
+import numpy as np
+import matplotlib.pyplot as plt
+
 def plotWeights(weightStruct,showPlot=True,savePlot=False,savePath=''):
+
+    margin = 2 #pixels
 
     # weightStruct should be dims [time, numArbors, numPatches, nyp, nxp, nfp]
     weight_vals = np.array(weightStruct["values"])
     weight_time = weightStruct["time"]
 
+    (numFrames,numArbors,numPatches,nyp,nxp,nfp) = weight_vals.shape
     i_frame = 400
     i_arbor = 0
-    i_patch = 0
 
-    patch_tmp = weight_vals[i_frame,i_arbor,i_patch,:,:,:]
-    #min_patch = min(patch_tmp)
+    if np.sqrt(numPatches)%1 != 0: #If numPaches has a square root
+        numPatchesX = np.sqrt(numPatches)
+        numPatchesY = numPatchesX
+    else:
+        numPatchesX = np.floor(np.sqrt(numPatches))
+        numPatchesY = numPatchesX+1
+
+    patch_set = np.zeros((numPatchesX*numPatchesY,nyp+margin,nxp+margin))
+
+    for i_patch in range(numPatches): 
+        patch_tmp = weight_vals[i_frame,i_arbor,i_patch,:,:,:]
+        min_patch = np.amin(patch_tmp)
+        max_patch = np.amax(patch_tmp)
+        # Normalize patch
+        patch_tmp = (patch_tmp - min_patch) * 255 / (max_patch - min_patch + np.finfo(float).eps) # re-scaling & normalizing TODO: why?
+        # Patches are padded with zeros - just fill in center
+        patch_set[i_patch,np.floor(margin/2):np.floor(margin/2)+nyp,np.floor(margin/2):np.floor(margin/2)+nxp] = np.uint8(np.squeeze(np.transpose(patch_tmp,(1,0,2)))) # re-ordering to [x,y,f] TODO: why?
+
+    #TODO: Does this actually reshape it how I want it to?
+    out_mat = np.reshape(patch_set,(numPatchesX*(nxp+margin),numPatchesY*(nyp+margin)))
+
+    plt.imshow(out_mat,cmap="Greys")
+    plt.show()
+
+#TODO:
+#def plotWeightHistograms(...):
