c09ec58d57cb59b401273ceb19a475add8dfda48
dpaiton <dpaiton@users.sourceforge.net>
2014-12-11 19:18:50 +0000

    moved plotWeights code into here for testing
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@9237 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 6a744138599c4646b66d528541730cc4053d9cbf

--- LIFLCA/python/main.py
+++ LIFLCA/python/main.py
@@ -60,9 +60,47 @@ skipFrames      = 1
 #plt.show()
 
 print 'Weights:'
-(weights_outStruct,weights_hdr) = pv.get_pvp_data(weightsFile,progressPeriod,lastFrame,startFrame,skipFrames)
+(weightStruct,weightsHdr) = pv.get_pvp_data(weightsFile,progressPeriod,lastFrame,startFrame,skipFrames)
 
 #l1_activityFile.close()
 #err_activityFile.close()
 weightsFile.close()
 
+
+###########################################
+###########################################
+###########################################
+
+margin = 6 #pixels
+
+# weightStruct should be dims [time, numArbors, numPatches, nyp, nxp, nfp]
+weight_vals = np.array(weightStruct["values"])
+weight_time = weightStruct["time"]
+
+(numFrames,numArbors,numPatches,nyp,nxp,nfp) = weight_vals.shape
+i_frame = 400
+i_arbor = 0
+
+if np.sqrt(numPatches)%1 == 0: #If numPaches has a square root
+    numPatchesX = np.sqrt(numPatches)
+    numPatchesY = numPatchesX
+else:
+    numPatchesX = np.floor(np.sqrt(numPatches))
+    numPatchesY = numPatchesX+1
+
+patch_set = np.zeros((numPatchesX*numPatchesY,nyp+margin,nxp+margin))
+
+for i_patch in range(numPatches): 
+    patch_tmp = weight_vals[i_frame,i_arbor,i_patch,:,:,:]
+    min_patch = np.amin(patch_tmp)
+    max_patch = np.amax(patch_tmp)
+    # Normalize patch
+    patch_tmp = (patch_tmp - min_patch) * 255 / (max_patch - min_patch + np.finfo(float).eps) # re-scaling & normalizing TODO: why? and what exactly is it doing?
+    # Patches are padded with zeros - just fill in center
+    patch_set[i_patch,np.floor(margin/2):np.floor(margin/2)+nyp,np.floor(margin/2):np.floor(margin/2)+nxp] = np.uint8(np.squeeze(np.transpose(patch_tmp,(1,0,2)))) # re-ordering to [x,y,f] TODO: why?
+
+#TODO: Does this actually reshape it how I want it to? (hint: no...)
+out_mat = np.reshape(patch_set,(numPatchesY*(nyp+margin),numPatchesX*(nxp+margin)),'C')
+
+plt.imshow(out_mat,cmap='Greys',interpolation='nearest')
+plt.show()
