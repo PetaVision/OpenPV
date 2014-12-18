be39abe2eae8696c6765398e016cb41307009667
slundquist <slundquist@users.sourceforge.net>
2014-12-18 23:17:15 +0000

    GPU System test now checks both max difference and std of difference for roundoff errors. Added and modified new param files for more testing
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@9294 7c0444ca-5550-0410-9008-faecdbe78eb5

--- GPUSystemTest/src/GPUSystemTestProbe.cpp
+++ GPUSystemTest/src/GPUSystemTestProbe.cpp
@@ -27,22 +27,26 @@ void GPUSystemTestProbe::ioParam_buffer(enum ParamsIOFlag ioFlag) {
    requireType(BufActivity);
 }
 
+//2 tests: max difference can be 5e-4, max std is 5e-5
 int GPUSystemTestProbe::outputState(double timed){
    int status = StatsProbe::outputState(timed);
    const PVLayerLoc * loc = getTargetLayer()->getLayerLoc();
    int numExtNeurons = getTargetLayer()->getNumExtended();
    const pvdata_t * A = getTargetLayer()->getLayerData();
    std::cout.precision(15);
+   float sumsq = 0;
    for (int i = 0; i < numExtNeurons; i++){
-      if(fabs(A[i]) != 0){
-         int xpos = kxPos(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
-         int ypos = kyPos(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
-         int fpos = featureIndex(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
-         //std::cout << "[" << xpos << "," << ypos << "," << fpos << "] = " << std::fixed << A[i] << "\n";
-      }
-      //For roundoff errors
-      assert(fabs(A[i]) < 1e-4);
+      //if(fabs(A[i]) != 0){
+      //   int xpos = kxPos(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
+      //   int ypos = kyPos(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
+      //   int fpos = featureIndex(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
+      //   //std::cout << "[" << xpos << "," << ypos << "," << fpos << "] = " << std::fixed << A[i] << "\n";
+      //}
+      //For max difference roundoff errors
+      assert(fabs(A[i]) < 5e-4);
    }
+   //For max std of 5e-5
+   assert(sigma <= 5e-5);
    return status;
 }
 
