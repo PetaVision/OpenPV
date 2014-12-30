77541667ca704ba30c40d8cdf155ac9dd600db7e
slundquist <slundquist@users.sourceforge.net>
2014-12-30 18:49:59 +0000

    ImageOffsetTest skeleton
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@9353 7c0444ca-5550-0410-9008-faecdbe78eb5

---
+++ ImageOffsetTest/src/MaxPoolTestLayer.hpp
@@ -0,0 +1,19 @@
+#ifndef MAXPOOLTESTLAYER_HPP_ 
+#define MAXPOOLTESTLAYER_HPP_
+
+#include <layers/ANNLayer.hpp>
+
+namespace PV {
+
+class MaxPoolTestLayer: public PV::ANNLayer{
+public:
+	MaxPoolTestLayer(const char* name, HyPerCol * hc);
+
+protected:
+   int updateState(double timef, double dt);
+
+private:
+};
+
+} /* namespace PV */
+#endif
