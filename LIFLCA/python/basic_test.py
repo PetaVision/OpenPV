e26ade80c194c9cada393cb449f583ff6ee5b77d
dpaiton <dpaiton@users.sourceforge.net>
2014-12-30 16:47:34 +0000

    added comments for experimentation
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@9349 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 74fffe8e295b59758e8660982b1044326cb2eb99

--- LIFLCA/python/basic_test.py
+++ LIFLCA/python/basic_test.py
@@ -1,11 +1,14 @@
 ###############################
 ##  LCA ANALYSIS
 ##  Dylan Paiton
-##  & Will
+##  Will Shainin
 ###############################
 
+#workspace_path = '/home/wshainin/workspace/'
+workspace_path = '/Users/dpaiton/Documents/workspace/'
+
 import os, sys
-lib_path = os.path.abspath('/home/wshainin/workspace/PetaVision/plab')
+lib_path = os.path.abspath(workspace_path+'PetaVision/plab')
 sys.path.append(lib_path)
 import pvAnalysis as pv
 import plotWeights as pw
@@ -13,7 +16,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # File Locations
-output_dir   = '/home/wshainin/awsMount/mountData/LIFLCA/output/LCA/'
+#output_dir   = workspace_path+'/awsMount/mountData/LIFLCA/output/LCA/'
+output_dir   = workspace_path+'/LIFLCA/output_small/LCA/'
 input_layer  = 'a0_Input.pvp'
 l1_layer     = 'a2_L1.pvp'
 err_layer    = 'a1_Residual.pvp'
@@ -32,21 +36,30 @@ weightsChkFile      = open(output_dir + weights_chk,'rb')
 progressPeriod  = 1
 startFrame      = 0
 lastFrame       = -1  # -1 for all
-skipFrames      = 1
+skipFrames      = 1   # 1 is every frame
 
 # outStruct has fields "time" and "values"
 
+#size is (numFrames,nf,nx,ny)
 #print('Input:')
-#(InputStruct,InputHdr)   = pv.get_pvp_data(input_activityFile,progressPeriod,lastFrame,startFrame,skipFrames)
+#(inputStruct,inputHdr)   = pv.get_pvp_data(input_activityFile,progressPeriod,lastFrame,startFrame,skipFrames)
+# np.array(InputStruct["values"]).shape
+# (361, 1, 512, 512)
+# has 361 "frames", which I guess makes sense if initialWriteTime = 40 (400-39 = 361)
+# why is it 512x512?
 
-#print('L1:')
-#(L1Struct,L1Hdr)   = pv.get_pvp_data(l1_activityFile,progressPeriod,lastFrame,startFrame,skipFrames)
+print('L1:')
+(L1Struct,L1Hdr)   = pv.get_pvp_data(l1_activityFile,progressPeriod,lastFrame,startFrame,skipFrames)
+# takes a long time but runs without error
+# 10 frames total
+# np.array(L1Struct["values]).shape
+# (10, 32, 32, 256)
 
 #print('Err_from_file:')
 #(errStruct,errHdr)   = pv.get_pvp_data(err_activityFile,progressPeriod,lastFrame,startFrame,skipFrames)
 
 #print('Recon:')
-#(ReconStruct,ReconHdr)   = pv.get_pvp_data(recon_activityFile,progressPeriod,lastFrame,startFrame,skipFrames)
+#(reconStruct,reconHdr)   = pv.get_pvp_data(recon_activityFile,progressPeriod,lastFrame,startFrame,skipFrames)
 
 # Gar Method
 # divide the L2 norm of the residual by the L2 of the input to the
@@ -68,11 +81,11 @@ skipFrames      = 1
 #weightsFile.close()
 #weightsChkFile.close()
 
-i_arbor    = 0
-i_frame    = 400 # index, not actual frame number
-margin     = 2 #pixels
-showPlot   = True
-savePlot   = True
-saveName   = output_dir+'analysis/'+weights[:-4]+'_'+str(i_frame).zfill(5)+'.png'
-
-weight_mat = pw.plotWeights(weightStruct,i_arbor,i_frame,margin,showPlot,savePlot,saveName)
+#i_arbor    = 0
+#i_frame    = 400 # index, not actual frame number
+#margin     = 2 #pixels
+#showPlot   = True
+#savePlot   = True
+#saveName   = output_dir+'analysis/'+weights[:-4]+'_'+str(i_frame).zfill(5)+'.png'
+#
+#weight_mat = pw.plotWeights(weightStruct,i_arbor,i_frame,margin,showPlot,savePlot,saveName)
