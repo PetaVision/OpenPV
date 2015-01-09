9c07729f8a510f2a658694ab913a492689b4df9b
dpaiton <dpaiton@users.sourceforge.net>
2015-01-09 22:07:08 +0000

    pretty major refactoring with normalization.
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@9444 7c0444ca-5550-0410-9008-faecdbe78eb5

--- plab/plotWeights.py
+++ plab/plotWeights.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt # only need if showPlot==True OR savePlot==true
 import os                       # only needed if savePlot==true
 import pdb
 
-def plotWeights(weightStruct,arborIdx=None,i_frame=0,margin=0,showPlot=False,savePlot=False,saveName=''):
+def plotWeights(weightStruct,arborIdxList=None,i_frame=0,margin=0,showPlot=False,savePlot=False,saveName=''):
     # NOTE: i_arbor and i_frame are indices for the given frame or arbor.
     #       They are not the actual arbor/frame number. This is because
     #       there may be a writeStep that is not 1.
@@ -38,56 +38,76 @@ def plotWeights(weightStruct,arborIdx=None,i_frame=0,margin=0,showPlot=False,sav
     if i_frame is -1:
         i_frame = numFrames-1
 
-    if arborIdx is None:
-       arborIdx = np.arange(numArbors)
+    if arborIdxList is None:
+       arborIdxList = np.arange(numArbors)
 
     if i_frame > numFrames:
         print("Warning: i_frame > numFrames. Setting i_frame to numFrames.")
         i_frame = numFrames
 
-    out_list = len(arborIdx) * [None] # pre-allocate list to hold weight matrices
-    for i_arbor in arborIdx:
+
+    if np.sqrt(numPatches)%1 == 0: #If numPaches has a square root
+        numPatchesX = np.sqrt(numPatches)
+        numPatchesY = numPatchesX
+    else:
+        numPatchesX = np.ceil(np.sqrt(numPatches))
+        numPatchesY = numPatchesX
+
+    patch_set = np.zeros((numPatchesX*numPatchesY,nyp+margin,nxp+margin,nfp))
+    out_mat   = np.zeros((numPatchesY*(nyp+margin),numPatchesX*(nxp+margin),nfp))
+
+    half_margin = np.floor(margin/2)
+
+    # If time is stored via delayed arbors (as it usually is) then we need to rescale
+    # and normalize across all arbors per patch index. Alternatively, time might be 
+    # stored in the feature dimension, so we need to normalize across that dim as well.
+    for i_patch in range(numPatches):
+        min_patch = np.amin(weight_vals[i_frame,:,i_patch,:,:,:])
+        max_patch = np.amax(weight_vals[i_frame,:,i_patch,:,:,:])
+        new_max = 255
+        new_min = 0
+
+        ## PER PATCH - normalizing contrast
+        ## Assign 0 to 128 (0 is gray)
+        ## Per dictionary element (but across time), find max abs, make that value min & max 
+        ##  i.e. max(abs(patch)) -> 255, -max(abs(patch)) -> 0
+        ## Need to take max over time if time is there
+
+        
+        # Rescale & normalize patch. Scale patch to increase contrast, across space/time.
+        #   NOTE: Rescaling will make low frequency (& thus low contrast) patches look like they are higher contrast
+        weight_vals[i_frame,:,i_patch,:,:,:] = (weight_vals[i_frame,:,i_patch,:,:,:] - min_patch) * ((new_max-new_min) / (max_patch - min_patch + np.finfo(float).eps)) + new_min
+
+
+    out_list = len(arborIdxList) * [None] # pre-allocate list to hold weight matrices
+    for i_arbor in arborIdxList:
        if i_arbor > numArbors:
            print("Warning: i_arbor > numArbors. Setting i_arbor to numArbors.")
            i_arbor = numArbors
 
-       if np.sqrt(numPatches)%1 == 0: #If numPaches has a square root
-           numPatchesX = np.sqrt(numPatches)
-           numPatchesY = numPatchesX
-       else:
-           numPatchesX = np.ceil(np.sqrt(numPatches))
-           numPatchesY = numPatchesX
-
-       patch_set = np.zeros((numPatchesX*numPatchesY,nyp+margin,nxp+margin,nfp))
-       out_mat   = np.zeros((numPatchesY*(nyp+margin),numPatchesX*(nxp+margin),nfp))
-
        xpos = 0
        ypos = 0
-       half_margin = np.floor(margin/2)
-
        for i_patch in range(numPatches):
-           patch_tmp = weight_vals[i_frame,i_arbor,i_patch,:,:,:]
-           min_patch = np.amin(patch_tmp)
-           max_patch = np.amax(patch_tmp)
-           # Normalize patch
-           patch_tmp = (patch_tmp - min_patch) * 255 / (max_patch - min_patch + np.finfo(float).eps) # re-scaling & normalizing TODO: why? and what exactly is it doing?
-           # Patches are padded with zeros - just fill in center
-           patch_set[i_patch,half_margin:half_margin+nyp,half_margin:half_margin+nxp,:] = np.uint8(np.squeeze(np.transpose(patch_tmp,(1,0,2)))) # re-ordering to [x,y,f] TODO: why?
+           # Patches are padded with zeros - just fill in center. Ordered as [x,y,f]
+           #patch_set[i_patch,half_margin:half_margin+nyp,half_margin:half_margin+nxp,:] = np.uint8(np.squeeze(np.transpose(weight_vals[i_frame,i_arbor,i_patch,:,:,:],(1,0,2)))) 
+           # Patches are padded with zeros - just fill in center. Ordered as [y,x,f]
+           patch_set[i_patch,half_margin:half_margin+nyp,half_margin:half_margin+nxp,:] = np.uint8(np.squeeze(weight_vals[i_frame,i_arbor,i_patch,:,:,:])) 
 
            out_mat[ypos:ypos+nyp+margin,xpos:xpos+nxp+margin,:] = patch_set[i_patch,:,:,:]
 
-           xpos += nxp+margin
 
+           xpos += nxp+margin
            if xpos > out_mat.shape[1]-(nxp+margin):
                ypos += nyp+margin
                xpos = 0
 
        out_list[i_arbor] = out_mat
 
+       #pdb.set_trace()
        if showPlot:
            for feat in range(nfp):
                plt.figure()
-               plt.imshow(out_mat[:,:,feat],cmap='Greys',interpolation='nearest')
+               plt.imshow(np.squeeze(out_mat[:,:,feat]),cmap='Greys',interpolation='nearest')
                plt.show(block=False)
        if savePlot:
            #TODO: Should be able to pass figure title?
@@ -106,7 +126,7 @@ def plotWeights(weightStruct,arborIdx=None,i_frame=0,margin=0,showPlot=False,sav
                    fileName = seps[0]
                    if not os.path.exists(filePath):
                        os.makedirs(filePath)
-               plt.imsave(filePath+fileName+'_fr'+str(i_frame).zfill(3)+'_a'+str(i_arbor).zfill(3)+'_fe'+str(feat).zfill(2)+'.'+fileExt,out_mat,vmin=0,vmax=255,cmap='Greys',origin='upper')
+               plt.imsave(filePath+fileName+'_fr'+str(i_frame).zfill(3)+'_a'+str(i_arbor).zfill(3)+'_fe'+str(feat).zfill(2)+'.'+fileExt,np.squeeze(out_mat[:,:,feat]),vmin=0,vmax=255,cmap='Greys',origin='upper')
 
     return out_list
 
