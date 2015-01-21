912ca06b83311141f10fd750bc5da1e6a7ea9524
slundquist <slundquist@users.sourceforge.net>
2015-01-21 21:55:53 +0000

    New imprint conn test
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@9527 7c0444ca-5550-0410-9008-faecdbe78eb5

--- ImprintConnTest/src/InputLayer.cpp
+++ ImprintConnTest/src/InputLayer.cpp
@@ -18,18 +18,32 @@ int InputLayer::updateState(double timef, double dt){
    int kx0 = loc->kx0;
    int ky0 = loc->ky0;
 
-   assert(nf == 1);
+   assert(nf == 4);
    assert(loc->nxGlobal == 2 && loc->nyGlobal == 2);
 
    //We only care about restricted space
    for(int iY = loc->halo.up; iY < ny + loc->halo.up; iY++){
       for(int iX = loc->halo.lt; iX < nx + loc->halo.lt; iX++){
-         int idx = kIndex(iX, iY, 0, nx+loc->halo.lt+loc->halo.rt, ny+loc->halo.dn+loc->halo.up, nf);
-         if(timef < 5){
-            A[idx] = 0;
-         }
-         else{
-            A[idx] = .1;
+         for(int iF = 0; iF < loc->nf; iF++){
+            int idx = kIndex(iX, iY, iF, nx+loc->halo.lt+loc->halo.rt, ny+loc->halo.dn+loc->halo.up, nf);
+            int xval = iX+kx0-loc->halo.lt;
+            int yval = iY+ky0-loc->halo.up;
+
+            if(timef == 10 && xval == 0 && yval == 0 && iF == 0){
+               A[idx] = 1;
+            }
+            else if(timef == 10 && xval == 1 && yval == 0 && iF == 1){
+               A[idx] = 1;
+            }
+            else if(timef == 10 && xval == 0 && yval == 1 && iF == 2){
+               A[idx] = 1;
+            }
+            else if(timef == 10 && xval == 1 && yval == 1 && iF == 3){ 
+               A[idx] = 1;
+            }
+            else{
+               A[idx] = 0;
+            }
          }
       }
    }
--- ImprintConnTest/src/TestLayer.cpp
+++ ImprintConnTest/src/TestLayer.cpp
@@ -18,6 +18,8 @@ int TestLayer::updateState(double timef, double dt){
    int kx0 = loc->kx0;
    int ky0 = loc->ky0;
    assert(nf == 3);
+   assert(loc->nxGlobal == 8);
+   assert(loc->nyGlobal == 8);
 
    bool isCorrect = true;
    //Grab the activity layer of current layer
@@ -27,35 +29,60 @@ int TestLayer::updateState(double timef, double dt){
       for(int iX = loc->halo.lt; iX < nx + loc->halo.lt; iX++){
          for(int iFeature = 0; iFeature < nf; iFeature++){
             int idx = kIndex(iX, iY, iFeature, nx+loc->halo.lt+loc->halo.rt, ny+loc->halo.dn+loc->halo.up, nf);
-            //Input image is set up to have max values in 3rd feature dimension
             //3rd dimension, top left is 128, bottom right is 191
             //Y axis spins fastest
             float actualvalue = A[idx];
 
             int xval = iX+kx0-loc->halo.lt;
             int yval = iY+ky0-loc->halo.up;
-            //Patches on edges have same answer as previous neuron
-            //if(xval == 7){
-            //   xval -= 1;
-            //}
-            //if(yval == 7){
-            //   yval -= 1;
-            //}
 
-            //// modified GTK: 1/10/15, modified to test spatial max pooling over a feature plane 
-            ////float expectedvalue = 8*xval+yval+137;
-            //float expectedvalue = (yval+1)+8*(xval+1)+64*iFeature;
-            float expectedvalue = -1;
-            //if(actualvalue != expectedvalue){
-               std::cout << "Connection " << name << " Mismatch at (" << iX << "," << iY << ") : actual value: " << actualvalue << " Expected value: " << expectedvalue << ".\n";
-               //isCorrect = false;
-            //}
+            //Averaged across all 4 patches. Calculate averages first
+            //Figure out location in patch size
+            int xPatchLoc = xval % 4;
+            int yPatchLoc = yval % 4;
+            int xPatchIdx = xval/4;
+            int yPatchIdx = yval/4;
+            //Value should be average of same spot in all 4 patches
+            float expectedvalue;
+            if(timef == 10){
+               expectedvalue = (iFeature*64 + xPatchLoc * 8 + yPatchLoc) + 
+                                     (iFeature*64 + (xPatchLoc+4) * 8 + yPatchLoc) +
+                                     (iFeature*64 + xPatchLoc * 8 + (yPatchLoc+4)) +
+                                     (iFeature*64 + (xPatchLoc+4) * 8 + (yPatchLoc+4));
+               expectedvalue = expectedvalue / 4;
+
+               //expectedvalue should also be dependent on which kernel is active
+               //Order of imprinting: kernel 0 at ts 4, kernel 1 at ts 5, kernel 2 at ts 6, kernel 3 at ts 7
+               //Values of imprinting should be orig value * (ts + 1)
+               int tsScale;
+               if(     xPatchIdx == 0 && yPatchIdx == 0){
+                  tsScale = 5;
+               }
+               else if(xPatchIdx == 1 && yPatchIdx == 0){
+                  tsScale = 6;
+               }
+               else if(xPatchIdx == 0 && yPatchIdx == 1){
+                  tsScale = 7;
+               }
+               else if(xPatchIdx == 1 && yPatchIdx == 1){
+                  tsScale = 8;
+               }
+               expectedvalue = expectedvalue * tsScale;
+            }
+            else{
+               expectedvalue = 0;
+            }
+
+            if(actualvalue != expectedvalue){
+               std::cout << "Connection " << name << " Mismatch at (" << xval << "," << yval << ") : actual value: " << actualvalue << " Expected value: " << expectedvalue << ".\n";
+               isCorrect = false;
+            }
          }
       }
    }
-   //if(!isCorrect){
-   //   exit(-1);
-   //}
+   if(!isCorrect){
+      exit(-1);
+   }
    return PV_SUCCESS;
 }
 
