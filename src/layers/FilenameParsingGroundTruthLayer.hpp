9e70aa516c656578a3d7fa7d384bee9ac404cc10
bbroompeltz <bbroompeltz@users.sourceforge.net>
2015-01-21 23:46:04 +0000

    Added doxygen documentation
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@9532 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/layers/FilenameParsingGroundTruthLayer.hpp
+++ src/layers/FilenameParsingGroundTruthLayer.hpp
@@ -30,10 +30,44 @@ private:
    float gtClassTrueValue;
    float gtClassFalseValue;
 protected:
+
+   /**
+    * List of protected paramters needed from FilenameParsingGroundTruthLayer
+    * @name FilenameParsingGroundTruthLayer Paramters
+    * @{
+    */
+   
+   /**
+    * @brief classes: list the name of the .txt file that holds the list of imageListPath features that will parse to different classifications
+    * @details classes.txt must be located in the output directory, the classifers separated by a new line, and must be discerning
+    * In the case of CIFAR images, the pictures are organized in folders /0/ /1/ /2/ ... etc, therefore those are the classifers
+    * When an image is passed to the movie layer, the classification is parsed and a corresponding neuron is activated to a value set by 
+    * gtClassTrueValue and the remaining neurons are set to the value set by gtClassFalseValue
+    */
+
    virtual void ioParam_classes(enum ParamsIOFlag ioFlag);
+
+   /**
+    * @brief movieLayerName: lists name of the movie layer from which the imageListPath is used to parse the classification
+    */
+
    virtual void ioParam_movieLayerName(enum ParamsIOFlag ioFlag);
+   
+   /**
+    * @brief gtClassTrueValue: defines value to be set for the neuron that matches classes.txt classifer
+    * @details Default: 1
+    */
+   
    virtual void ioParam_gtClassTrueValue(enum ParamsIOFlag ioFlag);
+
+   /**
+    * @brief gtClassFalseValue: defines value to be set for the neurons that do not match the classes.txt classifer
+    * @details Default: -1
+    */
    virtual void ioParam_gtClassFalseValue(enum ParamsIOFlag ioFlag);
+
+   /** @} */
+
 };
 
 } /* namespace PV */
