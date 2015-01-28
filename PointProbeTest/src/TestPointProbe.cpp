dacc93acc6df5f2aa3eb3aa7c7d7927224f2b958
slundquist <slundquist@users.sourceforge.net>
2015-01-28 00:14:59 +0000

    Fixed a bug where TestPointProbe was not calling initialize. New input pvp file
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@9568 7c0444ca-5550-0410-9008-faecdbe78eb5

--- PointProbeTest/src/TestPointProbe.cpp
+++ PointProbeTest/src/TestPointProbe.cpp
@@ -22,6 +22,7 @@ TestPointProbe::TestPointProbe() {
 TestPointProbe::TestPointProbe(const char * probeName, HyPerCol * hc) :
    PointProbe()
 {
+   initialize(probeName, hc);
 }
 
 TestPointProbe::~TestPointProbe()
@@ -39,11 +40,11 @@ int TestPointProbe::point_writeState(double timef, float outVVal, float outAVal)
    if(parent->columnId()==0){
       //Input pvp layer's spinning order is nf, nx, ny
       float expectedVal = fLoc * 64 + xLoc * 8 + yLoc;
-      //if(outAVal != expectedVal){
+      if(outAVal != expectedVal){
          std::cout << "Connection " << name << " Mismatch: actual value: " << outAVal << " Expected value: " << expectedVal << ".\n";
-         //MPI_Barrier(parent->icCommunicator()->communicator());
-         //exit(-1);
-      //}
+         MPI_Barrier(parent->icCommunicator()->communicator());
+         exit(-1);
+      }
    }
    return PV_SUCCESS;
 }
