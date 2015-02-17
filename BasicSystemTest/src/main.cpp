98a01f28db259196081132b968d0bc7d1c680f3c
peteschultz <peteschultz@users.sourceforge.net>
2015-02-17 19:35:37 +0000

    Changing BasicSystemTest to use ParamGroupHandler if MAIN_USES_CUSTOMGROUPS is set
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@9613 7c0444ca-5550-0410-9008-faecdbe78eb5

--- BasicSystemTest/src/main.cpp
+++ BasicSystemTest/src/main.cpp
@@ -9,24 +9,37 @@
 #define MAIN_USES_CUSTOMGROUPS
 
 #ifdef MAIN_USES_CUSTOMGROUPS
-void * customgroup(const char * name, const char * groupname, HyPerCol * hc);
-// customgroups is for adding objects not supported by build().
-#endif // MAIN_USES_ADDCUSTOM
+#include <io/ParamGroupHandler.hpp>
+// CustomGroupHandler is for adding objects not supported by CoreParamGroupHandler().
+class CustomGroupHandler: public ParamGroupHandler {
+public:
+   CustomGroupHandler() {}
+
+   virtual ~CustomGroupHandler() {}
+
+   virtual ParamGroupType getGroupType(char const * keyword) {
+      ParamGroupType result = UnrecognizedGroupType;
+      //
+      // This routine should compare keyword to the list of keywords handled by CustomGroupHandler and return one of
+      // LayerGroupType, ConnectionGroupType, ProbeGroupType, ColProbeGroupType, WeightInitializerGroupType, or WeightNormalizerGroupType
+      // according to the keyword, or UnrecognizedGroupType if this ParamGroupHandler object does not know the keyword.
+      //
+      return result;
+   }
+   // A CustomGroupHandler group should override createLayer, createConnection, etc., as appropriate, if there are custom objects
+   // corresponding to that group type.
+
+}; /* class CustomGroupHandler */
+#endif // MAIN_USES_CUSTOMGROUPS
 
 int main(int argc, char * argv[]) {
 
    int status;
 #ifdef MAIN_USES_CUSTOMGROUPS
-   status = buildandrun(argc, argv, NULL, NULL, &customgroup);
+   ParamGroupHandler * customGroupHandler = new CustomGroupHandler();
+   status = buildandrun(argc, argv, NULL, NULL, &customGroupHandler, 1/*numGroupHandlers*/);
 #else
    status = buildandrun(argc, argv);
 #endif // MAIN_USES_CUSTOMGROUPS
    return status==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
-#ifdef MAIN_USES_CUSTOMGROUPS
-void * customgroup(const char * keyword, const char * name, HyPerCol * hc) {
-   void * addedGroup = NULL;
-   return addedGroup;
-}
-#endif // MAIN_USES_CUSTOMGROUPS
