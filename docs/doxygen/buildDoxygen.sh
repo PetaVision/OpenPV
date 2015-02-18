0e0a8cbef20703481d22a90748c45e2e2cf9a324
slundquist <slundquist@users.sourceforge.net>
2015-02-18 22:06:47 +0000

    testing markdown web development
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@9623 7c0444ca-5550-0410-9008-faecdbe78eb5

--- docs/doxygen/buildDoxygen.sh
+++ docs/doxygen/buildDoxygen.sh
@@ -1,3 +1,4 @@
 mkdir html
-
 doxygen cfg/doxy.cfg
+cp -R bootstrap ./html/bootstrap
+
---
+++ docs/markdown/markdown.php
@@ -0,0 +1,20 @@
+<!-- markdown content goes in this file -->
+
+<?php 
+$markdown = <<<content
+Markdown
+-------------
+
+...is really just ordinary text, *plain and simple*. How is it good for you?
+
+- You just **type naturally**, and the result looks good.
+- You **don't have to worry** about clicking formatting buttons.
+  - Or fiddling with indentation. (Two spaces is all you need.)
+
+To see what else you can do with Markdown (including **tables**, **images**, **numbered lists**, and more) take a look at the [Cheatsheet][1]. And then try it out by typing in this box!
+
+[1]: https://github.com/adam-p/markdown-here/wiki/Markdown-Here-Cheatsheet
+content;
+?>
+
+And other text goes here!
