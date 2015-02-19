f6f3c2f0b3f58697fa6cdd0b166eeb5e897bcec7
slundquist <slundquist@users.sourceforge.net>
2015-02-19 00:24:20 +0000

    Modifying Markdown.php for testing
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@9631 7c0444ca-5550-0410-9008-faecdbe78eb5

--- docs/markdown/markdown.php
+++ docs/markdown/markdown.php
@@ -2,7 +2,7 @@
 
 <?php 
 $markdown = <<<content
-PetaVision Documentaionn from Markdown
+[PetaVision Documentaionn from Markdown]
 -------------
 
 ...is really just ordinary text, *plain and simple*. How is it good for you?
@@ -10,10 +10,17 @@ PetaVision Documentaionn from Markdown
 - You just **type naturally**, and the result looks good.
 - You **don't have to worry** about clicking formatting buttons.
 - Or fiddling with indentation. (Two spaces is all you need.)
+
+You can write all the text that you need. 
+This is no extra line in between.
+
+This is an extra line in between.
+
+
+This is two lines in between.
+
   
-* one reason
-* two reasons to use this 
-[www.google.com](Google.com)
+
 
 To see what else you can do with Markdown (including **tables**, **images**, **numbered lists**, and more) take a look at the [Cheatsheet][1]. And then try it out by typing in this box!
 
