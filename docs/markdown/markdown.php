e8370b1d1df3d68f4b48feb7c97ff1141deaadc7
slundquist <slundquist@users.sourceforge.net>
2015-02-19 00:24:58 +0000

    Modifying Markdown.php for testing
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@9632 7c0444ca-5550-0410-9008-faecdbe78eb5

--- docs/markdown/markdown.php
+++ docs/markdown/markdown.php
@@ -7,10 +7,6 @@ $markdown = <<<content
 
 ...is really just ordinary text, *plain and simple*. How is it good for you?
 
-- You just **type naturally**, and the result looks good.
-- You **don't have to worry** about clicking formatting buttons.
-- Or fiddling with indentation. (Two spaces is all you need.)
-
 You can write all the text that you need. 
 This is no extra line in between.
 
