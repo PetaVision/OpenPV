cbbd09871031311db45a396fbddd1b1d744965e6
slundquist <slundquist@users.sourceforge.net>
2015-02-19 19:27:36 +0000

    GPUProbe now asserts for failures. New params file for multiple transpose off one connection with GPU test
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@9642 7c0444ca-5550-0410-9008-faecdbe78eb5

--- GPUSystemTest/src/GPUSystemTestProbe.cpp
+++ GPUSystemTest/src/GPUSystemTestProbe.cpp
@@ -36,12 +36,12 @@ int GPUSystemTestProbe::outputState(double timed){
    std::cout.precision(15);
    float sumsq = 0;
    for (int i = 0; i < numExtNeurons; i++){
-      //if(fabs(A[i]) != 0){
-      //   int xpos = kxPos(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
-      //   int ypos = kyPos(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
-      //   int fpos = featureIndex(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
-      //   //std::cout << "[" << xpos << "," << ypos << "," << fpos << "] = " << std::fixed << A[i] << "\n";
-      //}
+      if(fabs(A[i]) != 0){
+         int xpos = kxPos(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
+         int ypos = kyPos(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
+         int fpos = featureIndex(i, loc->nx+loc->halo.lt+loc->halo.rt, loc->ny+loc->halo.dn+loc->halo.up, loc->nf);
+         std::cout << "[" << xpos << "," << ypos << "," << fpos << "] = " << std::fixed << A[i] << "\n";
+      }
       //For max difference roundoff errors
       assert(fabs(A[i]) < 5e-4);
    }
