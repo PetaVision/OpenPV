34a6873147c79c5a47280cc0bd1529432dccdb6e
peteschultz <peteschultz@users.sourceforge.net>
2015-03-09 23:38:19 +0000

    Using new buildandrun prototype in MaxPoolTest
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@9702 7c0444ca-5550-0410-9008-faecdbe78eb5

--- MaxPoolTest/src/MaxPoolTest.cpp
+++ MaxPoolTest/src/MaxPoolTest.cpp
@@ -1,37 +1,23 @@
 /*
- * MaxPoolTest
+ * ReceiveFromPostTest
  *
  *
  */
 
 
 #include <columns/buildandrun.hpp>
-#include "MaxPoolTestLayer.hpp"
+#include "CustomGroupHandler.hpp"
 
 #define MAIN_USES_CUSTOMGROUPS
 
-#ifdef MAIN_USES_CUSTOMGROUPS
-void * customgroup(const char * name, const char * groupname, HyPerCol * hc);
-// customgroups is for adding objects not supported by build().
-#endif // MAIN_USES_ADDCUSTOM
-
 int main(int argc, char * argv[]) {
 
-   int status;
 #ifdef MAIN_USES_CUSTOMGROUPS
-   status = buildandrun(argc, argv, NULL, NULL, &customgroup);
-#else
-   status = buildandrun(argc, argv);
+   ParamGroupHandler * customGroupHandler = new CustomGroupHandler;
+   int status = buildandrun(argc, argv, NULL, NULL, &customGroupHandler, 1);
+#else // MAIN_USES_CUSTOMGROUPS
+   int status = buildandrun(argc, argv, NULL, NULL, NULL, 0);
 #endif // MAIN_USES_CUSTOMGROUPS
+
    return status==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
-#ifdef MAIN_USES_CUSTOMGROUPS
-void * customgroup(const char * keyword, const char * name, HyPerCol * hc) {
-   void * addedGroup = NULL;
-   if (strcmp(keyword, "MaxPoolTestLayer") == 0){
-      addedGroup = new MaxPoolTestLayer(name, hc);
-   }
-   return addedGroup;
-}
-#endif // MAIN_USES_CUSTOMGROUPS
