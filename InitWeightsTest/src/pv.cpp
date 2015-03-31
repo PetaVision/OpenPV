0adcdce876ef0ce055f313cae5941f69090a75b3
peteschultz <peteschultz@users.sourceforge.net>
2015-03-31 17:53:30 +0000

    Fixes memory leak in InitWeightsTest
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@9833 7c0444ca-5550-0410-9008-faecdbe78eb5

--- InitWeightsTest/src/pv.cpp
+++ InitWeightsTest/src/pv.cpp
@@ -10,5 +10,6 @@ int main(int argc, char * argv[]) {
    ParamGroupHandler * customGroupHandler = new InitWeightsTestParamGroupHandler();
    assert(customGroupHandler != NULL);
    int status = buildandrun(argc, argv, NULL, NULL, &customGroupHandler, 1);
+   delete customGroupHandler;
    return status==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
 }
