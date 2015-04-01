8213553d8e799b9996ebd57aa094541973c42acb
peteschultz <peteschultz@users.sourceforge.net>
2015-04-01 23:18:15 +0000

    Same bugfix in writeActivitySparse, but for nonroot processes
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@9858 7c0444ca-5550-0410-9008-faecdbe78eb5

--- src/io/fileio.cpp
+++ src/io/fileio.cpp
@@ -1472,7 +1472,7 @@ int writeActivitySparse(PV_Stream * pvstream, Communicator * comm, double timed,
             if(globalResK == -1){
                continue;
             }
-            globalResIndices[indiciesIdx] = indices[j];
+            globalResIndices[indiciesIdx] = globalResK;
             indiciesIdx++;
          }
          data = (void *) globalResIndices;
