12cd62b633151e070e7052737e697f3be2dead3c
peteschultz <peteschultz@users.sourceforge.net>
2015-04-09 19:19:08 +0000

    delete command for a couple more system tests
    git-svn-id: http://svn.code.sf.net/p/petavision/code/PVSystemTests@9909 7c0444ca-5550-0410-9008-faecdbe78eb5

--- ReceiveFromPostTest/src/ReceiveFromPostTest.cpp
+++ ReceiveFromPostTest/src/ReceiveFromPostTest.cpp
@@ -12,5 +12,6 @@ int main(int argc, char * argv[]) {
 
    ParamGroupHandler * customGroupHandler = new CustomGroupHandler;
    int status = buildandrun(argc, argv, NULL, NULL, &customGroupHandler, 1);
+   delete customGroupHandler;
    return status==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
 }
