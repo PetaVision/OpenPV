d60b4082f7867da0f9d2bde6d09b4b6d83f75d06
slundquist <slundquist@users.sourceforge.net>
2015-04-13 19:55:57 +0000

    Updating sandbox with new params and scripts
    git-svn-id: http://svn.code.sf.net/p/petavision/code/sandbox@9931 7c0444ca-5550-0410-9008-faecdbe78eb5
    
    Former-commit-id: 066066ac7279ad0ffa1f41df31dd58fac0875692

---
+++ DepthLCA/scripts/depthResponse/aws_RELU.m
@@ -0,0 +1,15 @@
+outDir = '/nh/compneuro/Data/Depth/LCA/benchmark/train/aws_rcorr_RELU/';
+v1ActFile = ['~/workspace/DepthLCA/input/benchmark/Data/a7_RELU_V1_Rescale.pvp'];
+depthFile = ['/nh/compneuro/Data/Depth/LCA/benchmark/train/aws_rcorr_LCA/a3_DepthDownsample.pvp'];
+plotOutDir = [outDir, '/depthTuning/'];
+
+dictPvpDir = '/home/slundquist/workspace/DepthLCA/input/benchmark/Data/'
+dictPvpFiles = {[dictPvpDir, 'LCA_V1ToLeftError_W.pvp'];...
+                [dictPvpDir, 'LCA_V1ToRightError_W.pvp']};
+
+%Given a depth and a neuron, these values define how big of an x/y patch to look for that neuron at
+sampleDim = 7;
+numDepthBins = 64;
+
+calcDepthTuning(plotOutDir, v1ActFile, depthFile, dictPvpFiles, sampleDim, numDepthBins);
+
