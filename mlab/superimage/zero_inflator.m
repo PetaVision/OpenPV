ebc9c7dc582494c61134f964a50a3f3d4185fab4
maxtheiler <maxtheiler@users.sourceforge.net>
2015-07-10 22:48:44 +0000

    bug fixes and simple analyzer script for superimage
    git-svn-id: http://svn.code.sf.net/p/petavision/code/trunk@10286 7c0444ca-5550-0410-9008-faecdbe78eb5

--- mlab/superimage/zero_inflator.m
+++ mlab/superimage/zero_inflator.m
@@ -4,15 +4,15 @@ function out = zero_inflator(infile)
   endif
   N = head.nx * head.ny * head.nf;
   for i = 1:length(in)
-    current_index = in{1}.values(:,1);
-    current_values = in{1}.values(:,2);
+    current_index = in{i}.values(:,1);
+    current_values = in{i}.values(:,2);
     
     tmp = zeros(N,1);
     tmp(current_index + 1) = current_values;
     
     tmp = reshape(tmp, [head.nf, head.nx, head.ny]);
 
-    out{i}.time = in{1}.time;
+    out{i}.time = in{i}.time;
     out{i}.values = permute(tmp, [3,2,1]);
   endfor
 endfunction
