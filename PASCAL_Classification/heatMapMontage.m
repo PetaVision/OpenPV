eefbbf1bd0ce268d5131ffc2dfbd4350dda49d0f
wshainin <wshainin@users.sourceforge.net>
2015-07-14 22:01:20 +0000

    Removed debugging output
    git-svn-id: http://svn.code.sf.net/p/petavision/code/demo@10314 7c0444ca-5550-0410-9008-faecdbe78eb5

--- PASCAL_Classification/heatMapMontage.m
+++ PASCAL_Classification/heatMapMontage.m
@@ -85,19 +85,19 @@ end%if
 resultData = permute(resultPvp{1}.values,[2 1 3]);
 
 if isempty(displayCategoryIndices)
-   displayCategoryIndices=1:resultHdr.nf;
+   displayCategoryIndices=1:resultHdr.nf
 end%if
 
-numCategories=numel(displayCategoryIndices)
+numCategories=numel(displayCategoryIndices);
 
-numColumns = 1:numCategories
-numRows = ceil(numCategories./numColumns)
-totalSizeY = (size(imageData,1)+64+10)*numRows
-totalSizeX = (size(imageData,2)+64+10)*numColumns
-aspectRatio = totalSizeX./totalSizeY
-ldfgr = abs(log(aspectRatio) - log((1+sqrt(5))/2))
-numColumns = find(ldfgr==min(ldfgr),1)
-numRows = numRows(numColumns)
+numColumns = 1:numCategories;
+numRows = ceil(numCategories./numColumns);
+totalSizeY = (size(imageData,1)+64+10)*numRows;
+totalSizeX = (size(imageData,2)+64+10)*numColumns;
+aspectRatio = totalSizeX./totalSizeY;
+ldfgr = abs(log(aspectRatio) - log((1+sqrt(5))/2));
+numColumns = find(ldfgr==min(ldfgr),1);
+numRows = numRows(numColumns);
 while (numColumns-1)*numRows >= numCategories
     numColumns = numColumns-1;
 end%while
