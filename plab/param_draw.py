1cfbe04780b57fac36f39bcb923ebf478a3b3980
maxtheiler <maxtheiler@users.noreply.github.com>
2015-07-21 11:22:35 -0600

    cleaned up obsolete code in param_draw

--- plab/param_draw.py
+++ plab/param_draw.py
@@ -23,15 +23,3 @@ else:
         pp.mermaid_writeout(reader.parse(), 'scale', True)
     else:
         pp.mermaid_writeout(reader.parse(), 'scale', False)
-
-#if len(sys.argv) > 1:
-#    filename = sys.argv[1]
-#    if len(sys.argv) > 3:
-#        reader = pp.Param_Parser(filename, layers = sys.argv[2], conns = sys.argv[3])
-#        pp.mermaid_writeout(reader.parse(), 'phase')
-#    else:
-#        print('Warning: No layers/connections directory specified.')
-#        reader = pp.Param_Parser(filename, layers = '../src/layers', conns = '../src/connections/')
-#        pp.mermaid_writeout(reader.parse(),'phase')
-#else:
-#    print('No params file specified.')
