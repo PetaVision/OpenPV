b7df72fef204e1b475f99b96cb386e838e990ef3
Sheng Lundquist <slundqui007@gmail.com>
2015-08-21 14:23:41 -0600

    Cleaned up demo directory

--- demo/binocDemo/inputs/binoc_learn_512_white.lua
+++ demo/binocDemo/inputs/binoc_learn_512_white.lua
@@ -1,15 +1,15 @@
 -------------------------------------------------------------------
--- Encoding benchmark dataset with SCANN
+-- Learning binocular dictionaries with SCANN
 --
 -- Sheng Lundquist 6/9/15
 --
--- Implements encoding of benchmark training set with SCANN model as described in
+-- Implements binocular SCANN model as described in
 -- Lundquist et al., "Emergence of Depth-Tuned Hidden Units Through
 -- Sparse Encoding of Binocular Images"
 -------------------------------------------------------------------
 
 --Util module
-package.path = package.path .. ";" .. os.getenv("HOME") .. "/workspace/PetaVision/parameterWrapper/?.lua;"
+package.path = package.path .. ";" .. os.getenv("HOME") .. "/workspaceGit/OpenPV/pv-core/parameterWrapper/?.lua;"
 local pv = require("PVModule")
 
 ------------------------------------------------
@@ -18,28 +18,34 @@ local pv = require("PVModule")
 -- User defined variables
 local nxSize = 1200 --Size of image
 local nySize = 360 
-local outputPath = "/home/ec2-user/output/benchmarkEncoding/encode_RELU/" --Output directory
-local writePeriod = 1 --How often to write out
-local checkpointWriteStepInterval = 5000 --How often to checkpoint
-local stopTime = 195 --Stopping time, 195 images (rounded)
-local progressStep = 5000; --How often to print out a status report
+local outputPath = "/home/slundquist/workspaceGit/OpenPV/demo/binocDemo/output/" --Output directory
+local writePeriod = 1 --200 --How often to write out
+local checkpointWriteStepInterval = 10 --How often to checkpoint
+local stopTime= 1000 --Stopping time
+local progressStep = 10; --How often to print out a status report
 
 --Image parameters
-local leftImageListPath = "/home/ec2-user/dataset/list/benchmark_left.txt" --List of images, raw
-local rightImageListPath = "/home/ec2-user/dataset/list/benchmark_right.txt"
-local displayPeriod = 1 --Display period for sparse approximation
+local leftImageListPath = "/nh/compneuro/Data/KITTI/list/image_02.txt" --List of images, raw
+local rightImageListPath = "/nh/compneuro/Data/KITTI/list/image_03.txt"
+local displayPeriod = 200 --Display period for sparse approximation
 local startFrame = 1 --Starting at frame 1
 
 --LCA parameters
 local stride = 2 --Stride of LCA
 local numDictElements = 512 --Total number of dictionary elements
 local dictPatchSize = 66 --Square patch, with this as the dimension
-local VThresh = 0 --Threshold parameter, 0 for RELU
-local VWidth = 0 --Threshold parameter
+local VThresh = 0.006 --Threshold parameter
+local VWidth = .05 --Threshold parameter
+
+local learningRate = 0.05 --Learning rate
+local useMomentum = true  --Using momentum
+local learningMomentumTau = 100; --Learning Omega
 
 --nil for new dict, a checkpoint directory for loading weights
---local V1DictDir = nil
-local V1DictDir = "/home/ec2-user/saved_output/dictTrain/saved_binoc_512_white/"
+local V1DictDir = nil
+--local V1DictDir = "/nh/compneuro/Data/Depth/LCA/benchmark/LCATrain/Checkpoints/
+------------------------------------------------
+
 
 local pvParams = {
    --base column
@@ -76,7 +82,7 @@ local pvParams = {
       nxScale = 1;
       nyScale = 1;
       readPvpFile = false;
-      imageListPath = leftImageListPath;
+      inputPath = leftImageListPath;
       writeFrameToTimestamp = true;
       nf = 1;
       writeStep = writePeriod;
@@ -163,7 +169,7 @@ local pvParams = {
    };
 
    LeftError = {
-      groupType = "ANNErrorLayer";
+      groupType = "ANNNormalizedErrorLayer";
       restart = 0;
       nxScale = .5;
       nyScale = .5;
@@ -174,20 +180,43 @@ local pvParams = {
       writeSparseActivity = 0;
       InitVType = "ZeroV";
       VThresh = 0;
-      VMax =  INFINITY;
-      VMin = -INFINITY;
-      VShift = 0;
+      AMax =  INFINITY;
+      AMin = -INFINITY;
+      AShift = 0;
       VWidth = 0;
       valueBC = 0;
       errScale = 1;
       phase = 4;  
+   };
+
+   LeftRecon = {
+      groupType = "ANNLayer";
+      restart = 0;
+      nxScale = .5;
+      nyScale = .5;
+      nf = 1;
+      writeStep = writePeriod;
+      initialWriteTime = writePeriod;
+      mirrorBCflag = 0;
+      writeSparseActivity = 0;
+      InitVType = "ZeroV";
+      VThresh = -INFINITY;
+      AMax = INFINITY;
+      AMin = -INFINITY; 
+      AShift = 0;
+      VWidth = 0;
+      valueBC = 0;
+      phase = 6;
+      triggerFlag = 1;
+      triggerLayerName = "LeftImage";
+      triggerOffset = 1;
    };
 };
 
 --Right eye layers
 pv.addGroup(pvParams, "RightImage", pvParams["LeftImage"],
    {
-      imageListPath = rightImageListPath;
+      inputPath = rightImageListPath;
    }
 )
 
@@ -211,25 +240,39 @@ pv.addGroup(pvParams, "RightRescale", pvParams["LeftRescale"],
 
 pv.addGroup(pvParams, "RightError", pvParams["LeftError"])
 
+pv.addGroup(pvParams, "RightRecon", pvParams["LeftRecon"],
+   {
+      triggerLayerName = "RightImage";
+   }
+)
 
-pv.addGroup(pvParams, "V1_RELU",
+pv.addGroup(pvParams, "V1",
 {
-   groupType = "ANNLayer";
+   groupType = "HyPerLCALayer";
    restart = 0;
    nxScale = .5/stride;
    nyScale = .5/stride;
    nf = numDictElements;
+   numChannels = 1;
+   numWindowX = 1;
+   numWindowY = 1;
    writeStep = writePeriod;
    initialWriteTime = writePeriod;
    mirrorBCflag = 0;
    writeSparseActivity = 1;
    writeSparseValues   = 1;
-   InitVType = "ZeroV";
+   InitVType = "UniformRandomV";
+   minV = -1.0;
+   maxV = .02; 
+   timeConstantTau = 200.0;
+   slopeErrorStd = 0.01;
+   dVThresh = 0;
    VThresh = VThresh;
-   VMax = infinity;
-   VMin = 0;
-   VShift = 0.0;  
+   AMax = infinity;
+   AMin = 0;
+   AShift = 0.0;  
    VWidth = VWidth; 
+   updateGpu = true;
    phase = 5;
 });
 
@@ -237,7 +280,7 @@ pv.addGroup(pvParams, "V1_RELU",
 pv.addMultiGroups(pvParams, 
    {
    LeftImageToLeftBipolarCenter = {
-      groupType                           = "HyPerConn";
+      groupType                           = "KernelConn";
       preLayerName                        = "LeftImage";
       postLayerName                       = "LeftBipolar";
       channelCode                         = 0;
@@ -390,18 +433,16 @@ pv.addGroup(pvParams, "RightRescaleToRightError", pvParams["LeftRescaleToLeftErr
 --LCA connections
 pv.addGroup(pvParams, "V1ToLeftError", 
    {
-      groupType = "HyPerConn";
-      preLayerName = "V1_RELU";
+      groupType = useMomentum and "MomentumConn" or "KernelConn";
+      preLayerName = "V1";
       postLayerName = "LeftError";
-      channelCode = -1;
+      channelCode = 1;
       nxp = dictPatchSize;
       nyp = dictPatchSize;
       shrinkPatches = false;
       numAxonalArbors = 1;
       initFromLastFlag = 0;
       sharedWeights = true;
-      weightInitType = "FileWeight";
-      initWeightsFile = V1DictDir.."/V1ToLeftError_W.pvp";
       strength = 1;
       normalizeMethod = "normalizeL2";
       minL2NormTolerated = 0;
@@ -415,8 +456,14 @@ pv.addGroup(pvParams, "V1ToLeftError",
       writeStep = -1;
       writeCompressedWeights = false;
       writeCompressedCheckpoints = false;
-      plasticityFlag = false;
+      plasticityFlag = true;
+      triggerFlag = true;
+      triggerLayerName = "LeftImage";
+      triggerOffset = 1;
       initialWriteTime = 0.0;
+      dWMax = learningRate;
+      momentumTau = learningMomentumTau;
+      momentumMethod = "viscosity";
       selfFlag = false;
       shmget_flag = false;
       delay = 0;
@@ -426,16 +473,34 @@ pv.addGroup(pvParams, "V1ToLeftError",
    }
 )
 
+--This is by reference, so changing things in connGroup will change things in pvParams
+local v1ConnLeft = pvParams["V1ToLeftError"]
+--These flags are based on external variables
+if(V1DictDir == nil) then
+   v1ConnLeft["weightInitType"] = "UniformRandomWeight"
+   v1ConnLeft["wMinInit"] = -1
+   v1ConnLeft["wMaxInit"] = 1
+   v1ConnLeft["sparseFraction"] = .9;
+else
+   v1ConnLeft["weightInitType"] = "FileWeight"
+   v1ConnLeft["initWeightsFile"] = V1DictDir.."/V1ToToLeftError_W.pvp";
+end
+
 --Copy lefterror connection to righterror connection
 pv.addGroup(pvParams, "V1ToRightError",
    pvParams["V1ToLeftError"],
    {
       postLayerName = "RightError";
       normalizeMethod = "normalizeGroup";
-      initWeightsFile = V1DictDir.."/V1ToRightError_W.pvp";
    }
 )
-pvParams["V1ToRightError"]["normalizeGroupName"] = "V1ToLeftError"
+
+--Add extra overwrite parameters if nessessary
+local v1ConnRight = pvParams["V1ToRightError"]
+if(V1DictDir ~= nil) then
+   v1ConnRight["initWeightsFile"] = V1DictDir.."/V1ToRightError_W.pvp";
+end
+v1ConnRight["normalizeGroupName"] = "V1ToLeftError"
 
 --Recon and transpose conns
 pv.addMultiGroups(pvParams, 
@@ -443,8 +508,8 @@ pv.addMultiGroups(pvParams,
    LeftErrorToV1 = {
       groupType = "TransposeConn";
       preLayerName = "LeftError";
-      postLayerName = "V1_RELU";
-      channelCode = 1;
+      postLayerName = "V1";
+      channelCode = 0;
       originalConnName = "V1ToLeftError";
       selfFlag = false;
       preActivityIsNotRate = false;
@@ -456,6 +521,21 @@ pv.addMultiGroups(pvParams,
       updateGSynFromPostPerspective = true;
       receiveGpu = true;
    };
+
+   V1ToLeftRecon = {
+      groupType = "CloneKernelConn";
+      preLayerName = "V1";
+      postLayerName = "LeftRecon";
+      channelCode = 0;
+      writeStep = -1;
+      originalConnName = "V1ToLeftError";
+      selfFlag = false;
+      delay = 0;
+      preActivityIsNotRate = false;
+      useWindowPost = false;
+      updateGSynFromPostPerspective = false;
+      pvpatchAccumulateType = "convolve";
+   };
 }
 )
 
@@ -466,7 +546,13 @@ pv.addGroup(pvParams, "RightErrorToV1", pvParams["LeftErrorToV1"],
    }
 )
 
+pv.addGroup(pvParams, "V1ToRightRecon", pvParams["V1ToLeftRecon"], 
+   {
+      postLayerName = "RightRecon";
+      originalConnName = "V1ToRightError";
+   }
+)
+
 --Prints out a PetaVision approved parameter file to the console
 pv.printConsole(pvParams)
-
 
