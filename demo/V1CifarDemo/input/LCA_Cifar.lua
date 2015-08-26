c73e162b9b65cbbf5838c6651b971f21b38c11e5
Sheng Lundquist <slundqui007@gmail.com>
2015-08-26 16:25:44 -0600

    Added comments to LCA_Cifar demo

--- demo/V1CifarDemo/input/LCA_Cifar.lua
+++ demo/V1CifarDemo/input/LCA_Cifar.lua
@@ -5,60 +5,53 @@ package.path = package.path .. ";"
 local pv = require "PVModule";
 local subnets = require "PVSubnets";
 
-
 --Parameters
 local nxSize = 32; --Cifar is 32 x 32
 local nySize = 32;
-local xPatchSize = 5;
+local xPatchSize = 5; --Patch size of basis vectors
 local yPatchSize = 5;
-local displayPeriod = 1000;
+local displayPeriod = 1000; --Number of timesteps to find sparse approximation
+
+local basisVectorFile = nil; --nil for initial weights, otherwise, specifies the weights file to load for dictionaries
+local plasticityFlag = true; --Determines if we are learning weights or holding them constant
+local momentumTau = 100; --The momentum parameter. A single weight update will last for momentumTau timesteps.
+local dwMax = .5; --The learning rate
+
 local numImages = 50000; --Total number of images in dataset
 local numEpochs = 1; --Number of times to run through dataset
-local nbatch = 1;
+local nbatch = 1; --Batch size of learning
 
 local cifarInputPath = ""; --TODO
 local outputPath = ""; --TODO
 
-local basisVectorFile = nil; -- nil for initial weights, otherwise, specifies the weights file to load for dictionaries
-local plasticityFlag = true;
-local momentumTau = 100;
-local dwMax = .5;
-
-local numBasisVectors = 150;
-local VThresh = .015;
-local AShift = .015;
-local timeConstantTau = 100;
+local numBasisVectors = 128; --Total number of basis vectors being learned
+local VThresh = .015; --The threshold, or lambda, of the network
+local AShift = .015; --This being equal to VThresh is a soft threshold
+local timeConstantTau = 100; --The integration tau for sparse approximation
 
 
 local params = {};
+--HyPerCol, or the container of the model
 params['column'] = {
   groupType = "HyPerCol";
   nx = nxSize;
   ny = nySize;
   nbatch = nbatch;
-
   randomSeed = 123456890;
-
   startTime = 0;
   stopTime = (numImages * displayPeriod * numEpochs) / nbatch;
-
   outputPath = outputPath;
-
   checkpointWrite = true;
   checkpointWriteDir = outputPath .. "/Checkpoints";
   checkpointWriteStepInterval = (displayPeriod * 100) / nbatch;
-
   writeTimescales = true;
-  
   dt = 1;
   dtAdaptFlag = true;
   dtChangeMin = -0.01;
   dtChangeMax =  0.01;
   dtScaleMin  =  0.001;
   dtMinToleratedTimeScale = 0.0001;
-
   filenamesContainLayerNames = 2;
-
   progressInterval = (10 * displayPeriod) / nbatch;
 };
 
@@ -84,13 +77,16 @@ params['Input'] = {
   normalizeStdDev = false;
 };
 
---Creates a new layer that appends Scaled to the end of inputLayerName that scales the input
+--Creates a new layer that scales the input. This makes the scale of the input
+--match the scale of the basis vectors, as we are normalizing the basis vectors
+--to have a unit norm.
 subnets.addScaleValueConn{
    pvParams       = params;
    inputLayerName = "Input";
    scaleFactor    = 1/math.sqrt(xPatchSize * yPatchSize);
 };
 
+--Connection table input to addLCASubnet
 connTable = {
    nxp              = xPatchSize;
    nyp              = yPatchSize;
@@ -103,6 +99,7 @@ connTable = {
    triggerLayerName = "Input";
 };
 
+--Change parameters as needed based on if a basisVectorFile was specified
 if(basisVectorFile == nil) then
    connTable["weightInitType"] = "UniformRandomWeight"
    connTable["wMinInit"] = -1
@@ -113,6 +110,7 @@ else
    connTable["initWeightsFile"] = basisVectorFile;
 end
 
+--Retrieve an LCA module
 subnets.addLCASubnet{
    pvParams                      = params;
    lcaLayerName                  = "V1";
@@ -135,4 +133,5 @@ subnets.addLCASubnet{
    triggerLayerName = "Input";
 };
 
+--Print the OpenPV friendly parameter file to stdout
 pv.printConsole(params);
