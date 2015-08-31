616345756bd84109acfd8df01e7e0b13c0ee361a
Sheng Lundquist <slundqui007@gmail.com>
2015-08-31 18:42:49 +0000

    Added conv params and run script for max pooling weights

---
+++ projects/DCA/convRunMAX.sh
@@ -0,0 +1,3 @@
+~/workspace/PVSystemTests/BasicSystemTest/Release/BasicSystemTest -p ~/workspace/projects/DCA/input/MaxPool/convOnS1.params -t 8
+~/workspace/PVSystemTests/BasicSystemTest/Release/BasicSystemTest -p ~/workspace/projects/DCA/input/MaxPool/convOnS2.params -t 8
+~/workspace/PVSystemTests/BasicSystemTest/Release/BasicSystemTest -p ~/workspace/projects/DCA/input/MaxPool/convOnS3.params -t 8
