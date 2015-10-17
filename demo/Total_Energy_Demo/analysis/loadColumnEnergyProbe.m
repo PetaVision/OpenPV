1f3bbe83a62e3a2f875b86f26dabc341143e2700
Pete Schultz <peteschultz@users.noreply.github.com>
2015-10-16 18:28:37 -0600

    m-file for reading the output of an AbstractNormProbe (ignores the specifics of the norm description)

--- demo/Total_Energy_Demo/analysis/loadColumnEnergyProbe.m
+++ demo/Total_Energy_Demo/analysis/loadColumnEnergyProbe.m
@@ -1,12 +1,12 @@
 function [E,timestamps] = loadColumnEnergyProbe(outputFile)
-% [E,timestamps] = loadColumnEnergyProbe(outputDir)
+% [E,timestamps] = loadColumnEnergyProbe(outputFile)
 %
 % outputDir is a string giving the path to the file containing the output of a column energy probe.
 %
 % E is an array.  The number of columns is the batchwidth and the number of rows is the number
 % of timestamps.
 %
-% timestamps is a column vector with the same number of rows as A, that gives the timestamps.
+% timestamps is a column vector with the same number of rows as E, that gives the timestamps.
 
 [~,w] = system(['awk -F '','' ''{print $2" "$3" "$4}'' ' outputFile]);
 A = sscanf(w,"%f %f %f\n");
