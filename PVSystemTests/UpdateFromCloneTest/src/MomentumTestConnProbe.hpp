a4f28f8f0658d1707f82a2db24876f9b103d6d84
Sheng Lundquist <slundqui007@gmail.com>
2015-12-17 12:43:33 -0700

    Added momentum test to UpdateFromCloneTest

---
+++ PVSystemTests/UpdateFromCloneTest/src/MomentumTestConnProbe.hpp
@@ -0,0 +1,46 @@
+/*
+ * MomentumTestConnProbe.hpp
+ *
+ */
+
+#ifndef MOMENTUMTESTCONNPROBE_HPP_
+#define MOMENTUMTESTCONNPROBE_HPP_
+
+#include <io/BaseHyPerConnProbe.hpp>
+
+namespace PV {
+
+class MomentumTestConnProbe: public BaseHyPerConnProbe {
+
+// Methods
+public:
+   MomentumTestConnProbe(const char * probename, HyPerCol * hc);
+   virtual ~MomentumTestConnProbe();
+   virtual int outputState(double timed);
+
+protected:
+   MomentumTestConnProbe(); // Default constructor, can only be called by derived classes
+   
+   /**
+    * MomentumTestConnProbe::initNumValues() sets numValues to -1, indicating that getValues() and getValue() should not be used.
+    */
+   int initNumValues();
+   
+   /**
+    * MomentumTestConnProbe::needRecalc() always returns false since calcValues should not be called.
+    */
+   bool needRecalc(double timevalue) { return false; }
+   
+   /**
+    * MomentumTestConnProbe::calcValues() always fails since this probe does not use getValues() or getValue().
+    */
+   int calcValues(double timevalue) { return PV_FAILURE; }
+
+private:
+   int initialize_base();
+
+};
+
+}  // end of namespace PV block
+
+#endif /* BASECONNECTIONPROBE_HPP_ */
