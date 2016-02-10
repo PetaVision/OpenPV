70fd1b3409c4c65671fa76d39f2e280f3f90b967
Sheng Lundquist <slundqui007@gmail.com>
2016-02-10 14:22:39 -0800

    Added segmentify layer that remaps an input layer based on segments. Added a system test for segmentify layer

---
+++ PVSystemTests/SegmentTest/src/AssertZerosProbe.hpp
@@ -0,0 +1,28 @@
+/*
+ * AssertZerosProbe.hpp
+ * Author: slundquist
+ */
+
+#ifndef ASSERTZEROSPROBE_HPP_ 
+#define ASSERTZEROSPROBE_HPP_
+#include <io/StatsProbe.hpp>
+
+namespace PV{
+
+class AssertZerosProbe : public PV::StatsProbe{
+public:
+   AssertZerosProbe(const char * probeName, HyPerCol * hc);
+
+   virtual int outputState(double timed);
+
+protected:
+   int initAssertZerosProbe(const char * probeName, HyPerCol * hc);
+   void ioParam_buffer(enum ParamsIOFlag ioFlag);
+
+private:
+   int initAssertZerosProbe_base();
+
+};
+
+}
+#endif 
--- PVSystemTests/SegmentTest/src/CustomGroupHandler.cpp
+++ PVSystemTests/SegmentTest/src/CustomGroupHandler.cpp
@@ -6,7 +6,9 @@
  */
 
 #include "CustomGroupHandler.hpp"
+#include "AssertZerosProbe.hpp"
 #include "SegmentTestLayer.hpp"
+#include "SegmentifyTest.hpp"
 
 #include <stddef.h>
 #include <stdio.h>
@@ -24,7 +26,13 @@ ParamGroupType CustomGroupHandler::getGroupType(char const * keyword) {
    if (keyword==NULL) {
       return result;
    }
+   else if (!strcmp(keyword, "AssertZerosProbe")) {
+      result = ProbeGroupType;
+   }
    else if (!strcmp(keyword, "SegmentTestLayer")) {
+      result = LayerGroupType;
+   }
+   else if (!strcmp(keyword, "SegmentifyTest")) {
       result = LayerGroupType;
    }
    else {
@@ -40,6 +48,9 @@ HyPerLayer * CustomGroupHandler::createLayer(char const * keyword, char const * 
    if( !strcmp(keyword, "SegmentTestLayer") ) {
       addedLayer = new SegmentTestLayer(name, hc);
    }
+   if( !strcmp(keyword, "SegmentifyTest") ) {
+      addedLayer = new SegmentifyTest(name, hc);
+   }
 
    if( !addedLayer ) {
       fprintf(stderr, "Group \"%s\": Unable to create layer\n", name);
@@ -47,5 +58,18 @@ HyPerLayer * CustomGroupHandler::createLayer(char const * keyword, char const * 
    }
    return addedLayer;
 }
+
+BaseProbe * CustomGroupHandler::createProbe(char const * keyword, char const * name, HyPerCol * hc) {
+   int status;
+   BaseProbe * addedProbe = NULL;
+   if( !strcmp(keyword, "AssertZerosProbe") ) {
+      addedProbe = new AssertZerosProbe(name, hc);
+   }
+   if( !addedProbe) {
+      fprintf(stderr, "Group \"%s\": Unable to create probe\n", name);
+   }
+   return addedProbe;
+}
+
 
 } /* namespace PV */
