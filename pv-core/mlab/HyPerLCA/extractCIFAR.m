e1da44bd599cdc264094e13ede8b7825c34b0a3c
wshainin <wshainin@gmail.com>
2016-03-18 15:58:03 -0600

    Fixed LCACIFAR scripts
    Script now correctly creates the text file with paths to the images.

--- pv-core/mlab/HyPerLCA/extractCIFAR.m
+++ pv-core/mlab/HyPerLCA/extractCIFAR.m
@@ -4,5 +4,4 @@ extractImagesOctave('data_batch_3.mat',3);
 extractImagesOctave('data_batch_4.mat',4);
 extractImagesOctave('data_batch_5.mat',5);
 extractImagesOctave('test_batch.mat',0);
-system('cat *randorderatxt > mixed_cifar.txt');
 exit
