6d7e0cb96ed5163fcb6a7bff6aebd4c4b283a4d8
Jeff Bowles <jbowles@riskybacon.com>
2016-03-25 13:29:18 -0600

    Adding malloc, assert and logging macros to reduce boilerplate

--- pv-core/src/utils/PVAssert.hpp
+++ pv-core/src/utils/PVAssert.hpp
@@ -1,12 +1,35 @@
 #ifndef _pv_assert_h
 #define _pv_assert_h
 
+/**
+ * Replacement for the assert() macro. By default, assert() is compiled out in non-release builds. We
+ * can stop that behavior by redefining Release compiler arguments, but this introduces compiler portability
+ * issues.
+ *
+ * pvAssert() and pvAssertMessage() add additional functionality to assert()
+ *
+ * 1) The filename and line number are printing
+ * 2) A stack backtrace is provided
+ *
+ * This macro is intended to be used in C++ programs, not in C programs.
+ *
+ * Also, by providing a PetaVision implementation of this macro, an opportunity to handle sending assert failures
+ * to other MPI ranks becomes possible.
+ */
 #include <execinfo.h>
 #include <cxxabi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "utils/pv_log.h"
 
+/**
+ * Works just like assert(), except it is not compiled out in Release versions and provides a stack trace and
+ * prints out the failed condition and the filename:line number of the failure
+ */
 #define pvAssert(c) if (!(c)) { pv_assert_failed(__FILE__, __LINE__, #c); }
+/**
+ * Like pvAssert(). Adds an additional error message to the output.
+ */
 #define pvAssertMessage(c, fmt, ...) if (!(c)) { pv_assert_failed_message(__FILE__, __LINE__, #c, fmt, ##__VA_ARGS__); }
 
 void pv_assert_failed(const char *file, int line, const char *condition);
--- pv-core/src/utils/pv_log.h
+++ pv-core/src/utils/pv_log.h
@@ -1,6 +1,23 @@
 #ifndef _pv_log_h
 #define _pv_log_h
 
+/**
+ * logging macros and functions
+ *
+ * Provides a standardized method for logging error, debug and information messages.
+ *
+ * logDebug(), logError() and logInfo() all work the same as printf(). 
+ *
+ * logDebug() and logError() will prepend DEBUG: and ERROR: plus the file name and line number
+ * of the call.
+ *
+ * logInfo() works just like printf. Nothing is prepended.
+ *
+ * logDebug() calls are compiled out in a Release build. logError() and logInfo() calls are not
+ * compiled out.
+ *
+ * These macros provide an opportunity to handle sending debug, error and info messages to other MPI ranks
+ */
 #ifdef DEBUG
 #define LOG_DEBUG_OUTPUT 1
 #endif
@@ -20,8 +37,10 @@ extern "C" {
 
 void pv_log_error(const char *file, int line, const char *fmt, ...);
 void pv_log_debug(const char *file, int line, const char *fmt, ...);
+void pv_log_info(const char *file, int line, const char *fmt, ...);
 
-// Non-varargs versions
+// Non-varargs versions, used internally, but exposed just in case somebody
+// some other program wants to use them
 void vpv_log_error(const char *file, int line, const char *fmt, va_list args);
 void vpv_log_debug(const char *file, int line, const char *fmt, va_list args);
 
