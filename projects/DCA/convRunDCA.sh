560634e953a0ed85ecb334c901601a68c3c070a3
William Shainin <wshainin@users.noreply.github.com>
2016-05-09 16:33:23 -0600

    [WIP] Cleaning of OpenPV repository (#52)
    * Moved contents of pv-core into OpenPV root
    
    And removed psycho, input, scripts, tests, and misc unused files.
    
    * Updated .gitignore with pv-core/.gitignore
    
    * Remove auxlib and move contents to Projects repo
    
    * Deleted projects directory after moving elsewhere
    
    -renamed demo -> demos
    -moved HeatMapLocalization to demos
    -removed projects/AlexNet (redundant code)
    -removed projects/MLPLCA (redundant code)
    -moved all other projects to separate repository (PetaVision/Projects)
    
    * Modifying CMake for more shallow directory structure
    
    * Tests are now a CMake option, moved PVSystemTests to tests
    
    * Tests are now a CMake option, moved PVSystemTests to tests
    
    * Removed much of mlab
    
    * Moved scripts out of OpenPV
    
    * Removed outdated PASCAL demo and moved CIFAR demo
    
    * Removed web demo
    
    This should be VCed elsewhere until it is truly a demo that a third
    party could reasonably figure out.
    
    * Moved Total_Energy_Demo to tutorials/Total_Energy
    
    * Removed outdated docs and demos
    
    * Removing old references from sample-subdirectories

--- projects/DCA/convRunDCA.sh
+++
@@ -1,3 +0,0 @@
-~/workspace/PVSystemTests/BasicSystemTest/Release/BasicSystemTest -p ~/workspace/projects/DCA/input/DCA/convOnS1.params -t 8
-~/workspace/PVSystemTests/BasicSystemTest/Release/BasicSystemTest -p ~/workspace/projects/DCA/input/DCA/convOnS2.params -t 8
-~/workspace/PVSystemTests/BasicSystemTest/Release/BasicSystemTest -p ~/workspace/projects/DCA/input/DCA/convOnS3.params -t 8
--- pv-core/mlab/MNIST/plotAmoeba2D3.m
+++
@@ -1,8 +0,0 @@
-function plotAmoeba2D3(amoeba_image, amoeba_struct, trial_ndx)
-
-plotAmoebaPTB2(amoeba_image, amoeba_struct, trial_ndx, true);
-plotAmoebaPTB2(amoeba_image, amoeba_struct, trial_ndx, false);
-plotAmoebaMask(amoeba_image, amoeba_struct, trial_ndx);
-plotAmoebaPTB2(amoeba_image, amoeba_struct, trial_ndx, true, true);
-
-
