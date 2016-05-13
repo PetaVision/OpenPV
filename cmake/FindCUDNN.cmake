b4b01904cd7df85bdb00f05e5bed9be705abb8eb
wshainin <wshainin@gmail.com>
2016-05-13 13:40:14 -0600

    FindCUDNN shows respect for CUDNN_PATH

--- cmake/FindCUDNN.cmake
+++ cmake/FindCUDNN.cmake
@@ -8,66 +8,56 @@
 #
 # A CUDNN_PATH variable can be set prior find_package(CUDNN)
 # to specify exactly where to search
+#
+# Call FindCUDA before using FindCUDNN
+#
+# That will set the CUDA_FOUND variable and expand the search
+# for cuDNN to the cuda directories
 
 
 include(LibFindMacros)
 
 foreach(PATH_HINT ${CUDNN_PATH})
-  set(CUDNN_HEADER_SEARCH_PATHS ${CUDNN_HEADER_SEARCH_PATHS};${CUDNN_PATH}/include)
-  set(CUDNN_LIBRARY_SEARCH_PATHS ${CUDNN_LIBRARY_SEARCH_PATHS};${CUDNN_PATH}/lib)
-  set(CUDNN_LIBRARY_SEARCH_PATHS ${CUDNN_LIBRARY_SEARCH_PATHS};${CUDNN_PATH}/lib64)
+  list(APPEND CUDNN_HEADER_SEARCH_PATHS ${CUDNN_PATH}/include)
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS ${CUDNN_PATH}/lib64)
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS ${CUDNN_PATH}/lib)
 
   if(CUDA_FOUND)
-    set(CUDNN_HEADER_SEARCH_PATHS ${CUDNN_HEADER_SEARCH_PATHS};${CUDNN_PATH}/include)
-    set(CUDNN_HEADER_SEARCH_PATHS ${CUDNN_HEADER_SEARCH_PATH};${CUDA_TOOLKIT_ROOT_DIR}/include;${CUDA_TOOLKIT_ROOT_DIR}/targets/x86_64-linux/include)
-    set(CUDNN_LIBRARY_SEARCH_PATHS ${CUDNN_LIBRARY_SEARCH_PATH};${CUDA_TOOLKIT_ROOT_DIR}/lib64;${CUDA_TOOLKIT_ROOT_DIR}/targets/x86_64-linux/lib)
+    list(APPEND CUDNN_HEADER_SEARCH_PATHS ${CUDNN_PATH}/include)
+    list(APPEND CUDNN_HEADER_SEARCH_PATHS ${CUDA_TOOLKIT_ROOT_DIR}/include)
+    list(APPEND CUDNN_HEADER_SEARCH_PATHS ${CUDA_TOOLKIT_ROOT_DIR}/targets/x86_64-linux/include)
+    list(APPEND CUDNN_LIBRARY_SEARCH_PATHS ${CUDA_TOOLKIT_ROOT_DIR}/lib64)
+    list(APPEND CUDNN_LIBRARY_SEARCH_PATHS ${CUDA_TOOLKIT_ROOT_DIR}/targets/x86_64-linux/lib)
   endif()
 endforeach()
 
-foreach(PATH_HINT ${CUDNN_PATH})
-  set(CUDNN_HEADER_SEARCH_PATHS ${CUDNN_HEADER_SEARCH_PATHS};${CUDNN_PATH}/include)
-  set(CUDNN_LIBRARY_SEARCH_PATHS ${CUDNN_LIBRARY_SEARCH_PATHS};${CUDNN_PATH}/lib)
-  set(CUDNN_LIBRARY_SEARCH_PATHS ${CUDNN_LIBRARY_SEARCH_PATHS};${CUDNN_PATH}/lib64)
-endforeach()
-
 if (APPLE)
-  set(CUDNN_HEADER_SEARCH_PATHS
-    ${CUDNN_HEADER_SEARCH_PATHS}
-    /usr/local/cudnn-7.0/include
-    /usr/local/cudnn-6.5/include
-    )
+  list(APPEND CUDNN_HEADER_SEARCH_PATHS /usr/local/cudnn-7.0/include)
+  list(APPEND CUDNN_HEADER_SEARCH_PATHS /usr/local/cudnn-6.5/include)
 
-  set(CUDNN_LIBRARY_SEARCH_PATHS
-    ${CUDNN_LIBRARY_SEARCH_PATHS}
-    /usr/local/cudnn-7.0/lib
-    /usr/local/cudnn-7.0/lib64
-    /usr/local/cudnn-6.5/lib
-    /usr/local/cudnn-6.5/lib64
-    )
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS /usr/local/cudnn-7.0/lib)
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS /usr/local/cudnn-7.0/lib64)
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS /usr/local/cudnn-6.5/lib)
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS /usr/local/cudnn-6.5/lib64)
 
 else()
-  set(CUDNN_HEADER_SEARCH_PATHS
-    ${CUDNN_HEADER_SEARCH_PATHS}
-    /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v4/include
-    /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v3/include
-    /nh/compneuro/Data/cuDNN/cudnn-6.5-linux-x64-R2-rc1/include
-    )
-  set(CUDNN_LIBRARY_SEARCH_PATHS
-    ${CUDNN_LIBRARY_SEARCH_PATHS}
-    /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v4/lib
-    /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v4/lib64
-    /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v3/lib
-    /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v3/lib64
-    /nh/compneuro/Data/cuDNN/cudnn-6.5-linux-x64-R2-rc1/lib
-    /nh/compneuro/Data/cuDNN/cudnn-6.5-linux-x64-R2-rc1/lib64
-    )
+  list(APPEND CUDNN_HEADER_SEARCH_PATHS /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v4/include)
+  list(APPEND CUDNN_HEADER_SEARCH_PATHS /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v3/include)
+  list(APPEND CUDNN_HEADER_SEARCH_PATHS /nh/compneuro/Data/cuDNN/cudnn-6.5-linux-x64-R2-rc1/include)
+
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v4/lib)
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v4/lib64)
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v3/lib)
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS /nh/compneuro/Data/cuDNN/cudnn-7.0-linux-x64-v3/lib64)
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS /nh/compneuro/Data/cuDNN/cudnn-6.5-linux-x64-R2-rc1/lib)
+  list(APPEND CUDNN_LIBRARY_SEARCH_PATHS /nh/compneuro/Data/cuDNN/cudnn-6.5-linux-x64-R2-rc1/lib64)
 endif()
 
 find_path(CUDNN_INCLUDE_DIR
   NAMES cudnn.h
   PATHS ${CUDNN_HEADER_SEARCH_PATHS}
   DOC "cuDNN include"
-  )
+)
 
 # Find cudnn library
 find_library(CUDNN_LIBRARY cudnn cudnn_static ${CUDNN_LIBRARY_SEARCH_PATHS} DOC "cuDNN library")
