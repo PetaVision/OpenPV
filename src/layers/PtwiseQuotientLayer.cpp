b872915f450bdc94be202a2a078f19c57fea348a
Garrett T. Kenyon <garkenyon@msn.com>
2016-07-05 15:58:01 -0600

    added PtwiseQuotientLayer (based on PtwiseProductLayer)

---
+++ src/layers/PtwiseQuotientLayer.cpp
@@ -0,0 +1,65 @@
+/*
+ * PtwiseQuotientLayer.cpp
+ *
+ * The output V is the pointwise product of GSynExc and GSynInh
+ *
+ * "Exc" and "Inh" are really misnomers for this class, but the
+ * terminology is inherited from the base class.
+ *
+ *  Created on: Apr 25, 2011
+ *      Author: peteschultz
+ */
+
+#include "PtwiseQuotientLayer.hpp"
+
+namespace PV {
+
+PtwiseQuotientLayer::PtwiseQuotientLayer() {
+   initialize_base();
+}
+
+PtwiseQuotientLayer::PtwiseQuotientLayer(const char * name, HyPerCol * hc) {
+   initialize_base();
+   initialize(name, hc);
+}  // end PtwiseQuotientLayer::PtwiseQuotientLayer(const char *, HyPerCol *)
+
+PtwiseQuotientLayer::~PtwiseQuotientLayer() {
+}
+
+int PtwiseQuotientLayer::initialize_base() {
+   numChannels = 2;
+   return PV_SUCCESS;
+}
+
+int PtwiseQuotientLayer::initialize(const char * name, HyPerCol * hc) {
+   return ANNLayer::initialize(name, hc);
+}
+
+int PtwiseQuotientLayer::allocateDataStructures() {
+   int status = ANNLayer::allocateDataStructures();
+   assert(numChannels>=2);
+   return status;
+}
+
+int PtwiseQuotientLayer::updateState(double timef, double dt) {
+   int status;
+   status = doUpdateState(timef, dt, getLayerLoc(), getCLayer()->activity->data, getV(), getNumChannels(), GSyn[0]);
+   return status;
+}
+
+int PtwiseQuotientLayer::doUpdateState(double timef, double dt, const PVLayerLoc * loc, pvdata_t * A, pvdata_t * V, int num_channels, pvdata_t * gSynHead) {
+   int nx = loc->nx;
+   int ny = loc->ny;
+   int nf = loc->nf;
+   int num_neurons = nx*ny*nf;
+   int nbatch = loc->nbatch;
+   updateV_PtwiseQuotientLayer(nbatch, num_neurons, V, gSynHead);
+   setActivity_HyPerLayer(nbatch, num_neurons, A, V, nx, ny, nf, loc->halo.lt, loc->halo.rt, loc->halo.dn, loc->halo.up);
+   return PV_SUCCESS;
+}
+
+BaseObject * createPtwiseQuotientLayer(char const * name, HyPerCol * hc) {
+   return hc ? new PtwiseQuotientLayer(name, hc) : NULL;
+}
+
+}  // end namespace PV
