c9e09b94f80a6c4fe8ca811410d9da03809094c0
Pete Schultz <peteschultz@users.noreply.github.com>
2016-07-18 15:46:07 -0600

    Adds ConstantLayerTest

---
+++ tests/ConstantLayerTest/src/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * ConstantLayerTest.cpp
+ *
+ */
+
+
+#include <columns/buildandrun.hpp>
+
+int main(int argc, char * argv[]) {
+   int status = buildandrun(argc, argv);
+   return status==PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
+}
