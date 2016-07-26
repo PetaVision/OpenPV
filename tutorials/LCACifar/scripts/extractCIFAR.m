a76a8a918c7f9e7aba1e152e6a982178a2b47be0
William Shainin <wshainin@users.noreply.github.com>
2016-07-26 15:06:50 -0600

    Fixed broken LCA CIFAR tutorial and tools for analyzing it (#97)
    * Fixed LCA Tutorial and tools for analyzing it

--- tutorials/LCACifar/scripts/extractCIFAR.m
+++ tutorials/LCACifar/scripts/extractCIFAR.m
@@ -1,37 +1,13 @@
-function extractCIFAR(batch_name, cnt)
+% This script will unpack the MATLAB version of the CIFAR dataset. Assuming
+% the dataset was extracted to OpenPV/tutorials/LCACifar/cifar-10-batches-mat,
+% running 'octave extractCIFAR.m' will unpack the images into that directory.
 
-%
-%   ___     ___  __        __  ___     __     ___       __                   __   ___  __  
-%  |__  \_/  |  |__)  /\  /  `  |     /  ` | |__   /\  |__)    |  |\/|  /\  / _` |__  /__` 
-%  |___ / \  |  |  \ /~~\ \__,  |     \__, | |    /~~\ |  \    |  |  | /~~\ \__> |___ .__/ 
-%                                                                                          
-%
-% 1) Download CIFAR dataset from http://www.cs.toronto.edu/~kriz/cifar.html
-%	Make sure to download the Matlab version
-%	Unzip the .tar file
-%
-% 2) Extract the CIFAR dataset:
-%	Navigate to the location of your unzipped cifar.mat files
-%	Open octave 
-%
-%	> addpath('~/path/to/OpenPV/tutorials/LCACifar/scripts/')
-%	> extractCIFAR(batch_name, cnt)
-%
-%	 batch_name:	mat file given as string, e.g 'data_batch_1.mat'
-% 	 cnt:		most significant digit in unique file number
-%		        accounts for having individual .mat files
-%		        cnt = 3 will result in numbers 3xxxx
-%
-%	*NOTE: you can also navigate to ~/path/to/PetaVision/mlab/HyPerLCA
-%	       and avoid needing to use addpath('')
-%
-% 	You'll need to do this for each batch.mat file or you can make a 
-% 	wee lil script to loop over all the files yourself.
-%
-% 3) Go forth and prosper	
+function extractImagesOctave(batch_name, cnt, append_mixed=true)
+% batch_name: mat file given as string, e.g 'data_batch_1.mat'
+% cnt:        most significant digit in unique file number. Accounts for having 
+%             individual .mat files. cnt = 3 will result in numbers 3xxxx
 
-load(batch_name)
-
+ load(batch_name)
   if ~isempty(strfind(batch_name, 'data_batch_'))
     base_batch_name_start = strfind(batch_name, 'data_batch_');
   elseif ~isempty(strfind(batch_name, 'test_batch'))
@@ -41,11 +17,11 @@ load(batch_name)
   endif
   base_batch_name_end = strfind(batch_name, '.mat')-1;
   base_batch_name = batch_name(base_batch_name_start:base_batch_name_end);
-  output_dir = ['CIFAR_',base_batch_name];
+  parent_path = cd(cd(fileparts(batch_name)));
+  output_dir = [parent_path, '/', base_batch_name];
   mkdir(output_dir);
-	
-	% get dimension of data extracted from .mat file
-	% xl = number of images, yl = size of image
+				% get dimension of data extracted from .mat file
+				% xl = number of images, yl = size of image
   [xl,yl] = size(data);	
 				% initialize image matrix to CIFAR image dimensions
   xdim = 32;
@@ -63,11 +39,18 @@ load(batch_name)
 				% create randorder file for PV. Order within .mat file is random already
 				% appends new lines to the end of the file
   randorder_pathname = [output_dir,'/',base_batch_name,'_randorder.txt'];
-  fid = fopen(randorder_pathname, 'w');
-  if fid <=0 
-    error(["fid = ", num2str(fid), ": randorder_pathname = ", randorder_pathname]);
+  mixed_file_pathname = [parent_path, '/mixed_cifar.txt'];
+  
+  batch_file = fopen(randorder_pathname, 'w');
+  if batch_file <=0 
+    error(["batch_file = ", num2str(batch_file), ": randorder_pathname = ", randorder_pathname]);
   endif
   
+  mixed_file = fopen(mixed_file_pathname, 'a');
+  if mixed_file <=0 
+    error(["mixed_file = ", num2str(mixed_file), ": randorder_pathname = ", randorder_pathname]);
+  endif
+
 				% loop through all elements in .mat file
   for i=1 : xl
 				% get image in 3 1D vectors
@@ -86,11 +69,32 @@ load(batch_name)
     num = sprintf("%05d",i+cnt*10000);
     CIFAR_name = strcat(output_dir,"/", int2str(labels(i)),'/CIFAR_',num,'.png');
 				% save filename and path to randorder file
-    fprintf(fid,"%s\n",CIFAR_name);
+    fprintf(batch_file,"%s\n",CIFAR_name);
+    if append_mixed
+        fprintf(mixed_file,"%s\n",CIFAR_name);
+    endif
 				% convert to uint8 and write image
     im1 = uint8(im);
     imwrite(im1, CIFAR_name);
   endfor
-  fclose(fid);		
-
+  fclose(batch_file);		
+  fclose(mixed_file);		
+  copyfile(randorder_pathname, parent_path);
+  printf("Finished %s\n",base_batch_name);
 endfunction
+
+cifarPath = cd(cd("../cifar-10-batches-mat/"));
+mixed_file_pathname = [cifarPath, '/mixed_cifar.txt'];
+
+if exist(mixed_file_pathname, "file") == 2
+    printf("WARNING: %s exists and is being deleted to prevent duplicate entries.\n", mixed_file_pathname);
+    unlink(mixed_file_pathname);
+endif
+
+printf("Extracting Images..");
+extractImagesOctave([cifarPath, '/data_batch_1.mat'], 1);
+extractImagesOctave([cifarPath, '/data_batch_2.mat'], 2);
+extractImagesOctave([cifarPath, '/data_batch_3.mat'], 3);
+extractImagesOctave([cifarPath, '/data_batch_4.mat'], 4);
+extractImagesOctave([cifarPath, '/data_batch_5.mat'], 5);
+extractImagesOctave([cifarPath, '/test_batch.mat'], 0, 0);
