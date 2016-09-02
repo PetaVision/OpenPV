a6eb831a04573243efd4f4b2ab261a102a03b9ec
Pete Schultz <peteschultz@users.noreply.github.com>
2016-09-02 13:49:59 -0600

    Flushes before "Hit enter to begin" prompt

--- src/columns/HyPerCol.cpp
+++ src/columns/HyPerCol.cpp
@@ -234,6 +234,7 @@ int HyPerCol::initialize(const char * name, PV_Init* initObj)
 #ifdef PVP_DEBUG
    if (mPVInitObj->getRequireReturnFlag()) {
       if( rank == 0 ) {
+         fflush(stdout);
          printf("Hit enter to begin! ");
          fflush(stdout);
          int charhit = -1;
