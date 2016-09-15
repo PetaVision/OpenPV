d4b497dd1c3c6da6aaa4d301351367005b182c7d
austinthresher <austin@dangermomentum.com>
2016-09-15 16:06:40 -0600

    Added BufferSlicer::scatter test for extended buffers.

--- tests/BufferSlicerTest/src/main.cpp
+++ tests/BufferSlicerTest/src/main.cpp
@@ -26,6 +26,7 @@ void testScatterRestricted(int argc, char** argv) {
    unsigned int sliceX = 4 / comm.numCommColumns();
    unsigned int sliceY = 4 / comm.numCommRows();
 
+   // Send / receive the test data, depending on what rank we are
    vector<float> result;
    if (rank == 0) {
       vector<float> testData = {
@@ -44,9 +45,9 @@ void testScatterRestricted(int argc, char** argv) {
       slicer.scatter(recv, sliceX, sliceY);
       result = recv.asVector();
    }
-
    pvInfo() << "Scatter complete on rank " << rank << ".\n";
 
+   // Check to make sure the chunk of data we received is correct
    if (comm.commSize() == 1) {
       pvErrorIf(result.size() != 16,
          "Failed. Expected 16 values, found %d.\n", result.size());
@@ -70,6 +71,7 @@ void testScatterRestricted(int argc, char** argv) {
             0, 0, 1, 1
          };
       for (size_t i = 0; i < result.size(); ++i) {
+         // We expect 0s and 1s on rank 0, 2s and 3s on rank 1
          pvErrorIf(result.at(i) != rank * 2 + expected.at(i),
             "Failed. Expected to find %d, found %d instead.\n",
                rank * 2 + (int)expected.at(i), (int)result.at(i));
@@ -82,6 +84,145 @@ void testScatterRestricted(int argc, char** argv) {
          pvErrorIf(result.at(i) != rank,
             "Failed. Expected to find %d, found %d instead.\n",
                rank, (int)result.at(i));
+      }
+   }
+   else {
+      pvError() << "Failed. Must test using 1, 2, or 4 MPI processes.\n";
+   }
+}
+
+// BufferSlicer::scatter(Buffer &buffer, uint sliceStrideX, uint sliceStrideY)
+void testScatterExtended(int argc, char** argv) {
+   PV_Arguments *args = new PV_Arguments(argc, argv, false);
+   Communicator comm(args); 
+   BufferSlicer slicer(comm);
+   int rank = comm.commRank();
+
+   pvInfo() << "Setup complete on rank " << rank << ". Running test.\n";
+
+   unsigned int sliceX = 4 / comm.numCommColumns();
+   unsigned int sliceY = 4 / comm.numCommRows();
+
+   // Send / receive the test data, depending on what rank we are
+   vector<float> result;
+   if (rank == 0) {
+      vector<float> testData = {
+            9, 9, 9, 9, 9, 9,
+            9, 0, 0, 1, 1, 9,
+            9, 0, 0, 1, 1, 9,
+            9, 2, 2, 3, 3, 9,
+            9, 2, 2, 3, 3, 9,
+            9, 9, 9, 9, 9, 9
+         };
+
+      Buffer send(testData, 6, 6, 1); 
+      slicer.scatter(send, sliceX, sliceY);
+      result = send.asVector();
+   }
+   else {
+      // We have a 1 element margin on each side
+      Buffer recv(sliceX + 2, sliceY + 2, 1);
+      slicer.scatter(recv, sliceX, sliceY);
+      result = recv.asVector();
+   }
+   pvInfo() << "Scatter complete on rank " << rank << ".\n";
+
+   // Check to make sure the chunk of data we received is correct
+   if (comm.commSize() == 1) {
+      pvErrorIf(result.size() != 6 * 6,
+         "Failed. Expected 36 values, found %d.\n", result.size());
+      vector<float> expected = {
+            9, 9, 9, 9, 9, 9,
+            9, 0, 0, 1, 1, 9,
+            9, 0, 0, 1, 1, 9,
+            9, 2, 2, 3, 3, 9,
+            9, 2, 2, 3, 3, 9,
+            9, 9, 9, 9, 9, 9
+          };
+      for (size_t i = 0; i < result.size(); ++i) {
+         pvErrorIf(result.at(i) != expected.at(i),
+            "Failed. Expected to find %d, found %d instead.\n",
+               (int)expected.at(i), (int)result.at(i));
+      }
+   }
+   else if (comm.commSize() == 2) {
+      pvErrorIf(result.size() != 4 * 6,
+         "Failed. Expected 24 values, found %d.\n", result.size());
+
+      vector<float> expected; 
+      switch (rank) {
+         case 0:
+            expected = {
+               9, 9, 9, 9, 9, 9,
+               9, 0, 0, 1, 1, 9,
+               9, 0, 0, 1, 1, 9,
+               9, 2, 2, 3, 3, 9
+             };
+            break;
+         case 1:
+            expected = {
+               9, 0, 0, 1, 1, 9,
+               9, 2, 2, 3, 3, 9,
+               9, 2, 2, 3, 3, 9,
+               9, 9, 9, 9, 9, 9
+            };
+            break;
+         default:
+            pvError() << "Invalid rank.\n";
+      }
+
+      for (size_t i = 0; i < result.size(); ++i) {
+         pvErrorIf(result.at(i) != expected.at(i),
+            "Failed. Expected to find %d, found %d instead.\n",
+               (int)expected.at(i), (int)result.at(i));
+      }
+   }
+   else if (comm.commSize() == 4) {
+      pvErrorIf(result.size() != 4 * 4,
+         "Failed. Expected 16 values, found %d.\n", result.size());
+
+      vector<float> expected; 
+      switch (rank) {
+         case 0:
+            expected = {
+               9, 9, 9, 9,
+               9, 0, 0, 1,
+               9, 0, 0, 1,
+               9, 2, 2, 3
+             };
+            break;
+         case 1:
+            expected = {
+               9, 9, 9, 9,
+               0, 1, 1, 9,
+               0, 1, 1, 9,
+               2, 3, 3, 9 
+            };
+            break;
+         case 2:
+            expected = {
+               9, 0, 0, 1,
+               9, 2, 2, 3,
+               9, 2, 2, 3,
+               9, 9, 9, 9
+            };
+            break;
+         case 3:
+            expected = {
+               0, 1, 1, 9,
+               2, 3, 3, 9,
+               2, 3, 3, 9,
+               9, 9, 9, 9
+            };
+            break;
+          default:
+            pvError() << "Invalid rank.\n";
+      }
+
+      for (size_t i = 0; i < result.size(); ++i) {
+         pvErrorIf(result.at(i) != expected.at(i),
+            "Failed. Expected to find %d, found %d instead.\n",
+               (int)expected.at(i), (int)result.at(i));
       }
    }
    else {
@@ -111,8 +252,13 @@ int main(int argc, char** argv) {
            : strdup("2");
 
    pvInfo() << "Testing restricted BufferSlicer::scatter():\n";
-   testScatterRestricted(3, args);
+   testScatterRestricted(5, args);
    pvInfo() << "Completed.\n";
+
+   pvInfo() << "Testing extended BufferSlicer::scatter():\n";
+   testScatterExtended(5, args);
+   pvInfo() << "Completed.\n";
+
 
    MPI_Finalize();
 
