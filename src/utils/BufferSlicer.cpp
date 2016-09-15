1be081e68c472b05eea80bdddc8a913e87bde349
austinthresher <austin@dangermomentum.com>
2016-09-15 10:37:33 -0600

    BufferSlicer::gather implemented. Now BufferSlicer needs a test.

--- src/utils/BufferSlicer.cpp
+++ src/utils/BufferSlicer.cpp
@@ -8,10 +8,11 @@ BufferSlicer::BufferSlicer(Communicator &comm) : mComm(comm) {}
 void BufferSlicer::scatter(Buffer &buffer,
                            unsigned int sliceStrideX,
                            unsigned int sliceStrideY) {
-
    // This assumes buffer's dimensions are nxGlobal x nyGlobal
-   int xMargins = buffer.getWidth()  - (sliceStrideX * mComm.numCommColumns());
-   int yMargins = buffer.getHeight() - (sliceStrideY * mComm.numCommRows());
+   int xMargins    = buffer.getWidth()
+                   - (sliceStrideX * mComm.numCommColumns());
+   int yMargins    = buffer.getHeight()
+                   - (sliceStrideY * mComm.numCommRows());
    int numElements = (sliceStrideX + xMargins)
                    * (sliceStrideY + yMargins)
                    * buffer.getFeatures();
@@ -20,12 +21,10 @@ void BufferSlicer::scatter(Buffer &buffer,
    // data for the root process at the end
    Buffer croppedBuffer;
    if (mComm.commRank() == 0) {
-
       // Loop through each rank, ending on the root process.
       // Uses Buffer::crop and MPI_Send to give each process
       // the correct slice of input data.
       for (int sendRank = mComm.commSize()-1; sendRank >= 0; --sendRank) {
-         
          // Copy the input data to a temporary buffer.
          // This gets cropped to the layer size below.
          croppedBuffer = buffer;
@@ -58,7 +57,6 @@ void BufferSlicer::scatter(Buffer &buffer,
                      mComm.communicator());
          }
          else { 
-
             // This is root, keep a slice for ourselves
             buffer.set(croppedBuffer.asVector(),
                        sliceStrideX + xMargin,
@@ -68,30 +66,112 @@ void BufferSlicer::scatter(Buffer &buffer,
       }
    }
    else {
-      
       // Create a temporary array to receive from MPI, move the values into
       // a vector, and then set our Buffer's contents to that vector.
-      float *tempBuffer = (float*)calloc(numElements, sizeof(float));
-      MPI_Recv(tempBuffer,
+      // This set of conversions could be greatly reduced by giving Buffer
+      // a constructor that accepts raw memory.
+      float *tempMem = (float*)calloc(numElements, sizeof(float));
+      pvErrorIf(tempMem == nullptr,
+                "Could not allocate a receive buffer of %d bytes.\n",
+                numElements * sizeof(float));
+      MPI_Recv(tempMem,
                numElements,
                MPI_FLOAT,
                0,
                31,
                mComm.communicator(),
                MPI_STATUS_IGNORE);
-      std::vector<float> bufferData(numElements);
+      std::vector<float> tempVector(numElements);
       for (int i = 0; i < numElements; ++i) {
-         bufferData.at(i) = tempBuffer[i];
+         tempVector.at(i) = tempMem[i];
       }
-      free(tempBuffer);
-      buffer.set(bufferData,
+      free(tempMem);
+      buffer.set(tempVector,
                  sliceStrideX + xMargin,
                  sliceStrideY + xMargin,
                  buffer.getFeatures());
    }
 }
 
-void BufferSlicer::gather(Buffer &buffer) {
+void BufferSlicer::gather(Buffer &buffer,
+                          unsigned int sliceStrideX,
+                          unsigned int sliceStrideY) {
+   // Here, we assume that buffer is the size of local,
+   // not global, nx and ny. If we have margins, then
+   // buffer.getWidth != sliceStrideX. Same for Y.
+   int xMargin = buffer.getWidth()  - sliceStrideX;
+   int yMargin = buffer.getHeight() - sliceStrideY;
+
+   if (mComm.commRank() == 0) {
+      int globalWidth   = sliceStrideX * mComm.numCommColumns() + xMargin;
+      int globalHeight  = sliceStrideY * mComm.numCommRows()    + yMargin;
+      int numElements   = buffer.getTotalElements();
+
+      Buffer globalBuffer(globalWidth, globalHeight, buffer.getFeatures());
+
+      // Receive each slice of our full buffer from each MPI process
+      float *tempMem = (float*)calloc(numElements, sizeof(float));
+      pvErrorIf(tempMem == nullptr,
+            "Could not allocate a receive buffer of %d bytes.\n",
+            numElements * sizeof(float));
+      for (int recvRank = mComm.commSize() - 1; recvRank >= 0; --recvRank) {
+         Buffer smallBuffer;
+         if (recvRank != 0) {
+            // This is nearly identical to the non-root receive in scatter
+            MPI_Recv(tempMem,
+                     numElements,
+                     MPI_FLOAT,
+                     recvRank,
+                     171 + recvRank, // Unique tag for each rank
+                     mComm.communicator(),
+                     MPI_STATUS_IGNORE);
+            std::vector<float> tempVector(numElements);
+            for (int i = 0; i < numElements; ++i) {
+               tempVector.at(i) = tempMem[i];
+            }
+            smallBuffer.set(tempVector,
+                            buffer.getWidth(),
+                            buffer.getHeight(),
+                            buffer.getFeatures()); 
+         }
+         else {
+            smallBuffer = buffer;
+         }
+         unsigned int sliceX = sliceStrideX
+                             * columnFromRank(
+                                   recvRank,
+                                   mComm.numCommRows(),
+                                   mComm.numCommColumns());
+         unsigned int sliceY = sliceStrideY
+                             * rowFromRank(
+                                   recvRank,
+                                   mComm.numCommRows(),
+                                   mComm.numCommColumns());
+
+         // Place our chunk into the global buffer
+         for (int y = 0; y < buffer.getHeight(); ++y) {
+            for (int x = 0; x < buffer.getWidth(); ++x) {
+               for(int f = 0; f < buffer.getFeatures(); ++f) {
+                  globalBuffer.set(sliceX + x,
+                                   sliceY + y,
+                                   f,
+                                   smallBuffer.at(x, y, f));
+               }
+            }
+         }
+      }
+      free(tempMem);
+      buffer = globalBuffer;
+   }
+   else {
+      // Send our chunk of the global buffer to root for reassembly
+      MPI_Send(buffer.asVector().data(),
+               buffer.getTotalElements(),
+               MPI_FLOAT,
+               mComm.commRank(),
+               171 + mComm.commRank(),
+               mComm.communicator());
+   }
 }
 
 
--- src/utils/BufferSlicer.hpp
+++ src/utils/BufferSlicer.hpp
@@ -11,9 +11,20 @@ class BufferSlicer {
    public:
       BufferSlicer(Communicator &comm);
 
+      // Every MPI process should call this with a buffer with
+      // dimensions globalNx x globalNy, with the root process
+      // buffer having the actual contents to be scattered.
+      // Afterwards, buffer will contain the slice of the data 
+      // that has been assigned to the current MPI process,
       void scatter(Buffer &buffer,
                    unsigned int sliceStrideX,  // These values should be the
                    unsigned int sliceStrideY); // layer's local restricted nx and ny
+
+      // This performs the reverse of scatter. Every MPI process
+      // should call gather with the slice of data they have, and
+      // afterwards the root process will have a filled buffer with
+      // dimensions globalNx x globalNy. Buffer will be unchanged
+      // on non-root processess.
       void gather( Buffer &buffer,
                    unsigned int sliceStrideX,
                    unsigned int sliceStrideY);
