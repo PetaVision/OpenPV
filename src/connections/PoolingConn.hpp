696193d35f435238b79a9aaebedf002fbaf40371
Pete Schultz <peteschultz@users.noreply.github.com>
2016-09-23 17:09:40 -0600

    Pooling and TransposePooling conns override allocatePostDeviceWeights

--- src/connections/PoolingConn.hpp
+++ src/connections/PoolingConn.hpp
@@ -58,6 +58,7 @@ protected:
     */
    void ioParam_normalizeMethod(enum ParamsIOFlag ioFlag);
 #ifdef PV_USE_CUDA
+   virtual int allocatePostDeviceWeights() override { return PV_SUCCESS; }
    virtual int allocateDeviceWeights() override { return PV_SUCCESS; }
    virtual int initializeReceivePostKernelArgs() override { return PV_SUCCESS; }
    virtual int initializeReceivePreKernelArgs() override { return PV_SUCCESS; }
--- src/connections/TransposePoolingConn.hpp
+++ src/connections/TransposePoolingConn.hpp
@@ -60,6 +60,7 @@ protected:
     virtual int ioParamsFillGroup(enum ParamsIOFlag ioFlag);
     virtual int setPatchSize();
 #ifdef PV_USE_CUDA
+    virtual int allocatePostDeviceWeights() override { return PV_SUCCESS; }
     virtual int allocateDeviceWeights() override { return PV_SUCCESS; }
     virtual int initializeReceivePostKernelArgs() override { return PV_SUCCESS; }
     virtual int initializeReceivePreKernelArgs() override { return PV_SUCCESS; }
