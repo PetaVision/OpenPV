59bcfa197d8efe726ca9bbe99d53a5e976369090
Pete Schultz <peteschultz@users.noreply.github.com>
2016-09-28 21:54:30 -0600

    StochasticReleaseTestProbe no longer calls HyPerCol methods related to step number

--- tests/StochasticReleaseTest/src/StochasticReleaseTestProbe.cpp
+++ tests/StochasticReleaseTest/src/StochasticReleaseTestProbe.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "StochasticReleaseTestProbe.hpp"
+#include <algorithm>
+#include <cmath>
 
 namespace PV {
 
@@ -19,13 +21,11 @@ StochasticReleaseTestProbe::StochasticReleaseTestProbe() {
 }
 
 int StochasticReleaseTestProbe::initialize_base() {
-   conn = NULL;
-   pvalues = NULL;
-
    return PV_SUCCESS;
 }
 
 int StochasticReleaseTestProbe::initStochasticReleaseTestProbe(const char * name, HyPerCol * hc) {
+   pvAssert(hc->getInitialStep()==0L);
    int status = initStatsProbe(name, hc);
    return status;
 }
@@ -36,135 +36,119 @@ void StochasticReleaseTestProbe::ioParam_buffer(enum ParamsIOFlag ioFlag) {
 
 int StochasticReleaseTestProbe::communicateInitInfo() {
    int status = StatsProbe::communicateInitInfo();
-   pvErrorIf(!(getTargetLayer()), "Test failed.\n");
-   long int num_steps = getParent()->getFinalStep() - getParent()->getInitialStep();
-   pvalues = (double *) calloc(num_steps*getTargetLayer()->getLayerLoc()->nf, sizeof(double));
-   if (pvalues == NULL) {
-      pvError().printf("StochasticReleaseTestProbe error: unable to allocate memory for pvalues: %s\n", strerror(errno));
+   pvErrorIf(!(getTargetLayer()), ": %s did not set target layer.\n", getDescription_c());
+   pvErrorIf(conn!=nullptr, ": %s, communicateInitInfo called with connection already set.\n", getDescription_c());
+   int numconns = getParent()->numberOfConnections();
+   for (int c=0; c<numconns; c++) {
+      BaseConnection * baseConn = getParent()->getConnection(c);
+      if (!strcmp(baseConn->getPostLayerName(),getTargetLayer()->getName())) {
+         pvErrorIf(conn!=nullptr, ": %s cannot have more than one connnection going to target %s.\n", getDescription_c(), getTargetLayer()->getName());
+         conn = dynamic_cast<HyPerConn *>(baseConn);
+      }
    }
+   pvErrorIf(!(conn!=nullptr), ": %s requires a connection going to target %s.\n", getDescription_c(), getTargetLayer()->getName());
    return status;
 }
 
-int compar(const void * a, const void * b) {
+bool compar(double const& a, double const& b) {
    // routine for sorting p-values.
    // If the theoretical variance is zero and the observed mean is correct, the p-value will be not-a-number.
-   // If the theoretical variance is zero and the observed mean is incorrect, the p-value will by plus or minus infinity
+   // If the theoretical variance is zero and the observed mean is incorrect, the p-value will be plus or minus infinity
    // Sort so that all the nan's are at the end; they won't be included in the Holm-Bonferroni test.
-   double aval=*(double *) a;
-   double bval=*(double *) b;
-   if (isnan(aval)) return 1;
-   if (isnan(bval)) return -1;
-   return aval < bval ? -1 : aval > bval ? 1 : 0;
+   if (std::isnan(a)) return false;
+   if (std::isnan(b)) return true;
+   return a < b;
 }
 
 int StochasticReleaseTestProbe::outputState(double timed) {
-   // Set conn.  Can't do that in initStochasticReleaseTestProbe because we need to search for a conn with the given post, and connections' postLayerName is not necessarily set.
-   if (conn==NULL) {
-      HyPerCol * hc = getTargetLayer()->getParent();
-      int numconns = hc->numberOfConnections();
-      for (int c=0; c<numconns; c++) {
-         if (!strcmp(hc->getConnection(c)->getPostLayerName(),getTargetLayer()->getName())) {
-            pvErrorIf(!(conn==NULL), "Test failed.\n"); // Only one connection can go to this layer for this probe to work
-            BaseConnection * baseConn = hc->getConnection(c);
-            conn = dynamic_cast<HyPerConn *>(baseConn);
-         }
-      }
-      pvErrorIf(!(conn!=NULL), "Test failed.\n");
-   }
-   pvErrorIf(!(conn->numberOfAxonalArborLists()==1), "Test failed.\n");
-   pvErrorIf(!(conn->xPatchSize()==1), "Test failed.\n");
-   pvErrorIf(!(conn->yPatchSize()==1), "Test failed.\n");
-   pvErrorIf(!(conn->getNumDataPatches()==conn->fPatchSize()), "Test failed.\n");
+   pvErrorIf(!(conn->numberOfAxonalArborLists()==1), ": %s connection %s has %d arbors; only one is allowed.\n", getDescription_c(), conn->getName(), conn->numberOfAxonalArborLists());
+   pvErrorIf(!(conn->xPatchSize()==1), ": %s connection %s has nxp=%d, nxp=1 is required.\n", getDescription_c(), conn->getName(), conn->xPatchSize());
+   pvErrorIf(!(conn->yPatchSize()==1), ": %s connection %s has nyp=%d, nyp=1 is required.\n", getDescription_c(), conn->getName(), conn->yPatchSize());
+   pvErrorIf(!(conn->getNumDataPatches()==conn->fPatchSize()), ": %s connection %s must have number of data patches (%d) and nfp equal (%d).\n",
+         getDescription_c(), conn->getName(), conn->getNumDataPatches(), conn->fPatchSize());
    int status = StatsProbe::outputState(timed);
-   pvErrorIf(!(status==PV_SUCCESS), "Test failed.\n");
-   HyPerLayer * l = getTargetLayer();
-   HyPerCol * hc = l->getParent();
-   int nf = l->getLayerLoc()->nf;
+   pvErrorIf(!(status==PV_SUCCESS), ": %s failed in StatsProbe::outputState at time %f.\n", getDescription_c(), timed);
    if (timed>0.0) {
-      for (int f=0; f < nf; f++) {
-         if (computePValues(hc->getCurrentStep(), f)!=PV_SUCCESS) status = PV_FAILURE;
-      }
-      pvErrorIf(!(status == PV_SUCCESS), "Test failed.\n");
-      if (hc->columnId()==0 && hc->simulationTime()+hc->getDeltaTime()/2>=hc->getStopTime()) {
+      computePValues();
+      if (getParent()->getCommunicator()->commRank()==0 && timed+0.5*getParent()->getDeltaTime() >= getParent()->getStopTime()) {
          // This is the last timestep
          // sort the p-values and apply Holm-Bonferroni method since there is one for each timestep and each feature.
-         long int num_steps = hc->getFinalStep() - hc->getInitialStep();
-         long int N = num_steps * nf;
-         qsort(pvalues, (size_t) N, sizeof(*pvalues), compar);
-         while(N>0 && isnan(pvalues[N-1])) {
+         size_t N = pvalues.size();
+         std::sort(pvalues.begin(), pvalues.end(), compar);
+         while(N>0 && isnan(pvalues.at(N-1))) {
             N--;
          }
-         for (long int k=0; k<N; k++) {
-            if (pvalues[k]*(N-k)<0.05) {
-               pvErrorNoExit().printf("%s: p-value %ld out of %ld (ordered by size) with Holm-Bonferroni correction = %f\n", getTargetLayer()->getDescription_c(), k, N, pvalues[k]*(N-k));
+         pvalues.resize(N);
+         for (size_t k=0; k<N; k++) {
+            double hbCorr = pvalues.at(k)*(double)(N-k);
+            if (hbCorr<0.05) {
+               pvErrorNoExit().printf("%s: p-value %zu out of %zu (ordered by size) with Holm-Bonferroni correction = %f\n", getTargetLayer()->getDescription_c(), k, N, hbCorr);
                status = PV_FAILURE;
             }
          }
       }
 
    }
-   pvErrorIf(!(status==PV_SUCCESS), "Test failed.\n");
+   pvErrorIf(status!=PV_SUCCESS, ": %s failed in StochasticReleaseTestProbe::outputState at time %f.\n", timed);
    return status;
 }
 
-int StochasticReleaseTestProbe::computePValues(long int step, int f) {
-   int status = PV_SUCCESS;
-   pvErrorIf(!(step >=0 && step < INT_MAX), "Test failed.\n");
+void StochasticReleaseTestProbe::computePValues() {
    int nf = getTargetLayer()->getLayerLoc()->nf;
-   pvErrorIf(!(f >= 0 && f < nf), "Test failed.\n");
-   int idx = (step-1)*nf + f;
-   pvwdata_t wgt = conn->get_wDataStart(0)[f*(nf+1)]; // weights should be one-to-one weights
+   auto oldsize = pvalues.size();
+   pvalues.resize(oldsize+nf);
+   for (int f=0; f < nf; f++) {
+      int nf = getTargetLayer()->getLayerLoc()->nf;
+      pvErrorIf(!(f >= 0 && f < nf), "Test failed.\n");
+      pvwdata_t wgt = conn->get_wDataStart(0)[f*(nf+1)]; // weights should be one-to-one weights
 
-   HyPerLayer * pre = conn->preSynapticLayer();
-   const pvdata_t * preactPtr = pre->getLayerData();
-   const PVLayerLoc * preLoc = pre->getLayerLoc();
-   const int numPreNeurons = pre->getNumNeurons();
-   bool found=false;
-   pvdata_t preact = 0.0f;
-   for (int n=f; n<numPreNeurons; n+=nf) {
-      int nExt = kIndexExtended(n, preLoc->nx, preLoc->ny, preLoc->nf, preLoc->halo.lt, preLoc->halo.rt, preLoc->halo.dn, preLoc->halo.up);
-      pvdata_t a = preactPtr[nExt];
-      if (a!=0.0f) {
-         if (found) {
-            pvErrorIf(!(preact==a), "Test failed.\n");
-         }
-         else {
-            found = true;
-            preact = a;
+      HyPerLayer * pre = conn->preSynapticLayer();
+      const pvdata_t * preactPtr = pre->getLayerData();
+      const PVLayerLoc * preLoc = pre->getLayerLoc();
+      const int numPreNeurons = pre->getNumNeurons();
+      bool found=false;
+      pvdata_t preact = 0.0f;
+      for (int n=f; n<numPreNeurons; n+=nf) {
+         int nExt = kIndexExtended(n, preLoc->nx, preLoc->ny, preLoc->nf, preLoc->halo.lt, preLoc->halo.rt, preLoc->halo.dn, preLoc->halo.up);
+         pvdata_t a = preactPtr[nExt];
+         if (a!=0.0f) {
+            if (found) {
+               pvErrorIf(!(preact==a), "Test failed.\n");
+            }
+            else {
+               found = true;
+               preact = a;
+            }
          }
       }
-   }
-   preact *= (float)getParent()->getDeltaTime();
-   if (preact < 0.0f) preact = 0.0f;
-   if (preact > 1.0f) preact = 1.0f;
+      if (preact < 0.0f) preact = 0.0f;
+      if (preact > 1.0f) preact = 1.0f;
 
-   const PVLayerLoc * loc = getTargetLayer()->getLayerLoc();
-   const pvdata_t * activity = getTargetLayer()->getLayerData();
-   int nnzf = 0;
-   const int numNeurons = getTargetLayer()->getNumNeurons();
-   for (int n=f; n<numNeurons; n+=nf) {
-      int nExt = kIndexExtended(n, loc->nx, loc->ny, loc->nf, loc->halo.lt, loc->halo.rt, loc->halo.dn, loc->halo.up);
-      pvErrorIf(!(activity[nExt]==0 || activity[nExt]==wgt), "Test failed.\n");
-      if (activity[nExt]!=0) nnzf++;
+      const PVLayerLoc * loc = getTargetLayer()->getLayerLoc();
+      const pvdata_t * activity = getTargetLayer()->getLayerData();
+      int nnzf = 0;
+      const int numNeurons = getTargetLayer()->getNumNeurons();
+      for (int n=f; n<numNeurons; n+=nf) {
+         int nExt = kIndexExtended(n, loc->nx, loc->ny, loc->nf, loc->halo.lt, loc->halo.rt, loc->halo.dn, loc->halo.up);
+         pvErrorIf(!(activity[nExt]==0 || activity[nExt]==wgt), "Test failed.\n");
+         if (activity[nExt]!=0) nnzf++;
+      }
+      HyPerLayer * l = getTargetLayer();
+      MPI_Allreduce(MPI_IN_PLACE, &nnzf, 1, MPI_INT, MPI_SUM, getParent()->getCommunicator()->communicator());
+      if (getParent()->getCommunicator()->commRank()==0) {
+         const int neuronsPerFeature = l->getNumGlobalNeurons()/nf;
+         double mean = preact * neuronsPerFeature;
+         double stddev = sqrt(neuronsPerFeature*preact*(1-preact));
+         double numdevs = (nnzf-mean)/stddev;
+         double pval = std::erfc(std::fabs(numdevs)/std::sqrt(2));
+         pvalues.at(oldsize+f) = pval;
+         outputStream->printf("    Feature %d, nnz=%5d, expectation=%7.1f, std.dev.=%5.1f, discrepancy of %f deviations, p-value %f\n",
+               f, nnzf, mean, stddev, numdevs, pval);
+      }
    }
-   HyPerLayer * l = getTargetLayer();
-   HyPerCol * hc = l->getParent();
-   MPI_Allreduce(MPI_IN_PLACE, &nnzf, 1, MPI_INT, MPI_SUM, hc->getCommunicator()->communicator());
-   if (hc->columnId()==0) {
-      const int neuronsPerFeature = l->getNumGlobalNeurons()/nf;
-      double mean = preact * neuronsPerFeature;
-      double stddev = sqrt(neuronsPerFeature*preact*(1-preact));
-      double numdevs = (nnzf-mean)/stddev;
-      pvalues[idx] = erfc(fabs(numdevs)/sqrt(2));
-      outputStream->printf("    Feature %d, nnz=%5d, expectation=%7.1f, std.dev.=%5.1f, discrepancy of %f deviations, p-value %f\n",
-              f, nnzf, mean, stddev, numdevs, pvalues[idx]);
-   }
-   pvErrorIf(!(status==PV_SUCCESS), "Test failed.\n");
-   return status;
 }
 
 StochasticReleaseTestProbe::~StochasticReleaseTestProbe() {
-   free(pvalues);
 }
 
 } /* namespace PV */
