c7bb88ab5c0c2f8e637adb7147c4263a0b048f52
austinthresher <austin@dangermomentum.com>
2016-09-30 17:29:57 -0600

    Renamed .clang-format to clang-format.
    autoformat.sh copies the latter to the former, runs clang-format,
    and then deletes .clang-format.

--- autoformat.sh
+++ autoformat.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+cp clang-format .clang-format
 find src/ -iname *.h* -o -iname *.c* | xargs clang-format -i -style=file
 find tests/ -iname *.h* -o -iname *.c* | xargs clang-format -i -style=file
+rm .clang-format
