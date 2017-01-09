30347d060a5825dce6f125b5950e8faa4c114aba
Pete Schultz <peteschultz@users.noreply.github.com>
2017-01-09 14:09:35 -0700

    Test for checkpointing and MomentumConn with momentumMethod="viscosity"

---
+++ tests/MomentumConnViscosityCheckpointerTest/src/CorrectState.hpp
@@ -0,0 +1,126 @@
+/*
+ * CorrectState.hpp
+ *
+ *  Created on: Jan 6, 2017
+ *      Author: pschultz
+ */
+
+#ifndef CORRECTSTATE_HPP_
+#define CORRECTSTATE_HPP_
+
+#include "probes/ColProbe.hpp"
+
+#include "connections/HyPerConn.hpp"
+#include "layers/HyPerLayer.hpp"
+#include "layers/InputLayer.hpp"
+
+/**
+ * A class used by MomentumConnViscosityCheckpointerTest to encapsulate the
+ * correct values for the input and output layers and for the weight between
+ * them, and the number of updates.
+ *
+ * This test uses momentumMethod="viscosity". The value of momentumTau,
+ * 1/log(2)=1.4426950408889634,
+ * was chosen so that the tauFactor=exp(-1/momentumTau) = 0.5 is an
+ * easy-to-work-with value.
+ *
+ * The update rule for momentumMethod=viscosity, momentumDecay=0, dWMax=1, and
+ * momentumTau = 1/log(2)=1.4426950408889634 is:
+ *
+ * new update number = old update number + 1.
+ * new dw = old input * old output + 0.5*old dw
+ * new weight = old weight + new dw.
+ * new input  = new update number.
+ * new output = new input * new weight.
+ *
+
+ * The test uses an input layer display of 4 and a connection that, either
+ * using the input layer as a trigger layer or by defining weightUpdatePeriod=4
+ * and initialWeightUpdateTime=1, updates at times 1, 5, 9, etc.
+ *
+ * The frames of the input layer are constant 1, constant 2, etc, and the
+ * weight has dWMax = 1. The weight is initialized as 1 and the output layer
+ * is initialized as 2. Therefore, at the end of each timestep, the correct
+ * state of the system is as follows:
+ *
+ *   time update  dw  weight input   output
+ *     0     0      0     1    1         2
+ *     1     1      2     3    1         3
+ *     2     1      2     3    1         3
+ *     3     1      2     3    1         3
+ *     4     1      2     3    1         3
+ *     5     2      4     7    2        14
+ *     6     2      4     7    2        14
+ *     7     2      4     7    2        14
+ *     8     2      4     7    2        14
+ *     9     3     30    37    3       111
+ *    10     3     30    37    3       111
+ *    11     3     30    37    3       111
+ *    12     3     30    37    3       111
+ *    13     4    348   385    4      1540
+ *    14     4    348   385    4      1540
+ *    15     4    348   385    4      1540
+ *    16     4    348   385    4      1540
+ *    17     5   6334  6719    5     33595
+ *    18     5   6334  6719    5     33595
+ *    19     5   6334  6719    5     33595
+ *    20     5   6334  6719    5     33595
+ */
+class CorrectState {
+  public:
+   /**
+    * Public constructor for the CorrectState class, setting the initial update number,
+    * weight value, input value, and output value.
+    */
+   CorrectState(
+         int intialUpdateNumber,
+         float initialWeight,
+         float initial_dw,
+         float initialInput,
+         float initialOutput);
+
+   /**
+    * Destructor for the CorrectState class.
+    */
+   virtual ~CorrectState() {}
+
+   /**
+    * Applies the update rule to get the next weight, input and output values,
+    * and increments the update number.
+    */
+   void update();
+
+   /**
+    * Returns the current update number.
+    */
+   int getUpdateNumber() const { return mUpdateNumber; }
+
+   /**
+    * Returns the current value for the correct weight.
+    */
+   float getCorrectWeight() const { return mCorrectWeight; }
+
+   /**
+    * Returns the current value for the correct dw.
+    */
+   float getCorrect_dw() const { return mCorrect_dw; }
+
+   /**
+    * Returns the current value for the correct input.
+    */
+   float getCorrectInput() const { return mCorrectInput; }
+
+   /**
+    * Returns the current value for the correct output.
+    */
+   float getCorrectOutput() const { return mCorrectOutput; }
+
+  private:
+   int mUpdateNumber    = 0;
+   float mCorrectWeight = 0.0;
+   float mCorrect_dw    = 0.0;
+   float mCorrectInput  = 0.0;
+   float mCorrectOutput = 0.0;
+};
+
+#endif // CORRECTSTATE_HPP_
---
+++ tests/MomentumConnViscosityCheckpointerTest/src/MomentumConnViscosityCheckpointerTest.cpp
@@ -0,0 +1,117 @@
+/*
+ * pv.cpp
+ *
+ */
+#include "MomentumConnViscosityCheckpointerTestProbe.hpp"
+#include <columns/PV_Init.hpp>
+#include <columns/buildandrun.hpp>
+
+void checkConfiguration(PV::PV_Init &pv_initObj, char const *programName);
+int checkProbe(PV::HyPerCol *hc, int argc, char **argv);
+
+int main(int argc, char *argv[]) {
+   int status = PV_SUCCESS;
+   PV::PV_Init pv_initObj(&argc, &argv, false /*do not allow unrecognized arguments*/);
+   char const *programName = argv[0];
+   checkConfiguration(pv_initObj, programName);
+   pv_initObj.registerKeyword(
+         "MomentumConnViscosityCheckpointerTestProbe",
+         Factory::create<MomentumConnViscosityCheckpointerTestProbe>);
+
+   pv_initObj.setParams("input/MomentumConnViscosityCheckpointerTest_freshstart_triggering.params");
+   status = buildandrun(&pv_initObj, nullptr, checkProbe);
+   FatalIf(
+         status != PV_SUCCESS,
+         "%s failed on triggering connection, with run started from scratch.\n",
+         programName);
+
+   pv_initObj.setStringArgument(
+         "CheckpointReadDirectory", "output_freshstart_triggering/checkpoints/Checkpoint08");
+   status = buildandrun(&pv_initObj, nullptr, checkProbe);
+   FatalIf(
+         status != PV_SUCCESS,
+         "%s failed on triggering connection, with run started from Checkpoint08.\n",
+         programName);
+
+   pv_initObj.resetState();
+
+   pv_initObj.setParams("input/MomentumConnViscosityCheckpointerTest_initfromCP_triggering.params");
+   status = buildandrun(&pv_initObj, nullptr, checkProbe);
+   FatalIf(
+         status != PV_SUCCESS,
+         "%s failed on triggering connection, with run initialized from Checkpoint08.\n",
+         programName);
+
+   pv_initObj.resetState();
+
+   pv_initObj.setParams(
+         "input/MomentumConnViscosityCheckpointerTest_freshstart_updateperiod.params");
+   status = buildandrun(&pv_initObj, nullptr, checkProbe);
+   FatalIf(
+         status != PV_SUCCESS,
+         "%s failed on update period connection, with run started from scratch.\n",
+         programName);
+
+   pv_initObj.setStringArgument(
+         "CheckpointReadDirectory", "output_freshstart_updateperiod/checkpoints/Checkpoint08");
+   status = buildandrun(&pv_initObj, nullptr, checkProbe);
+   FatalIf(
+         status != PV_SUCCESS,
+         "%s failed on update period connection, with run started from Checkpoint08.\n",
+         programName);
+
+   pv_initObj.resetState();
+
+   pv_initObj.setParams(
+         "input/MomentumConnViscosityCheckpointerTest_initfromCP_updateperiod.params");
+   status = buildandrun(&pv_initObj, nullptr, checkProbe);
+   FatalIf(
+         status != PV_SUCCESS,
+         "%s failed on update period connection, with run initialized from Checkpoint08.\n",
+         programName);
+
+   return PV_SUCCESS;
+}
+
+void checkConfiguration(PV::PV_Init &pv_initObj, char const *programName) {
+   int status = PV_SUCCESS;
+   int rank   = pv_initObj.getCommunicator()->globalCommRank();
+   if (pv_initObj.getParams() != nullptr) {
+      if (rank == 0) {
+         ErrorLog().printf("%s should be run without the params file argument.\n", programName);
+      }
+      status = PV_FAILURE;
+   }
+   if (pv_initObj.getBooleanArgument("Restart")) {
+      if (rank == 0) {
+         ErrorLog().printf("%s should be run without the restart flag.\n", programName);
+      }
+      status = PV_FAILURE;
+   }
+   if (!pv_initObj.getStringArgument("CheckpointReadDirectory").empty()) {
+      if (rank == 0) {
+         ErrorLog().printf(
+               "%s should be run without the checkpoint read directory.\n", programName);
+      }
+      status = PV_FAILURE;
+   }
+   if (status != PV_SUCCESS) {
+      if (rank == 0) {
+         ErrorLog().printf(
+               "This test uses hard-coded params files and includes running from checkpoint; "
+               "therefore it needs to be run without params file or checkpoint arguments "
+               "on the command line.\n");
+      }
+      MPI_Barrier(pv_initObj.getCommunicator()->globalCommunicator());
+      exit(EXIT_FAILURE);
+   }
+}
+
+int checkProbe(PV::HyPerCol *hc, int argc, char **argv) {
+   auto probe = dynamic_cast<MomentumConnViscosityCheckpointerTestProbe *>(
+         hc->getColProbeFromName("probe"));
+   FatalIf(
+         probe == nullptr,
+         "Column does not have a MomentumConnViscosityCheckpointerTestProbe named \"probe\".\n");
+   return probe->getTestFailed() ? PV_FAILURE : PV_SUCCESS;
+}
