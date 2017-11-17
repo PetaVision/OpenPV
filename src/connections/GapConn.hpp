8976e6d17240cfc3a2091712dfa84895869ef9fd
Pete Schultz <peteschultz@users.noreply.github.com>
2017-11-16 19:32:36 -0700

    GapConn obsolete. Use HyPerConn with channelCode=3

--- src/connections/GapConn.hpp
+++ src/connections/GapConn.hpp
@@ -16,29 +16,6 @@ class GapConn : public PV::HyPerConn {
   public:
    GapConn(const char *name, HyPerCol *hc);
    virtual ~GapConn();
-
-  protected:
-   GapConn();
-   void ioParam_channelCode(enum ParamsIOFlag ioFlag) override;
-   // No channel argument in params because GapConn must always use CHANNEL_GAP
-
-   virtual void ioParam_sharedWeights(enum ParamsIOFlag ioFlag) override;
-   void ioParam_normalizeMethod(enum ParamsIOFlag ioFlag) override;
-
-   int initialize(const char *name, HyPerCol *hc);
-
-   virtual void createDeliveryObject() override {}
-
-   virtual int allocateDataStructures() override;
-
-   // Temporarily duplicating HyPerConn::deliver, so that HyPerConn::deliver can be overhauled
-   // without breaking subclasses. -pschultz, 2017-09-08
-   virtual int deliver() override;
-
-  private:
-   int initialize_base();
-   bool initNormalizeFlag;
-
 }; // end class GapConn
 
 } /* namespace PV */
