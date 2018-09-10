11181051e68c7d7c2a1fe7c34b6a763fe8643293
Pete Schultz <peteschultz@users.noreply.github.com>
2018-09-10 14:30:05 -0600

    Fixes for BinningLayer and BinningLayerTest
    BinningLayer parameter normalDist has been eliminated; if binSigma is
    positive, the Gaussian is always normalized to have a maximum possible
    value of 1. The values depend not just on which bin the input value
    falls in, but where in the bin it falls. Additionally, if binSigma > 0,
    values are computed for all bins, not just those within binSigma bins
    of the center.
    
    BinningLayerTest now tests positive binSigma case correctly.

---
+++ tests/BinningLayerTest/src/BinningTestProbe.hpp
@@ -0,0 +1,32 @@
+/*
+ * BinningTestProbe.hpp
+ *
+ *  Created on: Jan 15, 2015
+ *      Author: slundquist
+ */
+
+#ifndef BINNINGTESTPROBE_HPP_
+#define BINNINGTESTPROBE_HPP_
+
+#include <layers/BinningLayer.hpp>
+#include <probes/LayerProbe.hpp>
+
+namespace PV {
+
+class BinningTestProbe : public PV::LayerProbe {
+  public:
+   BinningTestProbe(const char *name, HyPerCol *hc);
+   virtual void calcValues(double timeValue) {}
+
+  protected:
+   virtual Response::Status
+   communicateInitInfo(std::shared_ptr<CommunicateInitInfoMessage const> message) override;
+
+   virtual Response::Status outputState(double simTime) override;
+
+  private:
+   BinningLayer *mBinningLayer = nullptr;
+};
+
+} /* namespace PV */
+#endif // BINNINGTESTPROBE_HPP_
