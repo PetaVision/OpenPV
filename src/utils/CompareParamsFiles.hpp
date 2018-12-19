7b3e77537d14f92be7770cd4da8bf0e422ee93aa
Pete Schultz <peteschultz@users.noreply.github.com>
2018-12-19 11:39:34 -0700

    Documentation for CompareParamsFiles functions

--- src/utils/CompareParamsFiles.hpp
+++ src/utils/CompareParamsFiles.hpp
@@ -20,24 +20,69 @@
 
 namespace PV {
 
+/**
+ * Compares two params files, printing error messages describing any differences.
+ * The Communicator argument is needed by the PVParams constructor.
+ * Return value is PV_SUCCESS if the params files are equivalent, and PV_FAILURE if not.
+ */
 int compareParamsFiles(
       std::string const &paramsFile1,
       std::string const &paramsFile2,
       Communicator *comm);
+
+/**
+ * Compares two ParameterGroup objects, printing error messages describing any differences.
+ * Return value is PV_SUCCESS if the groups are equivalent, and PV_FAILURE if not.
+ */
 int compareParameterGroups(ParameterGroup *group1, ParameterGroup *group2);
+
+/**
+ * Compares two ParameterStack objects, printing error messages describing any differences.
+ * The groupName argument is used in any error messages, since ParameterStack does not
+ * have a name data member.
+ * Return value is PV_SUCCESS if the params files are equivalent, and PV_FAILURE if not.
+ */
 int compareParameterNumericStacks(
       char const *groupName,
       ParameterStack *stack1,
       ParameterStack *stack2);
+
+/**
+ * Compares two ParameterArrayStack objects, printing error messages describing any differences.
+ * The groupName argument is used in any error messages, since ParameterArrayStack does not
+ * have a name data member.
+ * Return value is PV_SUCCESS if the params files are equivalent, and PV_FAILURE if not.
+ */
 int compareParameterArrayStacks(
       char const *groupName,
       ParameterArrayStack *stack1,
       ParameterArrayStack *stack2);
+
+/**
+ * Compares two ParameterArray objects, printing error messages describing any differences.
+ * The groupName argument is used in any error messages, since ParameterArray does not
+ * have a name data member.
+ * Return value is PV_SUCCESS if the params files are equivalent, and PV_FAILURE if not.
+ */
 int compareParameterArray(char const *groupName, ParameterArray *array1, ParameterArray *array2);
+
+/**
+ * Compares two ParameterStringStack objects, printing error messages describing any differences.
+ * The groupName argument is used in any error messages, since ParameterStringStack does not
+ * have a name data member.
+ * Return value is PV_SUCCESS if the params files are equivalent, and PV_FAILURE if not.
+ */
 int compareParameterStringStacks(
       char const *groupName,
       ParameterStringStack *stack1,
       ParameterStringStack *stack2);
+
+/**
+ * Compares two ParameterString objects, printing error messages describing any differences.
+ * The groupName argument is used in any error messages, since ParameterString does not
+ * have a name data member.
+ * Return value is PV_SUCCESS if the params files are equivalent, and PV_FAILURE if not.
+ */
 int compareParameterString(
       char const *groupName,
       ParameterString *string1,
