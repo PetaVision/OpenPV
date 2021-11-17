e52291e7d0142132e53dda8a54f1dc32df992c70
Pete Schultz <peteschultz@users.noreply.github.com>
2021-11-17 15:15:19 -0700

    Reduces code duplication in ProbeOutputTest

---
+++ tests/ProbeOutputTest/input/testProbeOutput.bash
@@ -0,0 +1,154 @@
+#! /usr/bin/env bash
+
+# A script to run the ProbeOutputTest executable under various params files,
+# and compare the probe output files to correct files.
+# The script uses compareProbeOutput.py, which includes a tolerance for
+# floating-point round-off effects.
+
+# This script requires several environmental variables to be set.
+# MPICOMMAND     is the mpiexec or mpirun command, together with options, e.g. "mpiexec -n 4"
+#                It can be empty for single process runs.
+# RUNNAME        is the name of the run, appearing in the config filename and output directories.
+#                Currently, the allowable RUNNAME values are "oneproc", "batchMPI", "MtoN", "MtoN2".
+# RUNDESC        is a brief description of the run, used in informational messages.
+# OUTPUTPVPTRUNC is the number of bytes to truncate Output.pvp to, when restarting or initializing
+#                from checkpoint, in order to simulate restarting from a previous interruped run.
+
+function toldiff() {
+   python input/compareProbeOutput.py "$@";
+}
+
+function toldiffloop() {
+    status=0
+    correctdir="input/correctProbeOutput/${1}"
+    for k in {0..7}
+    do
+        targetfile="OutputL2Norm_batchElement_${k}.txt"
+        targetpath="$(find output -name ${targetfile})"
+        if test "$(echo "$targetpath" | wc -l)" -ne 1
+        then
+            >&2 echo "${RUNDESC}, file \"${targetfile}\" not found"
+            status=1
+        else
+            toldiff "${correctdir}/correct_${k}.txt" "$targetpath" || status=1
+            if test $? -ne 0 ; then status=1 ; fi
+        fi
+    done
+    return $status
+}
+
+echo "RUNNAME is ${RUNNAME}"
+
+test -e output && rm -r output
+
+### Base run ###
+echo "${RUNDESC} test, base run..."
+test -e output_${RUNNAME} && rm -r output_${RUNNAME}
+${MPICOMMAND} Debug/ProbeOutputTest input/config_${RUNNAME}.txt
+mv -i ProbeOutputTest_${RUNNAME}*.log output/
+toldiffloop base
+status="$?"
+
+mv -i output output_${RUNNAME}
+
+if test $status -ne 0
+then
+    >&2 echo "${RUNDESC}, base run failed."
+    exit 1 # If this run doesn't work, no other runs would be expected to work either
+fi
+test $status -eq 0 && echo "${RUNDESC}, base run passed."
+
+failure=0
+
+### Initializing from checkpoint ###
+echo "${RUNDESC} test, initializing from checkpoint..."
+cp -pr output_${RUNNAME} output
+rm output/*.log
+for k in {0..7}
+do
+    targetfile=OutputL2Norm_batchElement_${k}.txt
+    filepospath="$(find output/checkpoints/Checkpoint040 -name "${targetfile}_filepos_FileStreamRead.txt")"
+    truncat="$(cat ${filepospath})"
+    truncat=$(($truncat + 100))
+    targetpath="$(find output -name "${targetfile}")"
+    truncate -s $truncat "${targetpath}"
+    echo "xxxxxxxxxx" >> "${targetpath}"
+done
+find output -name Output.pvp |
+while read f
+do
+    truncate -s ${OUTPUTPVPTRUNC} "$f"
+done
+${MPICOMMAND} Debug/ProbeOutputTest input/config_${RUNNAME}-ifcp.txt
+mv -i ProbeOutputTest_${RUNNAME}_initfromchkpt*.log output/
+toldiffloop initfromchkpt
+if test $? -ne 0
+then
+    >&2 echo "${RUNDESC}, initializing from checkpoint failed."
+    failure=1
+    #No need to exit here, since subsequent runs don't depend on initializing from checkpoint
+else
+    echo "${RUNDESC}, initializing from checkpoint passed."
+fi
+test -e output_${RUNNAME}_initfromchkpt && rm -r output_${RUNNAME}_initfromchkpt
+mv -i output output_${RUNNAME}_initfromchkpt
+
+### Restarting from checkpoint ###
+echo "${RUNDESC} test, restarting from checkpoint..."
+cp -pr output_${RUNNAME} output
+rm output/*.log
+for k in {0..7}
+do
+    targetfile=OutputL2Norm_batchElement_${k}.txt
+    filepospath="$(find output/checkpoints/Checkpoint040 -name "${targetfile}_filepos_FileStreamRead.txt")"
+    truncat="$(cat "${filepospath}")"
+    truncat=$(($truncat + 100))
+    targetpath="$(find output -name "${targetfile}")"
+    truncate -s $truncat "${targetpath}"
+    echo "xxxxxxxxxx" >> "${targetpath}"
+done
+find output -name Output.pvp |
+while read f
+do
+    truncate -s ${OUTPUTPVPTRUNC} "$f"
+done
+${MPICOMMAND} Debug/ProbeOutputTest input/config_${RUNNAME}-restartfromchkpt.txt
+mv -i ProbeOutputTest_${RUNNAME}_restartfromchkpt*.log output/
+toldiffloop restartfromchkpt
+if test $? -ne 0
+then
+    >&2 echo "${RUNDESC}, restarting from checkpoint failed."
+    failure=1
+    #No need to exit here, since subsequent runs don't depend on restarting from checkpoint
+else
+    echo "${RUNDESC}, restarting from checkpoint passed."
+fi
+test -e output_${RUNNAME}_restartfromchkpt && rm -r output_${RUNNAME}_restartfromchkpt
+mv -i output output_${RUNNAME}_restartfromchkpt
+
+### Restarting from end ###
+echo "${RUNDESC} test, restarting from end..."
+cp -pr output_${RUNNAME} output
+rm output/*.log
+${MPICOMMAND} Debug/ProbeOutputTest input/config_${RUNNAME}-restartfromend.txt
+mv -i ProbeOutputTest_${RUNNAME}_restartfromend*.log output/
+toldiffloop restartfromend
+if test $? -ne 0
+then
+    >&2 echo "${RUNDESC}, restarting from end failed."
+    failure=1
+    #No need to exit here, since subsequent runs don't depend on restarting from end
+else
+    echo "${RUNDESC}, restarting from end passed."
+fi
+test -e output_${RUNNAME}_restartfromend && rm -r output_${RUNNAME}_restartfromend
+mv -i output output_${RUNNAME}_restartfromend
+
+if test "$failure" -eq 0
+then
+    echo "Test passed."
+else
+    >&2 echo "Test failed."
+fi
+
+exit "$failure"
--- tests/ProbeOutputTest/input/testProbeOutput_batchMPI.bash
+++ tests/ProbeOutputTest/input/testProbeOutput_batchMPI.bash
@@ -1,123 +1,13 @@
 #! /usr/bin/env bash
 
-function toldiff() {
-   python input/compareProbeOutput.py "$@";
-}
+# ProbeOutputTest script for 4 MPI Processes in the batch dimension #
+
 test -e output && rm -r output
 
-######## 4 MPI Processes in the batch dimension ########
+export MPICOMMAND="mpiexec -n 4"
+export RUNNAME=batchMPI
+export RUNDESC="Four processes, batch MPI"
+export OUTPUTPVPTRUNC=5506448
 
-### Four processes, batch MPI, base run ###
-echo "Testing four processes, batch MPI, base run..."
-test -e output_batchMPI && rm -r output_batchMPI
-mpiexec -n 4 Debug/ProbeOutputTest input/config_batchMPI.txt
-mv -i BasicSystemTest*.log output/
-status=0
-for k in {0..7}
-do
-    toldiff input/correctProbeOutput/base/correct_${k}.txt output/block*/OutputL2Norm_batchElement_${k}.txt || status=1
-done
-
-mv -i output output_batchMPI
-
-if test -n "$diffs"
-then
-    >&2 echo "Four processes, batch MPI, base run failed."
-    exit 1 # If this run doesn't work, no other runs of 4 processes and higher would be expected to work either
-fi
-test -z "$diffs" && echo "Four processes, batch MPI, base run passed."
-
-failure=0
-
-### Four processes, batch MPI, initializing from checkpoint ###
-echo "Testing four processes, batch MPI, initializing from checkpoint..."
-cp -pr output_batchMPI output
-rm output/*.log
-for k in {0..7}
-do
-    targetfile=OutputL2Norm_batchElement_${k}.txt
-    truncat="$(cat output/checkpoints/Checkpoint040/block_*/${targetfile}_filepos_FileStreamRead.txt)"
-    truncat=$(($truncat + 100))
-    truncate -s $truncat output/block_*/$targetfile
-    echo "xxxxxxxxxx" >> output/block_*/$targetfile
-done
-truncate -s 5506448 output/Output.pvp
-mpiexec -n 4 Debug/ProbeOutputTest input/config_batchMPI-ifcp.txt
-mv -i BasicSystemTest-ifcp*.log output/
-status=0
-for k in {0..7}
-do
-    toldiff input/correctProbeOutput/initfromchkpt/correct_${k}.txt output/block_*/OutputL2Norm_batchElement_${k}.txt || status=1
-done
-if status -ne 0
-then
-    >&2 echo "Four processes, batch MPI, initializing from checkpoint failed."
-    failure=1
-    #No need to exit here, since subsequent runs don't depend on initializing from checkpoint
-else
-    echo "Four processes, batch MPI, initializing from checkpoint passed."
-fi
-test -e output_batchMPI_initfromchkpt && rm -r output_batchMPI_initfromchkpt
-mv -i output output_batchMPI_initfromchkpt
-
-### Four processes, batch MPI, restarting from checkpoint ###
-echo "Testing four processes, batch MPI, restarting from checkpoint..."
-cp -pr output_batchMPI output
-rm output/*.log
-for k in {0..7}
-do
-    targetfile=OutputL2Norm_batchElement_${k}.txt
-    truncat="$(cat output/checkpoints/Checkpoint040/block_*/${targetfile}_filepos_FileStreamRead.txt)"
-    truncat=$(($truncat + 100))
-    truncate -s $truncat output/block_*/$targetfile
-    echo "xxxxxxxxxx" >> output/block_*/$targetfile
-done
-truncate -s 11012816 output/Output.pvp
-mpiexec -n 4 Debug/ProbeOutputTest input/config_batchMPI-restartfromchkpt.txt
-mv -i BasicSystemTest-restart*.log output/
-status=0
-for k in {0..7}
-do
-    toldiff input/correctProbeOutput/restartfromchkpt/correct_${k}.txt output/block_*/OutputL2Norm_batchElement_${k}.txt || status=1
-done
-if status -ne 0
-then
-    >&2 echo "Four processes, batch MPI, restarting from checkpoint failed."
-    failure=1
-    #No need to exit here, since subsequent runs don't depend on restarting from checkpoint
-else
-    echo "Four processes, batch MPI, restarting from checkpoint passed."
-fi
-test -e output_batchMPI_restartfromchkpt && rm -r output_batchMPI_restartfromchkpt
-mv -i output output_batchMPI_restartfromchkpt
-
-### Four processes, batch MPI, restarting from end ###
-echo "Testing four processes, batch MPI, restarting from end..."
-cp -pr output_batchMPI output
-rm output/*.log
-mpiexec -n 4 Debug/ProbeOutputTest input/config_batchMPI-restartfromend.txt
-mv -i BasicSystemTest-restart*.log output/
-status=0
-for k in {0..7}
-do
-    toldiff input/correctProbeOutput/restartfromend/correct_${k}.txt output/block_*/OutputL2Norm_batchElement_${k}.txt || status=1
-done
-if status -ne 0
-then
-    >&2 echo "Four processes, batch MPI, restarting from end failed."
-    failure=1
-    #No need to exit here, since subsequent runs don't depend on restarting from end
-else
-    echo "Four processes, batch MPI, restarting from end passed."
-fi
-test -e output_batchMPI_restartfromend && rm -r output_batchMPI_restartfromend
-mv -i output output_batchMPI_restartfromend
-
-if test "$failure" -eq 0
-then
-    echo "Test passed."
-else
-    >&2 echo "Test failed."
-fi
-
-exit "$failure"
+scriptdir="$(dirname ${0})"
+bash "${scriptdir}/testProbeOutput.bash"
