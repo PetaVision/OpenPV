c4ab2ebd6e0c2a928df4b252556836c8ae255c9e
Pete Schultz <peteschultz@users.noreply.github.com>
2022-07-22 16:51:18 -0600

    Overhaul of RescaleLayerTest
    RescaleLayerTest has been reworked. Now each rescaleMethod has been
    separated into its own test, where the correct result has been
    precomputed in a .pvp file in the input directory, and the RescaleLayer
    values are compared with the precomputed values with a
    RequireAllZeroActivityProbe.
    
    The documentation of the rescaleMethod parameter in
    RescaleActivityBuffer.hpp has been expanded to describe each
    rescaleMethod option.

--- tests/RescaleLayerTest/src/RescaleLayerTest.cpp
+++ tests/RescaleLayerTest/src/RescaleLayerTest.cpp
@@ -1,16 +1,11 @@
 /*
- * pv.cpp
+ * RescaleLayerTest.cpp
  *
  */
-
-#include "RescaleLayerTestProbe.hpp"
-#include <columns/PV_Init.hpp>
-#include <columns/buildandrun.hpp>
+#include "columns/buildandrun.hpp"
+#include <cstdlib>
 
 int main(int argc, char *argv[]) {
-
-   PV_Init pv_initObj(&argc, &argv, false /*do not allow unrecognized arguments*/);
-   pv_initObj.registerKeyword("RescaleLayerTestProbe", Factory::create<RescaleLayerTestProbe>);
-   int status = buildandrun(&pv_initObj);
+   int status = buildandrun(argc, argv);
    return status == PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
 }
