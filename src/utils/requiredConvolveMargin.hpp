461ce9b740c9b47db48088e9b5677529f9c54671
Pete Schultz <peteschultz@users.noreply.github.com>
2024-08-21 12:26:52 -0600

    Add missing newline character to error message

--- src/utils/requiredConvolveMargin.hpp
+++ src/utils/requiredConvolveMargin.hpp
@@ -14,14 +14,16 @@
 inline int requiredConvolveMargin(int nPre, int nPost, int patchSize) {
    int margin = 0;
    if (nPre == nPost) {
-      FatalIf(patchSize % 2 != 1, "one-to-one weights with even patch size");
+      FatalIf(patchSize % 2 != 1, "one-to-one weights with even patch size\n");
       margin = (patchSize - 1) / 2;
    }
    else if (nPre > nPost) { // many-to-one
       FatalIf(nPre % nPost != 0, "many-to-one weights but pre is not a power of two times post\n");
       int densityRatio = nPre / nPost;
       double log2densityRatio = std::log2(densityRatio);
-      FatalIf(log2densityRatio != std::round(log2densityRatio), "many-to-one weights but pre is not a power of two times post\n");
+      FatalIf(
+            log2densityRatio != std::round(log2densityRatio),
+            "many-to-one weights but pre is not a power of two times post\n");
       margin = (patchSize - 1) * densityRatio / 2;
       assert(2 * margin * nPost == (patchSize - 1) * nPre);
    }
@@ -30,8 +32,12 @@ inline int requiredConvolveMargin(int nPre, int nPost, int patchSize) {
       FatalIf(nPost % nPre != 0, "one-to-many weights but post is not a power of two times pre\n");
       int densityRatio = nPost / nPre;
       double log2densityRatio = std::log2(densityRatio);
-      FatalIf(log2densityRatio != std::round(log2densityRatio), "one-to-many weights but post is not a power of two times pre\n");
-      FatalIf(patchSize % densityRatio != 0,  "one-to-many weights but patch size is not a multiple of post/pre\n");
+      FatalIf(
+            log2densityRatio != std::round(log2densityRatio),
+            "one-to-many weights but post is not a power of two times pre\n");
+      FatalIf(
+            patchSize % densityRatio != 0,
+            "one-to-many weights but patch size is not a multiple of post/pre\n");
       int numCells = patchSize / densityRatio;
       margin       = numCells / 2;
       // integer division is correct, no matter whether numCells is even or odd
