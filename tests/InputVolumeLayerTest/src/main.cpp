31ccb59374c9869c3e3cabdbb5b7fdfdb57b9ade
Pete Schultz <peteschultz@users.noreply.github.com>
2025-04-09 11:25:10 -0600

    Add InputVolumeLayerTest to system tests
    Currently this test fails; as described in the previous commit,
    InputVolumeLayer doesn't do anything yet. This test will provide
    an indication of when InputVolumeLayer is functional.

---
+++ tests/InputVolumeLayerTest/src/main.cpp
@@ -0,0 +1,7 @@
+#include <columns/buildandrun.hpp>
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+   int status = buildandrun(argc, argv);
+   return status == PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
+}
