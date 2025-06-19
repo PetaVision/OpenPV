3296e4731614b72ad2f09401ba2b578e8d5230f4
Pete Schultz <peteschultz@users.noreply.github.com>
2025-06-18 20:17:07 -0600

    SumPoolTest has been overhauled
    SumPoolTest now only uses standard layer and connection types, with
    a RequireAllZeroActivity probe used to detect errors.
    
    The params files have been separated into pre- and post- versions,
    differing in the setting of the updateGSynFromPostPerspective param.
    
    A GPU params file has been added for the post perspective (the pre
    perspective hasn't been implemented on the GPU yet).
    
    There is also a params file that pools into a broadcast layer,
    but that params file fails, because pooling hasn't been implemented
    for broadcast layers yet.

--- tests/SumPoolTest/src/SumPoolTest.cpp
+++ tests/SumPoolTest/src/SumPoolTest.cpp
@@ -1,29 +1,17 @@
 /*
- * ReceiveFromPostTest
- *
- *
+ * SumPoolTest.cpp
  */
 
-#include "GateSumPoolTestLayer.hpp"
-#include "SumPoolTestInputLayer.hpp"
-#include "SumPoolTestLayer.hpp"
-#include <columns/PV_Init.hpp>
 #include <columns/buildandrun.hpp>
-#include <columns/Factory.hpp>
-
-#define MAIN_USES_CUSTOMGROUPS
 
 int main(int argc, char *argv[]) {
-
-#ifdef MAIN_USES_CUSTOMGROUPS
-   PV_Init pv_initObj(&argc, &argv, false /*do not allow unrecognized arguments*/);
-   pv_initObj.registerKeyword("SumPoolTestLayer", Factory::create<SumPoolTestLayer>);
-   pv_initObj.registerKeyword("SumPoolTestInputLayer", Factory::create<SumPoolTestInputLayer>);
-   pv_initObj.registerKeyword("GateSumPoolTestLayer", Factory::create<GateSumPoolTestLayer>);
-   int status = buildandrun(&pv_initObj);
-#else // MAIN_USES_CUSTOMGROUPS
    int status = buildandrun(argc, argv, NULL, NULL);
-#endif // MAIN_USES_CUSTOMGROUPS
-
-   return status == PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
+   if (status == PV_SUCCESS) {
+      InfoLog().printf("Test succeeded.\n");
+      return EXIT_SUCCESS;
+   }
+   else {
+      ErrorLog().printf("Test failed.\n");
+      return EXIT_FAILURE;
+   }
 }
