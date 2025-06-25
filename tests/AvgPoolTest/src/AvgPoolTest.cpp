3e31956d37666bc18067ba5f921f9b573acd4269
Pete Schultz <peteschultz@users.noreply.github.com>
2025-06-25 17:40:11 -0600

    Bugfixes to average pooling and AvgPoolTest
    Average pooling now correctly handles the case where the post-layer is
    a broadcast layer.
    
    AvgPoolTest has been overhauled, to test more thoroughly and to
    eliminate reliance on custom classes.

--- tests/AvgPoolTest/src/AvgPoolTest.cpp
+++ tests/AvgPoolTest/src/AvgPoolTest.cpp
@@ -1,29 +1,17 @@
 /*
- * ReceiveFromPostTest
- *
- *
+ * AvgPoolTest.cpp
  */
 
-#include "AvgPoolTestInputLayer.hpp"
-#include "AvgPoolTestLayer.hpp"
-#include "GateAvgPoolTestLayer.hpp"
-#include <columns/PV_Init.hpp>
 #include <columns/buildandrun.hpp>
-#include <columns/Factory.hpp>
-
-#define MAIN_USES_CUSTOMGROUPS
 
 int main(int argc, char *argv[]) {
-
-#ifdef MAIN_USES_CUSTOMGROUPS
-   PV_Init pv_initObj(&argc, &argv, false /*do not allow unrecognized arguments*/);
-   pv_initObj.registerKeyword("AvgPoolTestLayer", Factory::create<AvgPoolTestLayer>);
-   pv_initObj.registerKeyword("AvgPoolTestInputLayer", Factory::create<AvgPoolTestInputLayer>);
-   pv_initObj.registerKeyword("GateAvgPoolTestLayer", Factory::create<GateAvgPoolTestLayer>);
-   int status = buildandrun(&pv_initObj);
-#else // MAIN_USES_CUSTOMGROUPS
    int status = buildandrun(argc, argv, NULL, NULL);
-#endif // MAIN_USES_CUSTOMGROUPS
-
-   return status == PV_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
+   if (status == PV_SUCCESS) {
+      InfoLog().printf("Test succeeded.\n");
+      return EXIT_SUCCESS;
+   }
+   else {
+      ErrorLog().printf("Test failed.\n");
+      return EXIT_FAILURE;
+   }
 }
