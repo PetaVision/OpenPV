374a635253a5fe0e23186c3402a33f85097eec03
Pete Schultz <peteschultz@users.noreply.github.com>
2025-11-18 17:31:57 -0700

    New error condition for group normalization
    Connections with normalizeMethod = normalizeGroup
    do not use any of the parameters
       normalizeArborsIndividually
       normalizeOnInitialize
       normalizeOnWeightUpdate
       strength
    Instead, the group head applies its parameters to all connections
    in the group, and the other connections in the group ignore these
    parameters.
    
    With this update, it is a fatal error for connections in a
    normalization group to have different values of the above parameter.
    
    One potential issue is that if the group head sets strength to a
    nondefault value and the other connections in the group do not
    set the strength value, the generated params file lists the strength
    for the other connections as having the default value. Running
    from the generated params file without modification will therefore
    trip an error.
    
    This issue is difficult to resolve because strength is read by the
    Gauss2DWeight initialization method as well as by normalizations. For
    this reason the strength parameter is not directly read by the
    normalization class and it is complicated to determine whether the
    strength parameter should be included or not.

--- src/normalizers/NormalizeGroup.hpp
+++ src/normalizers/NormalizeGroup.hpp
@@ -13,19 +13,7 @@
 namespace PV {
 
 class NormalizeGroup : public NormalizeBase {
-  public:
-   NormalizeGroup(char const *name, PVParams *params, Communicator const *comm);
-   virtual ~NormalizeGroup();
-
-   virtual int ioParamsFillGroup(enum ParamsIOFlag ioFlag) override;
-
-   virtual Response::Status
-   communicateInitInfo(std::shared_ptr<CommunicateInitInfoMessage const> message) override;
-
   protected:
-   NormalizeGroup();
-   void initialize(char const *name, PVParams *params, Communicator const *comm);
-
    /**
     * NormalizeGroup does not read the normalizeArborsIndividually parameter, but inherits it from
     * its group head.
@@ -49,6 +37,22 @@ class NormalizeGroup : public NormalizeBase {
     * The group head cannot itself be a NormalizeGroup.
     */
    virtual void ioParam_normalizeGroupName(enum ParamsIOFlag ioFlag);
+
+  public:
+   NormalizeGroup(char const *name, PVParams *params, Communicator const *comm);
+   virtual ~NormalizeGroup();
+
+   virtual int ioParamsFillGroup(enum ParamsIOFlag ioFlag) override;
+
+   virtual Response::Status
+   communicateInitInfo(std::shared_ptr<CommunicateInitInfoMessage const> message) override;
+
+  protected:
+   NormalizeGroup();
+
+   int checkCompatibilityWithGroupHead(char const *paramName, bool groupHeadValue); 
+
+   void initialize(char const *name, PVParams *params, Communicator const *comm);
 
    /**
     * Overrides normalizeWeights to do nothing.
